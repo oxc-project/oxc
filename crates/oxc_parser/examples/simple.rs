d8fd23524a4c57a8ace13bfc373aee425e0ed5ca
Boshen <boshenc@gmail.com>
2023-02-12 15:57:40 +0800

    feat(oxc_parser): allow filename as option in examples/simple.rs

--- crates/oxc_parser/examples/simple.rs
+++ crates/oxc_parser/examples/simple.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{env, path::Path};
 
 use oxc_allocator::Allocator;
 use oxc_ast::SourceType;
@@ -10,17 +10,18 @@ use oxc_parser::Parser;
 // or `cargo watch -x "run -p oxc_parser --example simple"`
 
 fn main() {
-    let path = Path::new("test.js");
-    let file = std::fs::read(path).expect("test.js should exist.");
+    let name = env::args().nth(1).unwrap_or_else(|| "test.js".to_string());
+    let path = Path::new(&name);
+    let file = std::fs::read(path).expect("{name} not found");
     let allocator = Allocator::default();
     let source = String::from_utf8(file).expect("utf8");
     let source_type = SourceType::from_path(path).unwrap();
     let ret = Parser::new(&allocator, &source, source_type).parse();
 
     if ret.errors.is_empty() {
-        println!("Successfully Parsed.");
+        println!("Parsed Successfully.");
     } else {
-        println!("Failed to Parse.");
+        println!("Parse Failed.");
         for error in &ret.errors {
             println!("{error:?}");
         }
