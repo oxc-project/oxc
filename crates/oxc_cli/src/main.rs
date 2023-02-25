f83614246dcb4e32be21824b799d88a8514581d0
Boshen <boshenc@gmail.com>
2023-02-26 01:48:05 +0800

    refactor(cli): clean up and report total number of diagnostics

--- crates/oxc_cli/src/main.rs
+++ crates/oxc_cli/src/main.rs
@@ -9,44 +9,8 @@ static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 use std::path::PathBuf;
-use std::process::{ExitCode, Termination};
 
-use oxc_cli::{Cli, Command, LintResult};
-
-#[derive(Debug)]
-pub enum CliRunResult {
-    None,
-    PathNotFound { path: PathBuf },
-    LintResult(Vec<LintResult>),
-}
-
-impl Termination for CliRunResult {
-    fn report(self) -> ExitCode {
-        match self {
-            Self::None => ExitCode::from(0),
-            Self::PathNotFound { path } => {
-                println!("Path {} does not exist.", path.to_string_lossy());
-                ExitCode::from(1)
-            }
-            Self::LintResult(results) => {
-                println!("Checked {} files.", results.len());
-
-                let has_errors = results.iter().any(|result| !result.diagnostics.is_empty());
-
-                if has_errors {
-                    for LintResult { path, diagnostics } in results {
-                        println!("File: {path:?}");
-                        for diagnostic in diagnostics {
-                            println!("{diagnostic:?}");
-                        }
-                    }
-                    return ExitCode::from(1);
-                }
-                ExitCode::from(0)
-            }
-        }
-    }
-}
+use oxc_cli::{Cli, CliRunResult, Command};
 
 fn main() -> CliRunResult {
     match Command::new().build().get_matches().subcommand() {
@@ -57,8 +21,7 @@ fn main() -> CliRunResult {
                 return CliRunResult::PathNotFound { path: path.clone() };
             }
 
-            let result = Cli::lint(path);
-            CliRunResult::LintResult(result.unwrap())
+            Cli::lint(path)
         }
         _ => CliRunResult::None,
     }
