7297ef906b56d1389dff61fef43d4b98b6280c12
Amit Dahan <amitdahan11@gmail.com>
2023-02-27 15:42:31 +0200

    feat(linter): `no-array-constructor`

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -1,6 +1,8 @@
+mod no_array_constructor;
 mod no_debugger;
 mod no_empty;
 
+pub use no_array_constructor::NoArrayConstructor;
 pub use no_debugger::NoDebugger;
 pub use no_empty::NoEmpty;
 
@@ -9,14 +11,17 @@ use crate::{context::LintContext, rule::Rule, AstNode};
 lazy_static::lazy_static! {
     pub static ref RULES: Vec<RuleEnum> = vec![
         RuleEnum::NoDebugger(NoDebugger::default()),
-        RuleEnum::NoEmpty(NoEmpty::default())
+        RuleEnum::NoEmpty(NoEmpty::default()),
+        RuleEnum::NoArrayConstructor(NoArrayConstructor::default()),
     ];
 }
 
 #[derive(Debug, Clone)]
+#[allow(clippy::enum_variant_names)]
 pub enum RuleEnum {
     NoDebugger(NoDebugger),
     NoEmpty(NoEmpty),
+    NoArrayConstructor(NoArrayConstructor),
 }
 
 impl RuleEnum {
@@ -24,6 +29,7 @@ impl RuleEnum {
         match self {
             Self::NoDebugger(_) => NoDebugger::NAME,
             Self::NoEmpty(_) => NoEmpty::NAME,
+            Self::NoArrayConstructor(_) => NoArrayConstructor::NAME,
         }
     }
 
@@ -35,6 +41,9 @@ impl RuleEnum {
             Self::NoEmpty(_) => {
                 Self::NoEmpty(maybe_value.map(NoEmpty::from_configuration).unwrap_or_default())
             }
+            Self::NoArrayConstructor(_) => Self::NoArrayConstructor(
+                maybe_value.map(NoArrayConstructor::from_configuration).unwrap_or_default(),
+            ),
         }
     }
 
@@ -42,6 +51,7 @@ impl RuleEnum {
         match self {
             Self::NoDebugger(rule) => rule.run(node, ctx),
             Self::NoEmpty(rule) => rule.run(node, ctx),
+            Self::NoArrayConstructor(rule) => rule.run(node, ctx),
         }
     }
 }
