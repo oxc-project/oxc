340c28b4cfce920ca28bab8ca2601da8c0742909
magic-akari <akari.ccino@gmail.com>
2023-03-01 11:01:46 +0800

    chore(linter/no-empty-pattern): update docs and examples

--- crates/oxc_linter/src/rules/no_empty_pattern.rs
+++ crates/oxc_linter/src/rules/no_empty_pattern.rs
@@ -3,6 +3,7 @@ use oxc_diagnostics::{
     miette::{self, Diagnostic},
     thiserror::Error,
 };
+use oxc_macros::declare_oxc_lint;
 
 use crate::{context::LintContext, rule::Rule, AstNode};
 
@@ -13,6 +14,63 @@ struct NoEmptyPatternDiagnostic(&'static str, #[label("Empty {0} binding pattern
 
 #[derive(Debug, Default, Clone)]
 pub struct NoEmptyPattern;
+
+declare_oxc_lint!(
+    /// ### What it does
+    /// Disallow empty destructuring patterns
+    ///
+    /// ### Why is this bad?
+    /// When using destructuring, it’s possible to create a pattern that has no effect.
+    /// This happens when empty curly braces are used to the right of
+    /// an embedded object destructuring pattern, such as:
+    ///
+    /// ```JavaScript
+    /// // doesn't create any variables
+    /// var {a: {}} = foo;
+    /// ```
+    /// In this code, no new variables are created because a is just a location helper
+    /// while the `{}` is expected to contain the variables to create, such as:
+    ///
+    /// ```JavaScript
+    /// // creates variable b
+    /// var {a: { b }} = foo;
+    /// ```
+    ///
+    /// In many cases, the empty object pattern is a mistake
+    /// where the author intended to use a default value instead, such as:
+    ///
+    /// ```JavaScript
+    /// // creates variable a
+    /// var {a = {}} = foo;
+    /// ```
+    ///
+    /// The difference between these two patterns is subtle,
+    /// especially because the problematic empty pattern looks just like an object literal.
+    ///
+    /// ### Examples of incorrect code for this rule:
+    ///
+    /// ```javascript
+    /// var {} = foo;
+    /// var [] = foo;
+    /// var {a: {}} = foo;
+    /// var {a: []} = foo;
+    /// function foo({}) {}
+    /// function foo([]) {}
+    /// function foo({a: {}}) {}
+    /// function foo({a: []}) {}
+    /// ```
+    ///
+    /// ### Examples of correct code for this rule:
+    ///
+    /// ```javascript
+    /// var {a = {}} = foo;
+    /// var {a = []} = foo;
+    /// function foo({a = {}}) {}
+    /// function foo({a = []}) {}
+    /// ```
+    ///
+    NoEmptyPattern
+);
 
 const RULE_NAME: &str = "no-empty-pattern";
 
