7ba4d3691388bf969c789c48a467d05fdb01866b
Shannon Rothe <shannon.michael.rothe@gmail.com>
2023-03-09 19:56:00 +1100

    convert `parse_test_code` to visitor pattern

--- tasks/rulegen/src/main.rs
+++ tasks/rulegen/src/main.rs
@@ -2,7 +2,10 @@ use std::borrow::Cow;
 
 use convert_case::{Case, Casing};
 use oxc_allocator::Allocator;
-use oxc_ast::ast::{ArrayExpression, ExpressionStatement, Program, Property, Statement};
+use oxc_ast::ast::{
+    ArrayExpression, CallExpression, ExpressionStatement, ObjectExpression, Program, Property,
+    Statement, StringLiteral, TemplateLiteral,
+};
 use oxc_ast::visit::Visit;
 use oxc_ast::{
     ast::{Argument, Expression, ObjectProperty, PropertyKey, PropertyValue},
@@ -17,17 +20,98 @@ mod template;
 const ESLINT_TEST_PATH: &str =
     "https://raw.githubusercontent.com/eslint/eslint/main/tests/lib/rules";
 
-#[derive(Debug)]
-enum TestCase<'a> {
-    Invalid(Cow<'a, str>),
-    Valid(Cow<'a, str>),
+struct TestCase<'a> {
+    source_text: &'a str,
+    code: Option<Cow<'a, str>>,
+    test_code: Option<Cow<'a, str>>,
 }
 
 impl<'a> TestCase<'a> {
-    fn to_code(test_case: &TestCase) -> String {
-        match test_case {
-            TestCase::Valid(code) | TestCase::Invalid(code) => code.clone().into_owned(),
+    fn new(source_text: &'a str, arg: &'a Argument<'a>) -> Option<Self> {
+        let mut test_case = TestCase { source_text, code: None, test_code: None };
+        if let Argument::Expression(expr) = arg {
+            test_case.visit_expression(expr);
+            return Some(test_case);
         }
+        None
+    }
+
+    fn code(&self) -> Option<Cow<'a, str>> {
+        self.code.as_ref().map(|test_code| {
+            let option_code =
+                self.test_code.as_ref().map_or(Cow::Borrowed("None"), |option_code| {
+                    Cow::Owned(format!("Some(serde_json::json!({option_code}))"))
+                });
+            Cow::Owned(format!(r#"({test_code:?}, {option_code})"#))
+        })
+    }
+
+    fn to_code(test_case: &TestCase) -> String {
+        test_case.code().map_or_else(String::new, |code| code.clone().into_owned())
+    }
+}
+
+impl<'a> Visit<'a> for TestCase<'a> {
+    fn visit_expression(&mut self, expr: &'a Expression<'a>) {
+        match expr {
+            Expression::StringLiteral(lit) => self.visit_string_literal(lit),
+            Expression::TemplateLiteral(lit) => self.visit_template_literal(lit),
+            Expression::ObjectExpression(obj_expr) => self.visit_object_expression(obj_expr),
+            Expression::CallExpression(call_expr) => self.visit_call_expression(call_expr),
+            _ => {}
+        }
+    }
+
+    fn visit_call_expression(&mut self, expr: &'a CallExpression<'a>) {
+        if let Expression::MemberExpression(member_expr) = &expr.callee {
+            if let Expression::ArrayExpression(array_expr) = member_expr.object() {
+                // ['class A {', '}'].join('\n')
+                let mut code = String::new();
+                for arg in &array_expr.elements {
+                    let Some(Argument::Expression(Expression::StringLiteral(lit))) = arg else { continue };
+                    code.push_str(lit.value.as_str());
+                    code.push('\n');
+                }
+                self.code = Some(Cow::Owned(code));
+                self.test_code = None;
+            }
+        }
+    }
+
+    fn visit_object_expression(&mut self, expr: &'a ObjectExpression<'a>) {
+        for obj_prop in &expr.properties {
+            match obj_prop {
+                ObjectProperty::Property(prop) => match &prop.key {
+                    PropertyKey::Identifier(ident) if ident.name == "code" => match &prop.value {
+                        PropertyValue::Expression(expr) => {
+                            let Expression::StringLiteral(s) = expr else {
+                                    continue;
+                                  };
+                            self.code = Some(Cow::Borrowed(s.value.as_str()));
+                        }
+                        PropertyValue::Pattern(_) => continue,
+                    },
+                    PropertyKey::Identifier(ident) if ident.name == "options" => {
+                        let span = prop.value.span();
+                        let option_text = &self.source_text[span.start as usize..span.end as usize];
+                        self.test_code =
+                            Some(Cow::Owned(json::wrap_property_in_quotes(option_text)));
+                    }
+                    _ => continue,
+                },
+                ObjectProperty::SpreadProperty(_) => continue,
+            }
+        }
+    }
+
+    fn visit_template_literal(&mut self, lit: &'a TemplateLiteral<'a>) {
+        self.code = Some(Cow::Borrowed(lit.quasi().unwrap().as_str()));
+        self.test_code = None;
+    }
+
+    fn visit_string_literal(&mut self, lit: &'a StringLiteral) {
+        self.code = Some(Cow::Borrowed(lit.value.as_str()));
+        self.test_code = None;
     }
 }
 
@@ -51,40 +135,29 @@ impl<'a> Context<'a> {
 }
 
 struct State<'a> {
+    source_text: &'a str,
     valid_tests: Vec<&'a ArrayExpression<'a>>,
     invalid_tests: Vec<&'a ArrayExpression<'a>>,
 }
 
 impl<'a> State<'a> {
-    fn new() -> Self {
-        Self { valid_tests: vec![], invalid_tests: vec![] }
+    fn new(source_text: &'a str) -> Self {
+        Self { source_text, valid_tests: vec![], invalid_tests: vec![] }
     }
 
-    fn pass_cases<'b>(&'b self, body: &'b str) -> Vec<TestCase> {
+    fn pass_cases(&self) -> Vec<TestCase> {
         self.valid_tests
             .iter()
             .flat_map(|array_expr| (&array_expr.elements).into_iter().flatten())
-            .filter_map(|arg| {
-                if let Argument::Expression(expr) = arg {
-                    let Some(code) = parse_test_code(body, expr) else { return None };
-                    return Some(TestCase::Valid(code));
-                }
-                None
-            })
+            .filter_map(|arg| TestCase::new(self.source_text, arg))
             .collect::<Vec<_>>()
     }
 
-    fn fail_cases<'b>(&'b self, body: &'b str) -> Vec<TestCase> {
+    fn fail_cases(&self) -> Vec<TestCase> {
         self.invalid_tests
             .iter()
             .flat_map(|array_expr| (&array_expr.elements).into_iter().flatten())
-            .filter_map(|arg| {
-                if let Argument::Expression(expr) = arg {
-                    let Some(code) = parse_test_code(body, expr) else { return None };
-                    return Some(TestCase::Invalid(code));
-                }
-                None
-            })
+            .filter_map(|arg| TestCase::new(self.source_text, arg))
             .collect::<Vec<_>>()
     }
 }
@@ -145,68 +218,6 @@ impl<'a> Visit<'a> for State<'a> {
     }
 }
 
-fn parse_test_code<'a>(source_text: &'a str, expr: &'a Expression) -> Option<Cow<'a, str>> {
-    let (test_code, option_code) = match expr {
-        Expression::StringLiteral(lit) => (Some(Cow::Borrowed(lit.value.as_str())), None),
-        Expression::TemplateLiteral(lit) => {
-            (Some(Cow::Borrowed(lit.quasi().unwrap().as_str())), None)
-        }
-        Expression::ObjectExpression(obj_expr) => {
-            let mut test_code = None;
-            let mut option_code: Option<Cow<'_, str>> = None;
-            for obj_prop in &obj_expr.properties {
-                match obj_prop {
-                    ObjectProperty::Property(prop) => match &prop.key {
-                        PropertyKey::Identifier(ident) if ident.name == "code" => match &prop.value
-                        {
-                            PropertyValue::Expression(expr) => {
-                                let Expression::StringLiteral(s) = expr else {
-                                return None;
-                              };
-                                test_code = Some(Cow::Borrowed(s.value.as_str()));
-                            }
-                            PropertyValue::Pattern(_) => continue,
-                        },
-                        PropertyKey::Identifier(ident) if ident.name == "options" => {
-                            let span = prop.value.span();
-                            let option_text = &source_text[span.start as usize..span.end as usize];
-                            option_code =
-                                Some(Cow::Owned(json::wrap_property_in_quotes(option_text)));
-                        }
-                        _ => continue,
-                    },
-                    ObjectProperty::SpreadProperty(_) => continue,
-                }
-            }
-            (test_code, option_code)
-        }
-        Expression::CallExpression(call_expr) => match &call_expr.callee {
-            Expression::MemberExpression(member_expr) => match &member_expr.object() {
-                // ['class A {', '}'].join('\n')
-                Expression::ArrayExpression(array_expr) => {
-                    let mut code = String::new();
-                    for arg in &array_expr.elements {
-                        let Some(Argument::Expression(Expression::StringLiteral(lit))) = arg else { continue };
-                        code.push_str(lit.value.as_str());
-                        code.push('\n');
-                    }
-                    (Some(Cow::Owned(code)), None)
-                }
-                _ => (None, None),
-            },
-            _ => (None, None),
-        },
-        _ => (None, None),
-    };
-
-    test_code.map(|test_code| {
-        let option_code = option_code.map_or(Cow::Borrowed("None"), |option_code| {
-            Cow::Owned(format!("Some(serde_json::json!({option_code}))"))
-        });
-        Cow::Owned(format!(r#"({test_code:?}, {option_code})"#))
-    })
-}
-
 fn main() {
     let mut args = std::env::args();
     let _ = args.next();
@@ -226,13 +237,13 @@ fn main() {
 
     let program = allocator.alloc(ret.program);
 
-    let mut state = State::new();
+    let mut state = State::new(&body);
     state.visit_program(program);
 
     let pass_cases =
-        state.pass_cases(&body).iter().map(TestCase::to_code).collect::<Vec<_>>().join(",\n");
+        state.pass_cases().iter().map(TestCase::to_code).collect::<Vec<_>>().join(",\n");
     let fail_cases =
-        state.fail_cases(&body).iter().map(TestCase::to_code).collect::<Vec<_>>().join(",\n");
+        state.fail_cases().iter().map(TestCase::to_code).collect::<Vec<_>>().join(",\n");
 
     let context = Context::new(&upper_rule_name, &rule_name, &pass_cases, &fail_cases);
     let template = template::Template::with_context(&context);
