1b93d83ff7d60c840bda56dd331cacefa782eb12
Boshen <boshenc@gmail.com>
2023-03-12 00:44:42 +0800

    chore(oxc_parser): setup fuzzer
    closes #155

---
+++ crates/oxc_parser/fuzz/fuzz_targets/parser.rs
@@ -0,0 +1,15 @@
+#![no_main]
+
+use oxc_allocator::Allocator;
+use oxc_ast::SourceType;
+use oxc_parser::Parser;
+
+libfuzzer_sys::fuzz_target!(|data: &[u8]| {
+    if let Ok(s) = std::str::from_utf8(data) {
+        if s.chars().all(|s| !s.is_control()) {
+            let allocator = Allocator::default();
+            let source_type = *SourceType::default().with_typescript(true).with_jsx(true);
+            let _ = Parser::new(&allocator, &s, source_type).parse();
+        }
+    }
+});
