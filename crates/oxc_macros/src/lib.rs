56349f4ad5ff573d774d1bd0ce85cbed76143a65
Ye Yangchen <eikasia30@gmail.com>
2023-03-13 14:15:11 -0500

    Use 256 hash table

--- crates/oxc_macros/src/lib.rs
+++ crates/oxc_macros/src/lib.rs
@@ -2,7 +2,6 @@ use syn::parse_macro_input;
 
 mod declare_all_lint_rules;
 mod declare_oxc_lint;
-mod generate_keyword_table;
 
 /// Macro used to declare an oxc lint rule
 ///
@@ -58,12 +57,4 @@ pub fn declare_all_lint_rules(input: proc_macro::TokenStream) -> proc_macro::Tok
     let metadata = parse_macro_input!(input as declare_all_lint_rules::AllLintRulesMeta);
 
     declare_all_lint_rules::declare_all_lint_rules(metadata).into()
-}
-
-#[proc_macro]
-pub fn generate_keyword_table(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let metadata = parse_macro_input!(input as generate_keyword_table::TableMeta);
-
-    let stream = generate_keyword_table::generate_keyword_table(&metadata);
-    stream.into()
 }
