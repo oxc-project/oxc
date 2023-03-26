92555e5a43589207de2f4430cda504a2b5592bb0
Boshen <boshenc@gmail.com>
2023-03-26 15:20:42 +0800

    feat(linter): add restriction category for lint rules

--- crates/oxc_linter/src/rule.rs
+++ crates/oxc_linter/src/rule.rs
@@ -4,9 +4,18 @@ use oxc_semantic::Symbol;
 
 use crate::{context::LintContext, AstNode};
 
+/// Rule categories defined by rust-clippy
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum RuleCategory {
+    /// Code that is outright wrong or useless
     Correctness,
+    /// Lints which prevent the use of language and library features
+    /// The restriction category should, emphatically, not be enabled as a whole.
+    /// The contained lints may lint against perfectly reasonable code, may not have an alternative suggestion,
+    /// and may contradict any other lints (including other categories).
+    /// Lints should be considered on a case-by-case basis before enabling.
+    Restriction,
+    /// New lints that are still under development
     Nursery,
 }
 
