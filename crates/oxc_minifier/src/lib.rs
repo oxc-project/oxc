bf6f6bb05fe2ccd5ac6606f20444f363154e4cc5
Boshen <boshenc@gmail.com>
2023-03-28 10:07:37 +0800

    feat(minifier): minifier prototype

---
+++ crates/oxc_minifier/src/lib.rs
@@ -0,0 +1,30 @@
+//! ECMAScript Minifier
+
+#![feature(let_chains, is_some_and)]
+
+mod compressor;
+
+use oxc_allocator::Allocator;
+use oxc_ast::ast::Program;
+
+pub use crate::compressor::CompressOptions;
+use crate::compressor::Compressor;
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct MinifierOptions {
+    pub compress: CompressOptions,
+}
+
+pub struct Minifier<'a> {
+    compressor: Compressor<'a>,
+}
+
+impl<'a> Minifier<'a> {
+    pub fn new(allocator: &'a Allocator, options: MinifierOptions) -> Self {
+        Self { compressor: Compressor::new(allocator, options.compress) }
+    }
+
+    pub fn build<'b>(self, program: &'b mut Program<'a>) {
+        self.compressor.build(program);
+    }
+}
