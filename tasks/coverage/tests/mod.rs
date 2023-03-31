05f163f2f764432dd5ce1099196c6f9c6bae99c6
Boshen <boshenc@gmail.com>
2023-03-31 23:45:43 +0800

    fix(coverage): fix lint on cfg feature

--- tasks/coverage/tests/mod.rs
+++ tasks/coverage/tests/mod.rs
@@ -1,11 +1,10 @@
-use oxc_coverage::{
-    AppArgs, BabelCase, BabelSuite, PrinterTest262Case, Suite, Test262Case, Test262Suite,
-    TypeScriptCase, TypeScriptSuite,
-};
-
 #[cfg(feature = "tarpaulin")]
 #[test]
 fn test() {
+    use oxc_coverage::{
+        AppArgs, BabelCase, BabelSuite, PrinterTest262Case, Suite, Test262Case, Test262Suite,
+        TypeScriptCase, TypeScriptSuite,
+    };
     let args = AppArgs { filter: None, detail: false, diff: false };
     Test262Suite::<Test262Case>::new().run("Test262", &args);
     BabelSuite::<BabelCase>::new().run("Babel", &args);
