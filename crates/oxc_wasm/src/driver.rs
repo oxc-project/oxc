59476e42d48aa1aefee172f39c9638ffbb7e40a5
Boshen <boshenc@gmail.com>
2023-04-01 14:09:20 +0800

    refactor(oxc_diagnostics): remove over complicated `Diagnostics` struct

--- crates/oxc_wasm/src/driver.rs
+++ crates/oxc_wasm/src/driver.rs
@@ -3,7 +3,6 @@ use std::{rc::Rc, sync::Arc};
 use miette::NamedSource;
 use oxc_allocator::Allocator;
 use oxc_ast::SourceType;
-use oxc_diagnostics::Diagnostics;
 use oxc_linter::Linter;
 use oxc_parser::Parser;
 use oxc_semantic::SemanticBuilder;
@@ -30,24 +29,21 @@ impl Driver {
             .allow_return_outside_function(true)
             .parse();
 
+        let mut diagnostics = ret.errors;
+
         let program = self.allocator.alloc(ret.program);
 
-        let diagnostics = Diagnostics::default();
         let semantic_ret =
             SemanticBuilder::new(source_text, source_type, &ret.trivias).build(program);
-        let mut diagnostics = diagnostics.into_inner();
+        diagnostics.extend(semantic_ret.errors);
 
         let source = Arc::new(NamedSource::new(path, source_text.to_string()));
 
-        diagnostics.extend(
-            Linter::from_json_str(eslintrc)
-                .with_fix(false)
-                .run(&Rc::new(semantic_ret.semantic))
-                .into_iter()
-                .map(|m| m.error.with_source_code(Arc::clone(&source)))
-                .chain(ret.errors)
-                .chain(semantic_ret.errors),
-        );
+        let messages =
+            Linter::from_json_str(eslintrc).with_fix(false).run(&Rc::new(semantic_ret.semantic));
+
+        diagnostics
+            .extend(messages.into_iter().map(|m| m.error.with_source_code(Arc::clone(&source))));
 
         if diagnostics.is_empty() {
             if let Ok(ast) = JsValue::from_serde(program) {
@@ -55,11 +51,12 @@ impl Driver {
             }
         }
 
-        let result = diagnostics
+        let diagnostics = diagnostics
             .into_iter()
             .map(|error| format!("{error:?}"))
             .collect::<Vec<String>>()
             .join("\n");
-        JsValue::from_str(&result)
+
+        JsValue::from_str(&diagnostics)
     }
 }
