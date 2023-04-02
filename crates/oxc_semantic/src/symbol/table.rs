06e532430790165a4c5ecb0872464560c27b9c28
Ye Yangchen <eikasia30@gmail.com>
2023-04-02 15:12:57 -0500

    feat(semantics): save mappings from span to symbols/references

--- crates/oxc_semantic/src/symbol/table.rs
+++ crates/oxc_semantic/src/symbol/table.rs
@@ -10,7 +10,11 @@ use crate::ResolvedReference;
 #[derive(Debug, Default)]
 pub struct SymbolTable {
     symbols: Vec<Symbol>,
-    resolved_references: FxHashMap<AstNodeId, ResolvedReference>,
+    symbols_index_span: FxHashMap<Span, SymbolId>,
+
+    resolved_references: Vec<ResolvedReference>,
+    reference_index_nodeid: FxHashMap<AstNodeId, usize>,
+    reference_index_span: FxHashMap<Span, usize>,
 }
 
 impl Index<SymbolId> for SymbolTable {
@@ -47,6 +51,11 @@ impl SymbolTable {
     }
 
     #[must_use]
+    pub fn get_symbol_at_span(&self, span: Span) -> Option<&Symbol> {
+        self.symbols_index_span.get(&span).and_then(|&id| self.symbols.get(id.index0()))
+    }
+
+    #[must_use]
     pub fn create(
         &mut self,
         declaration: AstNodeId,
@@ -57,15 +66,27 @@ impl SymbolTable {
         let symbol_id = SymbolId::new(self.symbols.len() + 1);
         let symbol = Symbol::new(symbol_id, declaration, name, span, flags);
         self.symbols.push(symbol);
+        self.symbols_index_span.insert(span, symbol_id);
         symbol_id
     }
 
     #[must_use]
     pub fn get_resolved_reference(&self, id: AstNodeId) -> Option<&ResolvedReference> {
-        self.resolved_references.get(&id)
+        let reference_id = self.reference_index_nodeid.get(&id);
+        reference_id.map(|&id| &self.resolved_references[id])
+    }
+
+    #[must_use]
+    pub fn get_resolved_reference_at_span(&self, span: Span) -> Option<&ResolvedReference> {
+        let reference_id = self.reference_index_span.get(&span);
+        reference_id.map(|&id| &self.resolved_references[id])
     }
 
     pub fn resolve_reference(&mut self, id: AstNodeId, reference: ResolvedReference) {
-        self.resolved_references.insert(id, reference);
+        let span = reference.span();
+        let reference_id = self.resolved_references.len();
+        self.resolved_references.push(reference);
+        self.reference_index_nodeid.insert(id, reference_id);
+        self.reference_index_span.insert(span, reference_id);
     }
 }
