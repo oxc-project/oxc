16611ffbc25eaaf6ddc2d059eeb6681d17dac412
Gwen <guan_wy@foxmail.com>
2023-04-14 22:30:43 +0800

    feat(site): init website

---
+++ website/src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import "virtual:windi.css";
+
+  import Header from "./layout/Header.svelte";
+  import Footer from "./layout/Footer.svelte";
+  import Home from "./pages/Home.svelte";
+</script>
+
+<main class="flex flex-col h-full overflow-hidden justify-between">
+  <Header />
+  <Home />
+  <Footer />
+</main>
+
+<style>
+  main {
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    min-height: 100vh;
+    background-color: #103e5f;
+    color: #babc8f;
+  }
+</style>
---
+++ website/src/layout/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+  import logo from "../assets/logo-square.png";
+  import LinkButton from "../components/LinkButton.svelte";
+</script>
+
+<header class="flex px-3 bg-gray-800">
+  <container
+    class="container mx-auto flex justify-between items-center h-80px py-16px"
+  >
+    <div>
+      <a href="/" class="flex items-center">
+        <img src={logo} class="w-56px h-56px" alt="OXC Logo" />
+        <h1 class="text-xl text-white ml-2">OXC</h1>
+      </a>
+    </div>
+    <div>
+      <span class="links">
+        <LinkButton href="https://github.com/Boshen/oxc">GitHub</LinkButton>
+      </span>
+    </div>
+  </container>
+</header>
