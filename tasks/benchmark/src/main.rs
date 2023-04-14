a7cd845fffc95b174ed7cc0fb740b16339e69c66
Boshen <boshenc@gmail.com>
2023-04-14 13:07:37 +0800

    feat: add `cargo minsize` task for tracking minification size (#276)
    * feat: add `cargo minsize` task for tracking minification size
    
    * add gzip

--- tasks/benchmark/src/main.rs
+++ tasks/benchmark/src/main.rs
@@ -10,41 +10,36 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 // See: `https://rust-lang.github.io/rfcs/2360-bench-black-box.html`
 use std::hint::black_box;
+use std::time::Duration;
 
 use criterion::{BenchmarkId, Criterion, Throughput};
 use oxc_allocator::Allocator;
 use oxc_ast::SourceType;
-use oxc_benchmark::Code;
 use oxc_parser::Parser;
 use oxc_semantic::SemanticBuilder;
+use oxc_tasks_common::TestFiles;
 use pico_args::Arguments;
 
 /// # Errors
 /// # Panics
 pub fn main() -> Result<(), String> {
-    let root = project_root::get_project_root().unwrap();
-    let codes = std::fs::read_to_string(root.join("./tasks/benchmark/libs.txt"))
-        .unwrap()
-        .lines()
-        .map(|file| Code::new(10, file).unwrap())
-        .collect::<Vec<_>>();
-
+    let files = TestFiles::new();
     let mut args = Arguments::from_env();
 
     let baseline: Option<String> = args.opt_value_from_str("--save-baseline").unwrap();
-
-    let mut criterion = Criterion::default().without_plots();
+    let measurement_time = Duration::new(/* seconds */ 10, 0);
+    let mut criterion = Criterion::default().without_plots().measurement_time(measurement_time);
     if let Some(ref baseline) = baseline {
         criterion = criterion.save_baseline(baseline.to_string());
     }
 
     // Check files
-    for code in &codes {
+    for file in files.files() {
         let allocator = Allocator::default();
         let ret =
-            Parser::new(&allocator, black_box(&code.source_text), SourceType::default()).parse();
+            Parser::new(&allocator, black_box(&file.source_text), SourceType::default()).parse();
         if !ret.errors.is_empty() {
-            println!("{} failed", &code.file_name);
+            println!("{} failed", &file.file_name);
             for error in &ret.errors {
                 println!("{error:?}");
             }
@@ -52,21 +47,20 @@ pub fn main() -> Result<(), String> {
         }
     }
 
-    bench_parser(&mut criterion, &codes);
-    bench_semantic(&mut criterion, &codes);
+    bench_parser(&mut criterion, &files);
+    bench_semantic(&mut criterion, &files);
     drop(criterion);
 
     Ok(())
 }
 
-fn bench_parser(criterion: &mut Criterion, codes: &[Code]) {
+fn bench_parser(criterion: &mut Criterion, files: &TestFiles) {
     let mut group = criterion.benchmark_group("parser");
-    for code in codes {
-        group.throughput(Throughput::Bytes(code.source_text.len() as u64));
-        group.measurement_time(code.measurement_time);
+    for file in files.files() {
+        group.throughput(Throughput::Bytes(file.source_text.len() as u64));
         group.bench_with_input(
-            BenchmarkId::from_parameter(&code.file_name),
-            &code.source_text,
+            BenchmarkId::from_parameter(&file.file_name),
+            &file.source_text,
             |b, source_text| {
                 let allocator = Allocator::default();
                 b.iter(|| {
@@ -80,17 +74,16 @@ fn bench_parser(criterion: &mut Criterion, codes: &[Code]) {
     group.finish();
 }
 
-fn bench_semantic(criterion: &mut Criterion, codes: &[Code]) {
+fn bench_semantic(criterion: &mut Criterion, files: &TestFiles) {
     let mut group = criterion.benchmark_group("semantic");
-    for code in codes {
-        group.throughput(Throughput::Bytes(code.source_text.len() as u64));
-        group.measurement_time(code.measurement_time);
+    for file in files.files() {
+        group.throughput(Throughput::Bytes(file.source_text.len() as u64));
         group.bench_with_input(
-            BenchmarkId::from_parameter(&code.file_name),
-            &code.source_text,
+            BenchmarkId::from_parameter(&file.file_name),
+            &file.source_text,
             |b, source_text| {
                 let allocator = Allocator::default();
-                let source_type = SourceType::from_path(&code.file_name).unwrap();
+                let source_type = SourceType::from_path(&file.file_name).unwrap();
                 let ret = Parser::new(&allocator, source_text, source_type).parse();
                 let program = allocator.alloc(ret.program);
                 b.iter(|| {
