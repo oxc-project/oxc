191def172376bed48904108daf95af927dcc937f
wenzhe <mysteryven@gmail.com>
2023-04-15 13:25:34 +0800

    feat(linter: implement basic rule

--- crates/oxc_linter/src/rules/deepscan/bad_comparison_sequence.rs
+++ crates/oxc_linter/src/rules/deepscan/bad_comparison_sequence.rs
@@ -1,4 +1,7 @@
-use oxc_ast::Span;
+use oxc_ast::{
+    ast::{BinaryOperator, Expression},
+    AstKind, Span,
+};
 use oxc_diagnostics::{
     miette::{self, Diagnostic},
     thiserror::Error,
@@ -11,11 +14,13 @@ use crate::{context::LintContext, rule::Rule, AstNode};
 #[error("Bad comparison sequence")]
 #[diagnostic(
     severity(warning),
-    help("Comparison result should not be used directly as an operand of another comparison.")
+    help(
+        "Comparison result should not be used directly as an operand of another comparison. If you need to compare three or more operands, you should connect each comparison operation with logical AND operator (`&&`)"
+    )
 )]
-struct BadComparisonSequenceDiagnostic(Span);
+struct BadComparisonSequenceDiagnostic(#[label] pub Span);
 
-/// https://deepscan.io/docs/rules/bad-comparison-sequence
+/// `https://deepscan.io/docs/rules/bad-comparison-sequence`
 #[derive(Debug, Default, Clone)]
 pub struct BadComparisonSequence;
 
@@ -38,16 +43,152 @@ declare_oxc_lint!(
 );
 
 impl Rule for BadComparisonSequence {
-    fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {}
+    fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
+        if let AstKind::BinaryExpression(expr) = node.get().kind() {
+            if is_bad_comparison(node) && has_no_bad_comparison_in_parent(node, ctx) {
+                ctx.diagnostic(BadComparisonSequenceDiagnostic(expr.span));
+            }
+        }
+    }
+}
+
+fn has_no_bad_comparison_in_parent<'a, 'b>(
+    node: &'b AstNode<'a>,
+    ctx: &'b LintContext<'a>,
+) -> bool {
+    let mut current_node = node;
+    loop {
+        current_node = ctx.parent_node(current_node).unwrap();
+        let kind = current_node.get().kind();
+
+        if matches!(kind, AstKind::Root | AstKind::ParenthesizedExpression(_))
+            || kind.is_declaration()
+            || kind.is_statement()
+        {
+            return true;
+        }
+
+        if is_bad_comparison(node) {
+            return false;
+        }
+    }
+}
+
+fn is_bad_comparison(node: &AstNode) -> bool {
+    if let AstKind::BinaryExpression(expr) = node.get().kind() {
+        if is_equality_operator(&expr.operator) {
+            if let Expression::BinaryExpression(left_expr) = &expr.left && is_equality_operator(&left_expr.operator) {
+                return true
+            }
+        }
+
+        if is_relational_operator(&expr.operator) {
+            if let Expression::BinaryExpression(left_expr) = &expr.left && is_relational_operator(&left_expr.operator) {
+                return true
+            }
+        }
+    }
+
+    false
+}
+
+fn is_equality_operator(operator: &BinaryOperator) -> bool {
+    match operator {
+        BinaryOperator::Equality
+        | BinaryOperator::StrictEquality
+        | BinaryOperator::Inequality
+        | BinaryOperator::StrictInequality => true,
+        _ => false,
+    }
+}
+
+fn is_relational_operator(operator: &BinaryOperator) -> bool {
+    match operator {
+        BinaryOperator::GreaterThan
+        | BinaryOperator::GreaterEqualThan
+        | BinaryOperator::LessThan
+        | BinaryOperator::LessEqualThan => true,
+        _ => false,
+    }
 }
 
 #[test]
 fn test() {
     use crate::tester::Tester;
 
-    let pass = vec![];
+    let pass = vec![
+        ("if (a == b > c) { console.log('foo') }", None),
+        ("if (a == b < c) { console.log('foo') }", None),
+        ("if (a == b >= c) { console.log('foo') }", None),
+        ("if (a == b <= c) { console.log('foo') }", None),
+        ("if (a === b > c) { console.log('foo') }", None),
+        ("if (a === b < c) { console.log('foo') }", None),
+        ("if (a === b >= c) { console.log('foo') }", None),
+        ("if (a === b <= c) { console.log('foo') }", None),
+        ("if (a != b > c) { console.log('foo') }", None),
+        ("if (a != b < c) { console.log('foo') }", None),
+        ("if (a != b >= c) { console.log('foo') }", None),
+        ("if (a != b <= c) { console.log('foo') }", None),
+        ("if (a !== b > c) { console.log('foo') }", None),
+        ("if (a !== b < c) { console.log('foo') }", None),
+        ("if (a !== b >= c) { console.log('foo') }", None),
+        ("if (a !== b <= c) { console.log('foo') }", None),
+        ("if (a > b == c) { console.log('foo') }", None),
+        ("if (a > b === c) { console.log('foo') }", None),
+        ("if (a > b != c) { console.log('foo') }", None),
+        ("if (a > b !== c) { console.log('foo') }", None),
+        ("if (a < b == c) { console.log('foo') }", None),
+        ("if (a < b === c) { console.log('foo') }", None),
+        ("if (a < b != c) { console.log('foo') }", None),
+        ("if (a < b !== c) { console.log('foo') }", None),
+        ("if (a >= b == c) { console.log('foo') }", None),
+        ("if (a >= b === c) { console.log('foo') }", None),
+        ("if (a >= b != c) { console.log('foo') }", None),
+        ("if (a >= b !== c) { console.log('foo') }", None),
+        ("if (a <= b == c) { console.log('foo') }", None),
+        ("if (a <= b === c) { console.log('foo') }", None),
+        ("if (a <= b != c) { console.log('foo') }", None),
+        ("if (a <= b !== c) { console.log('foo') }", None),
+        ("if ((a == b) && (b == c)) { console.log('foo') }", None),
+    ];
 
-    let fail = vec![];
+    let fail = vec![
+        ("if (a == b == c) { console.log('foo') }", None),
+        ("if (a == b == c == d) { console.log('foo') }", None),
+        ("if ((a == b == c) == d) { console.log('foo') }", None),
+        ("if ((a == b == c) == d == e == f) { console.log('foo') }", None),
+        ("if (a == b === c) { console.log('foo') }", None),
+        ("if (a == b != c) { console.log('foo') }", None),
+        ("if (a == b !== c) { console.log('foo') }", None),
+        ("if (a === b == c) { console.log('foo') }", None),
+        ("if (a === b === c) { console.log('foo') }", None),
+        ("if (a === b != c) { console.log('foo') }", None),
+        ("if (a === b !== c) { console.log('foo') }", None),
+        ("if (a != b == c) { console.log('foo') }", None),
+        ("if (a != b === c) { console.log('foo') }", None),
+        ("if (a != b != c) { console.log('foo') }", None),
+        ("if (a != b !== c) { console.log('foo') }", None),
+        ("if (a !== b == c) { console.log('foo') }", None),
+        ("if (a !== b === c) { console.log('foo') }", None),
+        ("if (a !== b != c) { console.log('foo') }", None),
+        ("if (a !== b !== c) { console.log('foo') }", None),
+        ("if (a > b > c) { console.log('foo') }", None),
+        ("if (a > b < c) { console.log('foo') }", None),
+        ("if (a > b >= c) { console.log('foo') }", None),
+        ("if (a > b <= c) { console.log('foo') }", None),
+        ("if (a < b > c) { console.log('foo') }", None),
+        ("if (a < b < c) { console.log('foo') }", None),
+        ("if (a < b >= c) { console.log('foo') }", None),
+        ("if (a < b <= c) { console.log('foo') }", None),
+        ("if (a >= b > c) { console.log('foo') }", None),
+        ("if (a >= b < c) { console.log('foo') }", None),
+        ("if (a >= b >= c) { console.log('foo') }", None),
+        ("if (a >= b <= c) { console.log('foo') }", None),
+        ("if (a <= b > c) { console.log('foo') }", None),
+        ("if (a <= b < c) { console.log('foo') }", None),
+        ("if (a <= b >= c) { console.log('foo') }", None),
+        ("if (a <= b <= c) { console.log('foo') }", None),
+    ];
 
     Tester::new(BadComparisonSequence::NAME, pass, fail).test_and_snapshot();
 }
