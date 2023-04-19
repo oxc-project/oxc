1cfcbb734ef4e6955aa0515d082e21b3074d87bb
wenzhe <mysteryven@gmail.com>
2023-04-19 23:16:23 +0800

    feat(linter): implement basic rule

--- crates/oxc_linter/src/rules/deepscan/bad_array_method_on_arguments.rs
+++ crates/oxc_linter/src/rules/deepscan/bad_array_method_on_arguments.rs
@@ -1,4 +1,7 @@
-use oxc_ast::{ AstKind, Span};
+use oxc_ast::{
+    ast::{Expression, MemberExpression},
+    AstKind, Atom, Span,
+};
 use oxc_diagnostics::{
     miette::{self, Diagnostic},
     thiserror::Error,
@@ -8,14 +11,6 @@ use oxc_macros::declare_oxc_lint;
 use crate::{context::LintContext, rule::Rule, AstNode};
 
 #[derive(Debug, Error, Diagnostic)]
-#[error("Bad bitwise operator")]
-#[diagnostic(
-    severity(warning),
-    help("Bitwise operator '{0}' seems unintended. Did you mean logical operator '{1}'?")
-)]
-struct BadArrayMethodOnArgumentsDiagnostic(&'static str, &'static str, #[label] pub Span);
-
-#[derive(Debug, Error, Diagnostic)]
 #[error("Bad array method on arguments")]
 #[diagnostic(
     severity(warning),
@@ -23,7 +18,7 @@ struct BadArrayMethodOnArgumentsDiagnostic(&'static str, &'static str, #[label] 
         "The 'arguments' object does not have '{0}()' method. If an array method was intended, consider converting the 'arguments' object to an array or using ES6 rest parameter instead."
     )
 )]
-struct BadBitwiseOrOperatorDiagnostic(&'static str, #[label] pub Span);
+struct BadArrayMethodOnArgumentsDiagnostic(Atom, #[label] pub Span);
 
 /// `https://deepscan.io/docs/rules/bad-array-method-on-arguments`
 #[derive(Debug, Default, Clone)]
@@ -52,17 +47,90 @@ declare_oxc_lint!(
 
 impl Rule for BadArrayMethodOnArguments {
     fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
-        match node.get().kind() {}
+        // only access the Array.prototype.method can also pass, so we don't need check if the parent not a CallExpression
+        let AstKind::CallExpression(_) = ctx.parent_kind(node) else {return};
+        let AstKind::MemberExpression(member_expr) = node.get().kind() else {return};
+
+        match member_expr {
+            MemberExpression::StaticMemberExpression(expr) => {
+                if let Some(reference) = expr.object.get_identifier_reference() {
+                    if reference.name != "arguments" {
+                        return;
+                    }
+
+                    if ARRAY_METHODS.iter().any(|method| method == &expr.property.name) {
+                        ctx.diagnostic(BadArrayMethodOnArgumentsDiagnostic(
+                            expr.property.name.clone(),
+                            expr.span,
+                        ));
+                    }
+                }
+            }
+            MemberExpression::ComputedMemberExpression(expr) => {
+                if let Some(reference) = expr.object.get_identifier_reference() {
+                    if reference.name != "arguments" {
+                        return;
+                    }
+
+                    match &expr.expression {
+                        Expression::StringLiteral(name) => {
+                            if ARRAY_METHODS.iter().any(|method| method == &name.value) {
+                                ctx.diagnostic(BadArrayMethodOnArgumentsDiagnostic(
+                                    name.value.clone(),
+                                    expr.span,
+                                ));
+                            }
+                        }
+                        Expression::TemplateLiteral(template) => {
+                            if template.expressions.is_empty() && template.quasis.len() == 1 {
+                                if let Some(name) = &template.quasis[0].value.cooked.as_deref() {
+                                    if ARRAY_METHODS.iter().any(|method| method == name) {
+                                        ctx.diagnostic(BadArrayMethodOnArgumentsDiagnostic(
+                                            template.quasis[0]
+                                                .value
+                                                .cooked
+                                                .as_ref()
+                                                .unwrap()
+                                                .clone(),
+                                            expr.span,
+                                        ));
+                                    }
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        }
     }
 }
+
+// `https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#instance_methods`
+const ARRAY_METHODS: [&str; 2] = ["reduce", "map"];
 
 #[test]
 fn test() {
     use crate::tester::Tester;
 
-    let pass = vec![];
+    let pass = vec![
+        ("function sum() {}", None),
+        ("function sum(...args) {return args.reduce((prev, cur) => prev + cur, 0)}", None),
+        ("function sum() { arguments.foo }", None),
+        ("function sum() { arguments.map }", None),
+        // keep those passing tests for Deepscan Compatible
+        ("function sum() {arguments[`map${''}`]((prev, cur) => prev + cur, 0)}", None),
+        ("function sum() {arguments[`${''}map`]((prev, cur) => prev + cur, 0)}", None),
+        ("function sum() {arguments[`${'map'}`]((prev, cur) => prev + cur, 0)}", None),
+    ];
 
-    let fail = vec![];
+    let fail = vec![
+        ("function sum() {arguments.map((prev, cur) => prev + cur, 0)}", None),
+        ("function sum() {arguments['map']((prev, cur) => prev + cur, 0)}", None),
+        ("function sum() {arguments[`map`]((prev, cur) => prev + cur, 0)}", None),
+        ("function sum() {arguments.reduce((prev, cur) => prev + cur, 0)}", None),
+    ];
 
     Tester::new(BadArrayMethodOnArguments::NAME, pass, fail).test_and_snapshot();
 }
