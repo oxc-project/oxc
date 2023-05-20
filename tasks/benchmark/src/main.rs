2c757638ab893cfdcfa6c3d3ada9857750d76351
Boshen <boshenc@gmail.com>
2023-05-20 19:06:08 +0800

    feat(benchmark): add minifier to benchmark

--- tasks/benchmark/src/main.rs
+++ tasks/benchmark/src/main.rs
@@ -14,9 +14,8 @@ use std::time::Duration;
 
 use criterion::{BenchmarkId, Criterion, Throughput};
 use oxc_allocator::Allocator;
-use oxc_ast_lower::AstLower;
+use oxc_minifier::{Minifier, MinifierOptions};
 use oxc_parser::Parser;
-use oxc_semantic::SemanticBuilder;
 use oxc_span::SourceType;
 use oxc_tasks_common::{TestFile, TestFiles};
 use pico_args::Arguments;
@@ -56,8 +55,7 @@ pub fn main() -> Result<(), String> {
     }
 
     bench_parser(&mut criterion, &files);
-    // bench_semantic(&mut criterion, &files);
-    bench_ast_lower(&mut criterion, &files);
+    bench_minifier(&mut criterion, &files);
     drop(criterion);
 
     Ok(())
@@ -86,31 +84,8 @@ fn bench_parser(criterion: &mut Criterion, files: &[&TestFile]) {
     group.finish();
 }
 
-#[allow(unused)]
-fn bench_semantic(criterion: &mut Criterion, files: &[&TestFile]) {
-    let mut group = criterion.benchmark_group("semantic");
-    for file in files {
-        group.throughput(Throughput::Bytes(file.source_text.len() as u64));
-        group.bench_with_input(
-            BenchmarkId::from_parameter(&file.file_name),
-            &file.source_text,
-            |b, source_text| {
-                let allocator = Allocator::default();
-                let source_type = SourceType::from_path(&file.file_name).unwrap();
-                let ret = Parser::new(&allocator, source_text, source_type).parse();
-                let program = allocator.alloc(ret.program);
-                b.iter(|| {
-                    let _semantic = SemanticBuilder::new(source_text, source_type, &ret.trivias)
-                        .build(black_box(program));
-                });
-            },
-        );
-    }
-    group.finish();
-}
-
-fn bench_ast_lower(criterion: &mut Criterion, files: &[&TestFile]) {
-    let mut group = criterion.benchmark_group("ast_lower");
+fn bench_minifier(criterion: &mut Criterion, files: &[&TestFile]) {
+    let mut group = criterion.benchmark_group("minifier");
     for file in files {
         group.throughput(Throughput::Bytes(file.source_text.len() as u64));
         group.bench_with_input(
@@ -118,11 +93,10 @@ fn bench_ast_lower(criterion: &mut Criterion, files: &[&TestFile]) {
             &file.source_text,
             |b, source_text| {
                 let source_type = SourceType::from_path(&file.file_name).unwrap();
+                let options = MinifierOptions::default();
                 b.iter(|| {
-                    let allocator = Allocator::default();
-                    let ret = Parser::new(&allocator, source_text, source_type).parse();
-                    let _hir =
-                        AstLower::new(&allocator, source_type).build(black_box(&ret.program));
+                    let _minified =
+                        Minifier::new(black_box(source_text), source_type, options).build();
                 });
             },
         );
