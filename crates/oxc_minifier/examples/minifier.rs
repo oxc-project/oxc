69b3aff8586d05e490237289e74145360785a67d
Boshen <boshenc@gmail.com>
2023-05-21 14:45:24 +0800

    feat(minifier): add --mangle option to example

--- crates/oxc_minifier/examples/minifier.rs
+++ crates/oxc_minifier/examples/minifier.rs
@@ -1,7 +1,8 @@
-use std::{env, path::Path};
+use std::path::Path;
 
 use oxc_minifier::{Minifier, MinifierOptions};
 use oxc_span::SourceType;
+use pico_args::Arguments;
 
 // Instruction:
 // create a `test.js`,
@@ -9,18 +10,16 @@ use oxc_span::SourceType;
 // or `cargo watch -x "run -p oxc_minifier --example minifier"`
 
 fn main() {
-    let name = env::args().nth(1).unwrap_or_else(|| "test.js".to_string());
+    let mut args = Arguments::from_env();
+
+    let name = args.subcommand().ok().flatten().unwrap_or_else(|| String::from("test.js"));
+    let mangle = args.contains("--mangle");
+
     let path = Path::new(&name);
-    let source_text = std::fs::read_to_string(path).expect("{name} not found");
+    let source_text = std::fs::read_to_string(path).unwrap_or_else(|_| panic!("{name} not found"));
     let source_type = SourceType::from_path(path).unwrap();
 
-    println!("------------------------------");
-    let options = MinifierOptions { mangle: false, ..MinifierOptions::default() };
-    let printed = Minifier::new(&source_text, source_type, options).build();
-    println!("{printed}");
-
-    println!("------ Mangle ----------------");
-    let options = MinifierOptions::default();
+    let options = MinifierOptions { mangle, ..MinifierOptions::default() };
     let printed = Minifier::new(&source_text, source_type, options).build();
     println!("{printed}");
 }
