c1d1846714fd427c1e02ba3ecd51d022cc365c9c
Yoni Feng <32789876+YoniFeng@users.noreply.github.com>
2023-05-23 12:00:49 +0300

    Add Brotli size to minsize task output (#377)

--- tasks/minsize/src/lib.rs
+++ tasks/minsize/src/lib.rs
@@ -3,6 +3,7 @@ use std::{
     io::{self, Write},
 };
 
+use brotlic::{BlockSize, BrotliEncoderOptions, CompressorWriter, Quality, WindowSize};
 use flate2::{write::GzEncoder, Compression};
 use humansize::{format_size, DECIMAL};
 use oxc_minifier::{Minifier, MinifierOptions};
@@ -23,14 +24,22 @@ pub fn run() -> Result<(), io::Error> {
     let path = project_root().join("tasks/minsize/minsize.snap");
 
     let mut out = String::new();
-    out.push_str(&format!("{:width$} -> {:width$} -> Gzip\n", "Original", "Minified", width = 10));
+    out.push_str(&format!(
+        "{:width$} -> {:width$} -> {:width$}",
+        "Original",
+        "Minified",
+        "Gzip",
+        width = 10
+    ));
+    out.push_str(&format!(" {:width$}\n", "Brotli", width = 10));
     for file in files.files() {
         let minified = minify(file);
         let s = format!(
-            "{:width$} -> {:width$} -> {:width$} {}\n",
+            "{:width$} -> {:width$} -> {:width$} {:width$} {}\n",
             format_size(file.source_text.len(), DECIMAL),
             format_size(minified.len(), DECIMAL),
             format_size(gzip_size(&minified), DECIMAL),
+            format_size(brotli_size(&minified), DECIMAL),
             &file.file_name,
             width = 10
         );
@@ -56,5 +65,19 @@ fn gzip_size(s: &str) -> usize {
     let mut e = GzEncoder::new(Vec::new(), Compression::best());
     e.write_all(s.as_bytes()).unwrap();
     let s = e.finish().unwrap();
+    s.len()
+}
+
+fn brotli_size(s: &str) -> usize {
+    let encoder = BrotliEncoderOptions::new()
+        .quality(Quality::best())
+        .window_size(WindowSize::best())
+        .block_size(BlockSize::best())
+        .build()
+        .unwrap();
+
+    let mut e = CompressorWriter::with_encoder(encoder, Vec::new());
+    e.write_all(s.as_bytes()).unwrap();
+    let s = e.into_inner().unwrap();
     s.len()
 }
