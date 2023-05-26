db51772c8c2c52b738e5c38d4ea4d991a10a0d4f
Boshen <boshenc@gmail.com>
2023-05-26 12:29:18 +0800

    chore(minifier): add "undefined" tests from closure compiler

--- crates/oxc_minifier/tests/closure/substitute_alternate_syntax.rs
+++ crates/oxc_minifier/tests/closure/substitute_alternate_syntax.rs
@@ -11,3 +11,17 @@ fn fold_return_result() {
     expect("function f(){return undefined;}", "function f(){return}");
     expect("function f(){if(a()){return undefined;}}", "function f(){if(a()){return}}");
 }
+
+#[test]
+fn undefined() {
+    expect("var x = undefined", "var x=void 0");
+    expect(
+        "var undefined = 1;function f() {var undefined=2;var x = undefined;}",
+        "var undefined=1;function f(){var undefined=2,x=undefined}",
+    );
+    expect("function f(undefined) {}", "function f(undefined){}");
+    expect("try {} catch(undefined) {}", "try{}catch(undefined){}");
+    expect("for (undefined in {}) {}", "for(undefined in {}){}");
+    expect("undefined++", "undefined++");
+    expect("undefined += undefined;", "undefined+=void 0");
+}
