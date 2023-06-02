a1cb1e688506967d90282ce663d43fefc057cb29
Boshen <boshenc@gmail.com>
2023-06-02 13:20:07 +0800

    feat(website): init playground

---
+++ website/vite.config.js
@@ -0,0 +1,13 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        playground: resolve(__dirname, 'playground/index.html'),
+      },
+    },
+  },
+})
