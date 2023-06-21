8bc94954aaad809ddf171b45ce97b348011b6f02
Maneren <49210777+Maneren@users.noreply.github.com>
2023-06-21 13:12:06 +0200

    comment out obnoxious warning

--- crates/oxc_cli/src/lint/runner_with_module_tree.rs
+++ crates/oxc_cli/src/lint/runner_with_module_tree.rs
@@ -240,7 +240,7 @@ fn run_for_file(path: &Path, runtime_data: &LinterRuntimeData) {
     }
 
     let Ok(source_type) = SourceType::from_path(path) else {
-        eprintln!("File {} is not supported, skipping.", path.display());
+        // eprintln!("File {} is not supported, skipping.", path.display());
         return;
     };
 
