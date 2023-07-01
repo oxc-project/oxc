13290f8612df8d5564da37727bf99d8a93151bb6
Boshen <boshenc@gmail.com>
2023-07-01 18:40:42 +0800

    chore(linter): port @typescript-eslint/no-dupe-class-members tests

--- crates/oxc_linter/src/rules/eslint/no_array_constructor.rs
+++ crates/oxc_linter/src/rules/eslint/no_array_constructor.rs
@@ -21,6 +21,7 @@ declare_oxc_lint!(
     /// Disallow array constructor
     ///
     /// ### Why is this bad?
+    ///
     /// Use of the Array constructor to construct a new array is generally discouraged in favor of array literal notation because of the single-argument pitfall and because the Array global may be redefined.
     /// The exception is when the Array constructor is used to intentionally create sparse arrays of a specified size by giving the constructor a single numeric argument.
     ///
