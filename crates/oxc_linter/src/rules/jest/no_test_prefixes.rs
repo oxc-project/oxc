5ce466a07a764881f6f43136375c302662bc20dc
cijiugechu <cijiugechu@gmail.com>
2023-07-10 01:52:41 +0800

    feat: implement `no-test-prefixes`

---
+++ crates/oxc_linter/src/rules/jest/no_test_prefixes.rs
@@ -0,0 +1,145 @@
+use std::borrow::Borrow;
+
+use oxc_ast::{AstKind, ast::Expression};
+use oxc_diagnostics::{
+    miette::{self, Diagnostic},
+    thiserror::Error,
+};
+use oxc_macros::declare_oxc_lint;
+use oxc_span::{Span, GetSpan};
+
+use crate::{context::LintContext, rule::Rule, AstNode, jest_ast_util::{parse_jest_fn_call, ParsedJestFnCall, JestFnKind}, fixer::Fix};
+
+#[derive(Debug, Error, Diagnostic)]
+#[error("eslint(jest/no-test-prefixes): Use {0:?} instead.")]
+#[diagnostic(severity(warning))]
+struct NoTestPrefixesDiagnostic<'a>(&'a str, #[label] pub Span);
+
+#[derive(Debug, Default, Clone)]
+pub struct NoTestPrefixes;
+
+declare_oxc_lint!(
+    /// ### What it does
+    /// 
+    /// Require using `.only` and `.skip` over `f` and `x`.
+    ///
+    /// ### Why is this bad?
+    ///
+    /// Jest allows you to choose how you want to define focused and skipped tests, 
+    /// with multiple permutations for each:
+    /// - only & skip: it.only, test.only, describe.only, it.skip, test.skip, describe.skip.
+    /// - 'f' & 'x': fit, fdescribe, xit, xtest, xdescribe.
+    /// 
+    /// This rule enforces usages from the only & skip list.
+    ///
+    /// ### Example
+    /// ```javascript
+    /// fit('foo'); // invalid
+    /// fdescribe('foo'); // invalid
+    /// xit('foo'); // invalid
+    /// xtest('foo'); // invalid
+    /// xdescribe('foo'); // invalid
+    /// ```
+    NoTestPrefixes,
+    correctness
+);
+
+fn get_prefered_node_names(jest_fn_call: &ParsedJestFnCall) -> String {
+
+    let ParsedJestFnCall { members, raw, .. } = jest_fn_call;
+
+    let prefered_modifier = if raw.starts_with('f') {
+        "only"
+    } else {
+        "skip"
+    };
+    let member_names = members.iter().map(Borrow::borrow).collect::<Vec<&str>>().join(".");
+    let name_slice = &raw[1..];
+
+    format!("{name_slice}.{prefered_modifier}.{member_names}")
+}
+
+impl Rule for NoTestPrefixes {
+    fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
+        if let AstKind::CallExpression(call_expr) = node.kind() {
+            if let Some(jest_fn_call) = parse_jest_fn_call(call_expr, ctx) {
+                let ParsedJestFnCall { kind, raw, .. } = &jest_fn_call;
+
+                if !matches!(kind, JestFnKind::Describe | JestFnKind::Test) {
+                    return;
+                }
+
+                if !raw.starts_with('f') && !raw.starts_with('x') {
+                    return;
+                }
+
+                let span = match &call_expr.callee {
+                    Expression::TaggedTemplateExpression(tagged_template_expr) => {
+                        tagged_template_expr.tag.span()
+                    }
+                    Expression::CallExpression(child_call_expr) => {
+                        child_call_expr.callee.span()
+                    }
+                    _ => {
+                        call_expr.callee.span()
+                    }
+                };
+
+                let prefered_node_name = get_prefered_node_names(&jest_fn_call);
+                let prefered_node_name_cloned = prefered_node_name.clone();
+
+                ctx.diagnostic_with_fix(
+                    NoTestPrefixesDiagnostic(Box::leak(prefered_node_name.into_boxed_str()), span),
+                    || Fix::new(prefered_node_name_cloned, span)
+                );
+            }
+        }
+    }
+}
+
+#[test]
+fn test() {
+    use crate::tester::Tester;
+
+    let pass = vec![
+        ("describe('foo', function () {})", None),
+        ("it('foo', function () {})", None),
+        ("it.concurrent('foo', function () {})", None),
+        ("test('foo', function () {})", None),
+        ("test.concurrent('foo', function () {})", None),
+        ("describe.only('foo', function () {})", None),
+        ("it.only('foo', function () {})", None),
+        ("it.each()('foo', function () {})", None),
+        ("it.each``('foo', function () {})", None),
+        ("test.only('foo', function () {})", None),
+        ("test.each()('foo', function () {})", None),
+        ("test.each``('foo', function () {})", None),
+        ("describe.skip('foo', function () {})", None),
+        ("it.skip('foo', function () {})", None),
+        ("test.skip('foo', function () {})", None),
+        ("foo()", None),
+        ("[1,2,3].forEach()", None),
+    ];
+
+    let fail = vec![
+        ("fdescribe('foo', function () {})", None),
+        ("xdescribe.each([])('foo', function () {})", None),
+        ("fit('foo', function () {})", None),
+        ("xdescribe('foo', function () {})", None),
+        ("xit('foo', function () {})", None),
+        ("xtest('foo', function () {})", None),
+        ("xit.each``('foo', function () {})", None),
+        ("xtest.each``('foo', function () {})", None),
+        ("xit.each([])('foo', function () {})", None),
+        ("xtest.each([])('foo', function () {})", None)
+        // TODO: Continue work on it when [#510](https://github.com/Boshen/oxc/issues/510) solved
+        // (r#"import { xit } from '@jest/globals';
+        // xit("foo", function () {})"#, None),
+        // (r#"import { xit as skipThis } from '@jest/globals';
+        // skipThis("foo", function () {})"#, None),
+        // (r#"import { fit as onlyThis } from '@jest/globals';
+        // onlyThis("foo", function () {})"#, None)
+    ];
+
+    Tester::new(NoTestPrefixes::NAME, pass, fail).test_and_snapshot();
+}
