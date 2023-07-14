2e970f8a0f436010ef7c0b6089623f56f6987340
Boshen <boshenc@gmail.com>
2023-07-14 15:15:31 +0800

    perf(resolver): add file system cache

--- crates/oxc_resolver/tests/mod.rs
+++ crates/oxc_resolver/tests/mod.rs
@@ -1,7 +1,7 @@
 mod enhanced_resolve;
 mod error_handling;
 
-use std::path::Path;
+use std::{env, path::Path, sync::Arc, thread};
 
 use oxc_resolver::{ResolveResult, Resolver};
 
@@ -15,4 +15,20 @@ use oxc_resolver::{ResolveResult, Resolver};
 pub fn resolve<P: AsRef<Path>>(path: P, request: &str) -> ResolveResult {
     assert!(!request.is_empty());
     Resolver::new().resolve(path.as_ref(), request)
+}
+
+#[test]
+fn threaded_environment() {
+    let cwd = env::current_dir().unwrap();
+    let resolver = Arc::new(Resolver::new());
+    for _ in 0..2 {
+        _ = thread::spawn({
+            let cwd = cwd.clone();
+            let resolver = Arc::clone(&resolver);
+            move || {
+                _ = resolver.resolve(cwd, ".");
+            }
+        })
+        .join();
+    }
 }
