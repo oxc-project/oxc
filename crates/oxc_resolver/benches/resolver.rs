75e00368152b33ee73dfdd959ad61660fabbee5a
Boshen <boshenc@gmail.com>
2023-07-18 15:59:12 +0800

    chore(resolver): add benchmark against nodejs_resolver

--- crates/oxc_resolver/benches/resolver.rs
+++ crates/oxc_resolver/benches/resolver.rs
@@ -7,35 +7,64 @@
 //! cargo bench --bench resolver -- --save-baseline pr
 //! critcmp
 //! ```
-use std::env;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+#[cfg(target_os = "windows")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+use std::{env, path::PathBuf};
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use oxc_resolver::Resolver;
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn data() -> Vec<(PathBuf, &'static str)> {
     let cwd = env::current_dir().unwrap().join("tests/enhanced_resolve/");
-    let resolver = Resolver::default();
-
-    let data = [
+    vec![
         (cwd.clone(), "./"),
         (cwd.clone(), "./lib/index"),
         (cwd.join("./test/fixtures/extensions"), "./foo"),
         (cwd.join("test/fixtures/extensions/module"), "module"),
-    ];
-
-    // Check path is valid
-    for (path, request) in &data {
-        assert!(resolver.resolve(path, request).is_ok(), "{path:?} {request}");
-    }
-
-    c.bench_with_input(BenchmarkId::new("resolve", ""), &data, |b, data| {
-        b.iter(|| {
-            for (path, request) in data {
-                _ = resolver.resolve(path, request);
-            }
-        });
-    });
+    ]
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn resolver_benchmark(c: &mut Criterion) {
+    let data = data();
+
+    // bench nodejs_resolver
+    {
+        let resolver = nodejs_resolver::Resolver::new(nodejs_resolver::Options::default());
+        // Check path is valid
+        for (path, request) in &data {
+            assert!(resolver.resolve(path, request).is_ok(), "{path:?} {request}");
+        }
+        c.bench_with_input(BenchmarkId::new("nodejs_resolver", ""), &data, |b, data| {
+            b.iter(|| {
+                for (path, request) in data {
+                    _ = resolver.resolve(path, request);
+                }
+            });
+        });
+    }
+
+    // bench oxc_resolver
+    {
+        let resolver = oxc_resolver::Resolver::default();
+        // Check path is valid
+        for (path, request) in &data {
+            assert!(resolver.resolve(path, request).is_ok(), "{path:?} {request}");
+        }
+        c.bench_with_input(BenchmarkId::new("oxc_resolver", ""), &data, |b, data| {
+            b.iter(|| {
+                for (path, request) in data {
+                    _ = resolver.resolve(path, request);
+                }
+            });
+        });
+    }
+}
+
+criterion_group!(benches, resolver_benchmark);
 criterion_main!(benches);
