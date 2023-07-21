06aac508f875375d195cc11e04cbc1ebd38acaa0
Boshen <boshenc@gmail.com>
2023-07-21 11:08:33 +0800

    fix(linter): change no-var-requires to severity warning

--- crates/oxc_linter/src/rules/typescript/no_var_requires.rs
+++ crates/oxc_linter/src/rules/typescript/no_var_requires.rs
@@ -12,7 +12,7 @@ use crate::{context::LintContext, rule::Rule, AstNode};
 #[error(
     "typescript-eslint(no-var-requires): Use ES6 style imports or import foo = require(\"foo\") imports."
 )]
-#[diagnostic(severity(error))]
+#[diagnostic(severity(warning))]
 struct NoVarRequiresDiagnostic(#[label] pub Span);
 
 #[derive(Debug, Default, Clone)]
@@ -20,13 +20,13 @@ pub struct NoVarRequires;
 
 declare_oxc_lint!(
     /// ### What it does
-    /// 
+    ///
     /// Disallow `require` statements except in import statements
-    /// 
+    ///
     /// ### Why is this bad?
-    /// 
+    ///
     /// In other words, the use of forms such as var foo = require("foo") are banned. Instead use ES6 style imports or import foo = require("foo") imports.
-    /// 
+    ///
     /// ```typescript
     /// var foo = require('foo');
     /// const foo = require('foo');
@@ -44,7 +44,7 @@ impl Rule for NoVarRequires {
         if let AstKind::CallExpression(expr) = node.kind() && expr.is_require_call() {
             if ctx.scopes().get_bindings(node.scope_id()).contains_key("require") {
                 return;
-            } 
+            }
 
             if let Some(parent_node) = ctx.nodes().parent_node(node.id()) {
                 if let AstKind::Argument(_) = parent_node.kind() {
@@ -64,12 +64,15 @@ impl Rule for NoVarRequires {
 }
 
 fn is_target_node(node_kind: &AstKind<'_>) -> bool {
-    matches!(node_kind, AstKind::CallExpression(_)
-        | AstKind::MemberExpression(_)
-        | AstKind::NewExpression(_)
-        | AstKind::TSAsExpression(_)
-        | AstKind::TSTypeAssertion(_)
-        | AstKind::VariableDeclarator(_))
+    matches!(
+        node_kind,
+        AstKind::CallExpression(_)
+            | AstKind::MemberExpression(_)
+            | AstKind::NewExpression(_)
+            | AstKind::TSAsExpression(_)
+            | AstKind::TSTypeAssertion(_)
+            | AstKind::VariableDeclarator(_)
+    )
 }
 
 #[test]
@@ -98,15 +101,13 @@ fn test() {
         "let foo = trick(require?.('foo'));",
         "let foo = trick?.(require('foo'));",
         "const foo = require('./foo.json') as Foo;",
-
         // Because of TypeScript disallows angle bracket type assertions in .tsx files, comment out this below case all tests parsing as tsx.
         // "const foo = <Foo>require('./foo.json');",
-
         "const foo: Foo = require('./foo.json').default;",
         r#"
             const configValidator = new Validator(require('./a.json'));
             configValidator.addSchema(require('./a.json'));
-        "#
+        "#,
     ];
 
     Tester::new_without_config(NoVarRequires::NAME, pass, fail).test_and_snapshot();
