2afe67032d3605177c8ed4df91a21e0c0b7e2765
U9G <git@u9g.dev>
2023-07-22 03:06:50 -0400

    remove code accidently committed

--- crates/oxc_query/src/oxc_query/adapter/entrypoints.rs
+++
@@ -1,7 +0,0 @@
-use trustfall::provider::{ResolveInfo, VertexIterator};
-
-use super::vertex::Vertex;
-
-pub(super) fn file<'a>(_resolve_info: &ResolveInfo) -> VertexIterator<'a, Vertex> {
-    todo!("implement resolving starting vertices for entrypoint edge 'File'")
-}
--- crates/oxc_query/src/oxc_query/adapter/mod.rs
+++
@@ -1,8 +0,0 @@
-mod adapter;
-mod vertex;
-mod entrypoints;
-mod properties;
-mod edges;
-
-pub use adapter::Adapter;
-pub use vertex::Vertex;
