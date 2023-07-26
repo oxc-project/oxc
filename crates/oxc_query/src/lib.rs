ebda6d5450188b1f2d46aaba2734269842e96d09
u9g <git@u9g.dev>
2023-07-26 14:53:05 -0400

    Update lib.rs

--- crates/oxc_query/src/lib.rs
+++ crates/oxc_query/src/lib.rs
@@ -103,7 +103,7 @@ query {
             span_asmt_type_end: u64,
             entire_span_start: u64,
             entire_span_end: u64,
-            __typename: String
+            __typename: String,
         }
 
         let mut results: Vec<Output> = execute_query(
