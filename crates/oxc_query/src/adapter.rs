228cab92699bf2433fc2202ff308ed352592f976
u9g <git@u9g.dev>
2023-07-30 18:15:57 -0400

    feat(query): add ast counterpart to ObjectLiteral (#666)

--- crates/oxc_query/src/adapter.rs
+++ crates/oxc_query/src/adapter.rs
@@ -136,11 +136,13 @@ impl<'a, 'b: 'a> trustfall::provider::Adapter<'a> for &'a Adapter<'b> {
                 property_name.as_ref(),
                 resolve_info,
             ),
-            "ObjectLiteral" => super::properties::resolve_object_literal_property(
-                contexts,
-                property_name.as_ref(),
-                resolve_info,
-            ),
+            "ObjectLiteralAST" | "ObjectLiteral" => {
+                super::properties::resolve_object_literal_property(
+                    contexts,
+                    property_name.as_ref(),
+                    resolve_info,
+                )
+            }
             "PathPart" => super::properties::resolve_path_part_property(
                 contexts,
                 property_name.as_ref(),
@@ -323,11 +325,12 @@ impl<'a, 'b: 'a> trustfall::provider::Adapter<'a> for &'a Adapter<'b> {
                 parameters,
                 resolve_info,
             ),
-            "ObjectLiteral" => super::edges::resolve_object_literal_edge(
+            "ObjectLiteral" | "ObjectLiteralAST" => super::edges::resolve_object_literal_edge(
                 contexts,
                 edge_name.as_ref(),
                 parameters,
                 resolve_info,
+                self,
             ),
             "PathPart" => super::edges::resolve_path_part_edge(
                 contexts,
