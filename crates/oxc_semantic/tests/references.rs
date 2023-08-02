86faa9e2286457c46a95ef531dc92ee3490e1bcd
Don Isaac <donald.isaac@gmail.com>
2023-08-01 23:55:22 -0400

    test(semantic): test harness

---
+++ crates/oxc_semantic/tests/references.rs
@@ -0,0 +1,25 @@
+mod util;
+use oxc_semantic::SymbolFlags;
+use util::SemanticTester;
+
+#[test]
+fn test_class_simple() {
+    SemanticTester::js("export class Foo {}")
+        .has_root_symbol("Foo")
+        .contains_flags(SymbolFlags::Class | SymbolFlags::Export)
+        .has_number_of_references(0)
+        .test();
+
+    SemanticTester::js("class Foo {}; let f = new Foo()")
+        .has_root_symbol("Foo")
+        .has_number_of_reads(1)
+        .test();
+}
+
+#[test]
+fn test_function_simple() {
+    SemanticTester::js("function foo() { return }")
+        .has_root_symbol("foo")
+        .contains_flags(SymbolFlags::Function)
+        .test();
+}
