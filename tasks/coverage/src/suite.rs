d6693d69897cbbfa7c2f33af7facccd1fabcdcb4
YunfeiHe <i.heyunfei@gmail.com>
2023-08-18 21:17:21 +0800

    Fix test

--- tasks/coverage/src/suite.rs
+++ tasks/coverage/src/suite.rs
@@ -365,19 +365,22 @@ pub trait Case: Sized + Sync + Send + UnwindSafe {
 }
 
 fn are_all_identifiers_resolved(semantic: &oxc_semantic::Semantic<'_>) -> bool {
+    use oxc_ast::ast;
     use oxc_ast::AstKind;
 
     let ast_nodes = semantic.nodes();
     let has_non_resolved = ast_nodes.iter().any(|node| {
         match node.kind() {
             AstKind::BindingIdentifier(id) => {
-                // match ast_nodes.parent_kind(node.id()) {
-                //     Some(AstKind::Function(func)) if func.r#type == ast::FunctionType::FunctionExpression => {
-                //         // FIXME: Currently, the name of `FunctionExpression` won't be assigned a `SymbolId`
-                //         return false
-                //     }
-                //     _ => {}
-                // }
+                match ast_nodes.parent_kind(node.id()) {
+                    Some(AstKind::Function(func))
+                        if func.r#type == ast::FunctionType::FunctionExpression =>
+                    {
+                        // FIXME: Currently, the name of `FunctionExpression` won't be assigned a `SymbolId`
+                        return false;
+                    }
+                    _ => {}
+                }
 
                 id.symbol_id.get().is_none()
             }
