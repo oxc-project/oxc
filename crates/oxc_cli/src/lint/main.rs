a9c4fddb6dd4a31c10ac2e0aaef167ddcc60b91c
Boshen <boshenc@gmail.com>
2023-08-18 17:14:06 +0800

    refactor(cli): use bpaf instead clap (#756)

--- crates/oxc_cli/src/lint/main.rs
+++ crates/oxc_cli/src/lint/main.rs
@@ -8,30 +8,10 @@ static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
-use clap::{Arg, Command};
 use oxc_cli::{CliRunResult, LintRunner, Runner};
 
-pub fn command() -> Command {
-    LintRunner::command()
-        .bin_name("oxlint")
-        .version("alpha")
-        .author("Boshen")
-        .about("Linter for the JavaScript Oxidation Compiler")
-        .arg_required_else_help(true)
-        .arg(
-            Arg::new("threads")
-                .long("threads")
-                .value_parser(clap::value_parser!(usize))
-                .help("Number of threads to use. Set to 1 for using only 1 CPU core."),
-        )
-}
-
 fn main() -> CliRunResult {
-    let matches = command().get_matches();
-
-    if let Some(threads) = matches.get_one::<usize>("threads") {
-        rayon::ThreadPoolBuilder::new().num_threads(*threads).build_global().unwrap();
-    }
-
-    LintRunner::new(&matches).run()
+    let command = oxc_cli::lint_command().fallback_to_usage().run();
+    command.handle_threads();
+    LintRunner::new(command.lint_options).run()
 }
--- crates/oxc_cli/src/lint/mod.rs
+++ crates/oxc_cli/src/lint/mod.rs
@@ -1,19 +1,17 @@
-mod command;
 mod error;
 mod isolated_handler;
 
-use clap::{ArgMatches, Command};
-use std::{collections::BTreeMap, env, path::PathBuf};
+use std::path::PathBuf;
 use std::{io::BufWriter, sync::Arc, time::Duration};
 
-use self::command::lint_command;
 pub use self::{error::Error, isolated_handler::IsolatedLintHandler};
 
 use oxc_index::assert_impl_all;
 use oxc_linter::{AllowWarnDeny, LintOptions, Linter, RuleCategory, RuleEnum, RULES};
 use rustc_hash::FxHashSet;
 
-use crate::{CliRunResult, Runner};
+use crate::FilterType;
+use crate::{command::LintOptions as CliLintOptions, CliRunResult, Runner};
 
 pub struct LintRunner {
     options: Arc<LintOptions>,
@@ -22,19 +20,14 @@ pub struct LintRunner {
 assert_impl_all!(LintRunner: Send, Sync);
 
 impl Runner for LintRunner {
-    const ABOUT: &'static str = "Lint this repository.";
-    const NAME: &'static str = "lint";
+    type Options = CliLintOptions;
 
-    fn new(matches: &ArgMatches) -> Self {
-        let options = parse_arg_matches(matches);
+    fn new(options: Self::Options) -> Self {
+        let options = parse_cli_options(options);
         let linter = Linter::from_rules(Self::derive_rules(&options))
             .with_fix(options.fix)
             .with_print_execution_times(options.print_execution_times);
         Self { options: Arc::new(options), linter: Arc::new(linter) }
-    }
-
-    fn init_command() -> Command {
-        lint_command()
     }
 
     fn run(&self) -> CliRunResult {
@@ -128,48 +121,37 @@ impl LintRunner {
     }
 }
 
-fn parse_arg_matches(matches: &ArgMatches) -> LintOptions {
-    let list_rules = matches.get_flag("rules");
+fn parse_cli_options(options: CliLintOptions) -> LintOptions {
+    let rules = get_rules(&options);
     LintOptions {
-        paths: matches.get_many("path").map_or_else(
-            || if list_rules { vec![] } else { vec![PathBuf::from(".")] },
-            |paths| paths.into_iter().cloned().collect(),
-        ),
-        rules: get_rules(matches),
-        fix: matches.get_flag("fix"),
-        quiet: matches.get_flag("quiet"),
-        ignore_path: matches
-            .get_one::<PathBuf>("ignore-path")
-            .map_or_else(|| PathBuf::from(".eslintignore"), Clone::clone),
-        no_ignore: matches.get_flag("no-ignore"),
-        ignore_pattern: matches
-            .get_many::<String>("ignore-pattern")
-            .map(|patterns| patterns.into_iter().cloned().collect())
-            .unwrap_or_default(),
-        max_warnings: matches.get_one("max-warnings").copied(),
-        list_rules,
-        print_execution_times: matches!(env::var("TIMING"), Ok(x) if x == "true" || x == "1"),
+        paths: options.paths,
+        rules,
+        fix: options.fix,
+        quiet: options.cli.quiet,
+        ignore_path: options.ignore.ignore_path.unwrap_or_else(|| PathBuf::from(".eslintignore")),
+        no_ignore: options.ignore.no_ignore,
+        ignore_pattern: options.ignore.ignore_pattern,
+        max_warnings: options.cli.max_warnings,
+        list_rules: options.rules,
+        print_execution_times: options.timing,
     }
 }
 
 /// Get all rules in order, e.g.
 /// `-A all -D no-var -D -eqeqeq` => [("allow", "all"), ("deny", "no-var"), ("deny", "eqeqeq")]
 /// Defaults to [("deny", "correctness")];
-fn get_rules(matches: &ArgMatches) -> Vec<(AllowWarnDeny, String)> {
-    let mut map: BTreeMap<usize, (AllowWarnDeny, String)> = BTreeMap::new();
-    for key in ["allow", "deny"] {
-        let allow_warn_deny = AllowWarnDeny::from(key);
-        if let Some(values) = matches.get_many::<String>(key) {
-            let indices = matches.indices_of(key).unwrap();
-            let zipped =
-                values.zip(indices).map(|(value, i)| (i, (allow_warn_deny, value.clone())));
-            map.extend(zipped);
-        }
-    }
-    if map.is_empty() {
+fn get_rules(options: &CliLintOptions) -> Vec<(AllowWarnDeny, String)> {
+    if options.filter.is_empty() {
         vec![(AllowWarnDeny::Deny, "correctness".into())]
     } else {
-        map.into_values().collect()
+        options
+            .filter
+            .iter()
+            .map(|f| match f.ty {
+                FilterType::Allow => (AllowWarnDeny::Allow, f.value.clone()),
+                FilterType::Deny => (AllowWarnDeny::Deny, f.value.clone()),
+            })
+            .collect()
     }
 }
 
@@ -177,21 +159,18 @@ fn get_rules(matches: &ArgMatches) -> Vec<(AllowWarnDeny, String)> {
 mod test {
     use std::path::PathBuf;
 
-    use super::{lint_command, parse_arg_matches, AllowWarnDeny, LintOptions};
-
-    #[test]
-    fn verify_command() {
-        lint_command().debug_assert();
-    }
+    use super::{parse_cli_options, AllowWarnDeny, LintOptions};
+    use crate::lint_command;
 
     fn get_lint_options(arg: &str) -> LintOptions {
-        let matches = lint_command().try_get_matches_from(arg.split(' ')).unwrap();
-        parse_arg_matches(&matches)
+        let args = arg.split(' ').map(std::string::ToString::to_string).collect::<Vec<String>>();
+        let options = lint_command().run_inner(args.as_slice()).unwrap();
+        parse_cli_options(options.lint_options)
     }
 
     #[test]
     fn default() {
-        let options = get_lint_options("lint .");
+        let options = get_lint_options(".");
         assert_eq!(options.paths, vec![PathBuf::from(".")]);
         assert!(!options.fix);
         assert!(!options.quiet);
@@ -203,7 +182,7 @@ mod test {
 
     #[test]
     fn multiple_paths() {
-        let options = get_lint_options("lint foo bar baz");
+        let options = get_lint_options("foo bar baz");
         assert_eq!(
             options.paths,
             [PathBuf::from("foo"), PathBuf::from("bar"), PathBuf::from("baz")]
@@ -212,9 +191,8 @@ mod test {
 
     #[test]
     fn rules_with_deny_and_allow() {
-        let options = get_lint_options(
-            "lint src -D suspicious --deny pedantic -A no-debugger --allow no-var",
-        );
+        let options =
+            get_lint_options("src -D suspicious --deny pedantic -A no-debugger --allow no-var");
         assert_eq!(
             options.rules,
             vec![
@@ -228,50 +206,49 @@ mod test {
 
     #[test]
     fn quiet_true() {
-        let options = get_lint_options("lint foo.js --quiet");
+        let options = get_lint_options("foo.js --quiet");
         assert!(options.quiet);
     }
 
     #[test]
     fn fix_true() {
-        let options = get_lint_options("lint foo.js --fix");
+        let options = get_lint_options("foo.js --fix");
         assert!(options.fix);
     }
 
     #[test]
     fn max_warnings() {
-        let options = get_lint_options("lint --max-warnings 10 foo.js");
+        let options = get_lint_options("--max-warnings 10 foo.js");
         assert_eq!(options.max_warnings, Some(10));
     }
 
     #[test]
     fn ignore_path() {
-        let options = get_lint_options("lint --ignore-path .xxx foo.js");
+        let options = get_lint_options("--ignore-path .xxx foo.js");
         assert_eq!(options.ignore_path, PathBuf::from(".xxx"));
     }
 
     #[test]
     fn no_ignore() {
-        let options = get_lint_options("lint --no-ignore foo.js");
+        let options = get_lint_options("--no-ignore foo.js");
         assert!(options.no_ignore);
     }
 
     #[test]
     fn single_ignore_pattern() {
-        let options = get_lint_options("lint --ignore-pattern ./test foo.js");
+        let options = get_lint_options("--ignore-pattern ./test foo.js");
         assert_eq!(options.ignore_pattern, vec![String::from("./test")]);
     }
 
     #[test]
     fn multiple_ignore_pattern() {
-        let options =
-            get_lint_options("lint --ignore-pattern ./test --ignore-pattern bar.js foo.js");
+        let options = get_lint_options("--ignore-pattern ./test --ignore-pattern bar.js foo.js");
         assert_eq!(options.ignore_pattern, vec![String::from("./test"), String::from("bar.js")]);
     }
 
     #[test]
     fn list_rules_true() {
-        let options = get_lint_options("lint --rules");
+        let options = get_lint_options("--rules");
         assert!(options.paths.is_empty());
         assert!(options.list_rules);
     }
