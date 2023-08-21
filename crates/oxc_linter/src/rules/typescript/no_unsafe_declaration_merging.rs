593d3cced9fb9e174de92e3670359579622e8f81
makotot <makoto.ttn@gmail.com>
2023-08-22 01:19:43 +0900

    refactor: clean up

--- crates/oxc_linter/src/rules/typescript/no_unsafe_declaration_merging.rs
+++ crates/oxc_linter/src/rules/typescript/no_unsafe_declaration_merging.rs
@@ -1,9 +1,10 @@
-use oxc_ast::AstKind;
+use oxc_ast::{ast::BindingIdentifier, AstKind};
 use oxc_diagnostics::{
     miette::{self, Diagnostic},
     thiserror::Error,
 };
 use oxc_macros::declare_oxc_lint;
+use oxc_semantic::SymbolId;
 use oxc_span::Span;
 
 use crate::{context::LintContext, rule::Rule, AstNode};
@@ -37,33 +38,29 @@ declare_oxc_lint!(
 
 impl Rule for NoUnsafeDeclarationMerging {
     fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
+        if !ctx.source_type().is_typescript() {
+            return;
+        }
+
         let scope_symbols = ctx.semantic().scopes().get_bindings(node.scope_id());
 
         match node.kind() {
             AstKind::Class(decl) => {
                 if let Some(ident) = decl.id.as_ref() {
                     for scope_symbol in scope_symbols {
-                        if let AstKind::TSInterfaceDeclaration(scope_decl) = ctx
-                            .nodes()
-                            .get_node(ctx.symbols().get_declaration(*scope_symbol.1))
-                            .kind()
+                        if let AstKind::TSInterfaceDeclaration(scope_interface) =
+                            get_symbol_kind(*scope_symbol.1, ctx)
                         {
-                            if ident.name.as_str() == scope_decl.id.name.as_str() {
-                                ctx.diagnostic(NoUnsafeDeclarationMergingDiagnostic(decl.span));
-                            }
+                            check_and_diagnostic(ident, &scope_interface.id, decl.span, ctx);
                         }
                     }
                 }
             }
             AstKind::TSInterfaceDeclaration(decl) => {
                 for scope_symbol in scope_symbols {
-                    if let AstKind::Class(scope_decl) =
-                        ctx.nodes().get_node(ctx.symbols().get_declaration(*scope_symbol.1)).kind()
-                    {
-                        if let Some(scope_ident) = scope_decl.id.as_ref() {
-                            if scope_ident.name.as_str() == decl.id.name.as_str() {
-                                ctx.diagnostic(NoUnsafeDeclarationMergingDiagnostic(decl.span));
-                            }
+                    if let AstKind::Class(scope_class) = get_symbol_kind(*scope_symbol.1, ctx) {
+                        if let Some(scope_class_ident) = scope_class.id.as_ref() {
+                            check_and_diagnostic(&decl.id, scope_class_ident, decl.span, ctx);
                         }
                     }
                 }
@@ -71,6 +68,21 @@ impl Rule for NoUnsafeDeclarationMerging {
             _ => {}
         }
     }
+}
+
+fn check_and_diagnostic(
+    ident: &BindingIdentifier,
+    scope_ident: &BindingIdentifier,
+    span: Span,
+    ctx: &LintContext<'_>,
+) {
+    if scope_ident.name.as_str() == ident.name.as_str() {
+        ctx.diagnostic(NoUnsafeDeclarationMergingDiagnostic(span));
+    }
+}
+
+fn get_symbol_kind<'a>(symbol_id: SymbolId, ctx: &LintContext<'a>) -> AstKind<'a> {
+    return ctx.nodes().get_node(ctx.symbols().get_declaration(symbol_id)).kind();
 }
 
 #[test]
