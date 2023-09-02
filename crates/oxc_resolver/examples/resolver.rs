bebdbfb9a62f4a52e4947f4b74484777d92b7b9a
Boshen <boshenc@gmail.com>
2023-09-02 14:57:28 +0800

    feat(resolver): add tracing example

--- crates/oxc_resolver/examples/resolver.rs
+++ crates/oxc_resolver/examples/resolver.rs
@@ -1,12 +1,31 @@
 // Instruction:
+//
 // run `cargo run -p oxc_resolver --example resolver -- `pwd` test.js`
 // or `cargo watch -x "run -p oxc_resolver --example resolver" -- `pwd` test.js`
+//
+// use `OXC_RESOLVER=DEBUG` for tracing
 
 use std::{env, path::PathBuf};
 
 use oxc_resolver::{AliasValue, ResolveOptions, Resolver};
 
+fn init_tracing_subscriber() {
+    use tracing_subscriber::{filter::Targets, fmt, prelude::*, registry};
+    registry()
+        .with(std::env::var("OXC_RESOLVER").map_or_else(
+            |_| Targets::new(),
+            |env_var| {
+                use std::str::FromStr;
+                Targets::from_str(&env_var).unwrap()
+            },
+        ))
+        .with(fmt::layer())
+        .init();
+}
+
 fn main() {
+    init_tracing_subscriber();
+
     let path = env::args().nth(1).expect("require path");
     let request = env::args().nth(2).expect("require request");
     let path = PathBuf::from(path).canonicalize().unwrap();
