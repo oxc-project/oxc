8984ba3c365a7da4dd93548f71599b53f3b4a923
Boshen <boshenc@gmail.com>
2023-09-03 09:58:49 +0800

    fmt

--- crates/oxc_semantic/src/lib.rs
+++ crates/oxc_semantic/src/lib.rs
@@ -177,7 +177,9 @@ mod tests {
 
         let top_level_a = semantic
             .scopes()
-            .iter_bindings().find(|(_scope_id, _symbol_id, name)| name == &Atom::from("Fn")).unwrap();
+            .iter_bindings()
+            .find(|(_scope_id, _symbol_id, name)| name == &Atom::from("Fn"))
+            .unwrap();
         assert_eq!(semantic.symbols.get_scope_id(top_level_a.1), top_level_a.0);
     }
 
