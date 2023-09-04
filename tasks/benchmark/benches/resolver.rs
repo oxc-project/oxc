4ada0ecbb83ac3fd644cdbb1dac8c24853c8f023
Boshen <boshenc@gmail.com>
2023-09-04 22:02:48 +0800

    chore(resolver): add more benchmark data (#853)

--- tasks/benchmark/benches/resolver.rs
+++ tasks/benchmark/benches/resolver.rs
@@ -14,32 +14,54 @@ use oxc_tasks_common::project_root;
 use rayon::prelude::*;
 
 fn data() -> Vec<(PathBuf, &'static str)> {
-    let cwd = project_root().join("crates/oxc_resolver/tests/enhanced_resolve/");
+    let cwd = project_root().join("crates/oxc_resolver/tests/enhanced_resolve");
+    let f = cwd.join("test/fixtures");
     vec![
         (cwd.clone(), "./"),
         (cwd.clone(), "./lib/index"),
+        (cwd, "/absolute/path"),
         // query fragment
-        (cwd.join("test/fixtures"), "./main1.js#fragment?query"),
-        (cwd.join("test/fixtures"), "m1/a.js?query#fragment"),
-        // extensions
-        (cwd.join("test/fixtures/extensions"), "./foo"),
-        (cwd.join("test/fixtures/extensions/module"), "module/"),
+        (f.clone(), "./main1.js#fragment?query"),
+        (f.clone(), "m1/a.js?query#fragment"),
         // browserField
-        (cwd.join("test/fixtures/browser-module"), "./lib/replaced"),
-        (cwd.join("test/fixtures/browser-module/lib"), "./replaced"),
+        (f.join("browser-module"), "./lib/replaced"),
+        (f.join("browser-module/lib"), "./replaced"),
+        // exportsField
+        (f.join("exports-field"), "exports-field"),
+        (f.join("exports-field"), "exports-field/dist/main.js"),
+        (f.join("exports-field"), "exports-field/dist/main.js?foo"),
+        (f.join("exports-field"), "exports-field/dist/main.js#foo"),
+        (f.join("exports-field"), "@exports-field/core"),
+        (f.join("imports-exports-wildcard"), "m/features/f.js"),
         // extensionAlias
-        (cwd.join("test/fixtures/extension-alias"), "./index.js"),
+        (f.join("extension-alias"), "./index.js"),
+        (f.join("extension-alias"), "./dir2/index.mjs"),
+        // extensions
+        (f.join("extensions"), "./foo"),
+        (f.join("extensions"), "."),
+        (f.join("extensions"), "./dir"),
+        (f.join("extensions"), "module/"),
+        // importsField
+        (f.join("imports-field"), "#imports-field"),
+        (f.join("imports-exports-wildcard/node_modules/m/"), "#internal/i.js"),
         // scoped
-        (cwd.join("test/fixtures/scoped"), "@scope/pack1"),
-        (cwd.join("test/fixtures/scoped"), "@scope/pack2/lib"),
-        // alias
-        (cwd.clone(), "/absolute/path"),
+        (f.join("scoped"), "@scope/pack1"),
+        (f.join("scoped"), "@scope/pack2/lib"),
+        // dashed name
+        (f.clone(), "dash"),
+        (f.clone(), "dash-name"),
+        (f.join("node_modules/dash"), "dash"),
+        (f.join("node_modules/dash"), "dash-name"),
+        (f.join("node_modules/dash-name"), "dash"),
+        (f.join("node_modules/dash-name"), "dash-name"),
     ]
 }
 
 fn oxc_resolver() -> oxc_resolver::Resolver {
     use oxc_resolver::{AliasValue, ResolveOptions, Resolver};
     Resolver::new(ResolveOptions {
+        extensions: vec![".ts".into(), ".js".into()],
+        condition_names: vec!["webpack".into()],
         alias: vec![("/absolute/path".into(), vec![AliasValue::Path("./".into())])],
         alias_fields: vec![vec!["browser".into()]],
         extension_alias: vec![
@@ -52,6 +74,11 @@ fn oxc_resolver() -> oxc_resolver::Resolver {
 
 fn bench_resolver(c: &mut Criterion) {
     let data = data();
+
+    // check validity
+    for (path, request) in &data {
+        assert!(oxc_resolver().resolve(path, request).is_ok(), "{path:?} {request}");
+    }
 
     let mut group = c.benchmark_group("resolver");
 
