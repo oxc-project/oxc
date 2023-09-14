a6d8d05cd303736937bd92b5c6ce60067386352c
Boshen <boshenc@gmail.com>
2023-09-14 22:44:50 +0800

    feat(cli): init miette everything else to reduce `is_atty` system calls

--- crates/oxc_cli/src/lint/main.rs
+++ crates/oxc_cli/src/lint/main.rs
@@ -12,10 +12,16 @@ use oxc_cli::{CliRunResult, LintRunner, Runner};
 
 fn main() -> CliRunResult {
     init_tracing();
+    init_miette();
 
     let command = oxc_cli::lint_command().fallback_to_usage().run();
     command.handle_threads();
     LintRunner::new(command.lint_options).run()
+}
+
+// Initialize the data which relies on `is_atty` system calls so they don't block subsequent threads.
+fn init_miette() {
+    miette::set_hook(Box::new(|_| Box::new(miette::MietteHandlerOpts::new().build()))).unwrap();
 }
 
 fn init_tracing() {
