65a8282b4e0b3dadc79e41ee00b682a0182493f6
Boshen <boshenc@gmail.com>
2023-09-14 12:56:21 +0800

    feat(resolver): add more tracing events to resolver

--- crates/oxc_resolver/src/tracing_subscriber.rs
+++ crates/oxc_resolver/src/tracing_subscriber.rs
@@ -1,19 +1,10 @@
 use once_cell::sync::OnceCell;
-use std::str::FromStr;
-use tracing_subscriber::{filter::Targets, fmt, prelude::*, registry};
+use tracing_subscriber::{fmt, prelude::*, registry, EnvFilter};
 
 static SUBSCRIBER: OnceCell<()> = OnceCell::new();
 
 pub fn init() {
     SUBSCRIBER.get_or_init(|| {
-        registry()
-            .with(
-                std::env::var("OXC_RESOLVER").map_or_else(
-                    |_| Targets::new(),
-                    |env_var| Targets::from_str(&env_var).unwrap(),
-                ),
-            )
-            .with(fmt::layer())
-            .init();
+        registry().with(fmt::layer()).with(EnvFilter::from_env("OXC_RESOLVER")).init();
     });
 }
