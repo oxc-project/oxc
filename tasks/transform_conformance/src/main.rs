db0162b06944c642c80cb75ab31652066ebb54a3
Boshen <boshenc@gmail.com>
2023-09-16 19:36:35 +0800

    feat(transformer_conformance): snapshot passed and failed tests

--- tasks/transform_conformance/src/main.rs
+++ tasks/transform_conformance/src/main.rs
@@ -1,9 +1,5 @@
 use oxc_transform_conformance::babel;
 
 fn main() {
-    let names = ["babel-plugin-transform-optional-catch-binding"];
-    for name in names {
-        babel(name);
-        println!("Passed: {name}");
-    }
+    babel();
 }
