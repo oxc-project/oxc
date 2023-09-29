fbbc1fe6bdceb5ae9f765a5b24df6a61daf604b7
Boshen <boshenc@gmail.com>
2023-09-29 22:39:31 +1300

    chore(wasm,playground): remove hir

--- crates/oxc_wasm/src/options.rs
+++ crates/oxc_wasm/src/options.rs
@@ -7,7 +7,6 @@ pub struct OxcRunOptions {
     syntax: bool,
     lint: bool,
     format: bool,
-    // hir: bool,
     minify: bool,
     type_check: bool,
 }
@@ -48,16 +47,6 @@ impl OxcRunOptions {
     pub fn set_format(&mut self, yes: bool) {
         self.format = yes;
     }
-
-    // #[wasm_bindgen(getter)]
-    // pub fn hir(self) -> bool {
-    //     self.hir
-    // }
-    //
-    // #[wasm_bindgen(setter)]
-    // pub fn set_hir(&mut self, yes: bool) {
-    //     self.hir = yes;
-    // }
 
     #[wasm_bindgen(getter)]
     pub fn minify(self) -> bool {
