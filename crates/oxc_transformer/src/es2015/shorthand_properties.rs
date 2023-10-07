6ed809dbdc98eb91c1ea325f9d86c3c128f85779
magic-akari <akari.ccino@gmail.com>
2023-10-07 16:35:38 +0800

    chore: add more comments

--- crates/oxc_transformer/src/es2015/shorthand_properties.rs
+++ crates/oxc_transformer/src/es2015/shorthand_properties.rs
@@ -26,25 +26,36 @@ impl<'a> ShorthandProperties<'a> {
         obj_prop.method = false;
 
         if obj_prop.computed {
+            // all computed key can never be transformed to `__proto__` setter unexpectedly
             return;
         }
 
-        let is_proto_id = obj_prop.key.is_specific_id("__proto__");
+        // We should handle the edge case of `__proto__` property.
+        // All shorthand properties with key `__proto__` can never be `__proto__` setter.
+        // But the transformed result can be `__proto__` setter unexpectedly.
+        // It's easy to fix it by using computed property.
+
         let is_proto_string = obj_prop.key.is_specific_string_literal("__proto__");
 
-        if !is_proto_id && !is_proto_string {
+        if !is_proto_string && obj_prop.key.is_specific_id("__proto__") {
             return;
         }
+        // We reach here, it means that the key is `__proto__` or `"__proto__"`.
 
+        // Transform `__proto__`/`"__proto__"` to computed property.
         obj_prop.computed = true;
 
         if is_proto_string {
+            // After the transformation, the string literal `"__proto__"` is already expected result.
+            //
             // input:
             // "__proto__"() {}
             // output:
             // ["__proto__"]: function() {}
             return;
         }
+
+        // Convert `[__proto__]` to `["__proto__"]`
 
         let proto = StringLiteral { span: obj_prop.key.span(), value: "__proto__".into() };
         let expr = self.ast.literal_string_expression(proto);
