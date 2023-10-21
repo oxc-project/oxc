f13fc2287a130f060887b13e3bcdb5ab0ba43643
Cameron <cameron.clark@hey.com>
2023-10-21 16:16:46 +0100

    feat(linter): eslint-plugin-react/no-useless-fragment (#1021)

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -125,6 +125,7 @@ mod jest {
 
 mod react {
     pub mod jsx_key;
+    pub mod jsx_no_useless_fragment;
     pub mod no_children_prop;
 }
 
@@ -248,8 +249,9 @@ oxc_macros::declare_all_lint_rules! {
     unicorn::no_thenable,
     unicorn::throw_new_error,
     unicorn::prefer_array_flat_map,
-    react::jsx_key,
     react::no_children_prop,
+    react::jsx_key,
+    react::jsx_no_useless_fragment,
     import::named,
     import::no_cycle,
     import::no_self_import,
