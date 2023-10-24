50cf748ba2197d6954a658db3219c6c9ce7e2615
Dunqing <dengqing0821@gmail.com>
2023-10-24 16:37:36 +0800

    feat: comment why

--- crates/oxc_linter/src/rules/react/no_render_return_value.rs
+++ crates/oxc_linter/src/rules/react/no_render_return_value.rs
@@ -130,8 +130,10 @@ fn test() {
         ("var render = (a, b) => ReactDOM.render(a, b)", None),
         ("this.o = ReactDOM.render(<div />, document.body);", None),
         ("var v; v = ReactDOM.render(<div />, document.body);", None),
-        // ("var inst = React.render(<div />, document.body);", None),
         ("var inst = ReactDOM.render(<div />, document.body);", None),
+        // This rule is only supported for react versions >= 15.0.0, so the following are not supported.
+        // See https://github.com/web-infra-dev/oxc/pull/1042#discussion_r1369762147
+        // ("var inst = React.render(<div />, document.body);", None),
         // ("var inst = React.render(<div />, document.body);", None),
     ];
 
