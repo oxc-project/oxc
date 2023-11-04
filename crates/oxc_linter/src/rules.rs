b8cc460c5cc8ee89d13913ec4f6798545bd12310
Cameron <cameron.clark@hey.com>
2023-11-04 23:47:47 +0000

    feat(linter) eslint plugin unicorn: prefer date now (#1150)

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -148,6 +148,7 @@ mod unicorn {
     pub mod no_thenable;
     pub mod no_unnecessary_await;
     pub mod prefer_array_flat_map;
+    pub mod prefer_date_now;
     pub mod prefer_logical_operator_over_ternary;
     pub mod prefer_query_selector;
     pub mod prefer_string_trim_start_end;
@@ -271,6 +272,7 @@ oxc_macros::declare_all_lint_rules! {
     unicorn::no_thenable,
     unicorn::no_unnecessary_await,
     unicorn::prefer_array_flat_map,
+    unicorn::prefer_date_now,
     unicorn::prefer_logical_operator_over_ternary,
     unicorn::require_number_to_fixed_digits_argument,
     unicorn::switch_case_braces,
