31b3dc8f2cbf935ed4f975c6c9392d23dca0094e
Cameron Clark <cameron.clark@hey.com>
2023-11-05 21:08:55 +0000

    do the thing

--- crates/oxc_linter/src/rules/unicorn/new_for_builtins.rs
+++ crates/oxc_linter/src/rules/unicorn/new_for_builtins.rs
@@ -1,4 +1,7 @@
-use oxc_ast::{ast::Expression, AstKind};
+use oxc_ast::{
+    ast::{Expression, IdentifierReference},
+    AstKind,
+};
 use oxc_diagnostics::{
     miette::{self, Diagnostic},
     thiserror::{self, Error},
@@ -11,9 +14,14 @@ use phf::phf_set;
 use crate::{context::LintContext, rule::Rule, AstNode};
 
 #[derive(Debug, Error, Diagnostic)]
-#[error("eslint-plugin-unicorn(new-for-builtins):")]
-#[diagnostic(severity(warning), help(""))]
-struct NewForBuiltinsDiagnostic(#[label] pub Span);
+enum NewForBuiltinsDiagnostic {
+    #[error("eslint-plugin-unicorn(new-for-builtins): Use `new {1}()` instead of `{1}()`")]
+    #[diagnostic(severity(warning))]
+    Enforce(#[label] Span, String),
+    #[error("eslint-plugin-unicorn(new-for-builtins): Use `{1}()` instead of `new {1}()`")]
+    #[diagnostic(severity(warning))]
+    Disallow(#[label] Span, String),
+}
 
 #[derive(Debug, Default, Clone)]
 pub struct NewForBuiltins;
@@ -21,48 +29,56 @@ pub struct NewForBuiltins;
 declare_oxc_lint!(
     /// ### What it does
     ///
+    /// Enforces the use of `new` for following builtins: `Object`, `Array`, `ArrayBuffer`, `BigInt64Array`, `BigUint64Array`, `DataView`, `Date`, `Error`, `Float32Array`, `Float64Array`, `Function`, `Int8Array`, `Int16Array`, `Int32Array`, `Map`, `WeakMap`, `Set`, `WeakSet`, `Promise`, `RegExp`, `Uint8Array`, `Uint16Array`, `Uint32Array`, `Uint8ClampedArray`, `SharedArrayBuffer`, `Proxy`, `WeakRef`, `FinalizationRegistry`.
+    ///
+    /// Disallows the use of `new` for following builtins: `String`, `Number`, `Boolean`, `Symbol`, `BigInt`.
+    ///
+    /// These should not use `new` as that would create object wrappers for the primitive values, which is not what you want. However, without `new` they can be useful for coercing a value to that type.
     ///
     /// ### Why is this bad?
+    ///
+    /// They work the same, but `new` should be preferred for consistency with other constructors.
     ///
     ///
     /// ### Example
     /// ```javascript
+    /// // bad
+    /// const foo = new String('hello world');
+    /// const bar = Array(1, 2, 3);
+    ///
+    /// // good
+    /// const foo = String('hello world');
+    /// const bar = new Array(1, 2, 3);
     /// ```
     NewForBuiltins,
-    correctness
+    pedantic
 );
 
 impl Rule for NewForBuiltins {
     fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
         match node.kind() {
             AstKind::NewExpression(new_expr) => {
-                let callee = &new_expr.callee.without_parenthesized();
+                let callee = new_expr.callee.without_parenthesized();
 
-                let Expression::Identifier(name) = callee else { return };
+                let Some(builtin_name) = is_expr_global_builtin(callee, ctx) else { return };
 
-                // if ENFORCE_NEW_FOR_BUILTINS.contains(name.name.as_str()) {
-                //     if new_expr.arguments.len() == 0 {
-                //         ctx.diagnostic(NewForBuiltinsDiagnostic(new_expr.span));
-                //     }
-                // } else
-                if DISALLOW_NEW_FOR_BUILTINS.contains(name.name.as_str()) {
-                    // if new_expr.arguments.len() == 0 {
-                    ctx.diagnostic(NewForBuiltinsDiagnostic(new_expr.span));
-                    // }
+                if DISALLOW_NEW_FOR_BUILTINS.contains(builtin_name) {
+                    ctx.diagnostic(NewForBuiltinsDiagnostic::Disallow(
+                        new_expr.span,
+                        builtin_name.to_string(),
+                    ));
                 }
             }
             AstKind::CallExpression(call_expr) => {
-                let callee = call_expr.callee.without_parenthesized();
+                let Some(builtin_name) =
+                    is_expr_global_builtin(call_expr.callee.without_parenthesized(), ctx)
+                else {
+                    return;
+                };
 
-                let Expression::Identifier(name) = callee else { return };
-
-                if ENFORCE_NEW_FOR_BUILTINS.contains(name.name.as_str()) {
-                    if name.name.as_str() == "Object" {
+                if ENFORCE_NEW_FOR_BUILTINS.contains(builtin_name) {
+                    if builtin_name == "Object" {
                         if let Some(parent) = ctx.nodes().parent_node(node.id()) {
-                            // parent.type === 'BinaryExpression'
-                            // && (parent.operator === '===' || parent.operator === '!==')
-                            // && (parent.left === node || parent.right === node)
-
                             if let AstKind::BinaryExpression(bin_expr) = parent.kind() {
                                 if bin_expr.operator == BinaryOperator::StrictEquality
                                     || bin_expr.operator == BinaryOperator::StrictInequality
@@ -73,18 +89,38 @@ impl Rule for NewForBuiltins {
                         }
                     }
 
-                    // if call_expr.arguments.len() == 0 {
-                    ctx.diagnostic(NewForBuiltinsDiagnostic(call_expr.span));
-                    // }
-                } //else
-                  //  if DISALLOW_NEW_FOR_BUILTINS.contains(name.name.as_str()) {
-                  //     if call_expr.arguments.len() == 0 {
-                  //         ctx.diagnostic(NewForBuiltinsDiagnostic(call_expr.span));
-                  //     }
-                  // }
+                    ctx.diagnostic(NewForBuiltinsDiagnostic::Enforce(
+                        call_expr.span,
+                        builtin_name.to_string(),
+                    ));
+                }
             }
             _ => {}
         }
+    }
+}
+
+fn is_expr_global_builtin<'a, 'b>(
+    expr: &'b Expression<'a>,
+    ctx: &'b LintContext<'a>,
+) -> Option<&'b str> {
+    match expr {
+        Expression::Identifier(ident) => {
+            if !ctx.semantic().is_reference_to_global_variable(ident) {
+                return None;
+            }
+            Some(ident.name.as_str())
+        }
+        Expression::MemberExpression(member_expr) => {
+            let Expression::Identifier(ident) = member_expr.object() else { return None };
+
+            if !GLOBAL_OBJECT_NAMES.contains(ident.name.as_str()) {
+                return None;
+            }
+
+            return member_expr.static_property_name();
+        }
+        _ => None,
     }
 }
 
@@ -127,6 +163,13 @@ const DISALLOW_NEW_FOR_BUILTINS: phf::Set<&'static str> = phf_set! {
     "String",
 };
 
+const GLOBAL_OBJECT_NAMES: phf::Set<&'static str> = phf_set! {
+    "global",
+    "globalThis",
+    "self",
+    "window",
+};
+
 #[test]
 fn test() {
     use crate::tester::Tester;
@@ -162,28 +205,27 @@ fn test() {
         r#"const foo = Number()"#,
         r#"const foo = String()"#,
         r#"const foo = Symbol()"#,
-        // r#"
-        // 				import { Map } from 'immutable';
-        // 				const m = Map();
-        // 			"#,
-        // r#"
-        // 				const {Map} = require('immutable');
-        // 				const foo = Map();
-        // 			"#,
-        // r#"
-        // 				const {String} = require('guitar');
-        // 				const lowE = new String();
-        // 			"#,
-        // r#"
-        // 				import {String} from 'guitar';
-        // 				const lowE = new String();
-        // 			"#,
+        r#"
+            import { Map } from 'immutable';
+            const m = Map();
+        "#,
+        r#"
+        	const {Map} = require('immutable');
+        	const foo = Map();
+        "#,
+        r#"
+        	const {String} = require('guitar');
+        	const lowE = new String();
+        "#,
+        r#"
+        	import {String} from 'guitar';
+        	const lowE = new String();
+        "#,
         r#"new Foo();Bar();"#,
         r#"Foo();new Bar();"#,
         r#"const isObject = v => Object(v) === v;"#,
         r#"const isObject = v => globalThis.Object(v) === v;"#,
         r#"(x) !== Object(x)"#,
-        // r#"new Symbol("")"#,
     ];
 
     let fail = vec![
@@ -191,15 +233,15 @@ fn test() {
         r#"const symbol = new (Symbol)("");"#,
         r#"const symbol = new /* comment */ Symbol("");"#,
         r#"const symbol = new Symbol;"#,
-        // r#"new globalThis.String()"#,
-        // r#"new global.String()"#,
-        // r#"new self.String()"#,
-        // r#"new window.String()"#,
-        // r#"globalThis.Array()"#,
-        // r#"global.Array()"#,
-        // r#"self.Array()"#,
-        // r#"window.Array()"#,
-        // r#"globalThis.Array()"#,
+        r#"new globalThis.String()"#,
+        r#"new global.String()"#,
+        r#"new self.String()"#,
+        r#"new window.String()"#,
+        r#"globalThis.Array()"#,
+        r#"global.Array()"#,
+        r#"self.Array()"#,
+        r#"window.Array()"#,
+        r#"globalThis.Array()"#,
         r#"const foo = Object()"#,
         r#"const foo = Array()"#,
         r#"const foo = ArrayBuffer()"#,
@@ -233,26 +275,26 @@ fn test() {
         r#"const foo = new String()"#,
         r#"const foo = new Symbol()"#,
         r#"
-							function varCheck() {
-								{
-									var WeakMap = function() {};
-								}
-								// This should not reported
-								return WeakMap()
-							}
-							function constCheck() {
-								{
-									const Array = function() {};
-								}
-								return Array()
-							}
-							function letCheck() {
-								{
-									let Map = function() {};
-								}
-								return Map()
-							}
-						"#,
+			function varCheck() {
+				{
+					var WeakMap = function() {};
+				}
+				// This should not reported
+				return WeakMap()
+			}
+			function constCheck() {
+				{
+					const Array = function() {};
+				}
+				return Array()
+			}
+			function letCheck() {
+				{
+					let Map = function() {};
+				}
+				return Map()
+			}
+        "#,
     ];
 
     Tester::new_without_config(NewForBuiltins::NAME, pass, fail).test_and_snapshot();
