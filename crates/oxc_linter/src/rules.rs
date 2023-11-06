0635d7b74088257e082cf9fc54d560b798e664e7
Cameron <cameron.clark@hey.com>
2023-11-06 01:57:48 +0000

    feat(linter) eslint plugin unicorn: no new array (#1160)

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -146,6 +146,7 @@ mod unicorn {
     pub mod no_empty_file;
     pub mod no_instanceof_array;
     pub mod no_invalid_remove_event_listener;
+    pub mod no_new_array;
     pub mod no_thenable;
     pub mod no_unnecessary_await;
     pub mod prefer_array_flat_map;
@@ -272,6 +273,7 @@ oxc_macros::declare_all_lint_rules! {
     unicorn::no_empty_file,
     unicorn::no_instanceof_array,
     unicorn::no_invalid_remove_event_listener,
+    unicorn::no_new_array,
     unicorn::no_thenable,
     unicorn::no_unnecessary_await,
     unicorn::prefer_array_flat_map,
