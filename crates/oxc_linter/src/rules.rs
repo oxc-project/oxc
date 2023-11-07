008b0e650cacbdda63ea530a20b389969ea76633
秦宇航 <qinyuhangxiaoxiang@gmail.com>
2023-11-07 20:28:39 +0800

    feat(linter) eslint-plugin-import: no-amd (#1171)

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -9,6 +9,7 @@
 mod import {
     pub mod default;
     pub mod named;
+    pub mod no_amd;
     pub mod no_cycle;
     pub mod no_self_import;
 }
@@ -318,5 +319,6 @@ oxc_macros::declare_all_lint_rules! {
     import::named,
     import::no_cycle,
     import::no_self_import,
-    jsx_a11y::alt_text
+    import::no_amd,
+    jsx_a11y::alt_text,
 }
