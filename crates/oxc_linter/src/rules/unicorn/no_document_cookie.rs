deda4711c833806f7d2a016372e2eab254c4cb06
Cameron Clark <cameron.clark@hey.com>
2023-11-13 00:10:21 +0000

    feat(linter) eslint plugin unicorn: no document cookie

--- crates/oxc_linter/src/rules/unicorn/no_document_cookie.rs
+++ crates/oxc_linter/src/rules/unicorn/no_document_cookie.rs
@@ -7,7 +7,7 @@ use oxc_diagnostics::{
     thiserror::Error,
 };
 use oxc_macros::declare_oxc_lint;
-use oxc_span::Span;
+use oxc_span::{GetSpan, Span};
 
 use crate::{
     ast_util::get_declaration_of_variable, context::LintContext, globals::GLOBAL_OBJECT_NAMES,
@@ -15,8 +15,8 @@ use crate::{
 };
 
 #[derive(Debug, Error, Diagnostic)]
-#[error("eslint-plugin-unicorn(no-document-cookie):")]
-#[diagnostic(severity(warning), help(""))]
+#[error("eslint-plugin-unicorn(no-document-cookie): Do not use `document.cookie` directly")]
+#[diagnostic(severity(warning), help("Use the Cookie Store API or a cookie library instead"))]
 struct NoDocumentCookieDiagnostic(#[label] pub Span);
 
 #[derive(Debug, Default, Clone)]
@@ -74,51 +74,51 @@ impl Rule for NoDocumentCookie {
             return;
         }
 
-        match ident.object().without_parenthesized() {
-            Expression::Identifier(ident) => {
-                if ident.name.as_str() != "document" {
-                    // TODO: attempt to resolve
-
-                    let Some(var_decl) = get_declaration_of_variable(ident, ctx) else {
-                        return;
-                    };
-
-                    let AstKind::VariableDeclarator(var_decl) = var_decl.kind() else {
-                        return;
-                    };
-
-                    let Some(init) = &var_decl.init else {
-                        return;
-                    };
-
-                    let Expression::Identifier(ident) = init.without_parenthesized() else {
-                        return;
-                    };
-
-                    if ident.name.as_str() != "document" {
-                        return;
-                    }
-                }
-            }
-            Expression::MemberExpression(member_expr) => {
-                let Some(static_prop_name) = member_expr.static_property_name() else {
-                    return;
-                };
-                if static_prop_name != "document" {
-                    return;
-                }
-
-                if let Expression::Identifier(ident) = member_expr.object().without_parenthesized()
-                {
-                    if !GLOBAL_OBJECT_NAMES.contains(ident.name.as_str()) {
-                        return;
-                    }
-                }
-            }
-            _ => {}
+        if !is_document_cookie_reference(ident.object(), ctx) {
+            return;
         }
 
-        ctx.diagnostic(NoDocumentCookieDiagnostic(assignment_expr.span));
+        ctx.diagnostic(NoDocumentCookieDiagnostic(assignment_expr.left.span()));
+    }
+}
+
+fn is_document_cookie_reference<'a, 'b>(
+    expr: &'a Expression<'b>,
+    ctx: &'a LintContext<'b>,
+) -> bool {
+    match expr {
+        Expression::Identifier(ident) => {
+            if ident.name.as_str() != "document" {
+                let Some(var_decl) = get_declaration_of_variable(ident, ctx) else {
+                    return false;
+                };
+
+                let AstKind::VariableDeclarator(var_decl) = var_decl.kind() else {
+                    return false;
+                };
+
+                let Some(init) = &var_decl.init else {
+                    return false;
+                };
+
+                return is_document_cookie_reference(init, ctx);
+            }
+            true
+        }
+        Expression::MemberExpression(member_expr) => {
+            let Some(static_prop_name) = member_expr.static_property_name() else { return false };
+            if static_prop_name != "document" {
+                return false;
+            }
+
+            if let Expression::Identifier(ident) = member_expr.object().without_parenthesized() {
+                if !GLOBAL_OBJECT_NAMES.contains(ident.name.as_str()) {
+                    return false;
+                }
+            }
+            true
+        }
+        _ => false,
     }
 }
 
@@ -148,7 +148,7 @@ fn test() {
         r#"foo = document.cookie = "foo=bar""#,
         r#"var doc = document; doc.cookie = "foo=bar""#,
         r#"let doc = document; doc.cookie = "foo=bar""#,
-        // r#"const doc = globalThis.document; doc.cookie = "foo=bar""#,
+        r#"const doc = globalThis.document; doc.cookie = "foo=bar""#,
         r#"window.document.cookie = "foo=bar""#,
     ];
 
