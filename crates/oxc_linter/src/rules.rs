c0d42b8a3368521d1e5ccecfce20c2cc193a4cfa
Cameron <cameron.clark@hey.com>
2023-11-13 09:46:41 +0000

    feat(linter) eslint plugin unicorn: prefer dom node dataset (#1257)

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -170,6 +170,7 @@ mod unicorn {
     pub mod prefer_code_point;
     pub mod prefer_date_now;
     pub mod prefer_dom_node_append;
+    pub mod prefer_dom_node_dataset;
     pub mod prefer_logical_operator_over_ternary;
     pub mod prefer_optional_catch_binding;
     pub mod prefer_query_selector;
@@ -321,6 +322,7 @@ oxc_macros::declare_all_lint_rules! {
     unicorn::prefer_code_point,
     unicorn::prefer_date_now,
     unicorn::prefer_dom_node_append,
+    unicorn::prefer_dom_node_dataset,
     unicorn::prefer_logical_operator_over_ternary,
     unicorn::prefer_optional_catch_binding,
     unicorn::prefer_query_selector,
