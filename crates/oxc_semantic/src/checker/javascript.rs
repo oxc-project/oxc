446ba169de6eab3cb95bef3b92f7fbf66e371dfd
Dunqing <dengqing0821@gmail.com>
2023-11-14 20:22:56 +0800

    feat(ast): add to_string function to VariableDelcartionKind (#1303)
    feat(ast): add to_string function to VariableDelcartionKind
    
    fix: lint error

--- crates/oxc_semantic/src/checker/javascript.rs
+++ crates/oxc_semantic/src/checker/javascript.rs
@@ -216,7 +216,8 @@ fn check_binding_identifier<'a>(
                     )]
                     #[diagnostic()]
                     struct InvalidLetDeclaration(String, #[label] Span);
-                    return ctx.error(InvalidLetDeclaration(decl.kind.to_string(), ident.span));
+                    return ctx
+                        .error(InvalidLetDeclaration(decl.kind.to_string().into(), ident.span));
                 }
                 AstKind::VariableDeclaration(_) | AstKind::Function(_) | AstKind::Program(_) => {
                     break;
