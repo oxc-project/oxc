71b3f48af176021403f4032ef0e88c348d379462
Hao Cheng <hcheng636@gmail.com>
2023-11-15 03:07:19 +0100

    feat(linter): eslint-plugin-unicorn/number-literal-case (#1271)
    This PR implements the
    [eslint-plugin-unicorn/number-literal-case](https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/number-literal-case.md)
    rule.
    
    Tests taken from
    https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/test/number-literal-case.mjs.
    (Test cases that rely on a Vue parser are removed.)
    
    Related issue: #684

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -166,6 +166,7 @@ mod unicorn {
     pub mod no_unnecessary_await;
     pub mod no_useless_fallback_in_spread;
     pub mod no_useless_promise_resolve_reject;
+    pub mod number_literal_case;
     pub mod prefer_add_event_listener;
     pub mod prefer_array_flat_map;
     pub mod prefer_blob_reading_methods;
@@ -320,6 +321,7 @@ oxc_macros::declare_all_lint_rules! {
     unicorn::no_unnecessary_await,
     unicorn::no_useless_fallback_in_spread,
     unicorn::no_useless_promise_resolve_reject,
+    unicorn::number_literal_case,
     unicorn::prefer_add_event_listener,
     unicorn::prefer_array_flat_map,
     unicorn::prefer_blob_reading_methods,
