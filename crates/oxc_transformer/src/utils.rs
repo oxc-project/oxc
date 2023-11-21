f66e4d8ac3975f09e89d675ce95192be268833c2
IWANABETHATGUY <iwanabethatguy@qq.com>
2023-11-21 23:07:43 +0800

    feat(transformer): add transform property-literal plugin (#1458)
    1. Add `transform-property-literal-plugin`
    2. Passing 2 testcases.
    
    ---------
    
    Co-authored-by: Wenzhe Wang <mysteryven@gmail.com>

--- crates/oxc_transformer/src/utils.rs
+++ crates/oxc_transformer/src/utils.rs
@@ -2,7 +2,8 @@ use std::mem;
 
 use oxc_allocator::Vec;
 use oxc_ast::ast::*;
-use oxc_span::Span;
+use oxc_span::{Atom, Span};
+use oxc_syntax::unicode_id_start::{is_id_continue, is_id_start};
 
 use crate::context::TransformerCtx;
 
@@ -49,4 +50,121 @@ pub trait CreateVars<'a> {
             Some(self.create_new_var(expr))
         }
     }
+}
+
+pub const RESERVED_WORDS_ES3_ONLY: phf::Set<&str> = phf::phf_set![
+    "abstract",
+    "boolean",
+    "byte",
+    "char",
+    "double",
+    "enum",
+    "final",
+    "float",
+    "goto",
+    "implements",
+    "int",
+    "interface",
+    "long",
+    "native",
+    "package",
+    "private",
+    "protected",
+    "public",
+    "short",
+    "static",
+    "synchronized",
+    "throws",
+    "transient",
+    "volatile",
+];
+
+const RESERVED_WORD_STRICT: phf::Set<&str> = phf::phf_set![
+    "implements",
+    "interface",
+    "let",
+    "package",
+    "private",
+    "protected",
+    "public",
+    "static",
+    "yield",
+];
+
+pub const KEYWORDS: phf::Set<&str> = phf::phf_set![
+    "break",
+    "case",
+    "catch",
+    "continue",
+    "debugger",
+    "default",
+    "do",
+    "else",
+    "finally",
+    "for",
+    "function",
+    "if",
+    "return",
+    "switch",
+    "throw",
+    "try",
+    "var",
+    "const",
+    "while",
+    "with",
+    "new",
+    "this",
+    "super",
+    "class",
+    "extends",
+    "export",
+    "import",
+    "null",
+    "true",
+    "false",
+    "in",
+    "instanceof",
+    "typeof",
+    "void",
+    "delete",
+];
+
+/// https://github.com/babel/babel/blob/ff3481746a830e0e94626de4c4cb075ea5f2f5dc/packages/babel-helper-validator-identifier/src/identifier.ts#L85-L109
+pub fn is_identifier_name(name: &Atom) -> bool {
+    let string = name.as_str();
+    if string.is_empty() {
+        return false;
+    }
+    let mut is_first = true;
+    for ch in string.chars() {
+        if is_first {
+            is_first = false;
+            if !is_id_start(ch) {
+                return false;
+            }
+        } else if !is_id_continue(ch) {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn is_valid_identifier(name: &Atom, reserved: bool) -> bool {
+    if reserved && (KEYWORDS.contains(name.as_str()) || is_strict_reserved_word(name, true)) {
+        return false;
+    }
+    is_identifier_name(name)
+}
+
+pub fn is_strict_reserved_word(name: &Atom, in_module: bool) -> bool {
+    is_reserved_word(name, in_module) || RESERVED_WORD_STRICT.contains(name.as_str())
+}
+
+pub fn is_reserved_word(name: &Atom, in_module: bool) -> bool {
+    (in_module && name.as_str() == "await") || name.as_str() == "enum"
+}
+
+/// https://github.com/babel/babel/blob/main/packages/babel-types/src/validators/isValidES3Identifier.ts#L35
+pub fn is_valid_es3_identifier(name: &Atom) -> bool {
+    is_valid_identifier(name, true) && !RESERVED_WORDS_ES3_ONLY.contains(name.as_str())
 }
