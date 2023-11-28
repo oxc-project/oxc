413407a0c0c048bb973cebba7870fea93b21be47
Shinyaigeek <Philispaxil@gmail.com>
2023-11-29 01:28:23 +0900

    feat(linter): Implement img-redundant-alt rule for jsx_a11y plugin

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -213,6 +213,7 @@ mod jsx_a11y {
     pub mod anchor_is_valid;
     pub mod heading_has_content;
     pub mod html_has_lang;
+    pub mod img_redundant_alt;
 }
 
 oxc_macros::declare_all_lint_rules! {
@@ -400,5 +401,6 @@ oxc_macros::declare_all_lint_rules! {
     jsx_a11y::anchor_has_content,
     jsx_a11y::anchor_is_valid,
     jsx_a11y::html_has_lang,
-    jsx_a11y::heading_has_content
+    jsx_a11y::heading_has_content,
+    jsx_a11y::img_redundant_alt
 }
