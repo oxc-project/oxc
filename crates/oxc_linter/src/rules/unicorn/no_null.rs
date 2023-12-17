33a6ba4f89a204ee640deb7acce902bc216e7231
KenHH-24 <1098862703@qq.com>
2023-12-17 16:14:20 +0800

    feat(linter): implement the no-null rule

--- crates/oxc_linter/src/rules/unicorn/no_null.rs
+++ crates/oxc_linter/src/rules/unicorn/no_null.rs
@@ -1,5 +1,7 @@
 use oxc_ast::{
-    ast::{Argument, Expression},
+    ast::{
+        Argument, BinaryExpression, CallExpression, Expression, NullLiteral, VariableDeclarator,
+    },
     AstKind,
 };
 use oxc_diagnostics::{
@@ -7,15 +9,20 @@ use oxc_diagnostics::{
     thiserror::Error,
 };
 use oxc_macros::declare_oxc_lint;
-use oxc_span::Span;
+use oxc_span::{GetSpan, Span};
 use oxc_syntax::operator::BinaryOperator;
 
-use crate::{ast_util::is_method_call, context::LintContext, rule::Rule, AstNode};
+use crate::{ast_util::is_method_call, context::LintContext, rule::Rule, AstNode, Fix};
 
 #[derive(Debug, Error, Diagnostic)]
 #[error("eslint-plugin-unicorn(no-null): Disallow the use of the `null` literal")]
 #[diagnostic(severity(warning), help("Replace the `null` literal with `undefined`."))]
-struct NoNullDiagnostic(#[label] pub Span);
+struct ReplaceNullDiagnostic(#[label] pub Span);
+
+#[derive(Debug, Error, Diagnostic)]
+#[error("eslint-plugin-unicorn(no-null): Disallow the use of the `null` literal")]
+#[diagnostic(severity(warning), help("Remove the `null` literal."))]
+struct RemoveNullDiagnostic(#[label] pub Span);
 
 #[derive(Debug, Default, Clone)]
 pub struct NoNull {
@@ -25,21 +32,138 @@ pub struct NoNull {
 declare_oxc_lint!(
     /// ### What it does
     ///
+    /// Disallow the use of the `null` literal, to encourage using `undefined` instead.
     ///
     /// ### Why is this bad?
     ///
+    /// There are some reasons for using `undefined` instead of `null`.
+    /// - From experience, most developers use `null` and `undefined` inconsistently and interchangeably, and few know when to use which.
+    /// - Supporting both `null` and `undefined` complicates input validation.
+    /// - Using `null` makes TypeScript types more verbose: `type A = {foo?: string | null}` vs `type A = {foo?: string}`.
     ///
     /// ### Example
     /// ```javascript
+    /// // Bad
+    /// let foo = null;
+    ///
+    /// // Good
+    /// let foo
     /// ```
     NoNull,
     correctness
 );
 
+fn match_null_arg(call_expr: &CallExpression, index: usize, span: Span) -> bool {
+    call_expr.arguments.get(index).map_or(false, |arg| {
+        if let Argument::Expression(Expression::NullLiteral(null_lit)) = arg {
+            return null_lit.span == span;
+        }
+
+        false
+    })
+}
+
+fn diagnose_binary_expression(
+    no_null: &NoNull,
+    ctx: &LintContext,
+    null_literal: &NullLiteral,
+    binary_expr: &BinaryExpression,
+) {
+    // checkStrictEquality=false && `if (foo !== null) {}`
+    if !no_null.check_strict_equality.is_some_and(|val| val)
+        && matches!(
+            binary_expr.operator,
+            BinaryOperator::StrictEquality | BinaryOperator::StrictInequality
+        )
+    {
+        return;
+    }
+
+    // `if (foo != null) {}`
+    if matches!(binary_expr.operator, BinaryOperator::Equality | BinaryOperator::Inequality) {
+        ctx.diagnostic_with_fix(ReplaceNullDiagnostic(null_literal.span), || {
+            Fix::new("undefined", null_literal.span)
+        });
+
+        return;
+    }
+
+    // checkStrictEquality=true && `if (foo !== null) {}`
+    ctx.diagnostic_with_fix(ReplaceNullDiagnostic(null_literal.span), || {
+        Fix::new("undefined", null_literal.span)
+    });
+}
+
+fn diagnose_variable_declarator(
+    ctx: &LintContext,
+    null_literal: &NullLiteral,
+    variable_declarator: &VariableDeclarator,
+    parent_kind: Option<AstKind>,
+) {
+    // `let foo = null;`
+    if matches!(&variable_declarator.init, Some(Expression::NullLiteral(expr)) if expr.span == null_literal.span)
+        && matches!(parent_kind, Some(AstKind::VariableDeclaration(var_declaration)) if !var_declaration.kind.is_const() )
+    {
+        ctx.diagnostic_with_fix(RemoveNullDiagnostic(null_literal.span), || {
+            Fix::delete(Span {
+                start: variable_declarator.id.span().end,
+                end: null_literal.span.end,
+            })
+        });
+
+        return;
+    }
+
+    // `const foo = null`
+    ctx.diagnostic_with_fix(ReplaceNullDiagnostic(null_literal.span), || {
+        Fix::new("undefined", null_literal.span)
+    });
+}
+
+fn match_call_expression_pass_case(null_literal: &NullLiteral, call_expr: &CallExpression) -> bool {
+    // `Object.create(null)`, `Object.create(null, foo)`
+    if is_method_call(call_expr, Some(&["Object"]), Some(&["create"]), Some(1), Some(2))
+        && !call_expr.optional
+        && !matches!(&call_expr.callee, Expression::MemberExpression(member_expr) if member_expr.is_computed())
+        && match_null_arg(call_expr, 0, null_literal.span)
+    {
+        return true;
+    }
+
+    // `useRef(null)`
+    if let Expression::Identifier(ident) = &call_expr.callee {
+        if ident.name == "useRef" && call_expr.arguments.len() == 1 && !call_expr.optional {
+            return true;
+        }
+    }
+
+    // `React.useRef(null)`
+    if is_method_call(call_expr, Some(&["React"]), Some(&["useRef"]), Some(1), Some(1))
+        && !call_expr.optional
+    {
+        return true;
+    }
+
+    // `foo.insertBefore(bar, null)`
+    if is_method_call(call_expr, None, Some(&["insertBefore"]), Some(2), Some(2))
+        && !call_expr
+            .arguments
+            .iter()
+            .any(|argument| matches!(argument, Argument::SpreadElement(_)))
+        && !call_expr.optional
+        && !matches!(&call_expr.callee, Expression::MemberExpression(member_expr) if member_expr.is_computed())
+        && match_null_arg(call_expr, 1, null_literal.span)
+    {
+        return true;
+    }
+
+    false
+}
+
 impl Rule for NoNull {
-    fn from_configuration(_value: serde_json::Value) -> Self {
+    fn from_configuration(value: serde_json::Value) -> Self {
         Self {
-            check_strict_equality: _value
+            check_strict_equality: value
                 .get(0)
                 .and_then(|v| v.get("checkStrictEquality"))
                 .and_then(serde_json::Value::as_bool),
@@ -47,77 +171,58 @@ impl Rule for NoNull {
     }
 
     fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
-        let AstKind::NullLiteral(null_lit) = node.kind() else {
+        let AstKind::NullLiteral(null_literal) = node.kind() else {
             return;
         };
 
-        if let Some(maybe_parent) = ctx.nodes().parent_node(node.id()) {
-            if let AstKind::BinaryExpression(binary_expr) = maybe_parent.kind() {
-                if !self.check_strict_equality.is_some_and(|val| val == true)
-                    && matches!(
-                        binary_expr.operator,
-                        BinaryOperator::StrictEquality | BinaryOperator::StrictInequality
-                    )
-                {
-                    return;
-                }
+        if let Some(parent_node) = ctx.nodes().parent_node(node.id()) {
+            if let AstKind::BinaryExpression(binary_expr) = parent_node.kind() {
+                diagnose_binary_expression(self, ctx, null_literal, binary_expr);
+                return;
             }
 
-            if matches!(maybe_parent.kind(), AstKind::Argument(_)) {
-                if let Some(AstKind::CallExpression(call_expr)) =
-                    ctx.nodes().parent_kind(maybe_parent.id())
-                {
-                    // `Object.create(null)`, `Object.create(null, foo)`
-                    if is_method_call(
-                        call_expr,
-                        Some(&["Object"]),
-                        Some(&["create"]),
-                        Some(1),
-                        Some(2),
-                    ) && !call_expr.optional
-                        && !matches!(&call_expr.callee, Expression::MemberExpression(member_expr) if member_expr.is_computed())
-                    {
-                        return;
-                    }
+            let grand_parent_kind = ctx.nodes().parent_kind(parent_node.id());
 
-                    // `useRef(null)`
-                    if let Expression::Identifier(ident) = &call_expr.callee {
-                        if ident.name == "useRef"
-                            && call_expr.arguments.len() == 1
-                            && !call_expr.optional
-                        {
-                            return;
-                        }
-                    }
+            if let AstKind::VariableDeclarator(variable_declarator) = parent_node.kind() {
+                diagnose_variable_declarator(
+                    ctx,
+                    null_literal,
+                    variable_declarator,
+                    grand_parent_kind,
+                );
 
-                    // `React.useRef(null)`
-                    if is_method_call(
-                        call_expr,
-                        Some(&["React"]),
-                        Some(&["useRef"]),
-                        Some(1),
-                        Some(1),
-                    ) && !call_expr.optional
-                    {
-                        return;
-                    }
+                return;
+            }
 
-                    // `foo.insertBefore(bar, null)`
-                    if is_method_call(call_expr, None, Some(&["insertBefore"]), Some(2), Some(2))
-                        && !call_expr
-                            .arguments
-                            .iter()
-                            .any(|argument| matches!(argument, Argument::SpreadElement(_)))
-                        && !call_expr.optional
-                        && !matches!(&call_expr.callee, Expression::MemberExpression(member_expr) if member_expr.is_computed())
-                    {
+            if matches!(parent_node.kind(), AstKind::ReturnStatement(_)) {
+                ctx.diagnostic_with_fix(RemoveNullDiagnostic(null_literal.span), || {
+                    Fix::delete(null_literal.span)
+                });
+
+                return;
+            }
+
+            if matches!(parent_node.kind(), AstKind::Argument(_)) {
+                // `function foo() { return null; }`,
+                if let Some(AstKind::ReturnStatement(_)) = grand_parent_kind {
+                    ctx.diagnostic_with_fix(RemoveNullDiagnostic(null_literal.span), || {
+                        Fix::delete(null_literal.span)
+                    });
+
+                    return;
+                }
+
+                if let Some(AstKind::CallExpression(call_expr)) = grand_parent_kind {
+                    if match_call_expression_pass_case(null_literal, call_expr) {
                         return;
                     }
                 }
             }
         }
 
-        ctx.diagnostic(NoNullDiagnostic(null_lit.span));
+        ctx.diagnostic_with_fix(ReplaceNullDiagnostic(null_literal.span), || {
+            Fix::new("undefined", null_literal.span)
+        });
     }
 }
 
@@ -164,7 +269,7 @@ fn test() {
         (
             "function foo() {
             return null;
-        }",
+            }",
             None,
         ),
         // Suggestion `VariableDeclaration`
