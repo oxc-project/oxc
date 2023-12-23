db6181775c3ac15b56a0723bfcb803297b503b25
poteboy <poteboy.dev@gmail.com>
2023-12-24 07:43:26 +0900

    Refine aria-props rule and update tests

--- crates/oxc_linter/src/rules/jsx_a11y/aria_props.rs
+++ crates/oxc_linter/src/rules/jsx_a11y/aria_props.rs
@@ -1,10 +1,10 @@
 use oxc_ast::{
-    ast::{Expression, JSXAttributeItem, JSXAttributeName},
+    ast::{JSXAttributeItem, JSXAttributeName},
     AstKind,
 };
 use oxc_diagnostics::{
     miette::{self, Diagnostic},
-    thiserror::{self, Error},
+    thiserror::Error,
 };
 use oxc_macros::declare_oxc_lint;
 use oxc_span::Span;
@@ -14,8 +14,8 @@ use crate::{context::LintContext, rule::Rule, AstNode};
 
 #[derive(Debug, Error, Diagnostic)]
 #[error("eslint-plugin-jsx-a11y(aria-props):")]
-#[diagnostic(severity(warning), help(""))]
-struct AriaPropsDiagnostic(#[label] pub Span);
+#[diagnostic(severity(warning), help("{1}"))]
+struct AriaPropsDiagnostic(#[label] pub Span, String);
 
 #[derive(Debug, Default, Clone)]
 pub struct AriaProps;
@@ -50,7 +50,9 @@ impl Rule for AriaProps {
                 if let JSXAttributeItem::Attribute(attr) = attr {
                     let name = get_attribute_name(&attr.name).to_lowercase();
                     if name.starts_with("aria-") && !valid_aria_props.contains(&name) {
-                        ctx.diagnostic(AriaPropsDiagnostic(attr.span));
+                        let error_message =
+                            format!("{}: This attribute is an invalid ARIA attribute.", name);
+                        ctx.diagnostic(AriaPropsDiagnostic(attr.span, error_message));
                     }
                 }
             }
@@ -67,12 +69,66 @@ fn get_attribute_name(attr: &JSXAttributeName) -> String {
     }
 }
 
+/// Creates a set of valid ARIA properties from the WAI-ARIA 1.1 specifications.
+///
+/// The properties list is based on W3C's WAI-ARIA standards.
+/// Reference: <https://www.w3.org/TR/wai-aria/#state_prop_def>
+///
+/// # Returns
+/// `HashSet<String>` containing valid ARIA properties.
 fn create_valid_aria_props() -> HashSet<String> {
-    let mut props = HashSet::new();
-    props.insert("aria-activedescendant".to_string());
-    props.insert("aria-valuetext".to_string());
+    let aria_props = [
+        "aria-activedescendant",
+        "aria-atomic",
+        "aria-autocomplete",
+        "aria-busy",
+        "aria-checked",
+        "aria-colcount",
+        "aria-colindex",
+        "aria-colspan",
+        "aria-controls",
+        "aria-current",
+        "aria-describedby",
+        "aria-details",
+        "aria-disabled",
+        "aria-dropeffect",
+        "aria-errormessage",
+        "aria-expanded",
+        "aria-flowto",
+        "aria-grabbed",
+        "aria-haspopup",
+        "aria-hidden",
+        "aria-invalid",
+        "aria-keyshortcuts",
+        "aria-label",
+        "aria-labelledby",
+        "aria-level",
+        "aria-live",
+        "aria-modal",
+        "aria-multiline",
+        "aria-multiselectable",
+        "aria-orientation",
+        "aria-owns",
+        "aria-placeholder",
+        "aria-posinset",
+        "aria-pressed",
+        "aria-readonly",
+        "aria-relevant",
+        "aria-required",
+        "aria-roledescription",
+        "aria-rowcount",
+        "aria-rowindex",
+        "aria-rowspan",
+        "aria-selected",
+        "aria-setsize",
+        "aria-sort",
+        "aria-valuemax",
+        "aria-valuemin",
+        "aria-valuenow",
+        "aria-valuetext",
+    ];
 
-    return props;
+    aria_props.into_iter().map(|s| s.to_string()).collect()
 }
 
 #[test]
@@ -82,18 +138,18 @@ fn test() {
     let pass = vec![
         r#"<div />"#,
         r#"<div></div>"#,
-        r#"<div aria=\"wee\"></div>"#,
-        r#"<div abcARIAdef=\"true\"></div>"#,
-        r#"<div fooaria-foobar=\"true\"></div>"#,
-        r#"<div fooaria-hidden=\"true\"></div>"#,
+        r#"<div aria="wee"></div>"#,
+        r#"<div abcARIAdef="true"></div>"#,
+        r#"<div fooaria-foobar="true"></div>"#,
+        r#"<div fooaria-hidden="true"></div>"#,
         r#"<Bar baz />"#,
-        r#"<input type=\"text\" aria-errormessage=\"foobar\" />"#,
+        r#"<input type="text" aria-errormessage="foobar" />"#,
     ];
 
     let fail = vec![
-        r#"<div aria-=\"foobar\" />"#,
-        r#"<div aria-labeledby=\"foobar\" />"#,
-        r#"<div aria-skldjfaria-klajsd=\"foobar\" />"#,
+        r#"<div aria-="foobar" />"#,
+        r#"<div aria-labeledby="foobar" />"#,
+        r#"<div aria-skldjfaria-klajsd="foobar" />"#,
     ];
 
     Tester::new_without_config(AriaProps::NAME, pass, fail).test_and_snapshot();
