32413f58ad1ee7f08b7da4397fe4bf6b599d6243
poteboy <59927325+poteboy@users.noreply.github.com>
2023-12-25 01:47:18 +0900

    feat(linter): eslint-plugin-jsx-a11y aria-props (#1797)
    Implemented eslint-plugin-jsx-a11y aria_props as part of #1141
    
    This PR introduces the aria-props rule, implemented based on the
    [WAI-ARIA 1.1
    specifications](https://www.w3.org/TR/wai-aria/#state_prop_def).

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -231,6 +231,7 @@ mod jsx_a11y {
     pub mod alt_text;
     pub mod anchor_has_content;
     pub mod anchor_is_valid;
+    pub mod aria_props;
     pub mod heading_has_content;
     pub mod html_has_lang;
     pub mod iframe_has_title;
@@ -453,6 +454,7 @@ oxc_macros::declare_all_lint_rules! {
     jsx_a11y::alt_text,
     jsx_a11y::anchor_has_content,
     jsx_a11y::anchor_is_valid,
+    jsx_a11y::aria_props,
     jsx_a11y::heading_has_content,
     jsx_a11y::html_has_lang,
     jsx_a11y::iframe_has_title,
---
+++ crates/oxc_linter/src/rules/jsx_a11y/aria_props.rs
@@ -0,0 +1,84 @@
+use oxc_ast::{
+    ast::{JSXAttributeItem, JSXAttributeName},
+    AstKind,
+};
+use oxc_diagnostics::{
+    miette::{self, Diagnostic},
+    thiserror::Error,
+};
+use oxc_macros::declare_oxc_lint;
+use oxc_span::Span;
+
+use crate::{context::LintContext, globals::VALID_ARIA_PROPS, rule::Rule, AstNode};
+
+#[derive(Debug, Error, Diagnostic)]
+#[error("eslint-plugin-jsx-a11y(aria-props): Invalid ARIA prop.")]
+#[diagnostic(severity(warning), help("`{1}` is an invalid ARIA attribute."))]
+struct AriaPropsDiagnostic(#[label] pub Span, String);
+
+#[derive(Debug, Default, Clone)]
+pub struct AriaProps;
+
+declare_oxc_lint!(
+    /// ### What it does
+    /// Enforces that elements do not use invalid ARIA attributes.
+    ///
+    /// ### Why is this bad?
+    /// Using invalid ARIA attributes can mislead screen readers and other assistive technologies.
+    /// It may cause the accessibility features of the website to fail, making it difficult
+    /// for users with disabilities to use the site effectively.
+    ///
+    /// ### Example
+    /// ```javascript
+    /// // Bad
+    /// <input aria-labeledby="address_label" />
+    ///
+    /// // Good
+    /// <input aria-labelledby="address_label" />
+    /// ```
+    AriaProps,
+    correctness
+);
+impl Rule for AriaProps {
+    fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
+        if let AstKind::JSXAttributeItem(JSXAttributeItem::Attribute(attr)) = node.kind() {
+            let name = get_attribute_name(&attr.name).to_lowercase();
+            if name.starts_with("aria-") && !VALID_ARIA_PROPS.contains(&name) {
+                ctx.diagnostic(AriaPropsDiagnostic(attr.span, name));
+            }
+        }
+    }
+}
+
+fn get_attribute_name(attr: &JSXAttributeName) -> String {
+    match attr {
+        JSXAttributeName::Identifier(ident) => ident.name.to_string(),
+        JSXAttributeName::NamespacedName(namespaced_name) => {
+            format!("{}:{}", namespaced_name.namespace.name, namespaced_name.property.name)
+        }
+    }
+}
+
+#[test]
+fn test() {
+    use crate::tester::Tester;
+
+    let pass = vec![
+        r"<div />",
+        r"<div></div>",
+        r#"<div aria="wee"></div>"#,
+        r#"<div abcARIAdef="true"></div>"#,
+        r#"<div fooaria-foobar="true"></div>"#,
+        r#"<div fooaria-hidden="true"></div>"#,
+        r"<Bar baz />",
+        r#"<input type="text" aria-errormessage="foobar" />"#,
+    ];
+
+    let fail = vec![
+        r#"<div aria-="foobar" />"#,
+        r#"<div aria-labeledby="foobar" />"#,
+        r#"<div aria-skldjfaria-klajsd="foobar" />"#,
+    ];
+
+    Tester::new_without_config(AriaProps::NAME, pass, fail).test_and_snapshot();
+}
