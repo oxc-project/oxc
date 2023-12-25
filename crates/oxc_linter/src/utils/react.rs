2f53c3e47e178075e22fc40a8b892c4f4d8048c0
msdlisper <1170167213@qq.com>
2023-12-26 00:26:14 +0800

    feat(linter): refine jsx-a11y settings

--- crates/oxc_linter/src/utils/react.rs
+++ crates/oxc_linter/src/utils/react.rs
@@ -163,20 +163,28 @@ pub fn get_parent_es6_component<'a, 'b>(ctx: &'b LintContext<'a>) -> Option<&'b 
         None
     })
 }
+
 pub fn get_element_type(context: &LintContext, element: &JSXOpeningElement) -> Option<String> {
     let JSXElementName::Identifier(ident) = &element.name else {
         return None;
     };
 
-    let mut element_type = String::from(ident.name.as_str());
-
     let LintSettings { jsx_a11y } = context.settings();
-    let JsxA11y { polymorphic_prop_name: _, components } = jsx_a11y;
-    if let Some(val) = components.get(&element_type) {
-        element_type = String::from(val);
+    let JsxA11y { polymorphic_prop_name, components } = jsx_a11y;
+
+    if let Some(polymorphic_prop_name_value) = polymorphic_prop_name {
+        if let Some(as_tag) = has_jsx_prop_lowercase(element, &polymorphic_prop_name_value) {
+            if let Some(JSXAttributeValue::StringLiteral(str)) = get_prop_value(as_tag) {
+                return Some(String::from(str.value.as_str()));
+            }
+        }
     }
 
-    Some(element_type)
+    let element_type = ident.name.as_str();
+    if let Some(val) = components.get(element_type) {
+        return Some(String::from(val));
+    }
+    Some(String::from(element_type))
 }
 
 pub fn parse_jsx_value(value: &JSXAttributeValue) -> Result<f64, ()> {
