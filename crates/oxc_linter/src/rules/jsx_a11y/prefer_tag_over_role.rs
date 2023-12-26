14a5fe35985c552fe66f5b3bde40b7607688510a
yossydev <codeyy021@gmail.com>
2023-12-27 01:21:25 +0900

    feat(linter): eslint-plugin-jsx-a11y prefer-tag-over-role rule

---
+++ crates/oxc_linter/src/rules/jsx_a11y/prefer_tag_over_role.rs
@@ -0,0 +1,137 @@
+use std::collections::HashMap;
+
+use crate::{context::LintContext, rule::Rule, utils::has_jsx_prop_lowercase, AstNode};
+use oxc_ast::{
+    ast::{JSXAttributeItem, JSXAttributeValue, JSXElementName},
+    AstKind,
+};
+use oxc_diagnostics::{
+    miette::{self, Diagnostic},
+    thiserror::Error,
+};
+use oxc_macros::declare_oxc_lint;
+use oxc_span::Span;
+
+#[derive(Debug, Error, Diagnostic)]
+#[error("eslint-plugin-jsx-a11y/prefer-tag-over-role: Prefer using semantic HTML tags over `role` attribute.")]
+#[diagnostic(
+    severity(warning),
+    help("Replace HTML elements with `role` attribute to corresponding semantic HTML tags.")
+)]
+struct PreferTagOverRoleDiagnostic {
+    pub span: Span,
+    pub tag: String,
+    pub role: String,
+}
+#[derive(Debug, Default, Clone)]
+pub struct PreferTagOverRole;
+
+declare_oxc_lint!(
+    /// ### What it does
+    /// Enforces using semantic HTML tags over `role` attribute.
+    ///
+    /// ### Why is this bad?
+    /// Using semantic HTML tags can improve accessibility and readability of the code.
+    ///
+    /// ### Example
+    /// ```javascript
+    /// // Bad
+    /// <div role="button" />
+    ///
+    /// // Good
+    /// <button />
+    ///
+    PreferTagOverRole,
+    correctness
+);
+
+impl PreferTagOverRole {
+    fn get_role_to_tag_map(&self) -> HashMap<&str, &str> {
+        [
+            ("checkbox", "input"),
+            ("button", "button"),
+            ("heading", "h1,h2,h3,h4,h5,h6"),
+            ("link", "a,area"),
+            ("rowgroup", "tbody,tfoot,thead"),
+            ("banner", "header"),
+        ]
+        .iter()
+        .cloned()
+        .collect()
+    }
+
+    fn check_roles<'a>(
+        &self,
+        role_prop: &JSXAttributeItem<'a>,
+        role_to_tag: &HashMap<&str, &str>,
+        jsx_name: &JSXElementName<'a>,
+        ctx: &LintContext<'a>,
+    ) {
+        if let JSXAttributeItem::Attribute(attr) = role_prop {
+            if let Some(JSXAttributeValue::StringLiteral(role_values)) = &attr.value {
+                let roles = role_values.value.split_whitespace();
+                for role in roles {
+                    self.check_role(role, role_to_tag, jsx_name, attr.span, ctx);
+                }
+            }
+        }
+    }
+
+    fn check_role<'a>(
+        &self,
+        role: &str,
+        role_to_tag: &HashMap<&str, &str>,
+        jsx_name: &JSXElementName<'a>,
+        span: Span,
+        ctx: &LintContext<'a>,
+    ) {
+        if let Some(tag) = role_to_tag.get(role) {
+            match jsx_name {
+                JSXElementName::Identifier(id) if id.name != *tag => {
+                    ctx.diagnostic(PreferTagOverRoleDiagnostic {
+                        span,
+                        tag: tag.to_string(),
+                        role: role.to_string(),
+                    });
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
+impl Rule for PreferTagOverRole {
+    fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
+        if let AstKind::JSXOpeningElement(jsx_el) = node.kind() {
+            let role_to_tag = self.get_role_to_tag_map();
+            if let Some(role_prop) = has_jsx_prop_lowercase(jsx_el, "role") {
+                self.check_roles(&role_prop, &role_to_tag, &jsx_el.name, ctx);
+            }
+        }
+    }
+}
+
+#[test]
+fn test() {
+    use crate::tester::Tester;
+    let pass = vec![
+        "<div />",
+        "<div role=\"unknown\" />",
+        "<div role=\"also unknown\" />",
+        "<other />",
+        "<img role=\"img\" />",
+        "<input role=\"checkbox\" />",
+    ];
+    let fail: Vec<&str> = vec![
+        r#"<div role="checkbox" />"#,
+        r#"<div role="button checkbox" />"#,
+        r#"<div role="heading" />"#,
+        r#"<div role="link" />"#,
+        r#"<div role="rowgroup" />"#,
+        r#"<span role="checkbox" />"#,
+        r#"<other role="checkbox" />"#,
+        r#"<other role="checkbox" />"#,
+        r#"<div role="banner" />"#,
+    ];
+    Tester::new_without_config(PreferTagOverRole::NAME, pass, fail).test_and_snapshot();
+}
