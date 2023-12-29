17a146991ef892298ab2fdecbe8a26f51a82ec58
Rintaro Itokawa <me@re-taro.dev>
2023-12-29 21:26:38 +0900

    refactor: moved utils used only in jsx_a11y to jsx_a11y.rs

--- crates/oxc_linter/src/utils/react.rs
+++ crates/oxc_linter/src/utils/react.rs
@@ -204,12 +204,3 @@ pub fn parse_jsx_value(value: &JSXAttributeValue) -> Result<f64, ()> {
         _ => Err(()),
     }
 }
-
-pub fn get_attribute_name(attr: &JSXAttributeName) -> String {
-    match attr {
-        JSXAttributeName::Identifier(ident) => ident.name.to_string(),
-        JSXAttributeName::NamespacedName(namespaced_name) => {
-            format!("{}:{}", namespaced_name.namespace.name, namespaced_name.property.name)
-        }
-    }
-}
