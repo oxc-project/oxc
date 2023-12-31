c1cfd1759ee16c4da212e84f48d0ef78551ac485
Deivid Almeida <46359732+DeividAlmeida@users.noreply.github.com>
2023-12-31 01:05:38 -0300

    feat(linter): no-irregular-whitespace rule (#1835)
    Parser, trivias and trivias_builder were edited to get all whitespaces.
    Now Trivias struct store comments and whitespaces Vec. After that, i
    will implement the no-irregular-whitespace rule.
    
    P.S.: There isn't a way to implement this feature without lose a little
    bit of performance, comparing with my last PR #1819 to minimax this
    trouble instead of store the irregular whitespace as Span it was stored
    as u32, i removed a map iterator and removed too a unused function. If
    you have a suggestion about it pls give me a feedback.

--- crates/oxc_parser/src/lexer/trivia_builder.rs
+++ crates/oxc_parser/src/lexer/trivia_builder.rs
@@ -1,4 +1,5 @@
 use oxc_ast::{CommentKind, Trivias};
+use oxc_span::Span;
 
 #[derive(Debug, Default)]
 pub struct TriviaBuilder {
@@ -12,11 +13,15 @@ impl TriviaBuilder {
 
     /// skip leading `//`
     pub fn add_single_line_comment(&mut self, start: u32, end: u32) {
-        self.trivias.push((start + 2, end, CommentKind::SingleLine));
+        self.trivias.comments.push((start + 2, end, CommentKind::SingleLine));
     }
 
     /// skip leading `/*` and trailing `*/`
     pub fn add_multi_line_comment(&mut self, start: u32, end: u32) {
-        self.trivias.push((start + 2, end - 2, CommentKind::MultiLine));
+        self.trivias.comments.push((start + 2, end - 2, CommentKind::MultiLine));
+    }
+
+    pub fn add_irregular_whitespace(&mut self, start: u32, end: u32) {
+        self.trivias.irregular_whitespaces.push(Span::new(start, end));
     }
 }
