0feeac540d2852bc158c8d0f0668788d50f144aa
Wenzhe Wang <mysteryven@gmail.com>
2023-12-31 22:19:27 +0800

    feat(lint): add partial loader register (#1760)

--- crates/oxc_cli/fixtures/walk_dir/bar.vue
+++ crates/oxc_cli/fixtures/walk_dir/bar.vue
@@ -1,4 +1,6 @@
 <template>
     <div>Hello World</div>
 </template>
-<script></script>
+<script>
+console.log('foo')
+</script>
--- crates/oxc_linter/src/partial_loader/mod.rs
+++ crates/oxc_linter/src/partial_loader/mod.rs
@@ -1,1 +1,22 @@
+use oxc_span::SourceType;
+
 pub const LINT_PARTIAL_LOADER_EXT: &[&str] = &["vue"];
+
+pub enum PartialLoader {
+    Vue,
+}
+
+pub struct PartialLoaderValue {
+    pub source_text: String,
+    pub source_type: SourceType,
+}
+
+impl PartialLoader {
+    // TODO: add parser
+    pub fn parse(&self, _source_text: &str) -> PartialLoaderValue {
+        PartialLoaderValue {
+            source_text: String::from("const a = 1"), // for not report "empty file error"
+            source_type: SourceType::default(),
+        }
+    }
+}
