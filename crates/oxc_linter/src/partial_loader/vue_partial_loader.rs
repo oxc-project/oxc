8bda5c417554f6c74161d77e9cdb474f6f71929c
Boshen <boshenc@gmail.com>
2024-01-05 15:40:05 +0800

    feat(linter): parse two script tags from vue

--- crates/oxc_linter/src/partial_loader/vue_partial_loader.rs
+++ crates/oxc_linter/src/partial_loader/vue_partial_loader.rs
@@ -1,253 +1,75 @@
-use std::str::Chars;
+use memchr::memmem::Finder;
 
-use oxc_span::{SourceType, Span};
+use oxc_span::SourceType;
 
 use super::JavaScriptSource;
 
+const SCRIPT_START: &str = "<script";
+const SCRIPT_END: &str = "</script>";
+
 pub struct VuePartialLoader<'a> {
     source_text: &'a str,
-    chars: Chars<'a>,
-    /// JS code start position
-    start: u32,
-    /// JS code end position
-    end: u32,
-    is_ts: bool,
-    is_jsx: bool,
-    is_reading_js: bool,
-    /// Record current <template> 's depth
-    template_depth: u32,
 }
 
 impl<'a> VuePartialLoader<'a> {
     pub fn new(source_text: &'a str) -> Self {
-        Self {
-            source_text,
-            chars: source_text.chars(),
-            start: 0,
-            end: 0,
-            is_ts: false,
-            is_jsx: false,
-            is_reading_js: false,
-            template_depth: 0,
-        }
+        Self { source_text }
     }
 
-    pub fn build(mut self) -> Option<JavaScriptSource<'a>> {
-        self.parse();
-        if self.end <= self.start {
-            return None;
-        }
-        let js_code = Span::new(self.start, self.end).source_text(self.source_text);
-        let source_type = SourceType::default()
-            .with_typescript(self.is_ts)
-            .with_jsx(self.is_jsx)
-            .with_module(true);
-        Some(JavaScriptSource::new(js_code, source_type))
+    pub fn parse(self) -> Vec<JavaScriptSource<'a>> {
+        self.parse_scripts()
     }
 
-    fn parse(&mut self) {
-        while let Some(ch) = self.advance() {
-            if self.is_reading_js {
-                match ch {
-                    '<' => {
-                        if self.can_eat("/script>") {
-                            self.is_reading_js = false;
-                            // minus 1 for '<'
-                            self.end = self.offset() - 1;
-                            break;
-                        }
-                    }
-                    '\'' | '"' => {
-                        self.skip_until_next_delimiter(ch);
-                    }
-                    '`' => {
-                        self.skip_to_end_of_template_literal();
-                    }
-                    '/' => {
-                        self.try_read_comment();
-                    }
-                    _ => {}
-                }
-            } else if ch == '<' {
-                self.try_read_tag_name();
-            }
-        }
+    /// Each *.vue file can contain at most
+    ///  * one <script> block (excluding <script setup>).
+    ///  * one <script setup> block (excluding normal <script>).
+    /// https://vuejs.org/api/sfc-spec.html#script
+    fn parse_scripts(&self) -> Vec<JavaScriptSource<'a>> {
+        let Some(result1) = self.parse_script(0) else { return vec![] };
+        let start = result1.source_text.len() + SCRIPT_END.len();
+        let Some(result2) = self.parse_script(start) else { return vec![result1] };
+        vec![result1, result2]
     }
 
-    fn try_read_tag_name(&mut self) {
-        if self.eat("template") {
-            self.template_depth += 1;
-        } else if self.eat("/template>") {
-            self.template_depth -= 1;
-        } else if self.template_depth == 0 && self.eat("script") {
-            let open_tag_start = self.offset();
-            if self.eat_to('>') {
-                let open_tag_end = self.offset();
+    fn parse_script(&self, start: usize) -> Option<JavaScriptSource<'a>> {
+        let script_start_finder = Finder::new(SCRIPT_START);
+        let script_end_finder = Finder::new(SCRIPT_END);
 
-                let attributes_text =
-                    Span::new(open_tag_start, open_tag_end).source_text(self.source_text);
-                self.is_ts = VuePartialLoader::contains_ts_flag(attributes_text);
-                self.is_jsx = VuePartialLoader::contains_jsx_flag(attributes_text);
-                self.is_reading_js = true;
-                self.start = self.offset();
-            }
-        }
-    }
+        let mut pointer = start;
 
-    fn try_read_comment(&mut self) {
-        match self.peek() {
-            // single line comment
-            Some('/') => {
-                self.eat_to('\n');
-            }
-            // multi line comment
-            Some('*') => {
-                while let Some(c) = self.advance() {
-                    if c == '*' && self.peek() == Some('/') {
-                        self.eat("/");
-                        break;
-                    }
-                }
-            }
-            _ => {}
-        }
-    }
+        // find opening "<script"
+        let offset = script_start_finder.find(self.source_text[pointer..].as_bytes())?;
+        pointer += offset + SCRIPT_START.len();
 
-    fn advance(&mut self) -> Option<char> {
-        self.chars.next()
-    }
+        // find closing ">"
+        let offset = self.source_text[pointer..].find('>')?;
 
-    fn peek(&self) -> Option<char> {
-        self.chars.clone().next()
-    }
-    fn skip_to_end_of_template_literal(&mut self) {
-        let mut last_is_escape = false;
+        // get ts and jsx attribute
+        let content = &self.source_text[pointer..pointer + offset];
+        let is_ts = content.contains("ts");
+        let is_jsx = content.contains("tsx") || content.contains("jsx");
+        pointer += offset + 1;
+        let js_start = pointer;
 
-        while let Some(c) = self.advance() {
-            if last_is_escape {
-                last_is_escape = false;
-                continue;
-            }
-            match c {
-                '\\' => last_is_escape = true,
-                '`' => {
-                    break;
-                }
-                '$' => {
-                    if self.peek() == Some('{') {
-                        self.skip_to_end_dollar_brace();
-                    }
-                }
-                _ => last_is_escape = false,
-            }
-        }
-    }
-    fn skip_to_end_dollar_brace(&mut self) {
-        self.advance();
-        let mut brace_depth = 0;
-        let mut last_is_escape = false;
+        // find "</script>"
+        let offset = script_end_finder.find(self.source_text[pointer..].as_bytes())?;
+        let js_end = pointer + offset;
 
-        while let Some(c) = self.advance() {
-            if last_is_escape {
-                last_is_escape = false;
-                continue;
-            }
-
-            match c {
-                '{' => {
-                    if !last_is_escape {
-                        brace_depth += 1;
-                    }
-                }
-                '}' => {
-                    if brace_depth == 0 {
-                        break;
-                    }
-                    brace_depth -= 1;
-                }
-                _ => {}
-            }
-        }
-    }
-    fn skip_until_next_delimiter(&mut self, delimiter: char) {
-        let mut last_is_escape = false;
-        for c in self.chars.by_ref() {
-            if last_is_escape {
-                last_is_escape = false;
-                continue;
-            }
-            match c {
-                '\\' => last_is_escape = true,
-                '\'' | '"' => {
-                    if c == delimiter {
-                        break;
-                    }
-                }
-                _ => last_is_escape = false,
-            }
-        }
-    }
-    // O(1) time complexity
-    // More info: https://oxc-project.github.io/docs/learn/parser_in_rust/lexer.html#token
-    #[allow(clippy::cast_possible_truncation)]
-    fn offset(&self) -> u32 {
-        (self.source_text.len() - self.chars.as_str().len()) as u32
-    }
-    fn eat(&mut self, target: &str) -> bool {
-        let mut chars = self.chars.clone();
-        for ch in target.chars() {
-            if let Some(c) = chars.next() {
-                if c != ch {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        }
-
-        self.chars = chars;
-        true
-    }
-    fn can_eat(&self, target: &str) -> bool {
-        let mut chars = self.chars.clone();
-        for ch in target.chars() {
-            if let Some(c) = chars.next() {
-                if c != ch {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        }
-
-        true
-    }
-    fn eat_to(&mut self, target: char) -> bool {
-        for ch in self.chars.by_ref() {
-            if target == ch {
-                return true;
-            }
-        }
-
-        false
-    }
-
-    fn contains_ts_flag(s: &str) -> bool {
-        ["lang=ts", "lang='ts'", r#"lang="ts""#, "lang=tsx", "lang='tsx'", r#"lang="tsx""#]
-            .iter()
-            .any(|flag| s.contains(flag))
-    }
-
-    fn contains_jsx_flag(s: &str) -> bool {
-        ["lang=jsx", "lang='jsx'", r#"lang="jsx""#, "lang=tsx", "lang='tsx'", r#"lang="tsx""#]
-            .iter()
-            .any(|flag| s.contains(flag))
+        let source_text = &self.source_text[js_start..js_end];
+        let source_type =
+            SourceType::default().with_module(true).with_typescript(is_ts).with_jsx(is_jsx);
+        Some(JavaScriptSource::new(source_text, source_type))
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::VuePartialLoader;
+    use super::{JavaScriptSource, VuePartialLoader};
+
+    fn parse_vue(source_text: &str) -> JavaScriptSource<'_> {
+        let sources = VuePartialLoader::new(source_text).parse();
+        *sources.first().unwrap()
+    }
 
     #[test]
     fn test_parse_vue_one_line() {
@@ -258,37 +80,8 @@ mod test {
         <script> console.log("hi") </script>
         "#;
 
-        let loader_value = VuePartialLoader::new(source_text).build().unwrap();
-        assert_eq!(loader_value.source_text, r#" console.log("hi") "#);
-    }
-
-    #[test]
-    fn test_build_vue_multi_line() {
-        let source_text = r#"
-        <template>
-          <h1>hello world</h1>
-        </template>
-        <script>
-            console.log("hi")
-            console.log("I am multi line")
-            console.log("<script></script>")
-            console.log(`<script></script>`)
-            console.log('<script></script>')
-        </script>
-        "#;
-
-        let loader_value = VuePartialLoader::new(source_text).build().unwrap();
-        assert!(!loader_value.source_type.is_typescript());
-        assert_eq!(
-            loader_value.source_text,
-            r#"
-            console.log("hi")
-            console.log("I am multi line")
-            console.log("<script></script>")
-            console.log(`<script></script>`)
-            console.log('<script></script>')
-        "#
-        );
+        let result = parse_vue(source_text);
+        assert_eq!(result.source_text, r#" console.log("hi") "#);
     }
 
     #[test]
@@ -299,9 +92,9 @@ mod test {
         </script>
         "#;
 
-        let loader_value = VuePartialLoader::new(source_text).build().unwrap();
-        assert!(loader_value.source_type.is_typescript());
-        assert_eq!(loader_value.source_text.trim(), "1/1");
+        let result = parse_vue(source_text);
+        assert!(result.source_type.is_typescript());
+        assert_eq!(result.source_text.trim(), "1/1");
     }
 
     #[test]
@@ -312,9 +105,9 @@ mod test {
         </script>
         ";
 
-        let loader_value = VuePartialLoader::new(source_text).build().unwrap();
-        assert!(loader_value.source_type.is_typescript());
-        assert_eq!(loader_value.source_text.trim(), "1/1");
+        let result = parse_vue(source_text);
+        assert!(result.source_type.is_typescript());
+        assert_eq!(result.source_text.trim(), "1/1");
     }
 
     #[test]
@@ -325,9 +118,9 @@ mod test {
         </script>
         ";
 
-        let loader_value = VuePartialLoader::new(source_text).build().unwrap();
-        assert!(loader_value.source_type.is_typescript());
-        assert_eq!(loader_value.source_text.trim(), "1/1");
+        let result = parse_vue(source_text);
+        assert!(result.source_type.is_typescript());
+        assert_eq!(result.source_text.trim(), "1/1");
     }
 
     #[test]
@@ -338,10 +131,10 @@ mod test {
         </script>
         ";
 
-        let loader_value = VuePartialLoader::new(source_text).build().unwrap();
-        assert!(loader_value.source_type.is_jsx());
-        assert!(loader_value.source_type.is_typescript());
-        assert_eq!(loader_value.source_text.trim(), "1/1");
+        let result = parse_vue(source_text);
+        assert!(result.source_type.is_jsx());
+        assert!(result.source_type.is_typescript());
+        assert_eq!(result.source_text.trim(), "1/1");
     }
 
     #[test]
@@ -353,9 +146,9 @@ mod test {
         <template> </template>
         ";
 
-        let loader_value = VuePartialLoader::new(source_text).build().unwrap();
-        assert!(!loader_value.source_type.is_typescript());
-        assert_eq!(loader_value.source_text.trim(), r"a.replace(/&#39;/g, '\''))");
+        let result = parse_vue(source_text);
+        assert!(!result.source_type.is_typescript());
+        assert_eq!(result.source_text.trim(), r"a.replace(/&#39;/g, '\''))");
     }
 
     #[test]
@@ -366,12 +159,10 @@ mod test {
         </script>
         ";
 
-        let loader_value = VuePartialLoader::new(source_text).build().unwrap();
-        assert_eq!(loader_value.source_text.trim(), r"`a${b( `c \`${d}\``)}`");
+        let result = parse_vue(source_text);
+        assert_eq!(result.source_text.trim(), r"`a${b( `c \`${d}\``)}`");
     }
 
-    // TODO: fix this, current did know whether / is the start of regex or not
-    #[ignore]
     #[test]
     fn test_brace_with_regex_in_template_literal() {
         let source_text = r"
@@ -380,45 +171,8 @@ mod test {
         </script>
         ";
 
-        let loader_value = VuePartialLoader::new(source_text).build().unwrap();
-        assert_eq!(loader_value.source_text.trim(), r"`${/{/}`");
-    }
-
-    #[test]
-    fn test_ignore_script_tag_in_template() {
-        let source_text = r"
-        <template>
-            <script>console.log('error')</script>
-        </template>
-        <script>
-            console.log('success')
-        </script>
-        ";
-
-        let loader_value = VuePartialLoader::new(source_text).build().unwrap();
-        assert!(!loader_value.source_type.is_typescript());
-        assert_eq!(loader_value.source_text.trim(), "console.log('success')");
-    }
-
-    #[test]
-    fn test_ignore_script_tag_in_template_2() {
-        let source_text = r"
-        <template>
-            <div>
-                <template>
-                    <script>console.log('error')</script>
-                </template>
-                <script>console.log('error')</script>
-            </div>
-        </template>
-        <script>
-            console.log('success')
-        </script>
-        ";
-
-        let loader_value = VuePartialLoader::new(source_text).build().unwrap();
-        assert!(!loader_value.source_type.is_typescript());
-        assert_eq!(loader_value.source_text.trim(), "console.log('success')");
+        let result = parse_vue(source_text);
+        assert_eq!(result.source_text.trim(), r"`${/{/}`");
     }
 
     #[test]
@@ -427,8 +181,8 @@ mod test {
             <template></template>
         ";
 
-        let loader_value = VuePartialLoader::new(source_text).build();
-        assert!(loader_value.is_none());
+        let sources = VuePartialLoader::new(source_text).parse();
+        assert!(sources.is_empty());
     }
 
     #[test]
@@ -437,8 +191,20 @@ mod test {
         <script>
             console.log('error')
         ";
-        let loader_value = VuePartialLoader::new(source_text).build();
-        assert!(loader_value.is_none());
+        let sources = VuePartialLoader::new(source_text).parse();
+        assert!(sources.is_empty());
+    }
+
+    #[test]
+    fn test_multiple_scripts() {
+        let source_text = r"
+        <script>a</script>
+        <script setup>b</script>
+        ";
+        let sources = VuePartialLoader::new(source_text).parse();
+        assert_eq!(sources.len(), 2);
+        assert_eq!(sources[0].source_text, "a");
+        assert_eq!(sources[1].source_text, "b");
     }
 
     #[test]
@@ -455,9 +221,9 @@ mod test {
         </script>
         ";
 
-        let loader_value = VuePartialLoader::new(source_text).build().unwrap();
+        let result = parse_vue(source_text);
         assert_eq!(
-            loader_value.source_text.trim(),
+            result.source_text.trim(),
             "let 日历 = '2000年';
         const t = useTranslate({
             'zh-CN': {
