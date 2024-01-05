2852c2b2ea5b8057642f71f274ab5cb8b463566a
Dunqing <dengqing0821@gmail.com>
2024-01-05 18:19:46 +0800

    refactor(semantic): improve ClassTable implmention and merge properties and methods to elements

--- crates/oxc_syntax/src/class.rs
+++ crates/oxc_syntax/src/class.rs
@@ -1,11 +1,32 @@
+use bitflags::bitflags;
 use oxc_index::define_index_type;
 
 define_index_type! {
     pub struct ClassId = u32;
 }
 define_index_type! {
-    pub struct PropertyId = u32;
+    pub struct ElementId = u32;
 }
-define_index_type! {
-    pub struct MethodId = u32;
+
+bitflags! {
+    #[derive(Debug, Clone, Copy)]
+    pub struct ElementKind: u8 {
+        const Accessor = 0;
+        const Method = 1;
+        const Property = 2;
+    }
+}
+
+impl ElementKind {
+    pub fn is_property(self) -> bool {
+        self.contains(Self::Property)
+    }
+
+    pub fn is_method(self) -> bool {
+        self.contains(Self::Method)
+    }
+
+    pub fn is_accessor(self) -> bool {
+        self.contains(Self::Accessor)
+    }
 }
