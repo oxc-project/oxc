d16cefd66b17ef8e54360d8bfbbb48304caf392b
underfin <2218301630@qq.com>
2024-01-09 17:48:48 +0800

    fix: fmt

--- crates/oxc_transformer/src/es2015/new_target.rs
+++ crates/oxc_transformer/src/es2015/new_target.rs
@@ -1,10 +1,10 @@
-use oxc_ast::{ast::*, AstBuilder, VisitMut, AstKind};
+use crate::{context::TransformerCtx, TransformOptions, TransformTarget};
+use oxc_allocator::Vec;
+use oxc_ast::{ast::*, AstBuilder, AstKind, VisitMut};
 use oxc_diagnostics::miette;
 use oxc_span::{Atom, Span, SPAN};
 use oxc_syntax::operator::BinaryOperator;
-use std::{rc::Rc};
-use crate::{TransformOptions, TransformTarget, context::TransformerCtx};
-use oxc_allocator::Vec;
+use std::rc::Rc;
 
 /// ES2015: New Target
 ///
@@ -19,22 +19,27 @@ pub struct NewTarget<'a> {
 enum NewTargetKind {
     Method,
     Constructor,
-    Function(Atom)
+    Function(Atom),
 }
 
 impl<'a> VisitMut<'a> for NewTarget<'a> {
     fn enter_node(&mut self, kind: AstKind<'a>) {
         if let Some(kind) = match kind {
-            AstKind::MethodDefinition(def) => {
-                match def.kind {
-                    MethodDefinitionKind::Get | MethodDefinitionKind::Set |MethodDefinitionKind::Method => Some(NewTargetKind::Method),
-                    MethodDefinitionKind::Constructor => Some( NewTargetKind::Constructor),
-                }
-            }
-            AstKind::Function(function) => {
-                Some(function.id.as_ref().map(|id| NewTargetKind::Function(id.name.clone())).unwrap_or_else(|| NewTargetKind::Function(self.ctx.scopes().generate_uid("target"))))
-            }
-            _ => None
+            AstKind::MethodDefinition(def) => match def.kind {
+                MethodDefinitionKind::Get
+                | MethodDefinitionKind::Set
+                | MethodDefinitionKind::Method => Some(NewTargetKind::Method),
+                MethodDefinitionKind::Constructor => Some(NewTargetKind::Constructor),
+            },
+            AstKind::Function(function) => Some(
+                function
+                    .id
+                    .as_ref()
+                    .map_or_else(|| {
+                        NewTargetKind::Function(self.ctx.scopes().generate_uid("target"))
+                    }, |id| NewTargetKind::Function(id.name.clone())),
+            ),
+            _ => None,
         } {
             self.kinds.push(kind);
         }
@@ -46,14 +51,26 @@ impl<'a> VisitMut<'a> for NewTarget<'a> {
 }
 
 impl<'a> NewTarget<'a> {
-    pub fn new(ast: Rc<AstBuilder<'a>>, ctx:  TransformerCtx<'a>, options: &TransformOptions) -> Option<Self> {
+    pub fn new(
+        ast: Rc<AstBuilder<'a>>,
+        ctx: TransformerCtx<'a>,
+        options: &TransformOptions,
+    ) -> Option<Self> {
         let kinds = ast.new_vec();
-        (options.target < TransformTarget::ES2015 || options.new_target)
-            .then(|| Self { ast, ctx, kinds })
+        (options.target < TransformTarget::ES2015 || options.new_target).then(|| Self {
+            ast,
+            ctx,
+            kinds,
+        })
     }
 
     fn create_constructor_expr(&self, span: Span) -> Expression<'a> {
-        self.ast.static_member_expression(span, self.ast.this_expression(span), IdentifierName { span: span, name: "constructor".into() }, false)
+        self.ast.static_member_expression(
+            span,
+            self.ast.this_expression(span),
+            IdentifierName { span, name: "constructor".into() },
+            false,
+        )
     }
 
     pub fn transform_expression<'b>(&mut self, expr: &'b mut Expression<'a>) {
@@ -68,16 +85,35 @@ impl<'a> NewTarget<'a> {
                             *expr = self.ast.void_0();
                         }
                         NewTargetKind::Function(name) => {
-                            let test = self.ast.binary_expression(SPAN, self.ast.this_expression(SPAN), BinaryOperator::Instanceof, self.ast.identifier_reference_expression( IdentifierReference::new(SPAN, name.clone())));
-                            let consequent = self.ast.static_member_expression(SPAN, self.ast.this_expression(SPAN), IdentifierName { span: SPAN, name: "constructor".into() }, false);
-                            *expr = self.ast.conditional_expression(meta.span, test, consequent, self.ast.void_0());
+                            let test = self.ast.binary_expression(
+                                SPAN,
+                                self.ast.this_expression(SPAN),
+                                BinaryOperator::Instanceof,
+                                self.ast.identifier_reference_expression(IdentifierReference::new(
+                                    SPAN,
+                                    name.clone(),
+                                )),
+                            );
+                            let consequent = self.ast.static_member_expression(
+                                SPAN,
+                                self.ast.this_expression(SPAN),
+                                IdentifierName { span: SPAN, name: "constructor".into() },
+                                false,
+                            );
+                            *expr = self.ast.conditional_expression(
+                                meta.span,
+                                test,
+                                consequent,
+                                self.ast.void_0(),
+                            );
                         }
-                    } 
+                    }
                 } else {
-                    self.ctx.error(miette::Error::msg("new.target must be under a (non-arrow) function or a class."))
+                    self.ctx.error(miette::Error::msg(
+                        "new.target must be under a (non-arrow) function or a class.",
+                    ));
                 }
             }
         }
     }
 }
-
