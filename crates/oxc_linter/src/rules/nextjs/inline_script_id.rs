ac3b44bd0c8cdfb69e0beeb364844c841035493d
Cameron <cameron.clark@hey.com>
2024-01-09 03:08:11 +0000

    feat: nextjs plugin (#1948)
    https://github.com/oxc-project/oxc/issues/1929#issuecomment-1880369624

--- crates/oxc_linter/src/rules/nextjs/inline_script_id.rs
+++ crates/oxc_linter/src/rules/nextjs/inline_script_id.rs
@@ -246,5 +246,7 @@ fn test() {
 			        }",
     ];
 
-    Tester::new_without_config(InlineScriptId::NAME, pass, fail).test_and_snapshot();
+    Tester::new_without_config(InlineScriptId::NAME, pass, fail)
+        .with_nextjs_plugin(true)
+        .test_and_snapshot();
 }
