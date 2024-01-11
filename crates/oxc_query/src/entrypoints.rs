0f6674563e3389b914c3bb18bcc72593eadfd34b
Boshen <boshenc@gmail.com>
2024-01-11 12:54:39 +0800

    chore: remove DSL based linter plugin (#1985)
    Due to maintenance issues, we are going to stop exploring a linter based
    DSL plugins.

--- crates/oxc_query/src/entrypoints.rs
+++
@@ -1,7 +0,0 @@
-use trustfall::provider::{ResolveInfo, VertexIterator};
-
-use super::vertex::Vertex;
-
-pub(super) fn file<'a, 'b: 'a>(_resolve_info: &ResolveInfo) -> VertexIterator<'a, Vertex<'b>> {
-    Box::new(std::iter::once(Vertex::File))
-}
--- website/playground/index.js
+++ website/playground/index.js
@@ -14,7 +14,6 @@ import { parser } from '@lezer/json'
 import { javascript, javascriptLanguage } from "@codemirror/lang-javascript";
 import { rust, rustLanguage } from "@codemirror/lang-rust";
 import { json, jsonLanguage } from "@codemirror/lang-json";
-import { graphql, graphqlLanguage } from "cm6-graphql";
 import { vscodeKeymap } from "@replit/codemirror-vscode-keymap";
 import { githubDark } from "@ddietr/codemirror-themes/github-dark";
 import { linter, lintGutter } from "@codemirror/lint";
@@ -22,7 +21,6 @@ import { language, syntaxTree } from "@codemirror/language";
 import { autocompletion } from "@codemirror/autocomplete";
 import { indentWithTab, deleteLine } from "@codemirror/commands";
 import throttle from "lodash.throttle";
-import { buildSchema } from "graphql";
 
 // lzma is a very old library, it writes to window when built in production with vite.
 import { LZMA } from 'lzma/src/lzma_worker.js';
@@ -36,7 +34,6 @@ import initWasm, {
   OxcMinifierOptions,
   OxcCodegenOptions,
   OxcTypeCheckingOptions,
-  graphql_schema_text,
 } from "@oxc/wasm-web";
 import { getSymbolAndReferencesSpan, renderSymbols } from "./symbols.js";
 
@@ -62,8 +59,6 @@ export default DummyComponent
 `.trim();
 
 const STORAGE_KEY_CODE = "playground.code";
-const STORAGE_KEY_QUERY = "playground.query";
-const STORAGE_KEY_QUERY_ARGUMENTS = "playground.query_arguments";
 
 const getStringFromStorage = (whatToGet) => {
   try {
@@ -93,24 +88,18 @@ class Playground {
 
   editor;
   viewer;
-  queryResultsViewer;
   currentView = "ast"; // "ast" | "format" | "minify" | "ir"
   languageConf;
   urlParams;
   viewerIsEditableConf;
-  queryResultViewerIsEditableConf;
-  showingQueryResultsOrArguments;
 
   constructor() {
     this.languageConf = new Compartment();
     this.urlParams = new URLParams();
     this.viewerIsEditableConf = new Compartment();
-    this.queryResultViewerIsEditableConf = new Compartment();
     this.linterConf = new Compartment();
     this.editor = this.initEditor();
     this.viewer = this.initViewer();
-    this.queryResultsViewer = this.initQueryResultsViewer();
-    this.showingQueryResultsOrArguments = "results";
 
   }
 
@@ -207,37 +196,7 @@ class Playground {
     return diagnostics;
   }
 
-  runQuery() {
-    if (
-      // run query, and put results in query result viewer pane
-      this.currentLanguage() === "graphql" &&
-      this.showingQueryResultsOrArguments === "results"
-    ) {
-      let queryResults = this.oxc.run_query(
-        this.parserOptions,
-        this.viewer.state.doc.toString(),
-        getStringFromStorage(STORAGE_KEY_QUERY_ARGUMENTS) ?? '{}' // must be a string of an empty object as this is a string param
-      );
-
-      let output =
-        typeof queryResults === "string"
-          ? queryResults
-          : JSON.stringify(queryResults, null, 2);
-
-      let stateUpdate = this.queryResultsViewer.state.update({
-        changes: {
-          from: 0,
-          to: this.queryResultsViewer.state.doc.length,
-          insert: output,
-        },
-      });
-
-      this.queryResultsViewer.dispatch(stateUpdate);
-    }
-  }
-
   initViewer() {
-
     // scroll selection into the middle https://discuss.codemirror.net/t/cm6-scroll-to-middle/2924/2
     const viewStateListener = EditorView.updateListener.of((update) => {
       if (update.transactions.some(tr => tr.scrollIntoView)) {
@@ -273,21 +232,6 @@ class Playground {
           },
           { delay: 0 }
         ),
-        linter(
-          (data) => {
-            try {
-              if (this.currentLanguage() === "graphql") {
-                setStringToStorage(
-                  STORAGE_KEY_QUERY,
-                  data.state.doc.toString()
-                );
-              }
-            } finally {
-              return [];
-            }
-          },
-          { delay: 0 }
-        ),
         viewStateListener,
         basicSetup,
         keymap.of([
@@ -301,15 +245,7 @@ class Playground {
         githubDark,
         EditorState.transactionExtender.of((tr) => {
           if (!tr.docChanged) return null;
-
-          let ext;
-
-          if (this.currentLanguage() === "graphql") {
-            ext = EditorView.editable.of(true);
-          } else {
-            ext = EditorView.editable.of(false);
-          }
-
+          let ext = EditorView.editable.of(false);
           return {
             effects: this.viewerIsEditableConf.reconfigure(ext),
           };
@@ -335,10 +271,6 @@ class Playground {
               if (currentLanguage == rustLanguage) return null;
               newLanguage = rust();
               break;
-            case "graphql":
-              if (currentLanguage == graphqlLanguage) return null;
-              newLanguage = graphql(buildSchema(graphql_schema_text()));
-              break;
           }
           return {
             effects: this.languageConf.reconfigure(newLanguage),
@@ -350,113 +282,6 @@ class Playground {
         autocompletion(),
       ],
       parent: document.querySelector("#viewer"),
-    });
-  }
-
-  initQueryResultsViewer() {
-    return new EditorView({
-      extensions: [
-        basicSetup,
-        githubDark,
-        keymap.of([
-          ...vscodeKeymap,
-          indentWithTab,
-          {
-            key: "Delete",
-            shift: deleteLine,
-          },
-        ]),
-        json(),
-        EditorState.transactionExtender.of((tr) => {
-          if (!tr.docChanged) return null;
-
-          let ext;
-
-          if (this.showingQueryResultsOrArguments === "arguments") {
-            ext = EditorView.editable.of(true);
-          } else {
-            ext = EditorView.editable.of(false);
-          }
-
-          return {
-            effects: this.queryResultViewerIsEditableConf.reconfigure(ext),
-          };
-        }),
-        this.queryResultViewerIsEditableConf.of(EditorView.editable.of(false)),
-        linter(
-          (data) => {
-            if (this.showingQueryResultsOrArguments === "arguments") {
-              try {
-                let parsed = JSON.parse(data.state.doc.toString()); // parse so that if the json is invalid we will not save it because we will have thrown an error instead
-                if (parsed) {
-                  if (typeof parsed === "object") {
-                    if (
-                      Object.entries(parsed).some(
-                        // todo: this only does depth 1, we should do depth n as in: inside arrays
-                        (x) => typeof x[1] === "object" && !Array.isArray(x[1])
-                      )
-                    ) {
-                      return [
-                        {
-                          from: 0,
-                          to: data.state.doc.length,
-                          message:
-                            "This is invalid for query arguments. The arguments will not be saved until there is are no subobjects in the object.",
-                          severity: "error",
-                        },
-                      ];
-                    } else {
-                      setStringToStorage(
-                        STORAGE_KEY_QUERY_ARGUMENTS,
-                        JSON.stringify(
-                          JSON.parse(data.state.doc.toString()), // parse so that if the json is invalid we will not save it because we will have thrown an error instead
-                          null,
-                          2
-                        )
-                      );
-                    }
-                  } else {
-                    return [
-                      {
-                        from: 0,
-                        to: data.state.doc.length,
-                        message:
-                          "This is invalid for query arguments. The arguments will not be saved until there is an object at the top level.",
-                        severity: "error",
-                      },
-                    ];
-                  }
-                } else {
-                  return [
-                    {
-                      from: 0,
-                      to: data.state.doc.length,
-                      message:
-                        "This is invalid for query arguments. The arguments will not be saved until there is an object at the top level.",
-                      severity: "error",
-                    },
-                  ];
-                }
-              } catch {
-                // invalid json in arguments view
-                return [
-                  {
-                    from: 0,
-                    to: data.state.doc.length,
-                    message:
-                      "This is invalid JSON. Will not be saved until it is valid.",
-                    severity: "error",
-                  },
-                ];
-              }
-            }
-            return [];
-          },
-          { delay: 0 }
-        ),
-        lintGutter(),
-      ],
-      parent: document.querySelector("#query-results-viewer"),
     });
   }
 
@@ -481,8 +306,6 @@ class Playground {
       case "ast":
       case "symbol":
         return "json";
-      case "query":
-        return "graphql";
       default:
         return "javascript";
     }
@@ -508,9 +331,6 @@ class Playground {
     this.currentView = view;
 
     document.getElementById("ir-copy").style.display = "none";
-    document.getElementById("query-args-or-outputs").style.display = "none";
-    document.getElementById("query-results-viewer").style.display = "none";
-    // disable #duration and #panel during query view
     document.getElementById("duration").style.display = "inline";
     document.getElementById("panel").style.display = "inline";
     this.runOptions.format = false;
@@ -559,67 +379,9 @@ class Playground {
         this.run();
         text = this.oxc.formattedText;
         break;
-      case "query":
-        document.getElementById("query-args-or-outputs").style.display =
-          "inline";
-        document.getElementById("query-results-viewer").style.display =
-          "inline";
-        document.getElementById("duration").style.display = "none";
-        document.getElementById("panel").style.display = "none";
-        let savedQuery = getStringFromStorage(STORAGE_KEY_QUERY);
-        if (!savedQuery) {
-          text = `
-query {
-  File {
-    import {
-      from_path @output
-
-      specific_import @fold {
-        original_name @output
-      }
-
-      default_import @fold {
-        local_name @output
-      }
-    }
-  }
-}`.trim();
-        } else {
-          text = savedQuery;
-        }
-        break;
     }
 
     this.updateEditorText(this.viewer, text);
-    this.runQuery();
-  }
-
-  changeBetweenQueryResultsAndQueryArgumentsView() {
-    this.showingQueryResultsOrArguments =
-      this.showingQueryResultsOrArguments === "results"
-        ? "arguments"
-        : "results";
-
-    const { classList } = document.getElementById("query-args-or-outputs");
-    switch (this.showingQueryResultsOrArguments) {
-      case "results":
-        this.runQuery();
-        classList.add("query-button-red");
-        classList.remove("query-button-green");
-        break;
-      case "arguments":
-        this.updateEditorText(
-          this.queryResultsViewer,
-          getStringFromStorage(STORAGE_KEY_QUERY_ARGUMENTS) ?? "{}"
-        );
-        classList.add("query-button-green");
-        classList.remove("query-button-red");
-        break;
-      default:
-        throw new Error(
-          `Unknown value for this.showingQueryResultsOrArguments: ${this.showingQueryResultsOrArguments}`
-        );
-    }
   }
 
   updateEditorText(instance, text) {
@@ -854,21 +616,6 @@ async function main() {
     playground.updateView("codegen");
   };
 
-
-  document.getElementById("query").onclick = () => {
-    playground.updateView("query");
-  };
-
-  document.getElementById("query-args-or-outputs").onclick = () => {
-    playground.changeBetweenQueryResultsAndQueryArgumentsView();
-    if (playground.showingQueryResultsOrArguments === "results") {
-      document.getElementById("query-args-or-outputs").innerText =
-        "Show Query Arguments";
-    } else {
-      document.getElementById("query-args-or-outputs").innerText =
-        "Show Query Results";
-    }
-  };
 
   document.getElementById("syntax").onchange = function () {
     const checked = document.getElementById("syntax-checkbox").checked;
