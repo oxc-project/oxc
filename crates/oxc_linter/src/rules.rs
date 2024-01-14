198f0e5d73cf2b66e9e4f5f25a464e610d9c5f10
keita hino <keitahino0415@gmail.com>
2024-01-14 20:34:07 +0900

    feat(linter): eslint-plugin-jsx-a11y aria-activedescendant-has-tabindex (#2012)
    Part of: #1141
    
    Based on:
    - docs: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/aria-activedescendant-has-tabindex.md
    - code: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/src/rules/aria-activedescendant-has-tabindex.js
    - test: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/__tests__/src/rules/aria-activedescendant-has-tabindex-test.js

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -243,6 +243,7 @@ mod jsx_a11y {
     pub mod alt_text;
     pub mod anchor_has_content;
     pub mod anchor_is_valid;
+    pub mod aria_activedescendant_has_tabindex;
     pub mod aria_props;
     pub mod aria_role;
     pub mod aria_unsupported_elements;
@@ -510,6 +511,7 @@ oxc_macros::declare_all_lint_rules! {
     jsx_a11y::alt_text,
     jsx_a11y::anchor_has_content,
     jsx_a11y::anchor_is_valid,
+    jsx_a11y::aria_activedescendant_has_tabindex,
     jsx_a11y::aria_props,
     jsx_a11y::aria_unsupported_elements,
     jsx_a11y::click_events_have_key_events,
