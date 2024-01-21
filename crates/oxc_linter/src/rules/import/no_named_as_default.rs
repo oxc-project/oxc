b6c80f48df3c5874eb913dacc1e4e396ba1ddc04
XantreGodlike <valerik007.vs@gmail.com>
2024-01-21 14:48:28 +0100

    implemented rule

--- crates/oxc_linter/src/rules/import/no_named_as_default.rs
+++ crates/oxc_linter/src/rules/import/no_named_as_default.rs
@@ -1,26 +1,17 @@
-#![allow(clippy::significant_drop_tightening)]
-use std::collections::HashMap;
-
-use dashmap::mapref::one::Ref;
-use oxc_ast::{
-    ast::{BindingPatternKind, Expression, IdentifierReference, MemberExpression},
-    AstKind,
-};
 use oxc_diagnostics::{
     miette::{self, Diagnostic},
     thiserror::Error,
 };
 use oxc_macros::declare_oxc_lint;
-use oxc_semantic::SymbolId;
-use oxc_span::{Atom, Span};
+use oxc_span::Span;
 use oxc_syntax::module_record::ImportImportName;
 
 use crate::{context::LintContext, rule::Rule};
 
 #[derive(Debug, Error, Diagnostic)]
-#[error("eslint-plugin-import(no-named-as-default-member): {1:?} also has a named export {2:?}")]
-#[diagnostic(severity(warning), help("Check if you meant to write `import {{{2:}}} from {3:?}`"))]
-struct NoNamedAsDefaultDiagnostic(#[label] pub Span, String, String, String);
+#[error("eslint-plugin-import(no-named-as-default): Using exported name {1:?} as identifier for default export.")]
+#[diagnostic(severity(warning), help("Use another name for default import to avoid confusion"))]
+struct NoNamedAsDefaultDiagnostic(#[label] pub Span, String);
 
 /// <https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-named-as-default-member.md>
 #[derive(Debug, Default, Clone)]
@@ -29,30 +20,27 @@ pub struct NoNamedAsDefault;
 declare_oxc_lint!(
     /// ### What it does
     ///
-    /// Reports use of an exported name as a property on the default export.
+    /// Reports use of an exported name as the locally imported name of a default export.
     ///
     /// ### Example
     ///
     /// ```javascript
-    /// // ./bar.js
-    /// export function bar() { return null }
-    /// export default () => { return 1 }
-    ///
-    /// // ./foo.js
-    /// import bar from './bar'
-    /// const bar = foo.bar // trying to access named export via default
+    /// // foo.js
+    /// export default 'foo';
+    /// export const bar = 'baz';
+    /// ```
+    /// Valid:
+    /// ```javascript
+    /// import foo from './foo.js';
+    /// ```
+    /// Invalid:
+    /// ```javascript
+    /// // using exported name 'bar' as identifier for default export.
+    /// import bar from './foo.js';
     /// ```
     NoNamedAsDefault,
     nursery
 );
-fn get_symbol_id_from_ident(
-    ctx: &LintContext<'_>,
-    ident: &IdentifierReference,
-) -> Option<SymbolId> {
-    let reference_id = ident.reference_id.get().unwrap();
-    let reference = &ctx.symbols().references[reference_id];
-    reference.symbol_id()
-}
 
 impl Rule for NoNamedAsDefault {
     fn run_once(&self, ctx: &LintContext<'_>) {
@@ -61,13 +49,22 @@ impl Rule for NoNamedAsDefault {
             let ImportImportName::Default(import_span) = import_entry.import_name else {
                 continue;
             };
+            let Some(import_name) = ctx
+                .symbols()
+                .get_symbol_id_from_span(&import_span)
+                .map(|it| ctx.symbols().get_name(it))
+            else {
+                continue;
+            };
 
             let specifier = import_entry.module_request.name();
             let Some(remote_module_record_ref) = module_record.loaded_modules.get(specifier) else {
                 continue;
             };
 
-            if !remote_module_record_ref.exported_bindings.is_empty() {}
+            if remote_module_record_ref.exported_bindings.contains_key(import_name) {
+                ctx.diagnostic(NoNamedAsDefaultDiagnostic(import_span, import_name.to_string()))
+            }
         }
     }
 }
@@ -77,28 +74,22 @@ fn test() {
     use crate::tester::Tester;
 
     let pass = vec![
-        r#"import baz, {a} from "./named-exports""#,
-        r#"import baz from "./named-exports"; const jjj = bar.jjj"#,
-        r#"import {a} from "./named-exports"; const baz = a.baz"#,
-        r#"import baz from "./default_export_default_property"; const d = baz.default;"#,
-        r#"import baz, {foo} from "./named-and-default-export"; const d = baz.default;"#,
-        r"import baz from './named-exports';
-        {
-            const baz = {};
-            const a = baz.a;
-        }",
+        r#"import "./malformed.js""#,
+        r#"import bar, { foo } from "./bar";"#,
+        r#"import bar, { foo } from "./empty-folder";"#,
+        // TODO: parser error
+        // r#"export default from "./bar";"#,
+        r#"import bar, { foo } from "./export-default-string-and-named""#,
     ];
 
     let fail = vec![
-        r#"import baz from "./named-exports"; const a = baz.a;"#,
-        r#"import baz from "./named-exports"; const a = baz["a"];"#,
-        r#"import baz from "./named-exports"; baz.a();"#,
-        r"import baz from './named-exports';
-        {
-            const a = baz.a;
-        }",
-        r#"import baz, { bar } from "./named-exports"; const {a} = baz"#,
-        r#"import baz from "./named-and-default-export"; const {foo: _foo} = baz"#,
+        r#"import foo from "./bar";"#,
+        r#"import foo, { foo as bar } from "./bar";"#,
+        // TODO: parser error
+        // r#"export default, { foo as bar } from "./bar";"#,
+        // r#"import foo from "./malformed.js""#,
+        r#"import foo from "./export-default-string-and-named""#,
+        r#"import foo, { foo as bar } from "./export-default-string-and-named""#,
     ];
 
     Tester::new(NoNamedAsDefault::NAME, pass, fail)
