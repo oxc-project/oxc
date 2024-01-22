f74a99d16c52cc640b95f05588eeee62f6192233
Yuji Sugiura <y.sugiura.0316@gmail.com>
2024-01-22 09:44:29 +0900

    Update task to generate whole markdown comment

--- tasks/lint_rules/src/main.rs
+++ tasks/lint_rules/src/main.rs
@@ -1,21 +1,39 @@
 mod generate_list;
 mod update_comment;
 
+const HELP: &str = r"
+Usage:
+  $ cargo run <plugin_name> [--update]
+
+Arguments:
+  - plugin_name: Name of the target plugin
+
+Options:
+  - --update: Update GitHub issue comment
+  - -h, --help: Show this message
+";
+
 fn main() {
     let mut args = std::env::args();
     args.next();
 
-    let plugin_name = args.next().expect("Plugin name");
-    let is_update = args.any(|arg| arg == "--update");
-
-    let list = generate_list::run(&plugin_name).unwrap();
-
-    if !is_update {
-        println!("{list}");
+    if args.any(|arg| arg == "--help" || arg == "-h") {
+        println!("{HELP}");
         return;
     }
 
-    let url = update_comment::run(&plugin_name, &list).unwrap();
+    let plugin_name =
+        args.next().unwrap_or_else(|| panic!("🆖 Target plugin_name is required!\n{HELP}"));
+    let is_update = args.any(|arg| arg == "--update");
+
+    let markdown = generate_list::run(&plugin_name).unwrap();
+
+    if !is_update {
+        println!("{markdown}");
+        return;
+    }
+
+    let url = update_comment::run(&plugin_name, &markdown).unwrap();
     println!("✨ Done! Status for {plugin_name} is updated!");
     println!("See {url}");
 }
