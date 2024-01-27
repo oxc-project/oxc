0c6a8703898c2d96d7f3bd2458fc540df3f5eabc
Yuji Sugiura <y.sugiura.0316@gmail.com>
2024-01-27 20:10:40 +0900

    Wip counters

--- tasks/lint_rules/src/markdown-renderer.cjs
+++ tasks/lint_rules/src/markdown-renderer.cjs
@@ -57,32 +57,50 @@ ${views
 exports.renderMarkdown = (pluginName, pluginMeta, ruleEntries) => {
   /** @type {RuleEntryView[][]} */
   const [deprecated, recommended, others] = [[], [], []];
+  const counters = { deprecated: [0, 0], recommended: [0, 0], others: [0, 0] };
   for (const [name, entry] of ruleEntries) {
     if (!name.startsWith(`${pluginName}/`)) continue;
 
     const view = { name, ...entry };
+    const isMarked = entry.isImplemented || entry.isNotSupported;
 
     if (entry.isDeprecated) {
       deprecated.push(view);
+      if (isMarked) counters.deprecated[0] += 1;
       continue;
     }
+
     if (entry.isRecommended) {
       recommended.push(view);
+      if (isMarked) counters.recommended[0] += 1;
       continue;
     }
     others.push(view);
+    if (isMarked) counters.others[0] += 1;
   }
+
+  counters.deprecated[1] = deprecated.length;
+  counters.recommended[1] = recommended.length;
+  counters.others[1] = others.length;
+
+  // TODO: How to display counters?
 
   return [
     renderWarning(),
     renderHeader({ npm: pluginMeta.npm }),
     0 < recommended.length &&
-      renderRulesList({ title: "Recommended rules", views: recommended }),
+      renderRulesList({
+        title: "Recommended",
+        views: recommended,
+      }),
     0 < others.length &&
-      renderRulesList({ title: "Not recommended rules", views: others }),
+      renderRulesList({
+        title: "Not recommended",
+        views: others,
+      }),
     0 < deprecated.length &&
       renderRulesList({
-        title: "Deprecated rules",
+        title: "Deprecated",
         views: deprecated,
         defaultOpen: false,
       }),
