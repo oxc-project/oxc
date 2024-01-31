3d79d77b409c22669687f4007b891c78b78ff51b
overlookmotel <theoverlookmotel@gmail.com>
2024-01-31 03:43:53 +0000

    refactor(parser): split lexer into multiple files (#2228)
    This PR has a large diff, but it contains no substantive changes
    whatsoever. It purely breaks up the lexer into multiple smaller files.
    
    I've been working quite intensively on the lexer over past few weeks,
    but still have been finding it hard to make sense of, due to most of the
    logic currently being contained in [a single 1800-line
    file](https://github.com/oxc-project/oxc/blob/018675ceb1a7e3442d1aeaa69e0256964675d207/crates/oxc_parser/src/lexer/mod.rs).
    
    I feel that breaking it up into multiple files makes it much easier to
    navigate and understand.
    
    An additional benefit is that many functions can have their visibility
    reduced to module scope, so sub-systems for e.g. lexing numbers have
    fewer exposed functions. This makes it clearer what the entry points
    are, and makes it harder to make mistakes when working on the lexer.
    
    I intend to later make changes to the lexer for performance which will
    introduce unsafe code. Keeping that unsafe code encapsulated in modules
    will make it more viable to validate the workings of that code, and
    avoid accidental UB.
    
    There is one downside to this change. Previously
    [`lexer/mod.rs`](https://github.com/oxc-project/oxc/blob/018675ceb1a7e3442d1aeaa69e0256964675d207/crates/oxc_parser/src/lexer/mod.rs)
    was laid out in same order as the JS spec. If you were trying to
    validate the lexer against the spec, this would make it easier. However,
    as OXC's parser is fairly mature at this point, and I imagine most
    spec-compliance issues have been flushed out by now, in my opinion this
    advantage is less compelling than it probably used to be. So in my view
    it's outweighed by the benefit of more readable code.
    
    Reviewing this could be a bit of a battle due to the size of the diff. I
    do have further changes I'd like to make, but I've intentionally kept
    this PR as 100% just:
    
    1. Moving code around.
    2. Reducing visibility of functions to module/super scope where that's
    possible to do without changing anything else.
    
    Aside from that, not even a single comment has changed.
    
    If you're willing to trust me on that promise, I think it can be merged
    without poring through it line by line.

---
+++ crates/oxc_parser/src/lexer/identifier.rs
@@ -0,0 +1,66 @@
+use super::{AutoCow, Kind, Lexer, Span};
+use crate::diagnostics;
+
+use oxc_syntax::identifier::{is_identifier_part, is_identifier_start};
+
+impl<'a> Lexer<'a> {
+    /// Section 12.7.1 Identifier Names
+    pub(super) fn identifier_name_handler(&mut self) -> &'a str {
+        let builder = AutoCow::new(self);
+        self.consume_char();
+        self.identifier_name(builder)
+    }
+
+    pub(super) fn identifier_name(&mut self, builder: AutoCow<'a>) -> &'a str {
+        self.identifier_tail(builder)
+    }
+
+    pub(super) fn private_identifier(&mut self) -> Kind {
+        let mut builder = AutoCow::new(self);
+        let start = self.offset();
+        match self.current.chars.next() {
+            Some(c) if is_identifier_start(c) => {
+                builder.push_matching(c);
+            }
+            Some('\\') => {
+                builder.force_allocation_without_current_ascii_char(self);
+                self.identifier_unicode_escape_sequence(&mut builder, true);
+            }
+            Some(c) => {
+                #[allow(clippy::cast_possible_truncation)]
+                self.error(diagnostics::InvalidCharacter(
+                    c,
+                    Span::new(start, start + c.len_utf8() as u32),
+                ));
+                return Kind::Undetermined;
+            }
+            None => {
+                self.error(diagnostics::UnexpectedEnd(Span::new(start, start)));
+                return Kind::Undetermined;
+            }
+        }
+        self.identifier_tail(builder);
+        Kind::PrivateIdentifier
+    }
+
+    fn identifier_tail(&mut self, mut builder: AutoCow<'a>) -> &'a str {
+        // ident tail
+        while let Some(c) = self.peek() {
+            if !is_identifier_part(c) {
+                if c == '\\' {
+                    self.current.chars.next();
+                    builder.force_allocation_without_current_ascii_char(self);
+                    self.identifier_unicode_escape_sequence(&mut builder, false);
+                    continue;
+                }
+                break;
+            }
+            self.current.chars.next();
+            builder.push_matching(c);
+        }
+        let has_escape = builder.has_escape();
+        let text = builder.finish(self);
+        self.save_string(has_escape, text);
+        text
+    }
+}
---
+++ crates/oxc_parser/src/lexer/regex.rs
@@ -0,0 +1,78 @@
+use super::{Kind, Lexer, RegExpFlags, Token};
+use crate::diagnostics;
+
+use oxc_syntax::identifier::is_line_terminator;
+
+impl<'a> Lexer<'a> {
+    /// Re-tokenize the current `/` or `/=` and return `RegExp`
+    /// See Section 12:
+    ///   The `InputElementRegExp` goal symbol is used in all syntactic grammar contexts
+    ///   where a `RegularExpressionLiteral` is permitted
+    /// Which means the parser needs to re-tokenize on `PrimaryExpression`,
+    /// `RegularExpressionLiteral` only appear on the right hand side of `PrimaryExpression`
+    pub(crate) fn next_regex(&mut self, kind: Kind) -> (Token, u32, RegExpFlags) {
+        self.current.token.start = self.offset()
+            - match kind {
+                Kind::Slash => 1,
+                Kind::SlashEq => 2,
+                _ => unreachable!(),
+            };
+        let (pattern_end, flags) = self.read_regex();
+        self.lookahead.clear();
+        let token = self.finish_next(Kind::RegExp);
+        (token, pattern_end, flags)
+    }
+
+    /// 12.9.5 Regular Expression Literals
+    fn read_regex(&mut self) -> (u32, RegExpFlags) {
+        let mut in_escape = false;
+        let mut in_character_class = false;
+        loop {
+            match self.current.chars.next() {
+                None => {
+                    self.error(diagnostics::UnterminatedRegExp(self.unterminated_range()));
+                    return (self.offset(), RegExpFlags::empty());
+                }
+                Some(c) if is_line_terminator(c) => {
+                    self.error(diagnostics::UnterminatedRegExp(self.unterminated_range()));
+                    #[allow(clippy::cast_possible_truncation)]
+                    let pattern_end = self.offset() - c.len_utf8() as u32;
+                    return (pattern_end, RegExpFlags::empty());
+                }
+                Some(c) => {
+                    if in_escape {
+                        in_escape = false;
+                    } else if c == '/' && !in_character_class {
+                        break;
+                    } else if c == '[' {
+                        in_character_class = true;
+                    } else if c == '\\' {
+                        in_escape = true;
+                    } else if c == ']' {
+                        in_character_class = false;
+                    }
+                }
+            }
+        }
+
+        let pattern_end = self.offset() - 1; // -1 to exclude `/`
+        let mut flags = RegExpFlags::empty();
+
+        while let Some(ch @ ('$' | '_' | 'a'..='z' | 'A'..='Z' | '0'..='9')) = self.peek() {
+            self.current.chars.next();
+            let flag = if let Ok(flag) = RegExpFlags::try_from(ch) {
+                flag
+            } else {
+                self.error(diagnostics::RegExpFlag(ch, self.current_offset()));
+                continue;
+            };
+            if flags.contains(flag) {
+                self.error(diagnostics::RegExpFlagTwice(ch, self.current_offset()));
+                continue;
+            }
+            flags |= flag;
+        }
+
+        (pattern_end, flags)
+    }
+}
