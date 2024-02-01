73ccf8a4da0420f64de2741552652ffe4b3253e2
Tzvi Melamed <tzvi@melamed.biz>
2024-02-01 07:46:38 -0500

    fix(oxc_semantic): proper traversal of try statements (#2250)
    Closes #2227

--- crates/oxc_semantic/src/pg.rs
+++ crates/oxc_semantic/src/pg.rs
@@ -1,8 +1,4 @@
-use std::collections::{HashMap, HashSet};
-
 use petgraph::{stable_graph::NodeIndex, visit::EdgeRef, Direction, Graph};
-
-use crate::control_flow::ControlFlowGraph;
 
 /// # Panics
 pub fn neighbors_filtered_by_edge_weight<
@@ -57,66 +53,4 @@ where
     }
 
     final_states
-}
-
-/// # Panics
-pub fn replicate_tree_to_leaves(
-    start_at: NodeIndex,
-    cfg: &mut ControlFlowGraph,
-) -> HashMap<NodeIndex, NodeIndex> {
-    fn duplicate_graph_node(basic_block_id: NodeIndex, cfg: &mut ControlFlowGraph) -> NodeIndex {
-        let new_basic_block_graph_ix = cfg.new_basic_block();
-
-        // todo: what's a better way to do this?
-        let items = cfg.basic_block_by_index(basic_block_id).clone();
-        for item in items {
-            cfg.current_basic_block().push(item);
-        }
-
-        // todo: should we add the functions copied here to the function_to_node_ix?
-
-        new_basic_block_graph_ix
-    }
-
-    let preserved_graph_ix = cfg.current_node_ix;
-
-    let mut old_to_new: HashMap<NodeIndex, NodeIndex> = HashMap::default();
-    let mut q = vec![];
-
-    let new_graph_start_ix = duplicate_graph_node(start_at, cfg);
-    old_to_new.insert(start_at, new_graph_start_ix);
-    q.push((new_graph_start_ix, start_at));
-    let mut edges_finished_already = HashSet::new();
-
-    while let Some(node) = q.pop() {
-        // todo: are there any incoming edges we won't cover?
-        let edges = cfg
-            .graph
-            .edges_directed(node.1, Direction::Outgoing)
-            .map(|x| (x.target(), x.weight().to_owned(), x.id()))
-            .collect::<Vec<_>>();
-
-        for (to, weight, id) in edges {
-            if edges_finished_already.contains(&id) {
-                continue;
-            }
-
-            edges_finished_already.insert(id);
-
-            let new_graph_ix = if old_to_new.contains_key(&to) {
-                old_to_new[&to]
-            } else {
-                let new_graph_ix = duplicate_graph_node(to, cfg);
-                old_to_new.insert(node.1, new_graph_start_ix);
-                new_graph_ix
-            };
-
-            cfg.add_edge(node.0, new_graph_ix, weight);
-
-            q.push((new_graph_ix, to));
-        }
-    }
-
-    cfg.current_node_ix = preserved_graph_ix;
-    old_to_new
 }
