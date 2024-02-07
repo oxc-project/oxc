669a36b84de3e40869b13e7fe48fe47428979a60
john xu <dyxushuai@gmail.com>
2024-02-07 20:06:42 +0800

    Refactor SIMD string literal lookup

--- crates/oxc_parser/src/lexer/simd/mod.rs
+++ crates/oxc_parser/src/lexer/simd/mod.rs
@@ -1,15 +1,12 @@
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "avx2"))]
 mod avx2;
 #[cfg(target_arch = "aarch64")]
 mod neon;
+#[cfg(all(not(target_feature = "avx2"), not(target_arch = "aarch64")))]
 mod swar;
 
 use crate::lexer::source::Source;
 use once_cell::sync::Lazy;
-
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-static AVX2_STRING_LITERAL_LOOKUP_TABLE: Lazy<avx2::LookupTable> =
-    Lazy::new(|| avx2::LookupTable::new(&[b'\r', b'\n', b'"', b'\'', b'\\']));
 
 pub(crate) struct Position {
     // the offset of the first found delimiter
@@ -18,15 +15,38 @@ pub(crate) struct Position {
     pub(crate) alignment: usize,
 }
 
+#[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "avx2"))]
+static AVX2_STRING_LITERAL_LOOKUP_TABLE: Lazy<avx2::LookupTable> =
+    Lazy::new(|| avx2::LookupTable::new(&[b'\r', b'\n', b'"', b'\'', b'\\']));
+
+#[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "avx2"))]
 pub(crate) fn string_literal_lookup(source: &Source) -> Position {
-    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-    {
-        if cfg!(target_feature = "avx2") {
-            return Position {
-                offset: AVX2_STRING_LITERAL_LOOKUP_TABLE.match_vectored(source),
-                alignment: avx2::ALIGNMENT,
-            };
-        }
+    Position {
+        offset: AVX2_STRING_LITERAL_LOOKUP_TABLE.match_vectored(source),
+        alignment: avx2::ALIGNMENT,
     }
-    todo!()
+}
+
+#[cfg(target_arch = "aarch64")]
+static NEON_STRING_LITERAL_LOOKUP_TABLE: Lazy<swar::LookupTable> =
+    Lazy::new(|| swar::LookupTable::new(&[b'\r', b'\n', b'"', b'\'', b'\\']));
+
+#[cfg(target_arch = "aarch64")]
+pub(crate) fn string_literal_lookup(source: &Source) -> Position {
+    Position {
+        offset: NEON_STRING_LITERAL_LOOKUP_TABLE.match_vectored(source),
+        alignment: swar::ALIGNMENT,
+    }
+}
+
+#[cfg(all(not(target_feature = "avx2"), not(target_arch = "aarch64")))]
+static SWAR_STRING_LITERAL_LOOKUP_TABLE: Lazy<swar::LookupTable> =
+    Lazy::new(|| swar::LookupTable::new(&[b'\r', b'\n', b'"', b'\'', b'\\']));
+
+#[cfg(all(not(target_feature = "avx2"), not(target_arch = "aarch64")))]
+pub(crate) fn string_literal_lookup(source: &Source) -> Position {
+    Position {
+        offset: SWAR_STRING_LITERAL_LOOKUP_TABLE.match_vectored(source),
+        alignment: swar::ALIGNMENT,
+    }
 }
---
+++ crates/oxc_parser/src/lexer/simd/neon.rs
@@ -0,0 +1,16 @@
+use crate::lexer::source::Source;
+
+pub(crate) const ALIGNMENT: usize = 16;
+
+pub struct LookupTable {}
+
+impl LookupTable {
+    pub fn new(delimiters: &[u8]) -> Self {
+        todo!()
+    }
+
+    #[inline]
+    pub fn match_vectored(&self, source: &Source) -> usize {
+        todo!()
+    }
+}
