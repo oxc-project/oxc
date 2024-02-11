8aa2ab086b45ce85eafa79c4403e58212a97eb1b
john xu <dyxushuai@gmail.com>
2024-02-11 21:28:42 +0800

    Refactor SIMD delimiter matching

--- crates/oxc_parser/src/lexer/simd/avx2.rs
+++ crates/oxc_parser/src/lexer/simd/avx2.rs
@@ -3,6 +3,7 @@ use core::arch::x86::*;
 #[cfg(target_arch = "x86_64")]
 use core::arch::x86_64::*;
 
+use super::tabulate;
 use crate::lexer::source::Source;
 
 pub(crate) const ALIGNMENT: usize = 32;
@@ -20,6 +21,7 @@ impl<const N: usize> LookupTable<N> {
         // delimiters must be an ASCII character and checked by `tabulate`
         unsafe {
             let table = tabulate(delimiters);
+            let table = _mm256_lddqu_si256(table.as_ptr().cast());
             #[rustfmt::skip]
             let arf = _mm256_setr_epi8(
                 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80,
@@ -39,7 +41,7 @@ impl<const N: usize> LookupTable<N> {
             let ptr = seg.as_ptr();
             // SAFETY:
             // seg is aligned and has ALIGNMENT bytes
-            (unsafe { self.match_delimiters_32_avx(ptr) }, actual_len)
+            (unsafe { self.match_delimiters(ptr) }, actual_len)
         } else {
             (None, 0)
         }
@@ -48,7 +50,7 @@ impl<const N: usize> LookupTable<N> {
     // match 32 bytes at a time, return the position of the first found delimiter
     #[inline]
     #[allow(overflowing_literals, clippy::cast_sign_loss)]
-    unsafe fn match_delimiters_32_avx(&self, ptr: *const u8) -> Option<usize> {
+    unsafe fn match_delimiters(&self, ptr: *const u8) -> Option<usize> {
         let data = _mm256_lddqu_si256(ptr.cast());
         let rbms = _mm256_shuffle_epi8(self.table, data);
         let cols = _mm256_and_si256(self.lsh, _mm256_srli_epi16(data, 4));
@@ -63,49 +65,4 @@ impl<const N: usize> LookupTable<N> {
             Some(unmatched)
         }
     }
-}
-
-// Create an ascii table with given delimiters, only fill
-// in the given delimiters with 1, leave other positions as 0.
-// then return a vector with each columns
-// For example:
-// delimiter = '\r'
-// table[0][13] = 1
-// the 13th element of returned vector is 1 0 0 0 0 0 0 0,
-// due to the use of little-endian layout in AVX2, so the result
-// is 0x01
-//
-//     0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15
-//
-//  0  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-//                                   \n       \r
-//  1  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-//  2  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-//     \w !  "  #  $  %  &  '  (  )  *  +  ,  -  .  /
-//  3  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-//     0  1  2  3  4  5  6  7  8  9  :  ;  <  =  >  ?
-//  4  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-//     @  A  B  C  D  E  F  G  H  I  J  K  L  M  N  O
-//  5  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-//     P  Q  R  S  T  U  V  W  X  Y  Z  [  \  ]  ^  _
-//  6  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-//     `  a  b  c  d  e  f  g  h  i  j  k  l  m  n  o
-//  7  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-//     p  q  r  s  t  u  v  w  x  y  z  {  |  }  ~  del
-#[inline]
-#[allow(clippy::cast_possible_truncation)]
-unsafe fn tabulate<const N: usize>(delimiters: [u8; N]) -> [i8; 16] {
-    let mut table = [false; 128];
-    for delimiter in delimiters {
-        debug_assert!(delimiter < 128, "delimiter must be an ASCII character");
-        table[delimiter as usize] = true;
-    }
-
-    let [e00, e01, e02, e03, e04, e05, e06, e07, e08, e09, e10, e11, e12, e13, e14, e15] = [0; 16]
-        .map(|i| (0..8).rev().fold(0, |acc, j| (acc << 1) | u32::from(table[i * 8 + j])) as i8);
-    #[rustfmt::skip]
-    _mm256_setr_epi8(
-        e00, e01, e02, e03, e04, e05, e06, e07, e08, e09, e10, e11, e12, e13, e14, e15,
-        e00, e01, e02, e03, e04, e05, e06, e07, e08, e09, e10, e11, e12, e13, e14, e15,
-    )
 }
--- crates/oxc_parser/src/lexer/simd/mod.rs
+++ crates/oxc_parser/src/lexer/simd/mod.rs
@@ -48,3 +48,42 @@ pub(crate) fn string_literal_lookup(source: &Source) -> Position {
     let (offset, actual_len) = SWAR_STRING_LITERAL_LOOKUP_TABLE.match_vectored(source);
     Position { offset, actual_len, alignment: swar::ALIGNMENT }
 }
+
+// Create an ascii table with given delimiters, only fill
+// in the given delimiters with 1, leave other positions as 0.
+// then return a vector with each columns
+// For example:
+// delimiter = '\r'
+// table[0][13] = 1
+// the 13th element of returned vector is 1 0 0 0 0 0 0 0,
+// due to the use of little-endian layout in AVX2 and ARM, so the result
+// is 0x01
+//
+//     0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15
+//
+//  0  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//                                   \n       \r
+//  1  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//  2  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//     \w !  "  #  $  %  &  '  (  )  *  +  ,  -  .  /
+//  3  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//     0  1  2  3  4  5  6  7  8  9  :  ;  <  =  >  ?
+//  4  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//     @  A  B  C  D  E  F  G  H  I  J  K  L  M  N  O
+//  5  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//     P  Q  R  S  T  U  V  W  X  Y  Z  [  \  ]  ^  _
+//  6  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//     `  a  b  c  d  e  f  g  h  i  j  k  l  m  n  o
+//  7  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//     p  q  r  s  t  u  v  w  x  y  z  {  |  }  ~  del
+#[inline]
+#[allow(clippy::cast_possible_truncation)]
+fn tabulate<const N: usize>(delimiters: [u8; N]) -> [u8; 16] {
+    let mut table = [0u8; 16];
+    for d in delimiters {
+        debug_assert!(d < 128, "delimiter must be an ASCII character");
+        let (col, row) = (d & 0x0F, d >> 4);
+        table[col as usize] |= 1 << row;
+    }
+    table
+}
