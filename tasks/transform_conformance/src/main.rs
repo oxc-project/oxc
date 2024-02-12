f6fe1bf97873e5d1ddeb9dc4e8786f848794d34b
Boshen <boshenc@gmail.com>
2024-02-12 14:02:31 +0800

    refactor: remove global allocator from non-user facing apps
    The runtime performance gains does not out weight the compilation speed from
    building the custom allocators, which takes about a minute to build on
    slower machines.

--- tasks/transform_conformance/src/main.rs
+++ tasks/transform_conformance/src/main.rs
@@ -1,13 +1,5 @@
 mod ts_fixtures;
 
-#[cfg(not(target_env = "msvc"))]
-#[global_allocator]
-static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
-
-#[cfg(target_os = "windows")]
-#[global_allocator]
-static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
-
 use oxc_transform_conformance::{TestRunner, TestRunnerOptions};
 use pico_args::Arguments;
 use ts_fixtures::TypeScriptFixtures;
