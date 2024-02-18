dd5eaeb2f1af1a8d6856fb6391875f30593f15fb
Alex Yip <tsztoyip@gmail.com>
2024-02-18 23:36:42 +0000

    Add documentation

--- crates/oxc_linter/src/rules/typescript/prefer_ts_expect_error.rs
+++ crates/oxc_linter/src/rules/typescript/prefer_ts_expect_error.rs
@@ -9,7 +9,7 @@ use crate::{context::LintContext, rule::Rule, AstNode};
 
 #[derive(Debug, Error, Diagnostic)]
 #[error("typescript-eslint(prefer-ts-expect-error):")]
-#[diagnostic(severity(warning), help(""))]
+#[diagnostic(severity(warning), help("Use " @ ts - expect - error" to ensure an error is actually being suppressed."))]
 struct PreferTsExpectErrorDiagnostic(#[label] pub Span);
 
 #[derive(Debug, Default, Clone)]
@@ -18,12 +18,25 @@ pub struct PreferTsExpectError;
 declare_oxc_lint!(
     /// ### What it does
     ///
+    /// Enforce using @ts-expect-error over @ts-ignore.
     ///
     /// ### Why is this bad?
+    /// TypeScript allows you to suppress all errors on a line by placing a comment starting with @ts-ignore or @ts-expect-error immediately before the erroring line.
+    /// The two directives work the same, except @ts-expect-error causes a type error if placed before a line that's not erroring in the first place.
     ///
+    /// This means it's easy for @ts-ignores to be forgotten about, and remain in code even after the error they were suppressing is fixed.
+    /// This is dangerous, as if a new error arises on that line it'll be suppressed by the forgotten about @ts-ignore, and so be missed.
     ///
     /// ### Example
     /// ```javascript
+    /// // @ts-ignore
+    /// const str: string = 1;
+    ///
+    /// /**
+    /// * Explaining comment
+    /// *
+    /// * @ts-ignore */
+    /// const multiLine: number = 'value';
     /// ```
     PreferTsExpectError,
     correctness
