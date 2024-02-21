35608c8eb1724a3a99fbd4b08cc34109df0a5ce1
Boshen <boshenc@gmail.com>
2024-02-21 18:06:37 +0800

    chore: fix all docs

--- crates/oxc_transformer/src/react_jsx/options.rs
+++ crates/oxc_transformer/src/react_jsx/options.rs
@@ -28,11 +28,11 @@ pub struct ReactJsxOptions {
 
     /// When spreading props, use Object.assign directly instead of Babel's extend helper.
     /// Use `Some<T>` instead of `bool` because we want to know if user set this field explicitly,
-    /// which used for creating warning, https://github.com/oxc-project/oxc/blob/c3e2098c04d8916cb812bdd16d2026bb430ac25f/crates/oxc_transformer/src/react_jsx/mod.rs#L111-L114
+    /// which used for creating warning, <https://github.com/oxc-project/oxc/blob/c3e2098c04d8916cb812bdd16d2026bb430ac25f/crates/oxc_transformer/src/react_jsx/mod.rs#L111-L114>
     pub use_built_ins: Option<bool>,
     /// When spreading props, use inline object with spread elements directly instead of Babel's extend helper or Object.assign.
     /// Use `Some<T>` instead of `bool` because we want to know if user set this field explicitly,
-    /// which used for creating warning, https://github.com/oxc-project/oxc/blob/c3e2098c04d8916cb812bdd16d2026bb430ac25f/crates/oxc_transformer/src/react_jsx/mod.rs#L111-L114
+    /// which used for creating warning, <https://github.com/oxc-project/oxc/blob/c3e2098c04d8916cb812bdd16d2026bb430ac25f/crates/oxc_transformer/src/react_jsx/mod.rs#L111-L114>
     pub use_spread: Option<bool>,
 }
 
