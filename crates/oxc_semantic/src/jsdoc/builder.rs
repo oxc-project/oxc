b548e39d94526ba68b0e5035b2f0ae00cae85339
Yuji Sugiura <y.sugiura.0316@gmail.com>
2024-02-24 13:39:01 +0900

    Wip fix up

--- crates/oxc_semantic/src/jsdoc/builder.rs
+++ crates/oxc_semantic/src/jsdoc/builder.rs
@@ -25,13 +25,36 @@ impl<'a> JSDocBuilder<'a> {
     }
 
     pub fn build(self) -> JSDoc<'a> {
+        // TODO: Correct JSDoc for EOF
         JSDoc::new(self.docs)
     }
 
-    pub fn retrieve_jsdoc_comments(&mut self, kind: &AstKind<'a>) -> bool {
-        // We may need to expand this kinds for our usecases
-        // e.g. TypeScript's `canHaveJSDoc()` function defines so many kinds for their usecases
-        // https://github.com/microsoft/TypeScript/blob/d04e3489b0d8e6bc9a8a9396a633632a5a467328/src/compiler/utilities.ts#L4195
+    // この紐づけ処理は、Semanticのビルドにあわせて行われる。
+    // つまり、各ユースケース（たとえばoxlintにおける各ルールの実行）よりも、"前"のタイミング。
+    //
+    // もしルール側の設定などで、動的にこのロジックを変えたい場合、
+    // - そもそもここで事前に紐づけるのを諦め、各ルール側で毎回探すようにするか
+    // - ここでより広く緩く（パフォーマンス影響があるため慎重に）紐づけておき、あとから絞り込めるようにする
+    // このいずれかが必要になる。
+    //
+    // そして、「どのノードに対して、JSDocをアタッチするか？」について、特定のSpecがあるわけではない。
+    // それぞれの実装が、それぞれのユースケースにあわせて決めている。
+    // たとえば、TypeScriptの場合、その対象はかなり広く定義されている。
+    // > https://github.com/microsoft/TypeScript/blob/d04e3489b0d8e6bc9a8a9396a633632a5a467328/src/compiler/utilities.ts#L4195
+    // `eslint-plugin-jsdoc`の場合、その対象はルールの設定で自由に変えることができる。（デフォルトは関数に関するもののみ）
+    // > https://github.com/gajus/eslint-plugin-jsdoc/blob/e948bee821e964a92fbabc01574eca226e9e1252/src/iterateJsdoc.js#L2517-L2536
+    //
+    // また、そのノードに対して「どのようにJSDocをアタッチするか？」についても、同様に実装依存である。
+    // TypeScriptの場合（特定のASTノード、および`endOfFileToken`が、`jsDoc`プロパティを保持）、複数の`JSDocComment`がアタッチされる。
+    // `eslint-plugin-jsdoc`の場合、`jsdoccomment`というライブラリが、一部の例外処理と共に、単一のコメントのみを取得している。
+    // > https://github.com/es-joy/jsdoccomment/blob/6aae5ea306015096e3d58cd22257e5222c54e3b4/src/jsdoccomment.js#L283
+    //
+    // OXC Semanticとして、どう振る舞うべきかは悩ましいが、現状の実装としては、
+    // - TypeScriptライクな直感的なアタッチ
+    // - 用途に応じて、単一のJSDocを取得するか、すべてのJSDocを取得するか選ぶ
+    // というハイブリッドな形になっている。
+    pub fn retrieve_attached_jsdoc(&mut self, kind: &AstKind<'a>) -> bool {
+        // This is not enough compare to TypeScript's `canHaveJSDoc()`, should expand if needed
         if !(kind.is_statement()
             || kind.is_declaration()
             || matches!(kind, AstKind::ParenthesizedExpression(_)))
@@ -49,7 +72,7 @@ impl<'a> JSDocBuilder<'a> {
             self.leading_comments_seen.insert(*start);
         }
 
-        // 2. Parse JSDoc comments only
+        // 2. Filter and parse JSDoc comments only
         let leading_jsdoc_comments = leading_comments
             .iter()
             .filter(|(_, comment)| comment.is_multi_line())
@@ -64,8 +87,7 @@ impl<'a> JSDocBuilder<'a> {
                 Some(comment_content)
             })
             .map(|comment_content| {
-                // Remove the very first `*`?
-                // Remove the first `*` and whitespaces in each line?
+                // Shold remove the very first `*`?
                 JSDocComment::new(comment_content)
             })
             .collect::<Vec<_>>();
@@ -111,7 +133,7 @@ mod test {
         source_type: Option<SourceType>,
     ) -> Option<Vec<JSDocComment<'a>>> {
         let semantic = build_semantic(allocator, source_text, source_type);
-        let start = source_text.find(symbol).unwrap() as u32;
+        let start = source_text.find(symbol).unwrap_or(0) as u32;
         let span = Span::new(start, start + symbol.len() as u32);
         semantic.jsdoc().get_by_span(span)
     }
@@ -135,56 +157,80 @@ mod test {
     #[test]
     fn not_found() {
         let source_texts = [
-            "function foo() {}",
-            "// test
-            function foo() {}",
-            "/* test */function foo() {}",
-            "/** test */ ; function foo() {}",
-            "/** test */ function foo1() {} function foo() {}",
-            "function foo() {} /** test */",
+            ("function foo() {}", "function foo() {}"),
+            ("// test", "function foo() {}"),
+            ("function foo() {}", "function foo() {}"),
+            ("/* test */function foo() {}", "function foo() {}"),
+            ("/** test */ ; function foo() {}", "function foo() {}"),
+            ("/** test */ function foo1() {} function foo() {}", "function foo() {}"),
+            ("function foo() {} /** test */", "function foo() {}"),
         ];
-        for source_text in source_texts {
-            test_jsdoc_not_found(source_text, "function foo() {}");
+        for (source_text, target) in source_texts {
+            test_jsdoc_not_found(source_text, target);
         }
     }
 
     #[test]
     fn found() {
         let source_texts = [
-            "/** test */function foo() {}",
-            "/*** test */function foo() {}",
-            "
+            ("/** test */function foo() {}", "function foo() {}"),
+            ("/*** test */function foo() {}", "function foo() {}"),
+            (
+                "
             /** test */
         function foo() {}",
-            "/** test */
+                "function foo() {}",
+            ),
+            (
+                "/** test */
                 function foo() {}",
-            "/**
+                "function foo() {}",
+            ),
+            (
+                "/**
              * test
              * */
             function foo() {}",
-            "/** test */
+                "function foo() {}",
+            ),
+            (
+                "/** test */
             function foo() {}",
-            "/** test */
+                "function foo() {}",
+            ),
+            (
+                "/** test */
             // noop
             function foo() {}",
-            "/** test */
+                "function foo() {}",
+            ),
+            (
+                "/** test */
             /*noop*/
             function foo() {}",
-            "/** foo1 */ function foo1() {} /** test */ function foo() {}",
+                "function foo() {}",
+            ),
+            ("/** foo1 */ function foo1() {} /** test */ function foo() {}", "function foo() {}"),
         ];
-        for source_text in source_texts {
-            test_jsdoc_found(source_text, "function foo() {}", None);
+        for (source_text, target) in source_texts {
+            test_jsdoc_found(source_text, target, None);
         }
     }
 
     #[test]
-    fn found_on_property_definition() {
-        let source = "class Foo {
+    fn found_ts() {
+        let source_texts = [(
+            "class Foo {
             /** jsdoc */
             bar: string;
-        }";
+        }",
+            "bar: string;",
+        )];
+
         let source_type = SourceType::default().with_typescript(true);
-        test_jsdoc_found(source, "bar: string;", Some(source_type));
+        for (source_text, target) in source_texts {
+            test_jsdoc_found(source_text, target, Some(source_type));
+        }
     }
 
     #[test]
@@ -198,18 +244,13 @@ mod test {
             /** 2. class X {} *//** 3. class X {} */
             class X {
                 /** 4. foo */
-                foo = /** 5. (1) */ (1);
-
-                /** THIS is ignored in TS too */
-                // bar() {
-                //     // ...
-                // }
+                foo = /** 5. () */ (() => {});
             }
 
-            /** THIS is ignored for now(bound to EOF token in TS) */
+            /** Not attached! */
             ",
             Some(SourceType::default()),
         );
-        assert_eq!(semantic.jsdoc().iter_all().count(), 5);
+        assert_eq!(semantic.jsdoc().iter_all().count(), 6);
     }
 }
