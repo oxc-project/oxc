4b32c46ab957034581bdcd503ab8d7b2cb06ad5a
Boshen <boshenc@gmail.com>
2024-03-03 14:15:45 +0800

    update

--- tasks/benchmark/benches/codegen_sourcemap.rs
+++ tasks/benchmark/benches/codegen_sourcemap.rs
@@ -8,15 +8,14 @@ use oxc_tasks_common::TestFiles;
 fn bench_codegen_sourcemap(criterion: &mut Criterion) {
     let mut group = criterion.benchmark_group("codegen_sourcemap");
 
-    for file in TestFiles::complicated().files() {
+    for file in TestFiles::minimal().files() {
         let id = BenchmarkId::from_parameter(&file.file_name);
         let source_type = SourceType::from_path(&file.file_name).unwrap();
-
         group.bench_with_input(id, &file.source_text, |b, source_text| {
+            let allocator = Allocator::default();
+            let program = Parser::new(&allocator, source_text, source_type).parse().program;
+            let codegen_options = CodegenOptions::default();
             b.iter_with_large_drop(|| {
-                let allocator = Allocator::default();
-                let program = Parser::new(&allocator, source_text, source_type).parse().program;
-                let codegen_options = CodegenOptions::default();
                 let mut codegen = Codegen::<false>::new(source_text.len(), codegen_options);
                 codegen.with_sourcemap(source_text, "").build(&program);
                 codegen.into_sourcemap();
