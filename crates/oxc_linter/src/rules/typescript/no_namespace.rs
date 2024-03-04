48fe55963caa734d56dd7c39a15021ca078ff14a
Boshen <boshenc@gmail.com>
2024-03-04 12:26:02 +0800

    fix

--- crates/oxc_linter/src/rules/typescript/no_namespace.rs
+++ crates/oxc_linter/src/rules/typescript/no_namespace.rs
@@ -61,6 +61,7 @@ impl Rule for NoNamespace {
         }
     }
 
+    #[allow(clippy::cast_possible_truncation)]
     fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
         let AstKind::TSModuleDeclaration(declaration) = node.kind() else { return };
         let TSModuleDeclarationName::Identifier(ident) = &declaration.id else { return };
