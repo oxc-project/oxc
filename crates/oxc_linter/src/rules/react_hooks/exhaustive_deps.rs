ced27b4c614ff68f9259e65871f2e7bec3a4cbb3
Andrei Lisnic <andrei@planable.io>
2024-03-07 10:23:14 +0200

    WIP: first tests

--- crates/oxc_linter/src/rules/react_hooks/exhaustive_deps.rs
+++ crates/oxc_linter/src/rules/react_hooks/exhaustive_deps.rs
@@ -8,7 +8,7 @@ use oxc_span::Span;
 use crate::{context::LintContext, rule::Rule, AstNode};
 
 #[derive(Debug, Error, Diagnostic)]
-#[error("eslint(exhaustive-deps):")]
+#[error("react-hooks(exhaustive-deps):")]
 #[diagnostic(severity(warning), help(""))]
 struct ExhaustiveDepsDiagnostic(#[label] pub Span);
 
@@ -37,9 +37,17 @@ impl Rule for ExhaustiveDeps {
 fn test() {
     use crate::tester::Tester;
 
-    let pass = vec![];
+    let pass = vec![
+        r"function MyComponent() { const local = {}; useEffect(() => { console.log(local) }); }",
+    ];
 
-    let fail = vec![];
+    let fail = vec![
+        r"function MyComponent(props) {
+          useCallback(() => {
+            console.log(props.foo?.toString());
+          }, []);
+        }",
+    ];
 
     Tester::new(ExhaustiveDeps::NAME, pass, fail).test_and_snapshot();
 }
