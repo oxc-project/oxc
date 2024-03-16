4cc0638828fe992f19158dfcbb7494c529be23c1
Boshen <boshenc@gmail.com>
2024-03-16 12:27:36 +0800

    update

--- crates/oxc_linter/src/rules/import/no_default_export.rs
+++ crates/oxc_linter/src/rules/import/no_default_export.rs
@@ -10,7 +10,7 @@ use crate::{context::LintContext, rule::Rule};
 #[derive(Debug, Error, Diagnostic)]
 #[error("eslint-plugin-import(no-default-export): Prefer named exports")]
 #[diagnostic(severity(warning))]
-struct NoDefaultExportDiagnostic(#[label] pub Span);
+struct NoDefaultExportDiagnostic(#[label] Span);
 
 #[derive(Debug, Default, Clone)]
 pub struct NoDefaultExport;
@@ -43,6 +43,15 @@ declare_oxc_lint!(
     nursery
 );
 
+impl Rule for NoDefaultExport {
+    fn run_once(&self, ctx: &LintContext<'_>) {
+        let module_record = ctx.semantic().module_record();
+        write_diagnostic_optional(ctx, module_record.export_default);
+        module_record.export_default_duplicated.iter().for_each(|it| write_diagnostic(ctx, *it));
+        write_diagnostic_optional(ctx, module_record.exported_bindings.get("default").copied());
+    }
+}
+
 fn write_diagnostic(ctx: &LintContext<'_>, span: Span) {
     ctx.diagnostic(NoDefaultExportDiagnostic(span));
 }
@@ -52,53 +61,41 @@ fn write_diagnostic_optional(ctx: &LintContext<'_>, span_option: Option<Span>) {
     }
 }
 
-impl Rule for NoDefaultExport {
-    fn run_once(&self, ctx: &LintContext<'_>) {
-        let module_record = ctx.semantic().module_record();
-
-        write_diagnostic_optional(ctx, module_record.export_default);
-        module_record.export_default_duplicated.iter().for_each(|it| write_diagnostic(ctx, *it));
-        write_diagnostic_optional(ctx, module_record.exported_bindings.get("default").copied());
-    }
-}
-
 #[test]
 fn test() {
     use crate::tester::Tester;
 
-    {
-        let pass = vec![
-            "export const foo = 'foo'; export const bar = 'bar';",
-            "export const foo = 'foo'; export function bar() {};",
-            "export const foo = 'foo';",
-            "const foo = 'foo'; export { foo };",
-            "let foo, bar; export { foo, bar }",
-            "export const { foo, bar } = item;",
-            "export const { foo, bar: baz } = item;",
-            "export const { foo: { bar, baz } } = item;",
-            "let item; export const foo = item; export { item };",
-            "export * from './foo';",
-            "export const { foo } = { foo: 'bar' };",
-            "export const { foo: { bar } } = { foo: { bar: 'baz' } };",
-            "export { a, b } from 'foo.js'",
-            "import * as foo from './foo';",
-            "import foo from './foo';",
-            "import {default as foo} from './foo';",
-            "export type UserId = number;",
-        ];
-        let fail = vec![
-            "export default function bar() {};",
-            "export const foo = 'foo';\nexport default bar;",
-            "export default class Bar {};",
-            "export default function() {};",
-            "export default class {};",
-            "let foo; export { foo as default }",
-            "export default from \"foo.js\"",
-        ];
+    let pass = vec![
+        "export const foo = 'foo'; export const bar = 'bar';",
+        "export const foo = 'foo'; export function bar() {};",
+        "export const foo = 'foo';",
+        "const foo = 'foo'; export { foo };",
+        "let foo, bar; export { foo, bar }",
+        "export const { foo, bar } = item;",
+        "export const { foo, bar: baz } = item;",
+        "export const { foo: { bar, baz } } = item;",
+        "let item; export const foo = item; export { item };",
+        "export * from './foo';",
+        "export const { foo } = { foo: 'bar' };",
+        "export const { foo: { bar } } = { foo: { bar: 'baz' } };",
+        "export { a, b } from 'foo.js'",
+        "import * as foo from './foo';",
+        "import foo from './foo';",
+        "import {default as foo} from './foo';",
+        "export type UserId = number;",
+    ];
+    let fail = vec![
+        "export default function bar() {};",
+        "export const foo = 'foo';\nexport default bar;",
+        "export default class Bar {};",
+        "export default function() {};",
+        "export default class {};",
+        "let foo; export { foo as default }",
+        // "export default from \"foo.js\"",
+    ];
 
-        Tester::new(NoDefaultExport::NAME, pass, fail)
-            .with_import_plugin(true)
-            .change_rule_path("index.ts")
-            .test();
-    }
+    Tester::new(NoDefaultExport::NAME, pass, fail)
+        .with_import_plugin(true)
+        .change_rule_path("index.ts")
+        .test_and_snapshot();
 }
