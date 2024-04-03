470a0a1ced50e18a4453c1327561e26a2ed85a14
underfin <likui.underfin@gmail.com>
2024-04-03 11:11:50 +0800

    chore: fix clippy error

--- crates/oxc_sourcemap/src/sourcemap.rs
+++ crates/oxc_sourcemap/src/sourcemap.rs
@@ -17,7 +17,7 @@ pub struct SourceMap {
     pub(crate) token_chunks: Option<Vec<TokenChunk>>,
 }
 
-#[allow(clippy::cast_possible_truncation, clippy::missing_errors_doc)]
+#[allow(clippy::cast_possible_truncation)]
 impl SourceMap {
     pub fn new(
         file: Option<Arc<str>>,
@@ -31,17 +31,25 @@ impl SourceMap {
     }
 
     /// Convert `SourceMap` to vlq sourcemap string.
-    #[allow(clippy::missing_errors_doc)]
+    /// # Errors
+    ///
+    /// The `serde_json` deserialize Error.
     pub fn from_json_string(value: &str) -> Result<Self> {
         decode(value)
     }
 
     /// Convert the vlq sourcemap string to `SourceMap`.
+    /// # Errors
+    ///
+    /// The `serde_json` serialization Error.
     pub fn to_json_string(&self) -> Result<String> {
         encode(self)
     }
 
     /// Convert `SourceMap` to vlq sourcemap data url.
+    /// # Errors
+    ///
+    /// The `serde_json` serialization Error.
     pub fn to_data_url(&self) -> Result<String> {
         let base_64_str =
             base64_simd::Base64::STANDARD.encode_to_boxed_str(self.to_json_string()?.as_bytes());
