c461280ab9c4b0362fd100d024bcae9f591435f8
Boshen <boshenc@gmail.com>
2024-04-09 20:50:13 +0800

    feat(transformer): add transform callback methods

--- crates/oxc_transformer/src/decorators/mod.rs
+++ crates/oxc_transformer/src/decorators/mod.rs
@@ -1,5 +1,7 @@
 use serde::Deserialize;
 
+use oxc_ast::ast::*;
+
 /// Only `"2023-11"` will be implemented because Babel 8 will only support "2023-11" and "legacy".
 #[derive(Debug, Default, Clone, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -16,4 +18,9 @@ impl Decorators {
     pub fn new(options: DecoratorsOptions) -> Self {
         Self { options }
     }
+}
+
+// Transformers
+impl Decorators {
+    pub fn transform_statement(&mut self, _stmt: &mut Statement<'_>) {}
 }
