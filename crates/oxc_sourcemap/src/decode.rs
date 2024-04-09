1a0702c9fcdc9a0edc2ce2a5efefb1aabf5a4830
underfin <likui.underfin@gmail.com>
2024-04-09 14:47:03 +0800

    chore: add doc

--- crates/oxc_sourcemap/src/decode.rs
+++ crates/oxc_sourcemap/src/decode.rs
@@ -6,11 +6,17 @@ use crate::{SourceMap, Token};
 #[derive(serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct JSONSourceMap {
+    // An optional name of the generated code that this source map is associated with.
     file: Option<String>,
+    // A string with the encoded mapping data.
     mappings: Option<String>,
+    // An optional source root, useful for relocating source files on a server or removing repeated values in the “sources” entry.  This value is prepended to the individual entries in the “source” field.
     source_root: Option<String>,
+    // A list of original sources used by the “mappings” entry.
     sources: Option<Vec<Option<String>>>,
+    // An optional list of source content, useful when the “source” can’t be hosted. The contents are listed in the same order as the sources in line 5. “null” may be used if some original sources should be retrieved by name.
     sources_content: Option<Vec<Option<String>>>,
+    // A list of symbol names used by the “mappings” entry.
     names: Option<Vec<String>>,
 }
 
