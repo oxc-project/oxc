8963d089a9a3fefdfe98f814a67572be6c6d6e5e
rzvxa <rzvxa@protonmail.com>
2024-04-15 05:04:12 +0330

    fix: add checks to make sure structs aren't deriving Copy trait.

--- crates/oxc_macros/src/ast_node.rs
+++ crates/oxc_macros/src/ast_node.rs
@@ -25,29 +25,36 @@ pub fn ast_node(mut item: Item) -> TokenStream2 {
 }
 
 fn modify_struct(item: &mut ItemStruct) -> NodeData {
-    println!("{:#?}", item.attrs);
     validate_struct_attributes(item.attrs.iter());
     NodeData { ident: &item.ident, generics: &item.generics }
 }
 
 fn modify_enum(item: &mut ItemEnum) -> NodeData {
+    validate_enum_attributes(item.attrs.iter());
     NodeData { ident: &item.ident, generics: &item.generics }
 }
 
 fn validate_struct_attributes<'a>(mut attrs: slice::Iter<'a, Attribute>) {
+    // make sure that no structure derives Clone/Copy trait.
+    // TODO: It might fail if there is a manual Clone/Copy trait implemented for the struct.
     assert!(!attrs.any(|attr| {
         let args = attr.parse_args_with(Punctuated::<Meta, Token![,]>::parse_terminated);
         attr.path().is_ident("derive")
-            && args.is_ok_and(|args| args.iter().any(|arg| arg.path().is_ident("Clone")))
+            && args.is_ok_and(|args| {
+                args.iter().any(|arg| arg.path().is_ident("Clone") || arg.path().is_ident("Copy"))
+            })
     }));
+}
+
+fn validate_enum_attributes<'a>(_: slice::Iter<'a, Attribute>) {
+    // TODO: Later on we may want to enforce deriving clone and copy traits for all enum types
 }
 
 fn impl_traversable_test_trait(node: &NodeData) -> TokenStream2 {
     let ident = node.ident;
     let generics = node.generics;
     quote! {
-        impl #generics crate::traverse::TraversableTest for #ident #generics {
-        }
+        impl #generics crate::traverse::TraversableTest for #ident #generics { }
     }
 }
 
