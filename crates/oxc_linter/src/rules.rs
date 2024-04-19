fa08abe20acc3bd09dec013edf88ce7dc2117859
Boshen <boshenc@gmail.com>
2024-04-19 12:45:50 +0800

    feat(linter): remove import/no-unresolved (#3023)
    closes #3015
    
    This plugin will always contain false positives due to module resolution
    complexity.

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -19,7 +19,6 @@ mod import {
     pub mod no_named_as_default;
     pub mod no_named_as_default_member;
     pub mod no_self_import;
-    pub mod no_unresolved;
     pub mod no_unused_modules;
 }
 
@@ -637,7 +636,6 @@ oxc_macros::declare_all_lint_rules! {
     import::no_named_as_default,
     import::no_named_as_default_member,
     import::no_self_import,
-    import::no_unresolved,
     import::no_unused_modules,
     import::no_duplicates,
     import::no_default_export,
