2112a8684b7530fe8320dff04adb9b2ccad065f9
rzvxa <rzvxa@protonmail.com>
2024-04-25 01:57:02 +0330

    improvement(linter/ts): use `root_node` method in the `triple_slash_reference` rule.

--- crates/oxc_linter/src/rules/typescript/triple_slash_reference.rs
+++ crates/oxc_linter/src/rules/typescript/triple_slash_reference.rs
@@ -103,8 +103,8 @@ impl Rule for TripleSlashReference {
         }))
     }
     fn run_once(&self, ctx: &LintContext) {
-        let Some(root) = ctx.nodes().iter().next() else { return };
-        let AstKind::Program(program) = root.kind() else { return };
+        let Some(root) = ctx.nodes().root_node() else { return };
+        let AstKind::Program(program) = root.kind() else { unreachable!() };
 
         // We don't need to iterate over all comments since Triple-slash directives are only valid at the top of their containing file.
         // We are trying to get the first statement start potioin, falling back to the program end if statement does not exist
