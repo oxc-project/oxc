c95276049249e1044d2dcbf10263130fc39f71d7
Miles Johnson <mileswjohnson@gmail.com>
2024-04-29 14:53:20 -0700

    TSThisType

--- crates/oxc_ast/src/ast/ts.rs
+++ crates/oxc_ast/src/ast/ts.rs
@@ -815,7 +815,7 @@ pub struct TSTypePredicate<'a> {
 #[cfg_attr(feature = "serialize", serde(untagged, rename_all = "camelCase"))]
 pub enum TSTypePredicateName<'a> {
     Identifier(Box<'a, IdentifierName<'a>>),
-    This(TSThisType),
+    This(Box<'a, TSThisType>),
 }
 
 #[ast_node]
