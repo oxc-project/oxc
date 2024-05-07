ca9f13f4f21a72355b584eea650b38a34b56ee45
Boshen <boshenc@gmail.com>
2024-05-07 17:10:17 +0800

    feat(linter): eslint/no-new-native-nonconstructor (#3187)
    feat(linter): eslint/no-new-native-nonconstructor
    
    closes #3179
    
    remove no new symbol

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -85,7 +85,7 @@ mod eslint {
     pub mod no_iterator;
     pub mod no_loss_of_precision;
     pub mod no_mixed_operators;
-    pub mod no_new_symbol;
+    pub mod no_new_native_nonconstructor;
     pub mod no_new_wrappers;
     pub mod no_nonoctal_decimal_escape;
     pub mod no_obj_calls;
@@ -444,7 +444,6 @@ oxc_macros::declare_all_lint_rules! {
     eslint::no_iterator,
     eslint::no_loss_of_precision,
     eslint::no_mixed_operators,
-    eslint::no_new_symbol,
     eslint::no_new_wrappers,
     eslint::no_nonoctal_decimal_escape,
     eslint::no_obj_calls,
@@ -475,6 +474,7 @@ oxc_macros::declare_all_lint_rules! {
     eslint::use_isnan,
     eslint::valid_typeof,
     eslint::no_await_in_loop,
+    eslint::no_new_native_nonconstructor,
     typescript::adjacent_overload_signatures,
     typescript::array_type,
     typescript::ban_ts_comment,
