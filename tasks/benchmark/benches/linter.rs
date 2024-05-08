5683aceae90d7dad7e2f5d2b103f67355e23e187
Boshen <boshenc@gmail.com>
2024-05-08 14:54:10 +0800

    ci: improve benchmark build time by only building once (#3201)
    relates #3200

--- tasks/benchmark/benches/linter.rs
+++ tasks/benchmark/benches/linter.rs
@@ -1,4 +1,4 @@
-use std::{env, path::PathBuf, rc::Rc};
+use std::{path::PathBuf, rc::Rc};
 
 use oxc_allocator::Allocator;
 use oxc_benchmark::{criterion_group, criterion_main, BenchmarkId, Criterion};
@@ -11,15 +11,7 @@ use oxc_tasks_common::TestFiles;
 fn bench_linter(criterion: &mut Criterion) {
     let mut group = criterion.benchmark_group("linter");
 
-    // If `FIXTURE` env is set, only run the specified benchmark. This is used for sharding in CI.
-    let test_files = if let Ok(fixture_index) = env::var("FIXTURE") {
-        let fixture_index = fixture_index.parse::<usize>().unwrap();
-        TestFiles::complicated_one(fixture_index)
-    } else {
-        TestFiles::complicated()
-    };
-
-    for file in test_files.files() {
+    for file in TestFiles::linter().files() {
         let source_type = SourceType::from_path(&file.file_name).unwrap();
         group.bench_with_input(
             BenchmarkId::from_parameter(&file.file_name),
