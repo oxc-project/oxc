5413eac57cd6b77c31fdbe4064e95043aaf8ea84
Cameron Clark <cameron.clark@hey.com>
2024-05-25 22:27:35 +0100

    feat(linter) eslint-plugin-unicorn no magic array flat depth

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -237,6 +237,7 @@ mod unicorn {
     pub mod no_instanceof_array;
     pub mod no_invalid_remove_event_listener;
     pub mod no_lonely_if;
+    pub mod no_magic_array_flat_depth;
     pub mod no_negated_condition;
     pub mod no_nested_ternary;
     pub mod no_new_array;
@@ -568,6 +569,7 @@ oxc_macros::declare_all_lint_rules! {
     unicorn::no_instanceof_array,
     unicorn::no_invalid_remove_event_listener,
     unicorn::no_lonely_if,
+    unicorn::no_magic_array_flat_depth,
     unicorn::no_negated_condition,
     unicorn::no_nested_ternary,
     unicorn::no_new_array,
