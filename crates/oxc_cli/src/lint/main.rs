4149d22aa3fae6a03eab52a737a64679b8380da8
Boshen <1430279+Boshen@users.noreply.github.com>
2024-05-26 04:46:25 +0000

    chore(cli): remove the regex feature from `tracing_subscriber` (#3412)

--- crates/oxc_cli/src/lint/main.rs
+++ crates/oxc_cli/src/lint/main.rs
@@ -26,7 +26,21 @@ fn init_miette() {
     miette::set_hook(Box::new(|_| Box::new(miette::MietteHandlerOpts::new().build()))).unwrap();
 }
 
+/// To debug `oxc_resolver`:
+/// `OXC_LOG=oxc_resolver oxlint --import-plugin`
 fn init_tracing() {
-    use tracing_subscriber::{fmt, prelude::*, registry, EnvFilter};
-    registry().with(fmt::layer()).with(EnvFilter::from_env("OXC_LOG")).init();
+    use tracing_subscriber::{filter::Targets, prelude::*};
+
+    // Usage without the `regex` feature.
+    // <https://github.com/tokio-rs/tracing/issues/1436#issuecomment-918528013>
+    tracing_subscriber::registry()
+        .with(std::env::var("OXC_LOG").map_or_else(
+            |_| Targets::new(),
+            |env_var| {
+                use std::str::FromStr;
+                Targets::from_str(&env_var).unwrap()
+            },
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 }
