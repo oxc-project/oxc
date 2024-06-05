5fd94c8b78b0a78855d9aa988fdde4da2b5df260
Don Isaac <donald.isaac@gmail.com>
2024-06-05 12:32:53 -0400

    style: cargo fmt

--- crates/oxc_span/src/span.rs
+++ crates/oxc_span/src/span.rs
@@ -18,21 +18,21 @@ pub const SPAN: Span = Span::new(0, 0);
 /// See the [`text-size`](https://docs.rs/text-size) crate for details.
 /// Utility methods can be copied from the `text-size` crate if they are needed.
 /// NOTE: `u32` is sufficient for "all" reasonable programs. Larger than u32 is a 4GB JS file.
-/// 
+///
 /// ## Hashing
 /// [`Span`]'s implementation of [`Hash`] is a no-op so that AST nodes can be
 /// compared by hash. This makes them unsuitable for use as keys in a hash map.
-/// 
+///
 /// ```
 /// use std::hash::{Hash, Hasher, DefaultHasher};
 /// use oxc_span::Span;
-/// 
+///
 /// let mut first = DefaultHasher::new();
 /// let mut second = DefaultHasher::new();
-/// 
+///
 /// Span::new(0, 5).hash(&mut first);
 /// Span::new(10, 20).hash(&mut second);
-/// 
+///
 /// assert_eq!(first.finish(), second.finish());
 /// ```
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
@@ -147,7 +147,7 @@ mod test {
 
     #[test]
     fn test_hash() {
-        use std::hash::{Hash, Hasher, DefaultHasher};
+        use std::hash::{DefaultHasher, Hash, Hasher};
         let mut first = DefaultHasher::new();
         let mut second = DefaultHasher::new();
         Span::new(0, 5).hash(&mut first);
