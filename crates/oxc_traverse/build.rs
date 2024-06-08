48bb97e53a2bb459809e9fc4d6bab8e1500dda58
Boshen <boshenc@gmail.com>
2024-06-08 16:31:21 +0800

    fix(traverse): do not publish the build script

--- crates/oxc_traverse/build.rs
+++ crates/oxc_traverse/build.rs
@@ -1,20 +1,17 @@
-use std::{env, process::Command};
-
 fn main() {
+    // Exit if on CI.
+    // The built files should be checked into git, so want to run tests etc on what's actually in repo,
+    // rather than regenerating them.
+    if std::option_env!("CI") == Some("true") {
+        return;
+    }
+
     // Re-run if NodeJS build script or AST types change
     println!("cargo:rerun-if-changed=scripts");
     println!("cargo:rerun-if-changed=../oxc_ast/src/ast");
 
-    // Exit if on CI.
-    // The built files should be checked into git, so want to run tests etc on what's actually in repo,
-    // rather than regenerating them.
-    match env::var("CI") {
-        Ok(value) if value == "true" => return,
-        _ => {}
-    }
-
     // Run NodeJS build script
-    let status = Command::new("node")
+    let status = std::process::Command::new("node")
         .arg("./scripts/build.mjs")
         .status()
         .expect("Failed to run NodeJS build script");
