8f76cb5ccac2528597e12efe4afe9ee4dba8e606
eryue0220 <eryue0220@gmail.com>
2024-06-10 11:19:22 +0800

    fix: cr

--- crates/oxc_linter/src/rules/jest/max_nested_describe.rs
+++ crates/oxc_linter/src/rules/jest/max_nested_describe.rs
@@ -17,7 +17,7 @@ fn exceeded_max_depth(current: usize, max: usize, span0: Span) -> OxcDiagnostic 
     OxcDiagnostic::warn(
         "eslint-plugin-jest(max-nested-describe): Enforces a maximum depth to nested describe calls.",
     )
-    .with_help(format!("Too many nested describe calls ({current:?}) - maximum allowed is {max:?}"))
+    .with_help(format!("Too many nested describe calls ({current}) - maximum allowed is {max}"))
     .with_labels([span0.into()])
 }
 
