d6370f19fe770d667d2fb0c4dffc6863da693caa
Yuji Sugiura <6259812+leaysgur@users.noreply.github.com>
2024-06-10 18:23:41 +0900

    feat(linter/jsdoc): Implement require-param-type rule (#3601)
    Part of #1170
    
    >
    https://github.com/gajus/eslint-plugin-jsdoc/blob/main/docs/rules/require-param-type.md

--- crates/oxc_linter/src/utils/jsdoc.rs
+++ crates/oxc_linter/src/utils/jsdoc.rs
@@ -1,6 +1,10 @@
 use crate::{config::JSDocPluginSettings, context::LintContext, AstNode};
-use oxc_ast::AstKind;
+use oxc_ast::{
+    ast::{BindingPattern, BindingPatternKind, Expression, FormalParameters},
+    AstKind,
+};
 use oxc_semantic::JSDoc;
+use oxc_span::Span;
 use rustc_hash::FxHashSet;
 
 /// JSDoc is often attached on the parent node of a function.
@@ -112,4 +116,116 @@ pub fn should_ignore_as_avoid(
     }
 
     jsdoc.tags().iter().any(|tag| ignore_tag_names.contains(tag.kind.parsed()))
+}
+
+#[derive(Debug, Clone)]
+pub struct Param {
+    pub span: Span,
+    pub name: String,
+    pub is_rest: bool,
+}
+
+#[derive(Debug, Clone)]
+pub enum ParamKind {
+    Single(Param),
+    Nested(Vec<Param>),
+}
+
+pub fn collect_params(params: &FormalParameters) -> Vec<ParamKind> {
+    // NOTE: Property level `is_rest` is implemented.
+    //   - fn(a, { b1, ...b2 })
+    //                 ^^^^^
+    // But Object|Array level `is_rest` is not implemented
+    //   - fn(a, ...{ b })
+    //           ^^^^   ^
+    // Tests are not covering these cases...
+    fn get_param_name(pattern: &BindingPattern, is_rest: bool) -> ParamKind {
+        match &pattern.kind {
+            BindingPatternKind::BindingIdentifier(ident) => {
+                ParamKind::Single(Param { span: ident.span, name: ident.name.to_string(), is_rest })
+            }
+            BindingPatternKind::ObjectPattern(obj_pat) => {
+                let mut collected = vec![];
+
+                for prop in &obj_pat.properties {
+                    let Some(name) = prop.key.name() else { continue };
+
+                    match get_param_name(&prop.value, false) {
+                        ParamKind::Single(param) => {
+                            collected.push(Param { name: format!("{name}"), ..param });
+                        }
+                        ParamKind::Nested(params) => {
+                            collected.push(Param {
+                                span: prop.span,
+                                name: format!("{name}"),
+                                is_rest: false,
+                            });
+
+                            for param in params {
+                                collected.push(Param {
+                                    name: format!("{name}.{}", param.name),
+                                    ..param
+                                });
+                            }
+                        }
+                    }
+                }
+
+                if let Some(rest) = &obj_pat.rest {
+                    match get_param_name(&rest.argument, true) {
+                        ParamKind::Single(param) => collected.push(param),
+                        ParamKind::Nested(params) => collected.extend(params),
+                    }
+                }
+
+                ParamKind::Nested(collected)
+            }
+            BindingPatternKind::ArrayPattern(arr_pat) => {
+                let mut collected = vec![];
+
+                for (idx, elm) in arr_pat.elements.iter().enumerate() {
+                    let name = format!("\"{idx}\"");
+
+                    if let Some(pat) = elm {
+                        match get_param_name(pat, false) {
+                            ParamKind::Single(param) => collected.push(Param { name, ..param }),
+                            ParamKind::Nested(params) => collected.extend(params),
+                        }
+                    }
+                }
+
+                if let Some(rest) = &arr_pat.rest {
+                    match get_param_name(&rest.argument, true) {
+                        ParamKind::Single(param) => collected.push(param),
+                        ParamKind::Nested(params) => collected.extend(params),
+                    }
+                }
+
+                ParamKind::Nested(collected)
+            }
+            BindingPatternKind::AssignmentPattern(assign_pat) => match &assign_pat.right {
+                Expression::Identifier(_) => get_param_name(&assign_pat.left, false),
+                _ => {
+                    // TODO: If `config.useDefaultObjectProperties` = true,
+                    // collect default parameters from `assign_pat.right` like:
+                    // { prop = { a: 1, b: 2 }} => [prop, prop.a, prop.b]
+                    //     get_param_name(&assign_pat.left, false)
+                    // }
+                    get_param_name(&assign_pat.left, false)
+                }
+            },
+        }
+    }
+
+    let mut collected =
+        params.items.iter().map(|param| get_param_name(&param.pattern, false)).collect::<Vec<_>>();
+
+    if let Some(rest) = &params.rest {
+        match get_param_name(&rest.argument, true) {
+            ParamKind::Single(param) => collected.push(ParamKind::Single(param)),
+            ParamKind::Nested(params) => collected.push(ParamKind::Nested(params)),
+        }
+    }
+
+    collected
 }
