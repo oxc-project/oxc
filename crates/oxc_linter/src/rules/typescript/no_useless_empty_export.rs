9dd7cb0a9dc1e40c9b6e53bd316550db3a8a1cee
Boshen <boshenc@gmail.com>
2024-06-12 11:49:50 +0800

    update

--- crates/oxc_linter/src/rules/typescript/no_useless_empty_export.rs
+++ crates/oxc_linter/src/rules/typescript/no_useless_empty_export.rs
@@ -1,8 +1,4 @@
-use oxc_allocator::Vec;
-use oxc_ast::{
-    ast::{ExportNamedDeclaration, Statement, TSModuleDeclarationBody},
-    AstKind,
-};
+use oxc_ast::AstKind;
 use oxc_diagnostics::OxcDiagnostic;
 use oxc_macros::declare_oxc_lint;
 use oxc_span::Span;
@@ -44,67 +40,22 @@ declare_oxc_lint!(
 
 impl Rule for NoUselessEmptyExport {
     fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
-        match node.kind() {
-            AstKind::Program(program) => {
-                if ctx.semantic().module_record().not_esm {
-                    return;
-                }
-                check_node(&program.body, ctx);
-            }
-            AstKind::TSModuleDeclaration(decl) => {
-                if let Some(TSModuleDeclarationBody::TSModuleBlock(block)) = &decl.body {
-                    check_node(&block.body, ctx);
-                }
-            }
-            _ => {}
+        let AstKind::ExportNamedDeclaration(decl) = node.kind() else { return };
+        if decl.declaration.is_some() || !decl.specifiers.is_empty() {
+            return;
         }
-    }
-}
-
-fn get_empty_export<'a>(statement: &'a Statement) -> Option<&'a ExportNamedDeclaration<'a>> {
-    if let Statement::ExportNamedDeclaration(export_decl) = statement {
-        if export_decl.specifiers.is_empty() && export_decl.declaration.is_none() {
-            return Some(export_decl);
+        let module_record = ctx.semantic().module_record();
+        if module_record.exported_bindings.is_empty()
+            && module_record.local_export_entries.is_empty()
+            && module_record.indirect_export_entries.is_empty()
+            && module_record.star_export_entries.is_empty()
+            && module_record.export_default.is_none()
+        {
+            return;
         }
-    }
-    None
-}
-
-fn is_export_or_import_node_types(statement: &Statement) -> bool {
-    matches!(
-        statement,
-        Statement::ExportAllDeclaration(_)
-            | Statement::ExportDefaultDeclaration(_)
-            | Statement::ExportNamedDeclaration(_)
-            | Statement::ImportDeclaration(_)
-            | Statement::TSExportAssignment(_)
-            | Statement::TSImportEqualsDeclaration(_)
-    )
-}
-
-fn check_node<'a>(statements: &Vec<'a, Statement<'a>>, ctx: &LintContext<'a>) {
-    if statements.is_empty() {
-        return;
-    }
-
-    let mut empty_exports = vec![];
-    let mut found_other_export = false;
-
-    for statement in statements {
-        if let Some(empty_export) = get_empty_export(statement) {
-            empty_exports.push(empty_export);
-        } else if is_export_or_import_node_types(statement) {
-            found_other_export = true;
-        }
-    }
-
-    if found_other_export {
-        for empty_export in &empty_exports {
-            ctx.diagnostic_with_fix(
-                no_useless_empty_export_diagnostic(empty_export.span),
-                |fixer| fixer.delete(&empty_export.span),
-            );
-        }
+        ctx.diagnostic_with_fix(no_useless_empty_export_diagnostic(decl.span), |fixer| {
+            fixer.delete(&decl.span)
+        });
     }
 }
 
@@ -158,20 +109,20 @@ fn test() {
             export { _ };
             export {};
         ",
-        "
-            import _ = require('_');
-            export {};
-        ",
+        // "
+        // import _ = require('_');
+        // export {};
+        // ",
     ];
 
     let fix = vec![
-        ("export const _ = {};export {};", "export const _ = {};", None),
-        ("export * from '_';export {};", "export * from '_';", None),
-        ("export {};export * from '_';", "export * from '_';", None),
-        ("const _ = {};export default _;export {};", "const _ = {};export default _;", None),
-        ("export {};const _ = {};export default _;", "const _ = {};export default _;", None),
-        ("const _ = {};export { _ };export {};", "const _ = {};export { _ };", None),
-        ("import _ = require('_');export {};", "import _ = require('_');", None),
+        ("export const _ = {};export {};", "export const _ = {};"),
+        ("export * from '_';export {};", "export * from '_';"),
+        ("export {};export * from '_';", "export * from '_';"),
+        ("const _ = {};export default _;export {};", "const _ = {};export default _;"),
+        ("export {};const _ = {};export default _;", "const _ = {};export default _;"),
+        ("const _ = {};export { _ };export {};", "const _ = {};export { _ };"),
+        // ("import _ = require('_');export {};", "import _ = require('_');"),
     ];
 
     Tester::new(NoUselessEmptyExport::NAME, pass, fail).expect_fix(fix).test_and_snapshot();
