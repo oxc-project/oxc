29c78db3589d3623aa654e11140556b117fefae4
kaykdm <34934746+kaykdm@users.noreply.github.com>
2024-06-12 17:45:02 +0900

    feat(linter): implement @typescript-eslint/explicit-function-return-type (#3455)
    Related issue: https://github.com/oxc-project/oxc/issues/2180
    
    original implementation
    
    - code:
    https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/rules/explicit-function-return-type.ts
    - test:
    https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/tests/rules/explicit-function-return-type.test.ts
    - doc: https://typescript-eslint.io/rules/explicit-function-return-type/
    
    ---------
    
    Co-authored-by: Boshen <boshenc@gmail.com>

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -126,6 +126,7 @@ mod typescript {
     pub mod ban_types;
     pub mod consistent_indexed_object_style;
     pub mod consistent_type_definitions;
+    pub mod explicit_function_return_type;
     pub mod no_duplicate_enum_values;
     pub mod no_empty_interface;
     pub mod no_explicit_any;
@@ -531,6 +532,7 @@ oxc_macros::declare_all_lint_rules! {
     typescript::prefer_ts_expect_error,
     typescript::triple_slash_reference,
     typescript::prefer_literal_enum_member,
+    typescript::explicit_function_return_type,
     jest::expect_expect,
     jest::max_expects,
     jest::max_nested_describe,
