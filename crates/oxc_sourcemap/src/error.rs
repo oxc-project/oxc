01572f037d4964d061d722cd077acfc7c10ff2d2
DonIsaac <22823424+DonIsaac@users.noreply.github.com>
2024-06-26 05:10:48 +0000

    feat(sourcemap): impl `std::fmt::Display` for `Error` (#3902)

--- crates/oxc_sourcemap/src/error.rs
+++ crates/oxc_sourcemap/src/error.rs
@@ -1,3 +1,5 @@
+use std::{error, fmt};
+
 #[derive(Debug)]
 pub enum Error {
     /// a VLQ string was malformed and data was left over
@@ -14,6 +16,35 @@ pub enum Error {
     BadSourceReference(u32),
     /// a reference to a non existing name was encountered
     BadNameReference(u32),
+}
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::VlqLeftover => write!(f, "VLQ string was malformed and data was left over"),
+            Error::VlqNoValues => write!(f, "VLQ string was empty and no values could be decoded"),
+            Error::VlqOverflow => write!(f, "The input encoded a number that didn't fit into i64"),
+            Error::BadJson(err) => write!(f, "JSON parsing error: {err}"),
+            Error::BadSegmentSize(size) => {
+                write!(f, "Mapping segment had an unsupported size of {size}")
+            }
+            Error::BadSourceReference(idx) => {
+                write!(f, "Reference to non-existing source at position {idx}")
+            }
+            Error::BadNameReference(idx) => {
+                write!(f, "Reference to non-existing name at position {idx}")
+            }
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        if let Self::BadJson(err) = self {
+            Some(err)
+        } else {
+            None
+        }
+    }
 }
 
 /// The result of decoding.
