4d3190da6f88bf943575aeb92c094da6b1b0ff6d
eryue0220 <eryue0220@gmail.com>
2024-07-04 23:47:25 +0800

    feat: add new rule

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -184,7 +184,9 @@ mod jest {
     pub mod prefer_comparison_matcher;
     pub mod prefer_equality_matcher;
     pub mod prefer_expect_resolves;
+    pub mod prefer_hooks_in_order;
     pub mod prefer_hooks_on_top;
+    pub mod prefer_importing_jest_globals;
     pub mod prefer_jest_mocked;
     pub mod prefer_lowercase_title;
     pub mod prefer_mock_promise_shorthand;
@@ -576,7 +578,9 @@ oxc_macros::declare_all_lint_rules! {
     jest::prefer_comparison_matcher,
     jest::prefer_equality_matcher,
     jest::prefer_expect_resolves,
+    jest::prefer_hooks_in_order,
     jest::prefer_hooks_on_top,
+    jest::prefer_importing_jest_globals,
     jest::prefer_jest_mocked,
     jest::prefer_lowercase_title,
     jest::prefer_mock_promise_shorthand,
