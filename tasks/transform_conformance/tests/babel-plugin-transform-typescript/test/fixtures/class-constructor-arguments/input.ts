4413e2d2981339ccc8d9cde4cdbcb23873fa4779
Don Isaac <donald.isaac@gmail.com>
2024-07-08 00:45:50 -0400

    fix(transformer): missing initializer for readonly consructor properties (#4103)

--- tasks/transform_conformance/tests/babel-plugin-transform-typescript/test/fixtures/class-constructor-arguments/input.ts
+++ tasks/transform_conformance/tests/babel-plugin-transform-typescript/test/fixtures/class-constructor-arguments/input.ts
@@ -1,5 +1,11 @@
 class Foo {
-  constructor(public foo, private bar, protected zoo, too) {
+  boom: number;
+  constructor(public foo, private bar, protected zoo, readonly bang, too) {
 
+  }
+}
+class Bar extends Foo {
+  constructor(public foo, private bar, protected zoo, readonly bang, override boom, too) {
+    super(foo, bar, zoo, bang, too);
   }
 }
--- tasks/transform_conformance/tests/babel-plugin-transform-typescript/test/fixtures/class-constructor-arguments/output.js
+++ tasks/transform_conformance/tests/babel-plugin-transform-typescript/test/fixtures/class-constructor-arguments/output.js
@@ -1,7 +1,19 @@
 class Foo {
-  constructor(foo, bar, zoo, too) {
-      this.foo = foo;
-      this.bar = bar;
-      this.zoo = zoo;
+  boom;
+  constructor(foo, bar, zoo, bang, too) {
+    this.foo = foo;
+    this.bar = bar;
+    this.zoo = zoo;
+    this.bang = bang;
+  }
+}
+class Bar extends Foo {
+  constructor(foo, bar, zoo, bang, boom, too) {
+    super(foo, bar, zoo, bang, too);
+    this.foo = foo;
+    this.bar = bar;
+    this.zoo = zoo;
+    this.bang = bang;
+    this.boom = boom;
   }
 }
