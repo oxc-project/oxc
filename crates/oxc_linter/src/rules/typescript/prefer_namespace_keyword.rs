9c10c3f713b52443b97a4fa7ecf8b8e48bb3b911
Aza Walker <azawalker.jr@gmail.com>
2024-07-23 17:24:11 -0400

    Adding prefer-keyword-namespce typescript rule

---
+++ crates/oxc_linter/src/rules/typescript/prefer_namespace_keyword.rs
@@ -0,0 +1,105 @@
+use oxc_ast::{
+    ast::{TSModuleDeclarationKind, TSModuleDeclarationName},
+    AstKind,
+};
+use oxc_diagnostics::OxcDiagnostic;
+use oxc_macros::declare_oxc_lint;
+use oxc_span::Span;
+
+use crate::{context::LintContext, rule::Rule, AstNode};
+
+fn prefer_namespace_keyword_diagnostic(span: Span) -> OxcDiagnostic {
+    OxcDiagnostic::warn("Use 'namespace' instead of 'module' to declare custom TypeScript modules.")
+        .with_label(span)
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct PreferNamespaceKeyword;
+
+declare_oxc_lint!(
+    /// ### What it does
+    /// This rule reports when the module keyword is used instead of namespace.
+    /// This rule does not report on the use of TypeScript module declarations to describe external APIs (declare module 'foo' {}).
+    ///
+    /// ### Why is this bad?
+    /// Namespaces are an outdated way to organize TypeScript code. ES2015 module syntax is now preferred (import/export).
+    /// For projects still using custom modules / namespaces, it's preferred to refer to them as namespaces.
+    ///
+    /// ### Example
+    /// ```javascript
+    /// module Example {}
+    /// ```
+    PreferNamespaceKeyword,
+    style
+);
+
+impl Rule for PreferNamespaceKeyword {
+    fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
+        let AstKind::TSModuleDeclaration(module) = node.kind() else { return };
+        if module.id.is_string_literal() {
+            return;
+        }
+        let TSModuleDeclarationName::Identifier(_) = &module.id else { return };
+        if module.kind != TSModuleDeclarationKind::Module {
+            return;
+        }
+
+        ctx.diagnostic_with_fix(prefer_namespace_keyword_diagnostic(module.span), |fixer| {
+            let span_start = module.span.start;
+            let span_start_with_declare = span_start + "declare ".len() as u32;
+            let span_size = "module".len() as u32;
+            let span = if module.declare {
+                Span::sized(span_start_with_declare, span_size)
+            } else {
+                Span::sized(span_start, span_size)
+            };
+            fixer.replace(span, "namespace")
+        });
+    }
+
+    fn should_run(&self, ctx: &LintContext) -> bool {
+        ctx.source_type().is_typescript()
+    }
+}
+
+#[test]
+fn test() {
+    use crate::tester::Tester;
+
+    let pass = vec![
+        "declare module 'foo';",
+        "declare module 'foo' {}",
+        "namespace foo {}",
+        "declare namespace foo {}",
+        "declare global {}",
+    ];
+
+    let fail = vec![
+        "module foo {}",
+        "declare module foo {}",
+        "
+			declare module foo {
+			  declare module bar {}
+			}
+			      ",
+    ];
+
+    let fix = vec![
+        ("module foo {}", "namespace foo {}", None),
+        ("declare module foo {}", "declare namespace foo {}", None),
+        (
+            "
+			declare module foo {
+			  declare module bar {}
+			}
+			      ",
+            "
+			declare namespace foo {
+			  declare namespace bar {}
+			}
+			      ",
+            None,
+        ),
+    ];
+    Tester::new(PreferNamespaceKeyword::NAME, pass, fail).expect_fix(fix).test_and_snapshot();
+}
