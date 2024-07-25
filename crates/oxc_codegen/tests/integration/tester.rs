e1ca4122d82bbaae4f44bd804e9a14d53d88532a
Boshen <1430279+Boshen@users.noreply.github.com>
2024-07-25 10:00:33 +0000

    chore(codegen): port over esbuild codegen tests (all ignored right now) (#4457)

--- crates/oxc_codegen/tests/integration/tester.rs
+++ crates/oxc_codegen/tests/integration/tester.rs
@@ -1,19 +1,30 @@
 use oxc_allocator::Allocator;
-use oxc_codegen::{CodeGenerator, CodegenOptions};
+use oxc_codegen::{CodeGenerator, CodegenOptions, WhitespaceRemover};
 use oxc_parser::Parser;
 use oxc_span::SourceType;
 
 pub fn test(source_text: &str, expected: &str) {
     let source_type = SourceType::default().with_module(true);
-    check(source_text, expected, source_type);
-}
-
-fn check(source_text: &str, expected: &str, source_type: SourceType) {
     let allocator = Allocator::default();
     let ret = Parser::new(&allocator, source_text, source_type).parse();
     let result = CodeGenerator::new()
         .with_options(CodegenOptions { single_quote: true })
         .build(&ret.program)
         .source_text;
-    assert_eq!(expected, result, "for source {source_text}, expect {expected}, got {result}");
+    check(source_text, &result, expected);
+}
+
+pub fn test_minify(source_text: &str, expected: &str) {
+    let source_type = SourceType::default().with_module(true);
+    let allocator = Allocator::default();
+    let ret = Parser::new(&allocator, source_text, source_type).parse();
+    let result = WhitespaceRemover::new()
+        .with_options(CodegenOptions { single_quote: true })
+        .build(&ret.program)
+        .source_text;
+    check(source_text, &result, expected);
+}
+
+fn check(source_text: &str, result: &str, expected: &str) {
+    assert_eq!(result, expected, "for source {source_text}, expect {expected}, got {result}");
 }
