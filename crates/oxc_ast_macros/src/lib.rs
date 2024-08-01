452e0eebb43c5ebaf1bfbbbfc7f80684abde2a58
overlookmotel <557937+overlookmotel@users.noreply.github.com>
2024-08-01 22:48:03 +0000

    refactor(ast): remove defunct `visit_as` + `visit_args` attrs from `#[ast]` macro (#4599)
    #4371 replaced `#[visit_as(...)]` and `#[visit_args(...)]` with `#[visit(as(...))]` and `#[visit(args(...))]`. Remove these defunct helper attributes from the `#[ast]` macro.

--- crates/oxc_ast_macros/src/lib.rs
+++ crates/oxc_ast_macros/src/lib.rs
@@ -25,7 +25,7 @@ pub fn ast(_args: TokenStream, input: TokenStream) -> TokenStream {
 ///
 /// Does not generate any code.
 /// Only purpose is to allow using `#[scope]`, `#[visit]`, and other attrs in the AST node type defs.
-#[proc_macro_derive(Ast, attributes(span, scope, visit, visit_as, visit_args, serde, tsify))]
+#[proc_macro_derive(Ast, attributes(scope, visit, span, serde, tsify))]
 pub fn ast_derive(_item: TokenStream) -> TokenStream {
     TokenStream::new()
 }
