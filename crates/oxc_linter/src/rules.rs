5992b7575e67671690aefcc0cf2be681f4be1e35
Jelle van der Waa <jvanderwaa@redhat.com>
2024-08-10 04:41:29 +0200

    feat(linter): implement `eslint-plugin-promise/no-return-in-finally, prefer-await-to-then` rule (#4318)
    Part of https://github.com/oxc-project/oxc/pull/4252
    
    ---------
    
    Co-authored-by: wenzhe <mysteryven@gmail.com>

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -444,7 +444,9 @@ mod tree_shaking {
 mod promise {
     pub mod avoid_new;
     pub mod no_new_statics;
+    pub mod no_return_in_finally;
     pub mod param_names;
+    pub mod prefer_await_to_then;
     pub mod valid_params;
 }
 
@@ -857,6 +859,8 @@ oxc_macros::declare_all_lint_rules! {
     promise::no_new_statics,
     promise::param_names,
     promise::valid_params,
+    promise::no_return_in_finally,
+    promise::prefer_await_to_then,
     vitest::no_import_node_test,
     vitest::prefer_to_be_falsy,
     vitest::prefer_to_be_truthy,
