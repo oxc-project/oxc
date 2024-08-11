a99b0c3ab59a415ee7cbf7415d06101009092c36
eryue0220 <eryue0220@gmail.com>
2024-08-11 21:11:04 +0800

    fix: remove vitest rule

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -451,7 +451,6 @@ mod vitest {
     pub mod no_import_node_test;
     pub mod prefer_to_be_falsy;
     pub mod prefer_to_be_truthy;
-    pub mod require_local_test_context_for_concurrent_snapshots;
 }
 
 oxc_macros::declare_all_lint_rules! {
@@ -859,5 +858,4 @@ oxc_macros::declare_all_lint_rules! {
     vitest::no_import_node_test,
     vitest::prefer_to_be_falsy,
     vitest::prefer_to_be_truthy,
-    vitest::require_local_test_context_for_concurrent_snapshots,
 }
