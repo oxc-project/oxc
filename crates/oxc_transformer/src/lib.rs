f1fcdde593f81d1007b04c8256d9c7fad7747d8f
Dunqing <29533304+Dunqing@users.noreply.github.com>
2024-08-15 16:41:30 +0000

    feat(transformer): support react fast refresh (#4587)
    close: #3943
    
    ## Further improvements
    
    There is a double visit here. We need to collect all react hooks calling in `Function` and `ArrowFunctionExpression`. If we want to remove this implementation, we may wait for #4188.
    
    https://github.com/oxc-project/oxc/blob/d797e595d286c613848b773c256bd43124ad1981/crates/oxc_transformer/src/react/refresh.rs#L744-L947
    
    ## Tests
    
    All tests copy from https://github.com/facebook/react/blob/main/packages/react-refresh/src/__tests__/ReactFresh-test.js
    
    There are still 4 tests that have not been passed
    
    **1. refresh/can-handle-implicit-arrow-returns/input.jsx**
    
    Related to #4767. transform correct, just output doesn't match the expected output
    
    **2. refresh/registers-identifiers-used-in-jsx-at-definition-site/input.jsx**
    **3. refresh/registers-identifiers-used-in-react-create-element-at-definition-site/input.jsx**
    
    Blocked by #4746
    
    **4. refresh/supports-typescript-namespace-syntax/input.tsx**
    
    oxc transforms ts to js first, so probably we can ignore this case. If we really want to pass this test, we also need to turn off `TypeScript` plugin.
    
    ## What's next?
    
    ### Options:
    
    1. Support transform `refresh_reg` and `refresh_sig` options to `MemberExpression`. Currently `import.meta.xxxx` still is an `Identifier`
    2. Support `emit_full_signatures` option
    
    ### Other
    NAPI, testing in `monitor-oxc`, etc..

--- crates/oxc_transformer/src/lib.rs
+++ crates/oxc_transformer/src/lib.rs
@@ -45,7 +45,7 @@ pub use crate::{
     env::EnvOptions,
     es2015::{ArrowFunctionsOptions, ES2015Options},
     options::{BabelOptions, TransformOptions},
-    react::{ReactJsxRuntime, ReactOptions},
+    react::{ReactJsxRuntime, ReactOptions, ReactRefreshOptions},
     typescript::TypeScriptOptions,
 };
 use crate::{
@@ -127,6 +127,7 @@ impl<'a> Transformer<'a> {
 impl<'a> Traverse<'a> for Transformer<'a> {
     fn enter_program(&mut self, program: &mut Program<'a>, ctx: &mut TraverseCtx<'a>) {
         self.x0_typescript.transform_program(program, ctx);
+        self.x1_react.transform_program(program, ctx);
     }
 
     fn exit_program(&mut self, program: &mut Program<'a>, ctx: &mut TraverseCtx<'a>) {
@@ -183,6 +184,7 @@ impl<'a> Traverse<'a> for Transformer<'a> {
     }
 
     fn exit_expression(&mut self, expr: &mut Expression<'a>, ctx: &mut TraverseCtx<'a>) {
+        self.x1_react.transform_expression_on_exit(expr, ctx);
         self.x3_es2015.transform_expression_on_exit(expr, ctx);
     }
 
@@ -265,6 +267,7 @@ impl<'a> Traverse<'a> for Transformer<'a> {
 
     fn enter_statements(&mut self, stmts: &mut Vec<'a, Statement<'a>>, ctx: &mut TraverseCtx<'a>) {
         self.x0_typescript.transform_statements(stmts);
+        self.x1_react.transform_statements(stmts, ctx);
         self.x2_es2021.transform_statements(stmts, ctx);
         self.x2_es2020.transform_statements(stmts, ctx);
         self.x2_es2016.transform_statements(stmts, ctx);
@@ -273,6 +276,7 @@ impl<'a> Traverse<'a> for Transformer<'a> {
 
     fn exit_statements(&mut self, stmts: &mut Vec<'a, Statement<'a>>, ctx: &mut TraverseCtx<'a>) {
         self.x0_typescript.transform_statements_on_exit(stmts, ctx);
+        self.x1_react.transform_statements_on_exit(stmts, ctx);
         self.x2_es2021.transform_statements_on_exit(stmts, ctx);
         self.x2_es2020.transform_statements_on_exit(stmts, ctx);
         self.x2_es2016.transform_statements_on_exit(stmts, ctx);
--- tasks/transform_conformance/oxc.snap.md
+++ tasks/transform_conformance/oxc.snap.md
@@ -1,14 +1,19 @@
 commit: 12619ffe
 
-Passed: 6/9
+Passed: 28/35
 
 # All Passed:
 * babel-plugin-transform-optional-catch-binding
-* babel-plugin-transform-react-jsx
 
 
 # babel-plugin-transform-typescript (4/7)
 * computed-constant-value/input.ts
 * enum-member-reference/input.ts
 * export-elimination/input.ts
+
+# babel-plugin-transform-react-jsx (23/27)
+* refresh/can-handle-implicit-arrow-returns/input.jsx
+* refresh/registers-identifiers-used-in-jsx-at-definition-site/input.jsx
+* refresh/registers-identifiers-used-in-react-create-element-at-definition-site/input.jsx
+* refresh/supports-typescript-namespace-syntax/input.tsx
 
