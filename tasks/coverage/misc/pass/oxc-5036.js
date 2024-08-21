622fce6ee5346330f95211f7970526f4b4b7cfdc
heygsc <1596920983@qq.com>
2024-08-22 01:11:03 +0800

    test: update path

---
+++ tasks/coverage/misc/pass/oxc-5036.js
@@ -0,0 +1,16 @@
+Parent1: {
+    Son1: {
+        Son11: console.log("Son11");
+        Son11: console.log("Son11");
+    }
+    Son2: {
+        Son21: console.log("Son21");
+        Son22: console.log("Son22");
+    }
+}
+Parent2: {
+    Son1: {
+        Son21: console.log("Son21");
+        Son22: console.log("Son22");
+    }
+}
