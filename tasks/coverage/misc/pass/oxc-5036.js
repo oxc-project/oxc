2df9d235bfe14d1a351703e78521978218491a4b
heygsc <1596920983@qq.com>
2024-08-22 16:25:35 +0800

    chore: update test

--- tasks/coverage/misc/pass/oxc-5036.js
+++ tasks/coverage/misc/pass/oxc-5036.js
@@ -1,41 +1,41 @@
 Parent: {
-    Child: {
-        Child1: console.log("Child1");
-        Child1: console.log("Child2");
+    Child1: {
+        Child2: console.log("Child1");
+        Child2: console.log("Child2");
     }
-    Child: {
-        Child1: console.log("Child3");
-        Child1: console.log("Child4");
+    Child1: {
+        Child2: console.log("Child3");
+        Child2: console.log("Child4");
     }
-    Child: {
+    Child2: {
         Child1: console.log("Child5");
         Child1: console.log("Child6");
     }
 }
 
 Parent: {
-    Child: {
-        Child1: console.log("Child7");
-        Child1: console.log("Child8");
+    Child1: {
+        Child2: console.log("Child7");
+        Child2: console.log("Child8");
     }
 }
 
 Parent: {
-    Child: {
-        Child1: {
-            Child2: {
-                Child3: console.log("Child9");
+    Child1: {
+        Child12: {
+            Child3: {
+                Child4: {
+                    Child5: console.log("Child9");
+                }
             }
         }
     }
-    Child: console.log("Child10");
-    Child: console.log("Child11");
-    Child: {
-        Child1: {
-            Child2: {
-                Child3: console.log("Child12");
-            }
+    Child1: console.log("Child10");
+    Child2: console.log("Child11");
+    Child1: {
+        Child2: {
+            Child3: console.log("Child12");
         }
     }
-    Child: console.log("Child13");
+    Child3: console.log("Child13");
 }
