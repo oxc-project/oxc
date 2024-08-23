dd3ad4d68e3404f125e99aa54d22849d9c7e54f6
Boshen <boshenc@gmail.com>
2024-08-23 16:13:35 +0800

    chore(regular_expression): remove circular dependency
    Error: Circular dependency detected: oxc_parser -> oxc_regular_expression

--- crates/oxc_regular_expression/examples/test.js
+++
@@ -1,7 +0,0 @@
-// All of them should be the same result!
-[
-  /\1(.)\\"'`a/v,
-  new RegExp("\\1(.)\\\\\"'`\a","v"),
-  new RegExp('\\1(.)\\\\"\'`\a','v'),
-  new RegExp(`\\1(.)\\\\"'\`\a`,`v`),
-]
