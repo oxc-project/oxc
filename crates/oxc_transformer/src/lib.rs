5136f011d9effbaf4b19e1c0f0c2bf3eeee4ae2e
overlookmotel <557937+overlookmotel@users.noreply.github.com>
2024-08-23 23:10:37 +0000

    refactor(transformer): remove unnecessary type annotation (#5131)
    Remove unnecessary type annotation introduced in #4925.

--- crates/oxc_transformer/src/lib.rs
+++ crates/oxc_transformer/src/lib.rs
@@ -108,7 +108,7 @@ impl<'a> Transformer<'a> {
         scopes: ScopeTree,
         program: &mut Program<'a>,
     ) -> TransformerReturn {
-        let allocator: &'a Allocator = self.ctx.ast.allocator;
+        let allocator = self.ctx.ast.allocator;
         let (symbols, scopes) = traverse_mut(&mut self, allocator, program, symbols, scopes);
         TransformerReturn { errors: self.ctx.take_errors(), symbols, scopes }
     }
