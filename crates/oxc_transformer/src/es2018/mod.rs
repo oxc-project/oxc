164511549f262fd07f26eedb107a72590e3e452c
Dunqing <29533304+Dunqing@users.noreply.github.com>
2024-08-29 07:35:08 +0000

    refactor(transformer/object-reset-spread): make plugin initialization unconditional (#5319)
    Align with other plugins

--- crates/oxc_transformer/src/es2018/mod.rs
+++ crates/oxc_transformer/src/es2018/mod.rs
@@ -17,15 +17,16 @@ pub struct ES2018<'a> {
     options: ES2018Options,
 
     // Plugins
-    object_rest_spread: Option<ObjectRestSpread<'a>>,
+    object_rest_spread: ObjectRestSpread<'a>,
 }
 
 impl<'a> ES2018<'a> {
     pub fn new(options: ES2018Options, ctx: Ctx<'a>) -> Self {
         Self {
-            object_rest_spread: options
-                .object_rest_spread
-                .map(|options| ObjectRestSpread::new(options, Rc::clone(&ctx))),
+            object_rest_spread: ObjectRestSpread::new(
+                options.object_rest_spread.unwrap_or_default(),
+                Rc::clone(&ctx),
+            ),
             ctx,
             options,
         }
@@ -34,8 +35,8 @@ impl<'a> ES2018<'a> {
 
 impl<'a> Traverse<'a> for ES2018<'a> {
     fn enter_expression(&mut self, expr: &mut Expression<'a>, ctx: &mut TraverseCtx<'a>) {
-        if let Some(object_rest_spread) = &mut self.object_rest_spread {
-            object_rest_spread.enter_expression(expr, ctx);
+        if self.options.object_rest_spread.is_some() {
+            self.object_rest_spread.enter_expression(expr, ctx);
         }
     }
 }
