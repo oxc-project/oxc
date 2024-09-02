53266cb1392c837ff9eadc8f86d53140d89e8c5b
Cameron McHenry <camchenry@users.noreply.github.com>
2024-09-01 22:38:08 -0400

    Add support for createElement calls

--- crates/oxc_linter/src/rules/react/no_danger_with_children.rs
+++ crates/oxc_linter/src/rules/react/no_danger_with_children.rs
@@ -41,27 +41,77 @@ declare_oxc_lint!(
 
 impl Rule for NoDangerWithChildren {
     fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
-        println!("\n-----------------\n");
-        dbg!(ctx.source_text());
+        match node.kind() {
+            AstKind::JSXElement(jsx) => {
+                let has_children = if !jsx.children.is_empty() && !is_line_break(&jsx.children[0]) {
+                    true
+                } else if has_jsx_prop(ctx, node, "children") {
+                    true
+                } else {
+                    false
+                };
 
-        if let AstKind::JSXElement(jsx) = node.kind() {
-            let has_children = if !jsx.children.is_empty() && !is_line_break(&jsx.children[0]) {
-                true
-            } else if has_jsx_prop(ctx, node, "children") {
-                true
-            } else {
-                false
-            };
+                if !has_children {
+                    return;
+                }
 
-            if !has_children {
-                return;
+                let has_danger_prop = has_jsx_prop(ctx, node, "dangerouslySetInnerHTML");
+
+                if has_danger_prop {
+                    ctx.diagnostic(no_danger_with_children_diagnostic(jsx.span));
+                }
             }
+            AstKind::CallExpression(call_expr) => {
+                if call_expr.arguments.len() <= 1 {
+                    return;
+                }
+                if let Expression::StaticMemberExpression(callee) = &call_expr.callee {
+                    if callee.property.name != "createElement" {
+                        return;
+                    }
 
-            let has_danger_prop = has_jsx_prop(ctx, node, "dangerouslySetInnerHTML");
+                    let props = call_expr
+                        .arguments
+                        .get(1)
+                        .expect("Could not get props argument for createElement call")
+                        .as_expression();
 
-            if has_danger_prop {
-                ctx.diagnostic(no_danger_with_children_diagnostic(jsx.span));
+                    dbg!(call_expr);
+
+                    match props {
+                        Some(Expression::ObjectExpression(obj_expr)) => {
+                            let has_danger_prop = is_object_with_prop_name(
+                                &obj_expr.properties,
+                                "dangerouslySetInnerHTML",
+                            );
+
+                            // If there are three arguments, then it is a JSX element with children.
+                            // If it's just two arguments, it only has children if the props object has a children property.
+                            let has_children = if call_expr.arguments.len() == 2 {
+                                is_object_with_prop_name(&obj_expr.properties, "children")
+                            } else {
+                                true
+                            };
+
+                            if has_danger_prop && has_children {
+                                ctx.diagnostic(no_danger_with_children_diagnostic(call_expr.span));
+                            }
+                        }
+                        Some(Expression::Identifier(ident)) => {
+                            if does_object_var_have_prop_name(
+                                ctx,
+                                node,
+                                &ident.name,
+                                "dangerouslySetInnerHTML",
+                            ) {
+                                ctx.diagnostic(no_danger_with_children_diagnostic(call_expr.span));
+                            }
+                        }
+                        _ => (),
+                    }
+                }
             }
+            _ => (),
         }
     }
 }
@@ -76,25 +126,25 @@ fn test() {
         r#"<div dangerouslySetInnerHTML={{ __html: "HTML" }} />"#,
         r#"<div children="Children" />"#,
         r#"
-			        const props = { dangerouslySetInnerHTML: { __html: "HTML" } };
-			        <div {...props} />
-			      "#,
+        const props = { dangerouslySetInnerHTML: { __html: "HTML" } };
+        <div {...props} />
+        "#,
         r#"
-			        const moreProps = { className: "eslint" };
-			        const props = { children: "Children", ...moreProps };
-			        <div {...props} />
-			      "#,
+        const moreProps = { className: "eslint" };
+        const props = { children: "Children", ...moreProps };
+        <div {...props} />
+        "#,
         r#"
-			        const otherProps = { children: "Children" };
-			        const { a, b, ...props } = otherProps;
-			        <div {...props} />
-			      "#,
+        const otherProps = { children: "Children" };
+        const { a, b, ...props } = otherProps;
+        <div {...props} />
+        "#,
         "<Hello>Children</Hello>",
         r#"<Hello dangerouslySetInnerHTML={{ __html: "HTML" }} />"#,
         r#"
-			        <Hello dangerouslySetInnerHTML={{ __html: "HTML" }}>
-			        </Hello>
-			      "#,
+        <Hello dangerouslySetInnerHTML={{ __html: "HTML" }}>
+        </Hello>
+        "#,
         r#"React.createElement("div", { dangerouslySetInnerHTML: { __html: "HTML" } });"#,
         r#"React.createElement("div", {}, "Children");"#,
         r#"React.createElement("Hello", { dangerouslySetInnerHTML: { __html: "HTML" } });"#,
@@ -102,79 +152,79 @@ fn test() {
         "<Hello {...undefined}>Children</Hello>",
         r#"React.createElement("Hello", undefined, "Children")"#,
         "
-			        const props = {...props, scratch: {mode: 'edit'}};
-			        const component = shallow(<TaskEditableTitle {...props} />);
-			      ",
+        const props = {...props, scratch: {mode: 'edit'}};
+        const component = shallow(<TaskEditableTitle {...props} />);
+        ",
     ];
 
     let fail = vec![
         r#"
-        	        <div dangerouslySetInnerHTML={{ __html: "HTML" }}>
-        	          Children
-        	        </div>
-        	      "#,
+        <div dangerouslySetInnerHTML={{ __html: "HTML" }}>
+            Children
+        </div>
+        "#,
         r#"<div dangerouslySetInnerHTML={{ __html: "HTML" }} children="Children" />"#,
         r#"
-			        const props = { dangerouslySetInnerHTML: { __html: "HTML" } };
-			        <div {...props}>Children</div>
-			      "#,
+        const props = { dangerouslySetInnerHTML: { __html: "HTML" } };
+        <div {...props}>Children</div>
+        "#,
         r#"
-        	        const props = { children: "Children", dangerouslySetInnerHTML: { __html: "HTML" } };
-        	        <div {...props} />
-        	      "#,
+        const props = { children: "Children", dangerouslySetInnerHTML: { __html: "HTML" } };
+        <div {...props} />
+        "#,
         r#"
-        	        <Hello dangerouslySetInnerHTML={{ __html: "HTML" }}>
-        	          Children
-        	        </Hello>
-        	      "#,
+        <Hello dangerouslySetInnerHTML={{ __html: "HTML" }}>
+            Children
+        </Hello>
+        "#,
         r#"<Hello dangerouslySetInnerHTML={{ __html: "HTML" }} children="Children" />"#,
         r#"<Hello dangerouslySetInnerHTML={{ __html: "HTML" }}> </Hello>"#,
         r#"
-        	        React.createElement(
-        	          "div",
-        	          { dangerouslySetInnerHTML: { __html: "HTML" } },
-        	          "Children"
-        	        );
-        	      "#,
+        React.createElement(
+            "div",
+            { dangerouslySetInnerHTML: { __html: "HTML" } },
+            "Children"
+        );
+        "#,
         r#"
-        	        React.createElement(
-        	          "div",
-        	          {
-        	            dangerouslySetInnerHTML: { __html: "HTML" },
-        	            children: "Children",
-        	          }
-        	        );
-        	      "#,
+        React.createElement(
+            "div",
+            {
+                dangerouslySetInnerHTML: { __html: "HTML" },
+                children: "Children",
+            }
+        );
+        "#,
         r#"
-        	        React.createElement(
-        	          "Hello",
-        	          { dangerouslySetInnerHTML: { __html: "HTML" } },
-        	          "Children"
-        	        );
-        	      "#,
+        React.createElement(
+            "Hello",
+            { dangerouslySetInnerHTML: { __html: "HTML" } },
+            "Children"
+        );
+        "#,
         r#"
-        	        React.createElement(
-        	          "Hello",
-        	          {
-        	            dangerouslySetInnerHTML: { __html: "HTML" },
-        	            children: "Children",
-        	          }
-        	        );
-        	      "#,
+        React.createElement(
+            "Hello",
+            {
+                dangerouslySetInnerHTML: { __html: "HTML" },
+                children: "Children",
+            }
+        );
+        "#,
         r#"
-        	        const props = { dangerouslySetInnerHTML: { __html: "HTML" } };
-        	        React.createElement("div", props, "Children");
-        	      "#,
+        const props = { dangerouslySetInnerHTML: { __html: "HTML" } };
+        React.createElement("div", props, "Children");
+        "#,
         r#"
-        	        const props = { children: "Children", dangerouslySetInnerHTML: { __html: "HTML" } };
-        	        React.createElement("div", props);
-        	      "#,
+        const props = { children: "Children", dangerouslySetInnerHTML: { __html: "HTML" } };
+        React.createElement("div", props);
+        "#,
         r#"
-        	        const moreProps = { children: "Children" };
-        	        const otherProps = { ...moreProps };
-        	        const props = { ...otherProps, dangerouslySetInnerHTML: { __html: "HTML" } };
-        	        React.createElement("div", props);
-        	      "#,
+        const moreProps = { children: "Children" };
+        const otherProps = { ...moreProps };
+        const props = { ...otherProps, dangerouslySetInnerHTML: { __html: "HTML" } };
+        React.createElement("div", props);
+        "#,
     ];
 
     Tester::new(NoDangerWithChildren::NAME, pass, fail).test_and_snapshot();
@@ -206,36 +256,8 @@ fn has_jsx_prop<'a>(ctx: &LintContext<'a>, node: &AstNode<'a>, prop_name: &'stat
                 }
             }
             JSXAttributeItem::SpreadAttribute(attr) => {
-                println!("I am a spread attribute");
-                dbg!(attr);
-                if let Some(Some(symbol)) = attr.argument.get_identifier_reference().map(|ident| {
-                    ctx.scopes()
-                        .find_binding(node.scope_id(), ident.name.as_str())
-                        .map(|symbol_id| ctx.semantic().symbol_declaration(symbol_id))
-                }) {
-                    dbg!(symbol);
-                    if let AstKind::VariableDeclarator(var_decl) = symbol.kind() {
-                        if let Some(init) = &var_decl.init {
-                            if let Expression::ObjectExpression(obj_expr) = init {
-                                obj_expr.properties.iter().any(|prop| match prop {
-                                    ObjectPropertyKind::ObjectProperty(obj_prop) => {
-                                        if let Some(key) = obj_prop.key.static_name() {
-                                            key == prop_name
-                                        } else {
-                                            false
-                                        }
-                                    }
-                                    _ => false,
-                                })
-                            } else {
-                                false
-                            }
-                        } else {
-                            false
-                        }
-                    } else {
-                        false
-                    }
+                if let Some(ident) = attr.argument.get_identifier_reference() {
+                    does_object_var_have_prop_name(ctx, node, &ident.name.as_str(), prop_name)
                 } else {
                     false
                 }
@@ -244,4 +266,50 @@ fn has_jsx_prop<'a>(ctx: &LintContext<'a>, node: &AstNode<'a>, prop_name: &'stat
     } else {
         false
     }
+}
+
+fn does_object_var_have_prop_name<'a>(
+    ctx: &LintContext<'a>,
+    node: &AstNode<'a>,
+    name: &str,
+    prop_name: &str,
+) -> bool {
+    if let Some(symbol) = ctx
+        .scopes()
+        .find_binding(node.scope_id(), name)
+        .map(|symbol_id| ctx.semantic().symbol_declaration(symbol_id))
+    {
+        if let AstKind::VariableDeclarator(var_decl) = symbol.kind() {
+            if let Some(init) = &var_decl.init {
+                if let Expression::ObjectExpression(obj_expr) = init {
+                    is_object_with_prop_name(&obj_expr.properties, prop_name)
+                } else {
+                    false
+                }
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    } else {
+        false
+    }
+}
+
+/// Returns whether a given object has a property with the given name.
+fn is_object_with_prop_name(
+    obj_props: &oxc_allocator::Vec<'_, ObjectPropertyKind<'_>>,
+    prop_name: &str,
+) -> bool {
+    obj_props.iter().any(|prop| match prop {
+        ObjectPropertyKind::ObjectProperty(obj_prop) => {
+            if let Some(key) = obj_prop.key.static_name() {
+                key == prop_name
+            } else {
+                false
+            }
+        }
+        _ => false,
+    })
 }
