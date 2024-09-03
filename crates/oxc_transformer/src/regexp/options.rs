26f85aa2f2e8e93b2fa753da70afe284dbc612ab
overlookmotel <theoverlookmotel@gmail.com>
2024-09-03 16:01:29 +0100

    Correct typos

--- crates/oxc_transformer/src/regexp/options.rs
+++ crates/oxc_transformer/src/regexp/options.rs
@@ -2,21 +2,21 @@ use crate::env::{can_enable_plugin, Versions};
 
 #[derive(Default, Debug, Clone, Copy)]
 pub struct RegExpOptions {
-    /// Enables plugin for transform the RegExp literal has `y` flag
+    /// Enables plugin to transform the RegExp literal has `y` flag
     pub sticky_flag: bool,
-    /// Enables plugin for transform the RegExp literal has `u` flag
+    /// Enables plugin to transform the RegExp literal has `u` flag
     pub unicode_flag: bool,
-    /// Enables plugin for transform the RegExp literal has `s` flag
+    /// Enables plugin to transform the RegExp literal has `s` flag
     pub dot_all_flag: bool,
-    /// Enables plugin for transform the RegExp literal has `(?<=)` or `(?<!)` lookbehind assertions
+    /// Enables plugin to transform the RegExp literal has `(?<=)` or `(?<!)` lookbehind assertions
     pub look_behind_assertions: bool,
-    /// Enables plugin for transform the RegExp literal has `(?<name>x)` named capture groups
+    /// Enables plugin to transform the RegExp literal has `(?<name>x)` named capture groups
     pub named_capture_groups: bool,
-    /// Enables plugin for transform the RegExp literal has `\p{}` and `\P{}` unicode property escapes
+    /// Enables plugin to transform the RegExp literal has `\p{}` and `\P{}` unicode property escapes
     pub unicode_property_escapes: bool,
-    /// Enables plugin for transform `d` flag
+    /// Enables plugin to transform `d` flag
     pub match_indices: bool,
-    /// Enables plugin for transform the RegExp literal has `v` flag
+    /// Enables plugin to transform the RegExp literal has `v` flag
     pub set_notation: bool,
 }
 
