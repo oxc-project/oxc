bc7be70077ff08aba75dd915136db12fe8419b54
dalaoshu <laipichan@qq.com>
2024-09-05 21:20:55 +0800

    chore(linter/react_perf): improve docs for react_perf rules (#5481)
    Please fill in the "why this is bad" section
    
    _Originally posted by @DonIsaac in
    https://github.com/oxc-project/oxc/pull/5360#discussion_r1739617907_

--- crates/oxc_linter/src/rules/react_perf/jsx_no_jsx_as_prop.rs
+++ crates/oxc_linter/src/rules/react_perf/jsx_no_jsx_as_prop.rs
@@ -10,16 +10,27 @@ pub struct JsxNoJsxAsProp;
 declare_oxc_lint!(
     /// ### What it does
     ///
-    /// Prevent JSX that are local to the current method from being used as values of JSX props
+    /// Prevent JSX elements that are local to the current method from being
+    /// used as values of JSX props.
+    ///
+    /// ### Why is this bad?
+    ///
+    /// Using locally defined JSX elements as values for props can lead to
+    /// unintentional re-renders and performance issues. Every time the parent
+    /// renders, a new instance of the JSX element is created, causing unnecessary
+    /// re-renders of child components. This also leads to harder-to-maintain code
+    /// as the component's props are not passed consistently.
     ///
     /// ### Example
+    /// Examples of **incorrect** code for this rule:
     /// ```jsx
-    /// // Bad
     /// <Item jsx={<SubItem />} />
     /// <Item jsx={this.props.jsx || <SubItem />} />
     /// <Item jsx={this.props.jsx ? this.props.jsx : <SubItem />} />
+    /// ```
     ///
-    /// // Good
+    /// Examples of **correct** code for this rule:
+    /// ```jsx
     /// <Item callback={this.props.jsx} />
     /// ```
     JsxNoJsxAsProp,
