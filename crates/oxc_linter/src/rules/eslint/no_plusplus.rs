f04d61c889853a6cfebfc2baacf56c0a51d13aeb
Cameron McHenry <camchenry@users.noreply.github.com>
2024-09-07 00:31:48 -0400

    Add checking for for-loop updates

--- crates/oxc_linter/src/rules/eslint/no_plusplus.rs
+++ crates/oxc_linter/src/rules/eslint/no_plusplus.rs
@@ -1,4 +1,4 @@
-use oxc_ast::AstKind;
+use oxc_ast::{ast::Expression, AstKind};
 use oxc_diagnostics::OxcDiagnostic;
 use oxc_macros::declare_oxc_lint;
 use oxc_span::Span;
@@ -10,16 +10,19 @@ fn no_plusplus_diagnostic(span: Span, operator: &str) -> OxcDiagnostic {
         OxcDiagnostic::warn(format!("Unary operator '{}' used.", operator)).with_label(span);
 
     if operator == "++" {
-        return diagnostic.with_help("Use the assignment expression `+=` instead.");
+        return diagnostic.with_help("Use the assignment operator `+=` instead.");
     } else if operator == "--" {
-        return diagnostic.with_help("Use the assignment expression `-=` instead.");
+        return diagnostic.with_help("Use the assignment operator `-=` instead.");
     }
 
     diagnostic
 }
 
 #[derive(Debug, Default, Clone)]
-pub struct NoPlusplus;
+pub struct NoPlusplus {
+    /// Whether to allow `++` and `--` in for loop afterthoughts.
+    allow_for_loop_afterthoughts: bool,
+}
 
 declare_oxc_lint!(
     /// ### What it does
@@ -74,13 +77,74 @@ declare_oxc_lint!(
 );
 
 impl Rule for NoPlusplus {
+    fn from_configuration(value: serde_json::Value) -> Self {
+        let obj = value.get(0);
+        Self {
+            allow_for_loop_afterthoughts: obj
+                .and_then(|v| v.get("allowForLoopAfterthoughts"))
+                .and_then(serde_json::Value::as_bool)
+                .unwrap_or(false),
+        }
+    }
+
     fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
         let AstKind::UpdateExpression(expr) = node.kind() else {
             return;
         };
 
+        if self.allow_for_loop_afterthoughts && is_for_loop_afterthought(node, ctx) {
+            return;
+        }
+
         ctx.diagnostic(no_plusplus_diagnostic(expr.span, expr.operator.as_str()));
     }
+}
+
+/// Whether the given AST node is a ++ or -- inside of a for-loop update.
+fn is_for_statement_update(node: &AstNode, ctx: &LintContext) -> bool {
+    let Some(parent) = ctx.nodes().parent_node(node.id()) else {
+        return false;
+    };
+    let AstKind::ForStatement(for_stmt) = parent.kind() else {
+        return false;
+    };
+
+    for_stmt.update.as_ref().is_some_and(|update| is_eq_node_expr(node, update))
+}
+
+/// Checks if the given node is equivalent to the given expression (i.e., they have the same span).
+fn is_eq_node_expr(node: &AstNode, expr: &Expression) -> bool {
+    // TODO: This logic should be moved to somewhere more general and shared across rules and expanded
+    // to cover all expressions and node types
+    let node_span = match node.kind() {
+        AstKind::UpdateExpression(expr) => expr.span,
+        AstKind::SequenceExpression(expr) => expr.span,
+        _ => return false,
+    };
+    let expr_span = match expr {
+        Expression::UpdateExpression(expr) => expr.span,
+        Expression::SequenceExpression(expr) => expr.span,
+        _ => return false,
+    };
+    node_span == expr_span
+}
+
+/// Determines whether the given node is considered to be a for loop "afterthought" by the logic of this rule.
+/// In particular, it returns `true` if the given node is either:
+///   - The update node of a `ForStatement`: for (;; i++) {}
+///   - An operand of a sequence expression that is the update node: for (;; foo(), i++) {}
+///   - An operand of a sequence expression that is child of another sequence expression, etc.,
+///     up to the sequence expression that is the update node: for (;; foo(), (bar(), (baz(), i++))) {}
+fn is_for_loop_afterthought(node: &AstNode, ctx: &LintContext) -> bool {
+    if let Some(parent) = ctx.nodes().parent_node(node.id()) {
+        match parent.kind() {
+            AstKind::SequenceExpression(_) => return is_for_loop_afterthought(parent, ctx),
+            AstKind::ParenthesizedExpression(_) => return is_for_loop_afterthought(parent, ctx),
+            _ => (),
+        }
+    }
+
+    is_for_statement_update(node, ctx)
 }
 
 #[test]
@@ -89,6 +153,8 @@ fn test() {
 
     let pass = vec![
         ("var foo = 0; foo=+1;", None),
+        ("var foo = 0; foo+=1;", None),
+        ("var foo = 0; foo-=1;", None),
         ("var foo = 0; foo=+1;", Some(serde_json::json!([{ "allowForLoopAfterthoughts": true }]))),
         (
             "for (i = 0; i < l; i++) { console.log(i); }",
@@ -125,6 +191,8 @@ fn test() {
     let fail = vec![
         ("var foo = 0; foo++;", None),
         ("var foo = 0; foo--;", None),
+        ("var foo = 0; --foo;", None),
+        ("var foo = 0; ++foo;", None),
         ("for (i = 0; i < l; i++) { console.log(i); }", None),
         ("for (i = 0; i < l; foo, i++) { console.log(i); }", None),
         ("var foo = 0; foo++;", Some(serde_json::json!([{ "allowForLoopAfterthoughts": true }]))),
