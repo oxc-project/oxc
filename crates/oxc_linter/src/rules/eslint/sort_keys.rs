f9547ff11a3e611adaa08dee8b68d4aea560a100
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-09-11 16:43:44 +0200

    chore: applied just r

--- crates/oxc_linter/src/rules/eslint/sort_keys.rs
+++ crates/oxc_linter/src/rules/eslint/sort_keys.rs
@@ -167,7 +167,6 @@ impl Rule for SortKeys {
 
             if !self.case_sensitive {
                 for group in &mut property_groups {
-
                     *group = group.iter().map(|s| s.to_lowercase()).collect::<Vec<String>>();
                 }
             }
