15b6b3b25be510437de0fa98b59a69f4ff283929
Don Isaac <donald.isaac@gmail.com>
2024-09-22 16:44:15 -0400

    add identifier reference test to default suite

---
+++ crates/oxc_semantic/tests/conformance/test_symbol_declaration.rs
@@ -0,0 +1,130 @@
+use oxc_ast::ast::BindingPattern;
+use oxc_ast::{ast::BindingIdentifier, AstKind};
+use oxc_diagnostics::OxcDiagnostic;
+use oxc_span::{GetSpan, Span};
+use oxc_syntax::symbol::SymbolId;
+
+use super::{ConformanceTest, TestResult};
+use crate::Semantic;
+
+/// Verifies that symbol binding relationships between the SymbolTable and AST nodes are reflexive.
+///
+/// What does this mean?
+/// 1. [`SymbolTable`] stores the AST node id of the node declaring a symbol.
+/// 2. That symbol should _always_ be a declaration-like node containing either a
+///    [`BindingIdentifier`] or a [`BindingPattern`].
+/// 3. The binding pattern or identifier in that node should be populated (e.g. not [`None`]) and
+///    contain the symbol id.
+///
+/// [`SymbolTable`]: oxc_semantic::SymbolTable
+#[derive(Debug, Clone, Default)]
+pub struct SymbolDeclarationTest;
+
+/// The binding pattern or identifier contained in the declaration node is [`None`].
+///
+/// See: [`BindingIdentifier::symbol_id`]
+fn bound_to_statement_with_no_binding_identifier(
+    symbol_id: SymbolId,
+    span: Span,
+    statement_kind: &str,
+) -> TestResult {
+    OxcDiagnostic::error(format!(
+        "Symbol {symbol_id:?} got bound to a {statement_kind} with no BindingIdentifier"
+    ))
+    .with_label(span.label("Symbol was declared here"))
+    .into()
+}
+
+/// [`BindingIdentifier::symbol_id`] contained [`Some`] value, but it was not the [`SymbolId`] used
+/// to find it in the [`SymbolTable`].
+fn symbol_declaration_not_in_ast_node(
+    expected_id: SymbolId,
+    binding: &BindingIdentifier,
+) -> TestResult {
+    let bound_id = binding.symbol_id.get();
+    OxcDiagnostic::error(format!(
+        "Expected binding to be bound to {expected_id:?} but it was bound to {bound_id:?}"
+    ))
+    .with_label(binding.span())
+    .into()
+}
+
+/// Found a non-destructuring [`BindingPattern`] that did not contain a [`BindingIdentifier`].
+fn malformed_binding_pattern(expected_id: SymbolId, pattern: &BindingPattern) -> TestResult {
+    OxcDiagnostic::error(format!("BindingPattern for {expected_id:?} is not a destructuring pattern but get_binding_identifier() still returned None"))
+        .with_label(pattern.span().label("BindingPattern is here"))
+        .into()
+}
+
+fn invalid_declaration_node(kind: AstKind) -> TestResult {
+    OxcDiagnostic::error(format!("Invalid declaration node kind: {}", kind.debug_name()))
+        .with_label(kind.span())
+        .into()
+}
+
+impl ConformanceTest for SymbolDeclarationTest {
+    fn name(&self) -> &'static str {
+        "symbol-declaration"
+    }
+
+    fn run_on_symbol(
+        &self,
+        symbol_id: oxc_semantic::SymbolId,
+        semantic: &Semantic<'_>,
+    ) -> TestResult {
+        let declaration_id = semantic.symbols().get_declaration(symbol_id);
+        let declaration = semantic.nodes().get_node(declaration_id);
+        let span = semantic.symbols().get_span(symbol_id);
+
+        match declaration.kind() {
+            AstKind::VariableDeclarator(decl) => check_binding_pattern(symbol_id, &decl.id),
+            AstKind::CatchParameter(caught) => check_binding_pattern(symbol_id, &caught.pattern),
+            AstKind::Function(func) => match func.id.as_ref() {
+                Some(id) => check_binding(symbol_id, id),
+                None => bound_to_statement_with_no_binding_identifier(symbol_id, span, "Function"),
+            },
+            AstKind::Class(class) => match class.id.as_ref() {
+                Some(id) => check_binding(symbol_id, id),
+                None => bound_to_statement_with_no_binding_identifier(symbol_id, span, "Class"),
+            },
+            AstKind::BindingRestElement(rest) => check_binding_pattern(symbol_id, &rest.argument),
+            AstKind::FormalParameter(param) => check_binding_pattern(symbol_id, &param.pattern),
+            AstKind::ImportSpecifier(import) => check_binding(symbol_id, &import.local),
+            AstKind::ImportNamespaceSpecifier(import) => check_binding(symbol_id, &import.local),
+            AstKind::ImportDefaultSpecifier(import) => check_binding(symbol_id, &import.local),
+            // =========================== TYPESCRIPT ===========================
+            AstKind::TSImportEqualsDeclaration(import) => check_binding(symbol_id, &import.id),
+            AstKind::TSTypeParameter(decl) => check_binding(symbol_id, &decl.name),
+            // NOTE: namespaces do not store the symbol id they create. We may want to add this in
+            // the future.
+            AstKind::TSModuleDeclaration(_decl) => TestResult::Pass,
+            AstKind::TSTypeAliasDeclaration(decl) => check_binding(symbol_id, &decl.id),
+            AstKind::TSInterfaceDeclaration(decl) => check_binding(symbol_id, &decl.id),
+            AstKind::TSEnumDeclaration(decl) => check_binding(symbol_id, &decl.id),
+            // NOTE: enum members do not store the symbol id they create. We may want to add this
+            // in the future.
+            AstKind::TSEnumMember(_member) => TestResult::Pass,
+            invalid_kind => invalid_declaration_node(invalid_kind),
+        }
+    }
+}
+
+fn check_binding_pattern(expected_id: SymbolId, binding: &BindingPattern) -> TestResult {
+    if binding.kind.is_destructuring_pattern() {
+        return TestResult::Pass;
+    }
+
+    let Some(id) = binding.kind.get_binding_identifier() else {
+        return malformed_binding_pattern(expected_id, binding);
+    };
+
+    check_binding(expected_id, id)
+}
+
+fn check_binding(expected_id: SymbolId, binding: &BindingIdentifier) -> TestResult {
+    if binding.symbol_id.get() == Some(expected_id) {
+        TestResult::Pass
+    } else {
+        symbol_declaration_not_in_ast_node(expected_id, binding)
+    }
+}
--- crates/oxc_semantic/tests/main.rs
+++ crates/oxc_semantic/tests/main.rs
@@ -1,10 +1,19 @@
+mod conformance;
+
 use std::{fs, path::Path};
 
+use conformance::SemanticConformance;
 use oxc_allocator::Allocator;
 use oxc_index::Idx;
 use oxc_parser::Parser;
 use oxc_semantic::{ScopeId, Semantic, SemanticBuilder};
 use oxc_span::SourceType;
+
+/// A test case representing an input source file.
+struct TestContext<'a> {
+    pub path: &'a Path,
+    pub semantic: Semantic<'a>,
+}
 
 fn get_scope_snapshot(semantic: &Semantic, scopes: impl Iterator<Item = ScopeId>) -> String {
     let scope_tree = semantic.scopes();
@@ -95,16 +104,55 @@ fn get_scope_snapshot(semantic: &Semantic, scopes: impl Iterator<Item = ScopeId>
     result
 }
 
-fn analyze(path: &Path, source_text: &str) -> String {
+fn run_scope_snapshot_test(ctx: &TestContext<'_>) -> String {
+    let scopes = vec![ctx.semantic.scopes().root_scope_id()].into_iter();
+    // this is a JSON object
+    let scopes = get_scope_snapshot(&ctx.semantic, scopes);
+
+    // pretty-print the results
+    let value: serde_json::Value = serde_json::from_str(scopes.as_str()).unwrap();
+    serde_json::to_string_pretty(&value).unwrap()
+}
+
+fn analyze(path: &Path, source_text: &str, conformance_suite: &SemanticConformance) -> String {
+    /// Separator between snapshot sections
+    const SEP: &str =
+        "================================================================================";
+
     let allocator = Allocator::default();
     let source_type = SourceType::from_path(path).unwrap();
 
     let ret = Parser::new(&allocator, source_text, source_type).parse();
-    let semantic = SemanticBuilder::new(source_text).build(&ret.program).semantic;
+    let semantic = SemanticBuilder::new(source_text)
+        .with_check_syntax_error(true)
+        .build(&ret.program)
+        .semantic;
 
-    let scopes = get_scope_snapshot(&semantic, vec![semantic.scopes().root_scope_id()].into_iter());
-    let value: serde_json::Value = serde_json::from_str(scopes.as_str()).unwrap();
-    serde_json::to_string_pretty(&value).unwrap()
+    let ctx = TestContext { path, semantic };
+
+    // scope tests
+    let scope_snapshot = run_scope_snapshot_test(&ctx);
+
+    // conformance tests
+    let mut conformance_snapshot = conformance_suite.run_on_source(&ctx);
+    if conformance_snapshot.is_empty() {
+        conformance_snapshot = "All tests passed.".to_string();
+    }
+
+    format!(
+        "{SEP}
+SCOPES
+{SEP}
+
+{scope_snapshot}
+
+{SEP}
+CONFORMANCE
+{SEP}
+
+{conformance_snapshot}
+"
+    )
 }
 
 /// # Panics
@@ -113,7 +161,8 @@ fn analyze(path: &Path, source_text: &str) -> String {
 fn main() {
     insta::glob!("fixtures/**/*.{js,jsx,ts,tsx}", |path| {
         let source_text = fs::read_to_string(path).unwrap();
-        let snapshot = analyze(path, &source_text);
+        let conformance_suite = conformance::conformance_suite();
+        let snapshot = analyze(path, &source_text, &conformance_suite);
         let name = path.file_stem().unwrap().to_str().unwrap();
         insta::with_settings!({ snapshot_path => path.parent().unwrap(), prepend_module_to_snapshot => false, snapshot_suffix => "", omit_expression => true }, {
             insta::assert_snapshot!(name, snapshot);
