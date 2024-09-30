a7591f15fc95ef787f494fed0d7c9e7c5f2e0978
overlookmotel <theoverlookmotel@gmail.com>
2024-09-30 21:23:03 +0100

    perf(transformer): speed up searching for position to insert new statements

--- crates/oxc_transformer/src/common/top_level_statements.rs
+++ crates/oxc_transformer/src/common/top_level_statements.rs
@@ -41,13 +41,66 @@ impl<'a, 'ctx> Traverse<'a> for TopLevelStatements<'a, 'ctx> {
         }
 
         // Insert statements after any existing `import` statements
-        let index = program
-            .body
-            .iter()
-            .rposition(|stmt| matches!(stmt, Statement::ImportDeclaration(_)))
-            .map_or(0, |i| i + 1);
+        let index = if self.ctx.source_type.is_module() {
+            find_insertion_index(&program.body)
+        } else {
+            // Scripts can't have `import` statements, so no need to search
+            0
+        };
 
         program.body.splice(index..index, stmts.drain(..));
+    }
+}
+
+/// Find index to insert statements at.
+///
+/// We want to insert after any `import` statements.
+///
+/// We could search from the end of the file backwards until we hit an `import` statement, but in
+/// a large file, that's a lot of statements to search through. So instead, search from the *start*
+/// for first statement which is *not* an `import`.
+/// Usually before that statement is the correct insertion point.
+///
+/// But there is one annoying Babel test that has a non-`import` statement followed by `import`s,
+/// and it expects new statements to be inserted after the `import`.
+/// `babel-plugin-transform-react-jsx/test/fixtures/autoImport/after-polyfills-2`
+/// To pass that test, we search again if the first statement is not an `import`.
+///
+/// TODO(improve-on-babel): Insertion position is not important. We only do this to pass Babel's tests.
+/// Remove this once we don't have to match Babel's output exactly, and just insert at the start.
+fn find_insertion_index(stmts: &[Statement]) -> usize {
+    let mut iter = stmts
+        .iter()
+        .enumerate()
+        .filter(|(_, stmt)| !matches!(stmt, Statement::ImportDeclaration(_)))
+        .map(|(index, _)| index);
+    let Some(first_non_import) = iter.next() else {
+        // All statements are `import`s, or empty file. Insert after them all.
+        return stmts.len();
+    };
+
+    if first_non_import != 0 {
+        // File starts with import statements. Insert after them.
+        return first_non_import;
+    }
+
+    if stmts.len() == 1 {
+        // Only 1 statement, and it's not an `import`. Insert before it.
+        return 0;
+    }
+
+    // First statement is not `import`. Check if there are imports after it.
+    // This is purely to pass this Babel test:
+    // babel-plugin-transform-react-jsx/test/fixtures/autoImport/after-polyfills-2
+    if let Some(second_non_import) = iter.next() {
+        if second_non_import == 1 {
+            // First 2 statements are not `import`s
+            return 0;
+        }
+        second_non_import
+    } else {
+        // All statements after the first one are `import`s. Insert after them.
+        stmts.len()
     }
 }
 
