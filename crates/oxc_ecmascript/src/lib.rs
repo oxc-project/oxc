1ba2a247e1e91f1e0e8685bcad0ad132a70c0603
Boshen <1430279+Boshen@users.noreply.github.com>
2024-10-12 08:48:45 +0000

    refactor(ecmascript): remove `HasProto` which is not part of the spec (#6470)

--- crates/oxc_ecmascript/src/lib.rs
+++ crates/oxc_ecmascript/src/lib.rs
@@ -1,12 +1,11 @@
 //! Methods defined in the [ECMAScript Language Specification](https://tc39.es/ecma262).
 
 mod bound_names;
-mod has_proto;
 mod is_simple_parameter_list;
 mod private_bound_identifiers;
 mod prop_name;
 
 pub use self::{
-    bound_names::BoundNames, has_proto::HasProto, is_simple_parameter_list::IsSimpleParameterList,
+    bound_names::BoundNames, is_simple_parameter_list::IsSimpleParameterList,
     private_bound_identifiers::PrivateBoundIdentifiers, prop_name::PropName,
 };
