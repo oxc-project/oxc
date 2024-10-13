fba188434a6bdcd6a4542841c0fff3512150a170
shulaoda <laipichan@qq.com>
2024-10-13 17:20:40 +0800

    add test cases for get_string_literal

--- crates/oxc_minifier/src/ast_passes/mod.rs
+++ crates/oxc_minifier/src/ast_passes/mod.rs
@@ -39,3 +39,51 @@ pub trait CompressorPass<'a>: Traverse<'a> {
 
     fn build(&mut self, program: &mut Program<'a>, ctx: &mut TraverseCtx<'a>);
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use oxc_allocator::Allocator;
+    use oxc_ast::ast::Statement;
+    use oxc_parser::Parser;
+    use oxc_semantic::SemanticBuilder;
+    use oxc_span::SourceType;
+
+    #[derive(Default)]
+    pub struct Tester;
+
+    fn build<'a>(allocator: &'a Allocator, source_text: &'a str) -> (TraverseCtx<'a>, Program<'a>) {
+        let source_type = SourceType::mjs();
+
+        let program = Parser::new(allocator, source_text, source_type).parse().program;
+        let (symbols, scopes) =
+            SemanticBuilder::new().build(&program).semantic.into_symbol_table_and_scope_tree();
+
+        (TraverseCtx::new(scopes, symbols, allocator), program)
+    }
+
+    fn get_string_literal(source_text: &str) -> Option<String> {
+        let allocator = Allocator::default();
+        let (ctx, program) = build(&allocator, source_text);
+
+        let Some(Statement::ExpressionStatement(expr_stmt)) = program.body.first() else {
+            return None;
+        };
+
+        ctx.get_string_literal(&expr_stmt.expression).map(Into::into)
+    }
+
+    #[test]
+    fn test_get_string_literal() {
+        assert_eq!(get_string_literal("`abc`"), Some("abc".to_string()));
+        assert_ne!(get_string_literal("`a${b}`"), Some("ab".to_string()));
+        assert_eq!(get_string_literal("`${null}`"), Some("null".to_string()));
+        assert_eq!(get_string_literal("`${undefined}`"), Some("undefined".to_string()));
+        assert_eq!(get_string_literal("`${{}}123`"), Some("[object Object]123".to_string()));
+        assert_eq!(get_string_literal("`a${1}${true}${NaN}0`"), Some("a1trueNaN0".to_string()));
+
+        // assert_eq!(get_string_literal("`${1,2}`"), Some("2".to_string()));
+        // assert_eq!(get_string_literal("`${[]}${[1,2]}`"), Some("1,2".to_string()));
+        // assert_eq!(get_string_literal("`${new Set()}`"), Some("[object Set]".to_string()));
+    }
+}
