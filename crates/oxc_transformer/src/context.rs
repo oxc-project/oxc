792d0c5f7f7c2c120ffa2cfb16e1bd27426074a3
Dunqing <dengqing0821@gmail.com>
2024-10-18 15:18:47 +0800

    feat(transformer): introduce StatementInjector helper

--- crates/oxc_transformer/src/context.rs
+++ crates/oxc_transformer/src/context.rs
@@ -10,7 +10,8 @@ use oxc_span::SourceType;
 use crate::{
     common::{
         helper_loader::HelperLoaderStore, module_imports::ModuleImportsStore,
-        top_level_statements::TopLevelStatementsStore, var_declarations::VarDeclarationsStore,
+        statement_injector::StatementInjectorStore, top_level_statements::TopLevelStatementsStore,
+        var_declarations::VarDeclarationsStore,
     },
     TransformOptions,
 };
@@ -35,6 +36,8 @@ pub struct TransformCtx<'a> {
     pub module_imports: ModuleImportsStore<'a>,
     /// Manage inserting `var` statements globally
     pub var_declarations: VarDeclarationsStore<'a>,
+    /// Manage inserting statements immediately before or after the target statement
+    pub statement_injector: StatementInjectorStore<'a>,
     /// Manage inserting statements at top of program globally
     pub top_level_statements: TopLevelStatementsStore<'a>,
 }
@@ -58,6 +61,7 @@ impl<'a> TransformCtx<'a> {
             helper_loader: HelperLoaderStore::new(&options.helper_loader),
             module_imports: ModuleImportsStore::new(),
             var_declarations: VarDeclarationsStore::new(),
+            statement_injector: StatementInjectorStore::new(),
             top_level_statements: TopLevelStatementsStore::new(),
         }
     }
