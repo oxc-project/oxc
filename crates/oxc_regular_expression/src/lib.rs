f8e1907c4f7888bda3928f55c24360b0e77ecd5d
leaysgur <6259812+leaysgur@users.noreply.github.com>
2024-10-21 07:07:27 +0000

    feat(regular_expression): Intro `ConstructorParser`(and `LiteralParser`) to handle escape sequence in RegExp('pat') (#6635)
    Preparation for #6141
    
    `oxc_regular_expression` can already parse and validate both `/regexp-literal/` and `new RegExp("string-literal")`.
    
    But one thing that is not well-supported was reporting `Span` for the `RegExp("string-literal-with-\\escape")` case.
    
    For example, these two cases produce the same `RegExp` instances in JavaScript:
    
    - `/\d+/`
    - `new RegExp("\\d+")`
    
    For now, mainly in `oxc_linter`, the latter case is parsed with `oxc_parser` -> `ast::literal::StringLiteral` AST node -> `value` property.
    
    At this point, escape sequences are resolved(!), `oxc_regular_expression` can handle aligned `&str` as an argument without any problem in both cases.
    
    However, in terms of `Span` representation, these cases should be handled differently because of the `\\` in string literals...
    
    As a result, the parsed AST's `Span` for `new RegExp("string-literal")` is not accurate if it contains escape sequences.
    
    e.g. https://github.com/oxc-project/oxc/blob/a01a5dfdafb9cd536cb87867697e3ae43b1990e6/crates/oxc_linter/src/snapshots/no_invalid_regexp.snap#L118-L122
    
    Each time the `\` appears, the subsequent position is shifted. `_` should be placed under `*` in this case.
    
    So... to resolve this issue, we need to implement `string_literal_parser` first, and use them as reading units of `oxc_regular_expression`.

--- crates/oxc_regular_expression/src/lib.rs
+++ crates/oxc_regular_expression/src/lib.rs
@@ -15,4 +15,92 @@ mod generated {
 }
 
 pub mod ast;
-pub use crate::{ast_impl::visit, options::ParserOptions, parser::Parser};
+pub use crate::{
+    ast_impl::visit,
+    options::Options,
+    parser::{ConstructorParser, LiteralParser},
+};
+
+// LEGACY APIS TO BE REMOVED SOON! ============================================
+
+#[derive(Clone, Copy, Debug, Default)]
+pub struct ParserOptions {
+    pub span_offset: u32,
+    pub unicode_mode: bool,
+    pub unicode_sets_mode: bool,
+    pub parse_string_literal: bool,
+}
+
+impl ParserOptions {
+    #[must_use]
+    pub fn with_span_offset(self, span_offset: u32) -> Self {
+        ParserOptions { span_offset, ..self }
+    }
+
+    #[must_use]
+    pub fn with_flags(self, flags: &str) -> Self {
+        let (mut unicode_mode, mut unicode_sets_mode) = (false, false);
+        for ch in flags.chars() {
+            if ch == 'u' {
+                unicode_mode = true;
+            }
+            if ch == 'v' {
+                unicode_mode = true;
+                unicode_sets_mode = true;
+            }
+        }
+
+        ParserOptions { unicode_mode, unicode_sets_mode, ..self }
+    }
+
+    #[must_use]
+    pub fn with_parse_string_literal(self) -> Self {
+        ParserOptions { parse_string_literal: true, ..self }
+    }
+}
+
+pub struct Parser<'a> {
+    allocator: &'a oxc_allocator::Allocator,
+    source_text: &'a str,
+    options: ParserOptions,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(
+        allocator: &'a oxc_allocator::Allocator,
+        source_text: &'a str,
+        options: ParserOptions,
+    ) -> Self {
+        Self { allocator, source_text, options }
+    }
+
+    pub fn parse(self) -> oxc_diagnostics::Result<crate::ast::Pattern<'a>> {
+        let ParserOptions { unicode_mode, unicode_sets_mode, span_offset, parse_string_literal } =
+            self.options;
+
+        let options = Options {
+            pattern_span_offset: span_offset,
+            flags_span_offset: 0, // Never be used
+        };
+
+        if parse_string_literal {
+            #[allow(clippy::match_same_arms)]
+            let flags_text = match (unicode_mode, unicode_sets_mode) {
+                (true, false) => Some("'u'"),
+                (false, true) => Some("'v'"),
+                (true, true) => Some("'v'"), // Do not validate this here
+                (false, false) => None,
+            };
+            ConstructorParser::new(self.allocator, self.source_text, flags_text, options).parse()
+        } else {
+            #[allow(clippy::match_same_arms)]
+            let flags_text = match (unicode_mode, unicode_sets_mode) {
+                (true, false) => Some("u"),
+                (false, true) => Some("v"),
+                (true, true) => Some("v"), // Do not validate this here
+                (false, false) => None,
+            };
+            LiteralParser::new(self.allocator, self.source_text, flags_text, options).parse()
+        }
+    }
+}
