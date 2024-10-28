610621cadf995fca01fa3bf557d768dbd8c37762
Albert Kaaman <albert@kaaman.nu>
2024-10-28 03:51:09 +0100

    feat(linter): implement `react/style-prop-object` (#6342)
    Implements not recommended rule `react/style-prop-object`
    (https://github.com/oxc-project/oxc/issues/1022)
    
    ---------
    
    Co-authored-by: Don Isaac <donald.isaac@gmail.com>

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -261,6 +261,7 @@ mod react {
     pub mod require_render_return;
     pub mod rules_of_hooks;
     pub mod self_closing_comp;
+    pub mod style_prop_object;
     pub mod void_dom_elements_no_children;
 }
 
@@ -808,6 +809,7 @@ oxc_macros::declare_all_lint_rules! {
     react::require_render_return,
     react::rules_of_hooks,
     react::self_closing_comp,
+    react::style_prop_object,
     react::void_dom_elements_no_children,
     react_perf::jsx_no_jsx_as_prop,
     react_perf::jsx_no_new_array_as_prop,
