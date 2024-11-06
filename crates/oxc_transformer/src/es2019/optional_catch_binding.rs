1dfd241125c7f61a8363931936681149d8910ece
overlookmotel <557937+overlookmotel@users.noreply.github.com>
2024-11-06 12:02:09 +0000

    refactor(transformer/optional-catch-binding): remove inactive `#[allow(clippy::unused_self)]` attr (#7158)

--- crates/oxc_transformer/src/es2019/optional_catch_binding.rs
+++ crates/oxc_transformer/src/es2019/optional_catch_binding.rs
@@ -48,7 +48,6 @@ impl OptionalCatchBinding {
 
 impl<'a> Traverse<'a> for OptionalCatchBinding {
     /// If CatchClause has no param, add a parameter called `unused`.
-    #[allow(clippy::unused_self)]
     fn enter_catch_clause(&mut self, clause: &mut CatchClause<'a>, ctx: &mut TraverseCtx<'a>) {
         if clause.param.is_some() {
             return;
