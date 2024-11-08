9b8973f42578f79f01ec2cb3eef6ddbfbabe4849
Dmitry Zakharov <pumano@gmail.com>
2024-11-08 03:54:37 +0300

    feat(linter): add `import/unambiguous` rule (#7187)

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -24,6 +24,7 @@ mod import {
     pub mod no_named_as_default_member;
     pub mod no_self_import;
     pub mod no_webpack_loader_syntax;
+    pub mod unambiguous;
 }
 
 mod eslint {
@@ -635,6 +636,7 @@ oxc_macros::declare_all_lint_rules! {
     import::no_named_as_default_member,
     import::no_self_import,
     import::no_webpack_loader_syntax,
+    import::unambiguous,
     jest::consistent_test_it,
     jest::expect_expect,
     jest::max_expects,
