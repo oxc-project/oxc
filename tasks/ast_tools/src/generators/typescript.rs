8b0121547aaf19f23d35c8a4f2e0cc2f554847c8
shulaoda <laipichan@qq.com>
2024-11-14 23:20:46 +0800

    fix(tasks): cargo clippy error on rust stable toolchain

--- tasks/ast_tools/src/generators/typescript.rs
+++ tasks/ast_tools/src/generators/typescript.rs
@@ -108,15 +108,18 @@ fn typescript_struct(def: &StructDef, always_flatten_structs: &FxHashSet<TypeId>
 
         let ident = field.ident().unwrap();
         if let Some(append_after) = append_to.get(&ident.to_string()) {
-            let after_type = match &append_after.markers.derive_attributes.estree.typescript_type {
-                Some(ty) => ty.clone(),
-                None => {
-                    let typ = append_after.typ.name();
-                    if let TypeName::Opt(inner) = typ {
-                        type_to_string(inner)
-                    } else {
-                        panic!("expected field labeled with append_to to be Option<...>, but found {typ}");
-                    }
+            let after_type = if let Some(ty) =
+                &append_after.markers.derive_attributes.estree.typescript_type
+            {
+                ty.clone()
+            } else {
+                let typ = append_after.typ.name();
+                if let TypeName::Opt(inner) = typ {
+                    type_to_string(inner)
+                } else {
+                    panic!(
+                        "expected field labeled with append_to to be Option<...>, but found {typ}"
+                    );
                 }
             };
             if let Some(inner) = ty.strip_prefix("Array<") {
