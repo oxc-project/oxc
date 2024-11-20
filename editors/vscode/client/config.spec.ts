878e7703ef1131d3456e2dcc200e1984bd5c8b9f
Sysix <sysix@sysix-coding.de>
2024-11-20 20:13:08 +0100

    refactor(vscode): split ConfigService and Config

--- editors/vscode/client/config.spec.ts
+++ editors/vscode/client/config.spec.ts
@@ -1,12 +1,16 @@
 import { strictEqual } from 'assert';
 import { Config } from './Config.js';
 
-suite('default values on initialization', () => {
-  const config = new Config();
+suite('Config', () => {
 
-  strictEqual(config.runTrigger, 'onType');
-  strictEqual(config.enable, true);
-  strictEqual(config.trace, 'off');
-  strictEqual(config.configPath, '.eslintrc');
-  strictEqual(config.binPath, '');
-});
+  test('default values on initialization', () => {
+    const config = new Config();
+  
+    strictEqual(config.runTrigger, 'onType');
+    strictEqual(config.enable, true);
+    strictEqual(config.trace, 'off');
+    strictEqual(config.configPath, '.eslintrc');
+    strictEqual(config.binPath, '');
+  });
+  
+})
