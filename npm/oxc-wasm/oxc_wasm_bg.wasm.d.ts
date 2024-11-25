97a9043010eb0050568259dcf0460832f16d200d
Boshen <boshenc@gmail.com>
2024-11-25 17:22:36 +0800

    ci: clean up wasm type check

---
+++ npm/oxc-wasm/oxc_wasm_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_oxc_free(a: number, b: number): void;
+export function __wbg_get_oxc_ast(a: number): number;
+export function __wbg_get_oxc_ir(a: number, b: number): void;
+export function __wbg_get_oxc_controlFlowGraph(a: number, b: number): void;
+export function __wbg_get_oxc_symbols(a: number): number;
+export function __wbg_get_oxc_scopeText(a: number, b: number): void;
+export function __wbg_get_oxc_codegenText(a: number, b: number): void;
+export function __wbg_get_oxc_formattedText(a: number, b: number): void;
+export function __wbg_get_oxc_prettierFormattedText(a: number, b: number): void;
+export function __wbg_get_oxc_prettierIrText(a: number, b: number): void;
+export function oxc_new(): number;
+export function oxc_getDiagnostics(a: number, b: number): void;
+export function oxc_getComments(a: number, b: number): void;
+export function oxc_run(a: number, b: number, c: number, d: number, e: number): void;
+export function browserslist(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
