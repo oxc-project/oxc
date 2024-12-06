66a680a119dee939ba84c21cfde53b7ee81d4790
Boshen <boshenc@gmail.com>
2024-12-06 14:34:01 +0800

    deps(coverage): bump test262 (#7695)

--- tasks/coverage/src/test262/mod.rs
+++ tasks/coverage/src/test262/mod.rs
@@ -107,6 +107,8 @@ impl Case for Test262Case {
         [
             // stage 3 https://github.com/tc39/proposal-source-phase-imports
             "source-phase-imports",
+            // stage 2.7 https://github.com/tc39/proposal-defer-import-eval
+            "import-defer",
         ]
         .iter()
         .any(|feature| self.meta.features.iter().any(|f| **f == **feature))
