9b3a2beaa30a87d39a7fbad9278adad446e18b21
camc314 <18101008+camc314@users.noreply.github.com>
2024-12-16 13:08:11 +0000

    refactor(benchmark): transform code before codegen (#7934)
    @overlookmotel suggested this [here](https://github.com/oxc-project/oxc/pull/7926#discussion_r1886023496).

--- tasks/benchmark/benches/codegen.rs
+++ tasks/benchmark/benches/codegen.rs
@@ -1,11 +1,13 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use oxc_allocator::Allocator;
 use oxc_benchmark::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use oxc_codegen::{CodeGenerator, CodegenOptions};
 use oxc_parser::Parser;
+use oxc_semantic::SemanticBuilder;
 use oxc_span::SourceType;
 use oxc_tasks_common::TestFiles;
+use oxc_transformer::{TransformOptions, Transformer};
 
 fn bench_codegen(criterion: &mut Criterion) {
     for file in TestFiles::complicated_one(0).files() {
@@ -13,16 +15,35 @@ fn bench_codegen(criterion: &mut Criterion) {
         let source_type = SourceType::from_path(&file.file_name).unwrap();
         let allocator = Allocator::default();
         let source_text = &file.source_text;
-        let ret = Parser::new(&allocator, source_text, source_type).parse();
+
+        let mut parser_ret = Parser::new(&allocator, source_text, source_type).parse();
+        assert!(parser_ret.errors.is_empty());
 
         let mut group = criterion.benchmark_group("codegen");
-        group.bench_with_input(id.clone(), &ret.program, |b, program| {
+        group.bench_with_input(id.clone(), &parser_ret.program, |b, program| {
             b.iter_with_large_drop(|| CodeGenerator::new().build(program).map);
         });
         group.finish();
 
+        let transformed_program = {
+            let transform_options = TransformOptions::enable_all();
+            let (symbols, scopes) = SemanticBuilder::new()
+                // Estimate transformer will triple scopes, symbols, references
+                .with_excess_capacity(2.0)
+                .build(&parser_ret.program)
+                .semantic
+                .into_symbol_table_and_scope_tree();
+
+            let transformer_ret =
+                Transformer::new(&allocator, Path::new(&file.file_name), &transform_options)
+                    .build_with_symbols_and_scopes(symbols, scopes, &mut parser_ret.program);
+
+            assert!(transformer_ret.errors.is_empty());
+            parser_ret.program
+        };
+
         let mut group = criterion.benchmark_group("codegen_sourcemap");
-        group.bench_with_input(id, &ret.program, |b, program| {
+        group.bench_with_input(id, &transformed_program, |b, program| {
             b.iter_with_large_drop(|| {
                 CodeGenerator::new()
                     .with_options(CodegenOptions {
