0b27bbb1bfa19b15bfcf337cd581eddcf55f1015
baseballyama <xydybaseball@gmail.com>
2024-12-28 15:42:20 +0900

    feat(linter): implement `prefer-rest-params` rule

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -141,6 +141,7 @@ mod eslint {
     pub mod prefer_exponentiation_operator;
     pub mod prefer_numeric_literals;
     pub mod prefer_object_has_own;
+    pub mod prefer_rest_params;
     pub mod prefer_spread;
     pub mod radix;
     pub mod require_await;
@@ -630,6 +631,7 @@ oxc_macros::declare_all_lint_rules! {
     eslint::no_var,
     eslint::no_void,
     eslint::no_with,
+    eslint::prefer_rest_params,
     eslint::prefer_exponentiation_operator,
     eslint::prefer_numeric_literals,
     eslint::prefer_object_has_own,
