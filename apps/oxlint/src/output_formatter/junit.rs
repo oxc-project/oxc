7b09eebe6739050ee3370b67631cc53f7fcbc304
Tapan Prakash <tapanprakasht@gmail.com>
2025-01-31 15:39:06 +0530

    Added test cases

--- apps/oxlint/src/output_formatter/junit.rs
+++ apps/oxlint/src/output_formatter/junit.rs
@@ -69,3 +69,41 @@ fn format_junit(diagnostics: &[Error]) -> String {
     }
     report.to_string().unwrap()
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use oxc_diagnostics::{reporter::DiagnosticResult, NamedSource, OxcDiagnostic};
+    use oxc_span::Span;
+
+    #[test]
+    fn test_junit_reporter() {
+        const EXPECTED_REPORT: &str = r#"<?xml version="1.0" encoding="UTF-8"?>
+<testsuites name="Oxlint" tests="2" failures="1" errors="1">
+    <testsuite name="file.js" tests="2" disabled="0" errors="1" failures="1">
+        <testcase name="">
+            <error message="error message">line 1, column 1, error message</error>
+        </testcase>
+        <testcase name="">
+            <failure message="warning message">line 1, column 1, warning message</failure>
+        </testcase>
+    </testsuite>
+</testsuites>
+"#;
+        let mut reporter = JUnitReporter::default();
+
+        let error = OxcDiagnostic::error("error message")
+            .with_label(Span::new(0, 8))
+            .with_source_code(NamedSource::new("file.js", "let a = ;"));
+
+        let warning = OxcDiagnostic::warn("warning message")
+            .with_label(Span::new(0, 9))
+            .with_source_code(NamedSource::new("file.js", "debugger;"));
+
+        reporter.render_error(error);
+        reporter.render_error(warning);
+
+        let output = reporter.finish(&DiagnosticResult::default()).unwrap();
+        assert_eq!(output.to_string(), EXPECTED_REPORT);
+    }
+}
