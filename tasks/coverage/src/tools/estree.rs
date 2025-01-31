54423a05103d7e3f965ee9df39b41d9f4e01c1b3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-01-31 11:49:17 +0900

    wip: output diff file

--- tasks/coverage/src/tools/estree.rs
+++ tasks/coverage/src/tools/estree.rs
@@ -42,11 +42,13 @@ impl Case for EstreeTest262Case {
             return;
         };
         // FIXME: `called `Result::unwrap()` on an `Err` value: Error("unexpected end of hex escape", line: 307, column: 33)`
-        let Ok(acorn_json) = serde_json::from_str::<serde_json::Value>(&acorn_file) else {
-            self.base.set_result(TestResult::Passed);
-            return;
+        let acorn_json = match serde_json::from_str::<serde_json::Value>(&acorn_file) {
+            Err(e) => {
+                self.base.set_result(TestResult::GenericError("serde_json", e.to_string()));
+                return;
+            }
+            Ok(acorn_json) => acorn_json,
         };
-
         let source_text = self.base.code();
         let is_module = self.base.is_module();
         let source_type = SourceType::default().with_module(is_module);
@@ -54,14 +56,44 @@ impl Case for EstreeTest262Case {
         let ret = Parser::new(&allocator, source_text, source_type).parse();
         // Ignore empty AST or parse errors.
         if ret.program.is_empty() || ret.panicked || !ret.errors.is_empty() {
-            self.base.set_result(TestResult::Passed);
+            self.base.set_result(TestResult::GenericError("Parser", "".to_string()));
             return;
         }
         let oxc_json = serde_json::from_str::<serde_json::Value>(&ret.program.to_json()).unwrap();
 
+        // TODO: check what mismatches
+        // - span
+        // - extra properties
+        // - others
+        // let mut mismatch_span = false;
+        // let mut mismatch_extra = false;
+        // let mut mismatch_other = false;
+
+        // check excess property
+        // check span difference
+        // check other differences
+
         let acorn_json = serde_json::to_string_pretty(&acorn_json).unwrap();
         let oxc_json = serde_json::to_string_pretty(&oxc_json).unwrap();
 
-        similar_asserts::assert_eq!(acorn_json, oxc_json);
+        if acorn_json == oxc_json {
+            self.base.set_result(TestResult::Passed);
+        } else {
+            let diff_path = Path::new("tasks/coverage/estree-diff")
+                .join(self.path().strip_prefix("test262").unwrap())
+                .with_extension("diff");
+            std::fs::create_dir_all(diff_path.parent().unwrap()).unwrap();
+            std::fs::write(
+                diff_path,
+                format!(
+                    "{}",
+                    similar::TextDiff::from_lines(&acorn_json, &oxc_json)
+                        .unified_diff()
+                        .missing_newline_hint(false)
+                ),
+            )
+            .unwrap();
+            self.base.set_result(TestResult::Mismatch("Mismatch", "".to_string(), "".to_string()));
+        }
     }
 }
