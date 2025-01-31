a716d78d65485c72a27b2f8c4472062628257188
Tapan Prakash <tapanprakasht@gmail.com>
2025-01-31 11:19:31 +0530

    Improved junit reporting

--- apps/oxlint/src/output_formatter/junit.rs
+++ apps/oxlint/src/output_formatter/junit.rs
@@ -1,5 +1,5 @@
-
-use oxc_diagnostics::{reporter::{DiagnosticReporter, DiagnosticResult, Info}, Error};
+use oxc_diagnostics::{reporter::{DiagnosticReporter, DiagnosticResult, Info}, Error, Severity};
+use rustc_hash::FxHashMap;
 
 use super::InternalFormatter;
 use quick_junit::{NonSuccessKind, Report, TestCase, TestCaseStatus, TestSuite};
@@ -30,16 +30,33 @@ impl DiagnosticReporter for JUnitReporter {
 }
 
 fn format_junit(diagnostics: &[Error]) -> String {
-    let mut report = Report::new("Oxlint");
+    let mut grouped: FxHashMap<String, Vec<&Error>> = FxHashMap::default();
+
     for diagnostic in diagnostics {
         let info = Info::new(diagnostic);
-        let mut test_suite = TestSuite::new(info.filename);
-        let mut status = TestCaseStatus::non_success(NonSuccessKind::Failure);
-        status.set_message(info.message);
-        let rule = diagnostic.code().map_or_else(String::new, |code| code.to_string());
-        let test_case = TestCase::new(rule, status); 
-        test_suite.add_test_case(test_case);
+        grouped.entry(info.filename).or_default().push(diagnostic);
+    }
 
+    let mut report = Report::new("Oxlint");
+    for diagnostics in grouped.values() {
+        let diagnostic = diagnostics[0];
+        let filename = Info::new(diagnostic).filename;
+
+        let mut test_suite = TestSuite::new(filename);
+
+        for diagnostic in diagnostics {
+            let rule = diagnostic.code().map_or_else(String::new, |code| code.to_string());
+            let Info { message, start, .. } = Info::new(diagnostic);
+
+            let mut status = match diagnostic.severity() {
+                Some(Severity::Error) => TestCaseStatus::non_success(NonSuccessKind::Error),
+                _ => TestCaseStatus::non_success(NonSuccessKind::Failure)
+            };
+            status.set_message(message.clone());
+            status.set_description(format!("line {}, column {}, {}", start.line, start.column, message.clone()));
+            let test_case = TestCase::new(rule, status);
+            test_suite.add_test_case(test_case);
+        }
         report.add_test_suite(test_suite);
     }
     report.to_string().unwrap()
