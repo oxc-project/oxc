743534c492c6e788b052dca7187792b7bf397fe6
Yuji Sugiura <y.sugiura.0316@gmail.com>
2025-02-05 17:47:31 +0900

    feat(prettier): Update print_array()

--- crates/oxc_prettier/src/format/print/array.rs
+++ crates/oxc_prettier/src/format/print/array.rs
@@ -9,118 +9,107 @@ use crate::{
 
 pub enum ArrayLike<'a, 'b> {
     ArrayExpression(&'b ArrayExpression<'a>),
-    TSTupleType(&'b TSTupleType<'a>),
     ArrayPattern(&'b ArrayPattern<'a>),
     ArrayAssignmentTarget(&'b ArrayAssignmentTarget<'a>),
+    TSTupleType(&'b TSTupleType<'a>),
 }
 
 impl ArrayLike<'_, '_> {
-    fn len(&self) -> usize {
+    /// This includes rest element for `ArrayPattern` and `ArrayAssignmentTarget`
+    fn total_len(&self) -> usize {
         match self {
             Self::ArrayExpression(array) => array.elements.len(),
+            Self::ArrayPattern(array) => array.elements.len() + usize::from(array.rest.is_some()),
+            Self::ArrayAssignmentTarget(array) => {
+                array.elements.len() + usize::from(array.rest.is_some())
+            }
             Self::TSTupleType(tuple) => tuple.element_types.len(),
-            Self::ArrayPattern(array) => array.elements.len(),
-            Self::ArrayAssignmentTarget(array) => array.elements.len(),
-        }
-    }
-
-    fn span(&self) -> Span {
-        match self {
-            Self::ArrayExpression(array) => array.span,
-            Self::TSTupleType(tuple) => tuple.span,
-            Self::ArrayPattern(array) => array.span,
-            Self::ArrayAssignmentTarget(array) => array.span,
         }
     }
 
     fn is_concisely_printed(&self) -> bool {
-        match self {
-            Self::ArrayExpression(array) => {
-                if array.elements.len() <= 1 {
-                    return false;
-                }
-
-                array.elements.iter().all(|element| match element {
-                    ArrayExpressionElement::NumericLiteral(_) => true,
-                    ArrayExpressionElement::UnaryExpression(unary_expr) => {
-                        matches!(
-                            unary_expr.operator,
-                            UnaryOperator::UnaryPlus | UnaryOperator::UnaryNegation
-                        ) && matches!(unary_expr.argument, Expression::NumericLiteral(_))
-                    }
-                    _ => false,
-                })
-            }
-            Self::ArrayPattern(_) | Self::ArrayAssignmentTarget(_) | Self::TSTupleType(_) => false,
+        let Self::ArrayExpression(array_expr) = self else {
+            return false;
+        };
+        if array_expr.elements.len() <= 1 {
+            return false;
         }
+        array_expr.elements.iter().all(
+            |el| match el {
+                ArrayExpressionElement::NumericLiteral(_) => true,
+                ArrayExpressionElement::UnaryExpression(unary_expr) => {
+                    matches!(
+                        unary_expr.operator,
+                        UnaryOperator::UnaryPlus | UnaryOperator::UnaryNegation
+                    ) && matches!(unary_expr.argument, Expression::NumericLiteral(_))
+                    // TODO: && !hasComment(el.argument)
+                }
+                _ => false,
+            }, // TODO: && !hasComment(...),
+        )
     }
 }
 
+// ---
+
 pub fn print_array<'a>(p: &mut Prettier<'a>, arr: &ArrayLike<'a, '_>) -> Doc<'a> {
-    if arr.len() == 0 {
+    if arr.total_len() == 0 {
+        // TODO: Comments
+        // group!(p, [text!("["), dangling_comment, softline!(), text!("]")])
         return text!("[]");
     }
 
-    let (needs_forced_trailing_comma, can_have_trailing_comma) =
-        if let ArrayLike::ArrayExpression(arr) = arr {
-            arr.elements.last().map_or((false, false), |last| {
-                (
-                    matches!(last, ArrayExpressionElement::Elision(_)),
-                    !matches!(last, ArrayExpressionElement::SpreadElement(_)),
-                )
-            })
-        } else {
-            (false, false)
-        };
-
-    let id = p.next_id();
+    let (needs_forced_trailing_comma, can_have_trailing_comma) = match arr {
+        ArrayLike::ArrayExpression(arr) => arr.elements.last().map_or((false, false), |last| {
+            (
+                matches!(last, ArrayExpressionElement::Elision(_)),
+                !matches!(last, ArrayExpressionElement::SpreadElement(_)),
+            )
+        }),
+        _ => (false, false),
+    };
+    let group_id = p.next_id();
     let should_use_concise_formatting = arr.is_concisely_printed();
 
     let trailing_comma_fn = |p: &Prettier<'a>| {
         if !can_have_trailing_comma {
-            text!("")
-        } else if needs_forced_trailing_comma {
-            text!(",")
-        } else if should_use_concise_formatting {
-            if_break!(p, text!(","), text!(""), Some(id))
-        } else {
-            if_break!(p, text!(","))
+            return text!("");
         }
+        if needs_forced_trailing_comma {
+            return text!(",");
+        }
+        if !p.should_print_es5_comma() {
+            return text!("");
+        }
+        if should_use_concise_formatting {
+            return if_break!(p, text!(","), text!(""), Some(group_id));
+        }
+        if_break!(p, text!(","))
     };
 
-    let mut parts = Vec::new_in(p.allocator);
-
-    let group = {
-        let mut group = Vec::new_in(p.allocator);
-        group.push(text!("["));
-
-        let indent_parts = {
-            let mut indent_parts = Vec::new_in(p.allocator);
-            indent_parts.push(softline!());
-
-            indent_parts.push(if should_use_concise_formatting {
+    let elements_doc = indent!(
+        p,
+        [
+            softline!(),
+            if should_use_concise_formatting {
                 print_array_elements_concisely(p, arr, trailing_comma_fn)
             } else {
-                let trailing_comma = trailing_comma_fn(p);
-                let elements = print_array_elements(p, arr);
-                array!(p, [elements, trailing_comma])
-            });
-            indent_parts
-        };
+                array!(p, [print_array_elements(p, arr), trailing_comma_fn(p)])
+            },
+            // TODO: Dangling comments
+        ]
+    );
 
-        group.push(indent!(p, indent_parts));
-        group.push(softline!());
-        group.push(text!("]"));
-
-        group
-    };
-    parts.push(group!(p, group, should_break(arr), Some(id)));
-
-    array!(p, parts)
+    group!(
+        p,
+        [text!("["), elements_doc, softline!(), text!("]")],
+        should_break(arr),
+        Some(group_id)
+    )
 }
 
-pub fn is_concisely_printed_array(arr: &Expression) -> bool {
-    match arr {
+pub fn is_concisely_printed_array(expr: &Expression) -> bool {
+    match expr {
         Expression::ArrayExpression(array) => {
             ArrayLike::ArrayExpression(array).is_concisely_printed()
         }
@@ -128,72 +117,110 @@ pub fn is_concisely_printed_array(arr: &Expression) -> bool {
     }
 }
 
+// ---
+
+// There are 4 branches and may look like similar, differences are:
+// - ArrayExpression: Handles elision
+// - ArrayPattern: Handles rest element and each element is optional
+// - ArrayAssignmentTarget: The same as ArrayPattern
+// - TSTupleType: The same as ArrayExpression but without elision
 fn print_array_elements<'a>(p: &mut Prettier<'a>, arr: &ArrayLike<'a, '_>) -> Doc<'a> {
-    let mut parts = Vec::new_in(p.allocator);
     match arr {
-        ArrayLike::ArrayExpression(array) => {
-            for (i, element) in array.elements.iter().enumerate() {
-                parts.push(element.format(p));
-                let is_last = i == array.elements.len() - 1;
-                if !is_last {
+        ArrayLike::ArrayExpression(arr_expr) => {
+            let mut parts = Vec::new_in(p.allocator);
+
+            let len = arr_expr.elements.len();
+            for (idx, el) in arr_expr.elements.iter().enumerate() {
+                parts.push(group!(p, [el.format(p)]));
+
+                if idx != len - 1 {
                     parts.push(text!(","));
                     parts.push(line!());
-                    if !element.is_elision() && is_line_after_element_empty(p, element.span().end) {
+
+                    if !el.is_elision() && is_line_after_element_empty(p, el.span().end) {
                         parts.push(softline!());
                     }
                 }
             }
+
+            array!(p, parts)
+        }
+        ArrayLike::ArrayPattern(arr_pat) => {
+            let mut parts = Vec::new_in(p.allocator);
+
+            let len = arr_pat.elements.len();
+            let has_rest = arr_pat.rest.is_some();
+            for (idx, el) in arr_pat.elements.iter().enumerate() {
+                if let Some(binding_pat) = el {
+                    parts.push(group!(p, [binding_pat.format(p)]));
+                }
+
+                if idx != len - 1 || has_rest {
+                    parts.push(text!(","));
+                    parts.push(line!());
+
+                    if el.as_ref().is_some_and(|binding_pat| {
+                        is_line_after_element_empty(p, binding_pat.span().end)
+                    }) {
+                        parts.push(softline!());
+                    }
+                }
+            }
+
+            if let Some(rest) = &arr_pat.rest {
+                parts.push(group!(p, [rest.format(p)]));
+            }
+
+            array!(p, parts)
+        }
+        ArrayLike::ArrayAssignmentTarget(arr_pat) => {
+            let mut parts = Vec::new_in(p.allocator);
+
+            let len = arr_pat.elements.len();
+            let has_rest = arr_pat.rest.is_some();
+            for (idx, el) in arr_pat.elements.iter().enumerate() {
+                if let Some(binding_pat) = el {
+                    parts.push(group!(p, [binding_pat.format(p)]));
+                }
+
+                if idx != len - 1 || has_rest {
+                    parts.push(text!(","));
+                    parts.push(line!());
+
+                    if el.as_ref().is_some_and(|binding_pat| {
+                        is_line_after_element_empty(p, binding_pat.span().end)
+                    }) {
+                        parts.push(softline!());
+                    }
+                }
+            }
+
+            if let Some(rest) = &arr_pat.rest {
+                parts.push(group!(p, [rest.format(p)]));
+            }
+
+            array!(p, parts)
         }
         ArrayLike::TSTupleType(tuple) => {
-            for (i, element) in tuple.element_types.iter().enumerate() {
-                if i > 0 && i < tuple.element_types.len() {
+            let mut parts = Vec::new_in(p.allocator);
+
+            let len = tuple.element_types.len();
+            for (idx, el) in tuple.element_types.iter().enumerate() {
+                parts.push(group!(p, [el.format(p)]));
+
+                if idx != len - 1 {
                     parts.push(text!(","));
                     parts.push(line!());
-                }
 
-                parts.push(element.format(p));
-            }
-        }
-        ArrayLike::ArrayPattern(array_pat) => {
-            let len = array_pat.elements.len();
-            let has_rest = array_pat.rest.is_some();
-            for (i, element) in array_pat.elements.iter().enumerate() {
-                if let Some(binding_pat) = element {
-                    let binding_pat_doc = binding_pat.format(p);
-                    parts.push(group!(p, [binding_pat_doc]));
-                }
-                if i == len - 1 && !has_rest {
-                    break;
-                }
-                parts.push(text!(","));
-                parts.push(line!());
-            }
-            if let Some(rest) = &array_pat.rest {
-                let rest_doc = rest.format(p);
-                parts.push(group!(p, [rest_doc]));
-            }
-        }
-        ArrayLike::ArrayAssignmentTarget(array_pat) => {
-            for (i, element) in array_pat.elements.iter().enumerate() {
-                if i > 0 && i < array_pat.elements.len() {
-                    parts.push(text!(","));
-                    parts.push(line!());
-                }
-
-                if let Some(binding_pat) = element {
-                    parts.push(binding_pat.format(p));
+                    if is_line_after_element_empty(p, el.span().end) {
+                        parts.push(softline!());
+                    }
                 }
             }
 
-            if let Some(rest) = &array_pat.rest {
-                parts.push(text!(","));
-                parts.push(line!());
-                parts.push(rest.format(p));
-            }
+            array!(p, parts)
         }
     }
-
-    array!(p, parts)
 }
 
 fn print_array_elements_concisely<'a, F>(
@@ -204,37 +231,35 @@ fn print_array_elements_concisely<'a, F>(
 where
     F: Fn(&Prettier<'a>) -> Doc<'a>,
 {
-    let mut parts = Vec::new_in(p.allocator);
-    if let ArrayLike::ArrayExpression(arr) = arr {
-        for (i, element) in arr.elements.iter().enumerate() {
-            let is_last = i == arr.elements.len() - 1;
-            let element_doc = element.format(p);
-            let part = if is_last {
-                array!(p, [element_doc, trailing_comma_fn(p)])
-            } else {
-                array!(p, [element_doc, text!(",")])
-            };
-            parts.push(part);
+    let ArrayLike::ArrayExpression(arr_expr) = arr else { unreachable!() };
 
-            if !is_last {
-                if is_line_after_element_empty(p, element.span().end) {
-                    parts.push(array!(p, [hardline!(p), hardline!(p)]));
-                } else {
-                    parts.push(line!());
-                }
+    let mut parts = Vec::new_in(p.allocator);
+
+    let len = arr_expr.elements.len();
+    for (idx, el) in arr_expr.elements.iter().enumerate() {
+        let is_last = idx == len - 1;
+
+        parts.push(array!(
+            p,
+            [el.format(p), if is_last { trailing_comma_fn(p) } else { text!(",") }]
+        ));
+
+        if !is_last {
+            if is_line_after_element_empty(p, el.span().end) {
+                parts.push(array!(p, [hardline!(p), hardline!(p)]));
+            } else {
+                // TODO: hasComment(next_el) ? hardline : line
+                parts.push(line!());
             }
         }
-    } else {
-        // TODO: implement
-        let elements = print_array_elements(p, arr);
-        array!(p, [elements, trailing_comma_fn(p)]);
     }
 
     fill!(p, parts)
 }
 
+// TODO: VERIFY
 fn should_break(array: &ArrayLike) -> bool {
-    if array.len() <= 1 {
+    if array.total_len() <= 1 {
         return false;
     }
 
@@ -286,21 +311,22 @@ fn should_break(array: &ArrayLike) -> bool {
     }
 }
 
-fn skip_comment(p: &Prettier<'_>, idx: u32) -> Option<u32> {
-    p.skip_inline_comment(p.skip_trailing_comment(Some(idx)))
-}
-
-#[allow(clippy::cast_possible_truncation)]
-fn skip_to_comma(p: &Prettier<'_>, current_idx: Option<u32>) -> Option<u32> {
-    let current_idx = current_idx?;
-    match p.source_text[current_idx as usize..].chars().next() {
-        Some(',') => Some(current_idx),
-        Some(c) => skip_to_comma(p, skip_comment(p, current_idx + c.len_utf8() as u32)),
-        None => None,
-    }
-}
-
+// TODO: VERIFY
 fn is_line_after_element_empty(p: &Prettier<'_>, index: u32) -> bool {
+    fn skip_comment(p: &Prettier<'_>, idx: u32) -> Option<u32> {
+        p.skip_inline_comment(p.skip_trailing_comment(Some(idx)))
+    }
+
+    #[allow(clippy::cast_possible_truncation)]
+    fn skip_to_comma(p: &Prettier<'_>, current_idx: Option<u32>) -> Option<u32> {
+        let current_idx = current_idx?;
+        match p.source_text[current_idx as usize..].chars().next() {
+            Some(',') => Some(current_idx),
+            Some(c) => skip_to_comma(p, skip_comment(p, current_idx + c.len_utf8() as u32)),
+            None => None,
+        }
+    }
+
     let Some(start_index) = skip_to_comma(p, Some(index)) else { return false };
     p.is_next_line_empty_after_index(start_index)
 }
