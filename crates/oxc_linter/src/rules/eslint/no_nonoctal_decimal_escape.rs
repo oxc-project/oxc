02cb45b1785af858d06dd7df3d8467b3dc88b4b6
camchenry <1514176+camchenry@users.noreply.github.com>
2025-02-08 19:47:02 +0000

    docs(linter): add prettier-ignore where formatting ruins code (#8978)
    While running `just website`, I noticed that these docs were causing some issues when running `npm run fmt` subsequently, I think due to the usage of character escapes. Disabling prettier seems to stop this from occurring.
    
    Also the code examples in `no-nonoctal-decimal-escape` had weird formatting due to the usage of text which gets parsed as a label, so I updated the example.

--- crates/oxc_linter/src/rules/eslint/no_nonoctal_decimal_escape.rs
+++ crates/oxc_linter/src/rules/eslint/no_nonoctal_decimal_escape.rs
@@ -30,13 +30,23 @@ declare_oxc_lint!(
     /// ECMAScript specification treats \8 and \9 in string literals as a legacy feature
     ///
     /// ### Example
+    ///
+    /// Examples of **incorrect** code for this rule:
+    ///
+    /// <!-- prettier-ignore-start -->
+    ///
     /// ```javascript
-    /// incorrect:
-    /// "\8"
-    /// "\9"
-    /// correct:
-    /// "8"
-    /// "\\9"
+    /// let x = "\8"
+    /// let y = "\9"
+    /// ```
+    ///
+    /// <!-- prettier-ignore-end -->
+    ///
+    /// Examples of **correct** code for this rule:
+    ///
+    /// ```javascript
+    /// let x = "8"
+    /// let y = "\\9"
     /// ```
     NoNonoctalDecimalEscape,
     eslint,
