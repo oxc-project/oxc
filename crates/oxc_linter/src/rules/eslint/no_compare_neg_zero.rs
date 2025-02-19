7d89e19d144d3a7c677c1fff1b2a6b75c879dc1b
Tom <tomw08@gmail.com>
2025-02-19 19:25:22 +0000

    chore(linter): Improve the documentation of eslint/no-compare-neg-zero (#9245)
    Add more documentation aligned with doc template for eslint rule
    `no-compare-neg-zero`.
    
    Relates to [#6050](https://github.com/oxc-project/oxc/issues/6050)

--- crates/oxc_linter/src/rules/eslint/no_compare_neg_zero.rs
+++ crates/oxc_linter/src/rules/eslint/no_compare_neg_zero.rs
@@ -17,16 +17,47 @@ pub struct NoCompareNegZero;
 
 declare_oxc_lint!(
     /// ### What it does
+    ///
     /// Disallow comparing against -0
     ///
     /// ### Why is this bad?
+    ///
     /// The rule should warn against code that tries to compare against -0,
     /// since that will not work as intended. That is, code like x === -0 will
     /// pass for both +0 and -0. The author probably intended Object.is(x, -0).
     ///
-    /// ### Example
+    /// ### Examples
+    ///
+    /// Examples of **incorrect** code for this rule:
     /// ```javascript
-    /// if (x === -0) {}
+    /// if (x === -0) {
+    ///     // doSomething()...
+    /// }
+    /// ```
+    ///
+    /// ```javascript
+    /// if (-0 > x) {
+    ///     // doSomething()...
+    /// }
+    /// ```
+    ///
+    /// Examples of **correct** code for this rule:
+    /// ```javascript
+    /// if (x === 0) {
+    ///     // doSomething()...
+    /// }
+    /// ```
+    ///
+    /// ```javascript
+    /// if (Object.is(x, -0)) {
+    ///     // doSomething()...
+    /// }
+    /// ```
+    ///
+    /// ```javascript
+    /// if (0 > x) {
+    ///     // doSomething()...
+    /// }
     /// ```
     NoCompareNegZero,
     eslint,
