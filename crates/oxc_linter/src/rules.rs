8c71590023a6ae3cbb6a53b43bef39952a00ff31
yefan <yuxuan1491433427@qq.com>
2025-02-28 19:28:53 +0800

    feat(linter): add import/no-mutable-exports rule (#9434)
    Relates to #1117 
    Rule
    detail：https://github.com/import-js/eslint-plugin-import/blob/v2.31.0/docs/rules/no-mutable-exports.md

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -7,6 +7,7 @@
 
 /// <https://github.com/import-js/eslint-plugin-import>
 mod import {
+    pub mod no_mutable_exports;
     // pub mod no_deprecated;
     // pub mod no_unused_modules;
     pub mod default;
@@ -693,6 +694,7 @@ oxc_macros::declare_all_lint_rules! {
     import::default,
     import::export,
     import::first,
+    import::no_mutable_exports,
     import::no_named_default,
     import::no_namespace,
     import::max_dependencies,
