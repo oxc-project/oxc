dd7efea59a4dc789f81ae7ed9ea5c194ae796e2b
therewillbecode <tomw08@gmail.com>
2025-03-07 19:16:56 +0000

    Improve docs for no-shadow-restricted-names

--- crates/oxc_linter/src/rules/eslint/no_shadow_restricted_names.rs
+++ crates/oxc_linter/src/rules/eslint/no_shadow_restricted_names.rs
@@ -18,12 +18,27 @@ pub struct NoShadowRestrictedNames;
 declare_oxc_lint!(
     /// ### What it does
     ///
-    /// Disallow redefine the global variables like 'undefined', 'NaN', 'Infinity', 'eval', 'arguments'.
+    /// Disallows the redefining of global variables such as `undefined`, `NaN`, `Infinity`, `eval`
+    /// and `arguments`.
     ///
     /// ### Why is this bad?
     ///
+    /// Value properties of the Global Object `NaN`, `Infinity`, `undefined` as well as the strict
+    /// mode restricted identifiers `eval` and `arguments` are considered to be restricted names in
+    /// JavaScript. Defining them to mean something else can have unintended consequences and
+    /// confuse others reading the code. For example, there’s nothing preventing you from
+    /// writing:
     ///
-    /// ### Example
+    /// ```javascript
+    /// var undefined = "foo";
+    /// ```
+    ///
+    /// Then any code used within the same scope would not get the global undefined, but rather the
+    /// local version with a very different meaning.
+    ///
+    /// ### Examples
+    ///
+    /// Examples of **incorrect** code for this rule:
     /// ```javascript
     /// function NaN(){}
     ///
@@ -32,6 +47,28 @@ declare_oxc_lint!(
     /// var undefined = 5;
     ///
     /// try {} catch(eval){}
+    /// ```
+    ///
+    /// ```javascript
+    /// import NaN from "foo";
+    ///
+    /// import { undefined } from "bar";
+    ///
+    /// class Infinity {}
+    /// ```
+    ///
+    /// Examples of **correct** code for this rule:
+    /// ```javascript
+    /// var Object;
+    ///
+    /// function f(a, b){}
+    ///
+    /// // Exception: `undefined` may be shadowed if the variable is never assigned a value.
+    /// var undefined;
+    /// ```
+    ///
+    /// ```javascript
+    /// import { undefined as undef } from "bar";
     /// ```
     NoShadowRestrictedNames,
     eslint,
