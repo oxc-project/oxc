06f28f7256ff199b28d3d88d4234f6eabd533a3a
therewillbecode <tomw08@gmail.com>
2025-03-08 13:29:03 +0000

    Add correctness examples to docs for ts non-null nullish coale rule

--- crates/oxc_linter/src/rules/typescript/no_non_null_asserted_nullish_coalescing.rs
+++ crates/oxc_linter/src/rules/typescript/no_non_null_asserted_nullish_coalescing.rs
@@ -18,12 +18,39 @@ declare_oxc_lint!(
     /// Disallow non-null assertions in the left operand of a nullish coalescing operator.
     ///
     /// ### Why is this bad?
-    /// The ?? nullish coalescing runtime operator allows providing a default value when dealing with null or undefined. Using a ! non-null assertion type operator in the left operand of a nullish coalescing operator is redundant, and likely a sign of programmer error or confusion over the two operators.
+    /// The ?? nullish coalescing runtime operator allows providing a default value when dealing
+    /// with null or undefined. Using a ! non-null assertion type operator in the left operand of
+    /// a nullish coalescing operator is redundant, and likely a sign of programmer error or
+    /// confusion over the two operators.
     ///
-    /// ### Example
+    /// ### Examples
+    ///
+    /// Examples of **incorrect** code for this rule:
     /// ```ts
     /// foo! ?? bar;
+    /// foo.bazz! ?? bar;
+    /// foo!.bazz! ?? bar;
+    /// foo()! ?? bar;
     ///
+    /// let x!: string;
+    /// x! ?? '';
+    ///
+    /// let x: string;
+    /// x = foo();
+    /// x! ?? '';
+    /// ```
+    ///
+    /// Examples of **correct** code for this rule:
+    /// ```ts
+    /// foo ?? bar;
+    /// foo ?? bar!;
+    /// foo!.bazz ?? bar;
+    /// foo!.bazz ?? bar!;
+    /// foo() ?? bar;
+    /// ```
+    ///
+    /// ```ts
+    /// // This is considered correct code because there's no way for the user to satisfy it.
     /// let x: string;
     /// x! ?? '';
     /// ```
