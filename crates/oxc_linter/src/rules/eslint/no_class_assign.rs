e831ad3037315cf3157f58c40e44e2932980479f
therewillbecode <tomw08@gmail.com>
2025-03-14 08:58:15 +0000

    Add correctness examples to no-class-assign

--- crates/oxc_linter/src/rules/eslint/no_class_assign.rs
+++ crates/oxc_linter/src/rules/eslint/no_class_assign.rs
@@ -17,17 +17,64 @@ pub struct NoClassAssign;
 
 declare_oxc_lint!(
     /// ### What it does
+    ///
     /// Disallow reassigning class variables.
     ///
     /// ### Why is this bad?
-    /// `ClassDeclaration` creates a variable that can be re-assigned,
-    /// but the re-assignment is a mistake in most cases.
     ///
-    /// ### Example
+    /// `ClassDeclaration` creates a variable that can be re-assigned, but the re-assignment is a
+    /// mistake in most cases.
+    ///
+    /// ### Examples
+    ///
+    /// Examples of **incorrect** code for this rule:
     /// ```javascript
-    /// class A {}
-    /// A = 123;
-    /// let a = new A() // Error
+    /// class A { }
+    /// A = 0;
+    /// ```
+    ///
+    /// ```javascript
+    /// A = 0;
+    /// class A { }
+    /// ```
+    ///
+    /// ```javascript
+    /// class A {
+    ///   b() {
+    ///     A = 0;
+    ///   }
+    /// }
+    /// ```
+    ///
+    /// ```javascript
+    /// let A = class A {
+    ///   b() {
+    ///     A = 0;
+    ///     // `let A` is shadowed by the class name.
+    ///   }
+    /// }
+    /// ```
+    ///
+    /// Examples of **correct** code for this rule:
+    /// ```javascript
+    /// let A = class A { }
+    /// A = 0; // A is a variable.
+    /// ```
+    ///
+    /// ```javascript
+    /// let A = class {
+    ///   b() {
+    ///     A = 0; // A is a variable.
+    ///   }
+    /// }
+    /// ```
+    ///
+    /// ```javascript
+    /// class A {
+    ///   b(A) {
+    ///     A = 0; // A is a parameter.
+    ///   }
+    /// }
     /// ```
     NoClassAssign,
     eslint,
