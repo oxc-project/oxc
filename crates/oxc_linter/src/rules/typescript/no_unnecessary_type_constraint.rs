ec65b8f4a922ac071cc48b4c22f9e19453d13181
therewillbecode <tomw08@gmail.com>
2025-03-15 18:54:45 +0000

    Improve docs

--- crates/oxc_linter/src/rules/typescript/no_unnecessary_type_constraint.rs
+++ crates/oxc_linter/src/rules/typescript/no_unnecessary_type_constraint.rs
@@ -32,20 +32,46 @@ declare_oxc_lint!(
     ///
     /// ### Why is this bad?
     ///
-    /// Generic type parameters (`<T>`) in TypeScript may be "constrained" with an extends keyword.
-    /// When no extends is provided, type parameters default a constraint to unknown. It is therefore redundant to extend from any or unknown.
+    /// Generic type parameters (`<T>`) in TypeScript may be "constrained" with an `extends`
+    /// keyword. When no `extends` is provided, type parameters default a constraint to `unknown`.
+    /// It is therefore redundant to `extend` from `any` or `unknown`.
     ///
-    /// ### Example
+    /// ### Examples
+    ///
+    /// Examples of **incorrect** code for this rule:
     /// ```typescript
     /// interface FooAny<T extends any> {}
     /// interface FooUnknown<T extends unknown> {}
+    ///
     /// type BarAny<T extends any> = {};
     /// type BarUnknown<T extends unknown> = {};
+    ///
+    /// const QuuxAny = <T extends any>() => {};
+    ///
+    /// function QuuzAny<T extends any>() {}
+    /// ```
+    ///
+    /// ```typescript
     /// class BazAny<T extends any> {
     ///   quxAny<U extends any>() {}
     /// }
-    /// const QuuxAny = <T extends any>() => {};
-    /// function QuuzAny<T extends any>() {}
+    /// ```
+    ///
+    /// Examples of **correct** code for this rule:
+    /// ```typescript
+    /// interface Foo<T> {}
+    ///
+    /// type Bar<T> = {};
+    ///
+    /// const Quux = <T>() => {};
+    ///
+    /// function Quuz<T>() {}
+    /// ```
+    ///
+    /// ```typescript
+    /// class Baz<T> {
+    ///   qux<U>() {}
+    /// }
     /// ```
     NoUnnecessaryTypeConstraint,
     typescript,
