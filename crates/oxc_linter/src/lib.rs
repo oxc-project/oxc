8e3d9be3c3b650c34755f2f8fde899a4c09aa3bb
1zumii <524123601@qq.com>
2025-03-19 19:34:25 +0800

    feat(linter): support `--report-unused-disable-directive` (#9223)
    and `--report-unused-disable-directives-severity`
    
    closed #7544
    
    ---------
    
    Co-authored-by: Boshen <boshenc@gmail.com>

--- crates/oxc_linter/src/lib.rs
+++ crates/oxc_linter/src/lib.rs
@@ -102,6 +102,12 @@ impl Linter {
         self
     }
 
+    #[must_use]
+    pub fn with_report_unused_directives(mut self, report_config: Option<AllowWarnDeny>) -> Self {
+        self.options.report_unused_directive = report_config;
+        self
+    }
+
     pub(crate) fn options(&self) -> &LintOptions {
         &self.options
     }
@@ -203,6 +209,12 @@ impl Linter {
                         rule.run_on_jest_node(&jest_node, ctx);
                     }
                 }
+            }
+        }
+
+        if let Some(severity) = self.options.report_unused_directive {
+            if severity.is_warn_deny() {
+                ctx_host.report_unused_directives(severity.into());
             }
         }
 
