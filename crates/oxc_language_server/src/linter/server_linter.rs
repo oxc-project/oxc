31e051333a7a5d54be8d576d8e7fd93c0acce725
Cameron McHenry <camchenry@users.noreply.github.com>
2025-03-22 21:56:44 -0400

    test(editor): add end-to-end tests for linter output

--- crates/oxc_language_server/src/linter/server_linter.rs
+++ crates/oxc_language_server/src/linter/server_linter.rs
@@ -28,3 +28,30 @@ impl ServerLinter {
             .run_single(&uri.to_file_path().unwrap(), content)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::linter::tester::Tester;
+    use oxc_linter::{LintFilter, LintFilterKind};
+
+    #[test]
+    fn test_no_errors() {
+        Tester::new()
+            .with_snapshot_suffix("no_errors")
+            .test_and_snapshot_single_file("fixtures/linter/hello_world.js");
+    }
+
+    #[test]
+    fn test_no_console() {
+        let config_store = ConfigStoreBuilder::default()
+            .with_filter(LintFilter::deny(LintFilterKind::parse("no-console".into()).unwrap()))
+            .build()
+            .unwrap();
+        let linter = Linter::new(LintOptions::default(), config_store).with_fix(FixKind::SafeFix);
+
+        Tester::new_with_linter(linter)
+            .with_snapshot_suffix("deny_no_console")
+            .test_and_snapshot_single_file("fixtures/linter/hello_world.js");
+    }
+}
