c3af9a45100ec0a30695b20163af24237661d7c1
Sysix <3897725+Sysix@users.noreply.github.com>
2025-03-23 13:39:59 +0000

    test(editor): add tests for listing all oxc commands (#9930)

---
+++ editors/vscode/client/extension.spec.ts
@@ -0,0 +1,56 @@
+import { deepStrictEqual } from 'assert';
+import { commands, extensions } from 'vscode';
+
+// const WORKSPACE_DIR = workspace.workspaceFolders![0].uri.toString();
+// const filePath = WORKSPACE_DIR + '/debugger.js';
+// const fileUri = Uri.parse(filePath);
+
+suite('commands', () => {
+  setup(async () => {
+    const ext = extensions.getExtension('oxc.oxc-vscode')!;
+    await ext.activate();
+  });
+
+  // suiteTeardown(async () => {
+  //   const edit = new WorkspaceEdit();
+  //   edit.deleteFile(fileUri);
+  //   await workspace.applyEdit(edit);
+  // });
+
+  test('listed commands', async () => {
+    const oxcCommands = (await commands.getCommands(true)).filter(x => x.startsWith('oxc.'));
+
+    deepStrictEqual([
+      'oxc.restartServer',
+      'oxc.showOutputChannel',
+      'oxc.toggleEnable',
+      'oxc.applyAllFixesFile',
+      'oxc.fixAll',
+    ], oxcCommands);
+  });
+
+  // ToDo: check why this is not working,
+  // even with .gitignore deleted in th test_workspace
+  //
+  // test('oxc.fixAll', async () => {
+  //   const edit = new WorkspaceEdit();
+  //   edit.createFile(fileUri, {
+  //     contents: Buffer.from('/* 😊 */debugger;'),
+  //   });
+  //
+  //   await workspace.applyEdit(edit);
+  //   await window.showTextDocument(fileUri);
+  //   await sleep(500);
+  //   await commands.executeCommand('oxc.fixAll', {
+  //     uri: fileUri.toString(),
+  //   });
+  //   await workspace.saveAll();
+  //   await sleep(1000);
+  //
+  //   const content = await readFile(fileUri.fsPath, {
+  //     encoding: 'utf8',
+  //   });
+  //
+  //  strictEqual(content, '/* 😊 */');
+  // });
+});
