85c046c609e4e47ea71463c23b0eeab1c5d483a8
Boshen <boshenc@gmail.com>
2025-03-28 21:32:01 +0800

    fix bench

--- tasks/benchmark/benches/formatter.rs
+++ tasks/benchmark/benches/formatter.rs
@@ -7,20 +7,29 @@ use oxc_tasks_common::TestFiles;
 
 fn bench_formatter(criterion: &mut Criterion) {
     let mut group = criterion.benchmark_group("formatter");
+
     for file in TestFiles::minimal().files() {
         let id = BenchmarkId::from_parameter(&file.file_name);
         let source_text = file.source_text.as_str();
         let source_type = SourceType::from_path(&file.file_name).unwrap();
+
+        let mut allocator = Allocator::default();
+
         group.bench_function(id, |b| {
-            b.iter(|| {
-                let allocator1 = Allocator::default();
-                let allocator2 = Allocator::default();
-                let ret = Parser::new(&allocator1, source_text, source_type).parse();
-                let _ =
-                    Formatter::new(&allocator2, FormatterOptions::default()).build(&ret.program);
+            b.iter_with_setup_wrapper(|runner| {
+                allocator.reset();
+
+                let program = Parser::new(&allocator1, source_text, source_type).parse().program;
+
+                let options = FormatterOptions::default();
+
+                runner.run(|| {
+                    Formatter::new(&allocator, options).build(&ret.program);
+                })
             });
         });
     }
+
     group.finish();
 }
 
