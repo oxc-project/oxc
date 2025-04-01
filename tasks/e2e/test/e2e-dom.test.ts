0fff1025deb80242c16e5fc7526c985e49a7ab5b
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-04-01 21:26:14 +0900

    feat(tasks/e2e): add utility libraries

--- tasks/e2e/test/e2e-dom.test.ts
+++ tasks/e2e/test/e2e-dom.test.ts
@@ -6,15 +6,22 @@ import { getModules } from './utils';
 const info = '$type $options';
 
 describe('alpine', async () => {
-  const modules = await getModules('alpinejs/dist/', 'module.cjs.js', false);
+  const modules = await getModules('alpinejs/dist/', 'module.cjs.js', 'cjs');
   test.each(modules)(info, ({ module: { Alpine } }) => {
     expect(Alpine.reactive({ count: 1 }).count).toBe(1);
   });
 });
 
 describe('lit', async () => {
-  const modules = await getModules('lit/all/', 'lit-all.min.js', true);
+  const modules = await getModules('lit/all/', 'lit-all.min.js', 'esm');
   test.each(modules)(info, ({ module: Lit }) => {
     expect(Lit.html`<div>rendered</div>`).toBeDefined();
+  });
+});
+
+describe('jquery', async () => {
+  const modules = await getModules('jquery/dist/', 'jquery.js', 'cjs');
+  test.each(modules)(info, ({ module: jQuery }) => {
+    expect(jQuery('<div>rendered</div>').text()).toBe('rendered');
   });
 });
