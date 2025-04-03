297d07fe9970ff54528e02a49bf424f2e91b5dc5
Sysix <3897725+Sysix@users.noreply.github.com>
2025-04-03 17:04:10 +0000

    test(editor): add e2e tests for creating oxlint configurations on the fly (#10138)

--- editors/vscode/client/extension.spec.ts
+++ editors/vscode/client/extension.spec.ts
@@ -11,7 +11,13 @@ import {
   workspace,
   WorkspaceEdit,
 } from 'vscode';
-import { activateExtension, sleep, WORKSPACE_DIR } from './test-helpers';
+import {
+  activateExtension,
+  createOxlintConfiguration,
+  deleteOxlintConfiguration,
+  sleep,
+  WORKSPACE_DIR,
+} from './test-helpers';
 import assert = require('assert');
 
 const filePath = WORKSPACE_DIR + '/debugger.js';
@@ -92,18 +98,20 @@ suite('E2E Diagnostics', () => {
     await activateExtension();
   });
 
-  suiteTeardown(async () => {
+  teardown(async () => {
     const edit = new WorkspaceEdit();
     edit.deleteFile(fileUri, {
       ignoreIfNotExists: true,
     });
     await workspace.applyEdit(edit);
+    await deleteOxlintConfiguration();
   });
 
   test('simple debugger statement', async () => {
     const edit = new WorkspaceEdit();
     edit.createFile(fileUri, {
       contents: Buffer.from('/* 😊 */debugger;'),
+      overwrite: true,
     });
 
     await workspace.applyEdit(edit);
@@ -163,5 +171,64 @@ suite('E2E Diagnostics', () => {
         },
       ],
     );
+  });
+
+  test('empty oxlint configuration behaves like default configuration', async () => {
+    await createOxlintConfiguration({});
+
+    const edit = new WorkspaceEdit();
+    edit.createFile(fileUri, {
+      contents: Buffer.from('/* 😊 */debugger;'),
+      overwrite: true,
+    });
+
+    await workspace.applyEdit(edit);
+    await window.showTextDocument(fileUri);
+    const diagnostics = languages.getDiagnostics(fileUri);
+
+    strictEqual(diagnostics.length, 1);
+    assert(typeof diagnostics[0].code == 'object');
+    strictEqual(diagnostics[0].code.target.authority, 'oxc.rs');
+    strictEqual(diagnostics[0].message, '`debugger` statement is not allowed\nhelp: Delete this code.');
+    strictEqual(diagnostics[0].severity, DiagnosticSeverity.Warning);
+    strictEqual(diagnostics[0].range.start.line, 0);
+    strictEqual(diagnostics[0].range.start.character, 8);
+    strictEqual(diagnostics[0].range.end.line, 0);
+    strictEqual(diagnostics[0].range.end.character, 17);
+
+    await deleteOxlintConfiguration();
+  });
+
+  test('setting rule to error, will report the diagnostic as error', async () => {
+    const edit = new WorkspaceEdit();
+    edit.createFile(fileUri, {
+      contents: Buffer.from('/* 😊 */debugger;'),
+      overwrite: true,
+    });
+
+    await workspace.applyEdit(edit);
+    await window.showTextDocument(fileUri);
+    const diagnosticsWarning = languages.getDiagnostics(fileUri);
+
+    strictEqual(diagnosticsWarning.length, 1);
+    strictEqual(diagnosticsWarning[0].severity, DiagnosticSeverity.Warning, 'Expect default severity to be warning');
+
+    const resolve = new Promise<void>((resolve) =>
+      languages.onDidChangeDiagnostics(() => {
+        const diagnosticsError = languages.getDiagnostics(fileUri);
+
+        strictEqual(diagnosticsError.length, 1);
+        strictEqual(diagnosticsError[0].severity, DiagnosticSeverity.Error, 'Expect changed severity to be error');
+        resolve();
+      })
+    );
+
+    await createOxlintConfiguration({
+      rules: {
+        'no-debugger': 'error',
+      },
+    });
+    await workspace.saveAll();
+    await resolve;
   });
 });
