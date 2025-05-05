89693cb7ac16cd47f0bcf7d00b1ef93491195c44
overlookmotel <theoverlookmotel@gmail.com>
2025-05-05 13:18:03 +0100

    perf(napi/parser): simplify recursion and avoid function calls in fixup visitor

--- napi/parser/wrap.mjs
+++ napi/parser/wrap.mjs
@@ -28,41 +28,41 @@ export function wrap(result) {
 // Used by napi/playground/patch.mjs
 export function jsonParseAst(programJson) {
   const program = JSON.parse(programJson);
-  visitNode(program, transform);
+  transformNode(program);
   return program;
 }
 
-function transform(node) {
-  // Set `value` field of `Literal`s for `BigInt`s and `RegExp`s.
-  // This is not possible to do on Rust side, as neither can be represented correctly in JSON.
-  if (node.type === 'Literal') {
-    if (node.bigint) {
-      node.value = BigInt(node.bigint);
-    }
-    if (node.regex) {
-      try {
-        node.value = RegExp(node.regex.pattern, node.regex.flags);
-      } catch (_err) {
-        // Invalid regexp, or valid regexp using syntax not supported by this version of NodeJS
-      }
-    }
-  }
-}
-
-function visitNode(node, fn) {
+function transformNode(node) {
   if (!node) return;
   if (Array.isArray(node)) {
     for (const el of node) {
-      visitNode(el, fn);
+      transformNode(el);
     }
     return;
   }
 
-  fn(node);
+  if (node.type === 'Literal') {
+    transformLiteral(node);
+    return;
+  }
 
   const keys = visitorKeys[node.type];
   if (!keys) return;
   for (const key of keys) {
-    visitNode(node[key], fn);
+    transformNode(node[key]);
+  }
+}
+
+function transformLiteral(node) {
+  // Set `value` field of `Literal`s for `BigInt`s and `RegExp`s.
+  // This is not possible to do on Rust side, as neither can be represented correctly in JSON.
+  if (node.bigint) {
+    node.value = BigInt(node.bigint);
+  } else if (node.regex) {
+    try {
+      node.value = RegExp(node.regex.pattern, node.regex.flags);
+    } catch (_err) {
+      // Invalid regexp, or valid regexp using syntax not supported by this version of NodeJS
+    }
   }
 }
