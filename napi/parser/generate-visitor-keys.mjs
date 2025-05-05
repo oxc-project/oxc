cf71c3d1d43383278a3567be6a462b090c952b42
overlookmotel <theoverlookmotel@gmail.com>
2025-05-05 11:05:19 +0100

    Refactor generator

--- napi/parser/generate-visitor-keys.mjs
+++ napi/parser/generate-visitor-keys.mjs
@@ -1,17 +1,17 @@
 import { visitorKeys } from '@typescript-eslint/visitor-keys';
 import { writeFileSync } from 'node:fs';
 
-writeFileSync(
-  './generated/visitor-keys.js',
-  `// Auto-generated code, DO NOT EDIT DIRECTLY!
+const keys = Object.entries(visitorKeys)
+  .filter(([, v]) => v?.length)
+  .map(([k, v]) => `  ${k}: [${v.map((v) => `'${v}'`).join(', ')}],`)
+  .join('\n');
+
+const code = `// Auto-generated code, DO NOT EDIT DIRECTLY!
 // To edit this generated file you have to edit \`napi/parser/generate-visitor-keys.mjs\`.
 
-module.exports = {${
-    Object.entries(visitorKeys)
-      .filter(([, v]) => v?.length)
-      .map(([k, v]) => `\n  ${k}: [${v.map((v) => `"${v}"`).join(', ')}],`)
-      .join('')
-  }
+module.exports = {
+${keys}
 };
-`,
-);
+`;
+
+writeFileSync('./generated/visitor-keys.js', code);
