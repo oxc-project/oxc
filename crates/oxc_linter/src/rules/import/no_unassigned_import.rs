7f02ccab683a3d7a060d45721d04874981acf10a
yefan <yuxuan1491433427@qq.com>
2025-05-15 20:16:45 +0800

    use GlobSet

--- crates/oxc_linter/src/rules/import/no_unassigned_import.rs
+++ crates/oxc_linter/src/rules/import/no_unassigned_import.rs
@@ -1,4 +1,4 @@
-use fast_glob::glob_match;
+use globset::{Glob, GlobSet, GlobSetBuilder};
 use oxc_ast::{
     AstKind,
     ast::{Argument, Expression},
@@ -11,71 +11,91 @@ use serde_json::Value;
 use crate::{AstNode, context::LintContext, rule::Rule};
 
 fn no_unassigned_import_diagnostic(span: Span, msg: &str) -> OxcDiagnostic {
-    // See <https://oxc.rs/docs/contribute/linter/adding-rules.html#diagnostics> for details
-    OxcDiagnostic::warn(msg.to_string()).with_label(span)
+    OxcDiagnostic::warn(msg.to_string())
+        .with_help("Consider assigning the import to a variable or removing it if it's unused.")
+        .with_label(span)
 }
 
 #[derive(Debug, Default, Clone)]
 pub struct NoUnassignedImport {
-    allow: Vec<CompactStr>,
+    globs: GlobSet,
 }
 
 // See <https://github.com/oxc-project/oxc/issues/6050> for documentation details.
 declare_oxc_lint!(
     /// ### What it does
     ///
-    /// Briefly describe the rule's purpose.
+    /// This rule aims to remove modules with side-effects by reporting when a module is imported but not assigned.
     ///
     /// ### Why is this bad?
     ///
-    /// Explain why violating this rule is problematic.
+    /// With both CommonJS' require and the ES6 modules' import syntax,
+    /// it is possible to import a module but not to use its result.
+    /// This can be done explicitly by not assigning the module to as variable.
+    /// Doing so can mean either of the following things:
+    /// * The module is imported but not used
+    /// * The module has side-effects (like should). Having side-effects,
+    /// makes it hard to know whether the module is actually used or can be removed.
+    /// It can also make it harder to test or mock parts of your application.
     ///
     /// ### Examples
     ///
     /// Examples of **incorrect** code for this rule:
     /// ```js
-    /// FIXME: Tests will fail if examples are missing or syntactically incorrect.
+    /// import 'should'
+    /// require('should')
     /// ```
     ///
     /// Examples of **correct** code for this rule:
     /// ```js
-    /// FIXME: Tests will fail if examples are missing or syntactically incorrect.
+    /// import _ from 'foo'
+    /// import _, {foo} from 'foo'
+    /// import _, {foo as bar} from 'foo'
+    /// const _ = require('foo')
+    /// const {foo} = require('foo')
+    /// const {foo: bar} = require('foo')
+    /// bar(require('foo'))
     /// ```
     NoUnassignedImport,
     import,
-    nursery, // TODO: change category to `correctness`, `suspicious`, `pedantic`, `perf`, `restriction`, or `style`
-             // See <https://oxc.rs/docs/contribute/linter.html#rule-category> for details
-    pending  // TODO: describe fix capabilities. Remove if no fix can be done,
-             // keep at 'pending' if you think one could be added but don't know how.
-             // Options are 'fix', 'fix_dangerous', 'suggestion', and 'conditional_fix_suggestion'
+    suspicious,
 );
+
+fn build_globset(patterns: Vec<CompactStr>) -> Result<GlobSet, globset::Error> {
+    if patterns.is_empty() {
+        return Ok(GlobSet::empty());
+    }
+    let mut builder = GlobSetBuilder::new();
+    for pattern in patterns {
+        let pattern_str = pattern.as_str();
+        builder.add(Glob::new(pattern_str)?);
+    }
+    builder.build()
+}
 
 impl Rule for NoUnassignedImport {
     fn from_configuration(value: Value) -> Self {
         let obj = value.get(0);
-        Self {
-            allow: obj
-                .and_then(|v| v.get("allow"))
-                .and_then(Value::as_array)
-                .map(|v| v.iter().filter_map(Value::as_str).map(CompactStr::from).collect())
-                .unwrap_or_default(),
-        }
+        let allow = obj
+            .and_then(|v| v.get("allow"))
+            .and_then(Value::as_array)
+            .map(|v| v.iter().filter_map(Value::as_str).map(CompactStr::from).collect())
+            .unwrap_or_default();
+        Self { globs: build_globset(allow).unwrap_or_else(|_| GlobSet::empty()) }
     }
     fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
         match node.kind() {
             AstKind::ImportDeclaration(import_decl) => {
-                if !import_decl.specifiers.is_none() {
+                if import_decl.specifiers.is_some() {
                     return;
                 }
-
                 let source_str = import_decl.source.value.as_str();
-                if self.allow.iter().any(|pattern| glob_match(pattern.as_str(), source_str)) {
-                    return;
+                if !self.globs.is_match(source_str) {
+                    ctx.diagnostic(no_unassigned_import_diagnostic(
+                        import_decl.span,
+                        "Imported module should be assigned",
+                    ));
                 }
-                ctx.diagnostic(no_unassigned_import_diagnostic(
-                    import_decl.span,
-                    "Imported module should be assigned",
-                ));
             }
             AstKind::ExpressionStatement(statement) => {
                 let Expression::CallExpression(call_expr) = &statement.expression else {
@@ -84,15 +104,11 @@ impl Rule for NoUnassignedImport {
                 if !call_expr.is_require_call() {
                     return;
                 }
-                let first_arge = &call_expr.arguments[0];
-                if let Argument::StringLiteral(source_str) = first_arge {
-                    if self
-                        .allow
-                        .iter()
-                        .any(|pattern| glob_match(pattern.as_str(), source_str.value.as_str()))
-                    {
-                        return;
-                    }
+                let first_arg = &call_expr.arguments[0];
+                let Argument::StringLiteral(source_str) = first_arg else {
+                    return;
+                };
+                if !self.globs.is_match(source_str.value.as_str()) {
                     ctx.diagnostic(no_unassigned_import_diagnostic(
                         call_expr.span,
                         "A `require()` style import is forbidden.",
@@ -114,14 +130,35 @@ fn test() {
         ("import foo from 'foo'", None),
         ("import foo, { bar } from 'foo'", None),
         ("import * as _ from 'foo'", None),
+        ("require('lodash')()", None),
+        ("require('lodash').foo", None),
+        ("require('lodash').foo()", None),
+        ("const _ = require('./')", None),
+        ("bar(require('foo'))", None),
+        ("const [a, b] = require('lodash')", None),
+        ("import 'app.css'", Some(json!([{ "allow": ["**/*.css"]}]))),
+        ("import 'app.css'", Some(json!([{ "allow": ["*.css"]}]))),
+        ("import './app.css'", Some(json!([{ "allow": ["**/*.css"]}]))),
+        ("import '../dist/app.css'", Some(json!([{ "allow": ["**/*.css"]}]))),
+        ("import '../dist/app.js'", Some(json!([{ "allow": ["**/dist/**"]}]))),
+        ("import 'foo/bar'", Some(json!([{ "allow": ["foo/**"]}]))),
+        ("import 'foo/bar'", Some(json!([{ "allow": ["foo/bar"]}]))),
+        ("import 'babel-register'", Some(json!([{ "allow": ["babel-register"]}]))),
+        ("require('./app.css')", Some(json!([{ "allow": ["**/*.css"]}]))),
+        ("import './styles/app.css'", Some(json!([{ "allow": ["**/styles/*.css"]}]))),
     ];
 
     let fail = vec![
         ("require('should')", None),
         ("import 'foo'", None),
-        ("import '../styles/app.css'", Some(json!([{ "allow": ["/styles/*.css"]}]))),
+        ("import './styles/app.css'", Some(json!([{ "allow": ["styles/*.css"]}]))),
+        ("import './app.css'", Some(json!([{ "allow": ["**/*.js"]}]))),
+        ("import './app.css'", Some(json!([{ "allow": ["**/dir/**"]}]))),
+        ("require('./app.css')", Some(json!([{ "allow": ["**/*.js"]}]))),
     ];
 
     Tester::new(NoUnassignedImport::NAME, NoUnassignedImport::PLUGIN, pass, fail)
+        .change_rule_path("no-unassigned-import.js")
+        .with_import_plugin(true)
         .test_and_snapshot();
 }
