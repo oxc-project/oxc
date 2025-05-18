6e3e37bec5a95de393a5a6358624552e35cfc689
shulaoda <165626830+shulaoda@users.noreply.github.com>
2025-05-18 05:17:03 +0000

    perf(unicorn/no-instanceof-array): reduce memory allocations in fixer (#11109)
    We can reduce additional memory allocation by specifying capacity.

--- crates/oxc_linter/src/rules/unicorn/no_instanceof_array.rs
+++ crates/oxc_linter/src/rules/unicorn/no_instanceof_array.rs
@@ -55,14 +55,12 @@ impl Rule for NoInstanceofArray {
         match &expr.right.without_parentheses() {
             Expression::Identifier(identifier) if identifier.name == "Array" => {
                 ctx.diagnostic_with_fix(no_instanceof_array_diagnostic(expr.span), |fixer| {
-                    let modified_code = {
-                        let mut codegen = String::new();
-                        codegen.push_str("Array.isArray(");
-                        codegen.push_str(fixer.source_range(expr.left.span()));
-                        codegen.push(')');
-                        codegen
-                    };
-                    fixer.replace(expr.span, modified_code)
+                    let argument = fixer.source_range(expr.left.span());
+                    let mut code = String::with_capacity(15 + argument.len());
+                    code.push_str("Array.isArray(");
+                    code.push_str(argument);
+                    code.push(')');
+                    fixer.replace(expr.span, code)
                 });
             }
             _ => {}
