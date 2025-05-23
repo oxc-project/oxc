6b6c648b0cd87604769ea62c78556131c1d2d99e
yefan <yuxuan1491433427@qq.com>
2025-05-23 20:04:53 +0800

    add test case

--- crates/oxc_linter/src/rules/unicorn/no_static_only_class.rs
+++ crates/oxc_linter/src/rules/unicorn/no_static_only_class.rs
@@ -322,6 +322,7 @@ fn test() {
             "class A { static foo = 2; static bar = 3; static baz() { return 2; } }",
             "const A = { foo: 2, bar: 3, baz() { return 2; }, }",
         ),
+        ("class A { static foo; }", "const A = { foo: undefined, }"),
     ];
 
     Tester::new(NoStaticOnlyClass::NAME, NoStaticOnlyClass::PLUGIN, pass, fail)
