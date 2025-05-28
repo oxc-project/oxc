5e1cb06bd3905d352435013588c0fd5bfd1c0548
overlookmotel <theoverlookmotel@gmail.com>
2025-05-28 17:46:26 +0100

    refactor(ast_tools/estree): order `type` and `span` fields first by default

--- crates/oxc_syntax/src/module_record.rs
+++ crates/oxc_syntax/src/module_record.rs
@@ -87,7 +87,7 @@ impl<'a> ModuleRecord<'a> {
 #[ast]
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[generate_derive(ESTree)]
-#[estree(no_type, no_ts_def)]
+#[estree(no_type, no_ts_def, field_order(name, span))]
 pub struct NameSpan<'a> {
     /// Name
     #[estree(rename = "value")]
