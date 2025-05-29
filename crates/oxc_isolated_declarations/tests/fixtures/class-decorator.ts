daaa8f522ed21b72ba01e84ab2f9ebd5bf784929
magic-akari <akari.ccino@gmail.com>
2025-05-29 23:12:54 +0800

    fix(parser): correctly parse decorators of property declaration (#11370)
    - Closes: #11367
    
    ---------
    
    Co-authored-by: Dunqing <dengqing0821@gmail.com>

---
+++ crates/oxc_isolated_declarations/tests/fixtures/class-decorator.ts
@@ -0,0 +1,24 @@
+declare const decorator: any;
+
+export class Test1 {
+  /**
+   * This methond will trigger the feature highlight dialog load/show based on dialogId and analyticsId
+   */
+  @decorator
+  property: (() => any) | undefined;
+}
+
+export class Test2 {
+  /**
+   * This methond will trigger the feature highlight dialog load/show based on dialogId and analyticsId
+   */
+  @decorator
+  property: ((arg: any) => any) | undefined;
+}
+
+export class Test3 {
+  @decorator
+  property?: {
+    property?: (arg: any) => any;
+  };
+}
