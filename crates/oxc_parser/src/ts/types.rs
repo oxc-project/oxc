ed57fa30e96a9325d49b4a44fe218d2d624bfee4
therewillbecode <tomw08@gmail.com>
2025-06-01 12:44:18 +0000

    refactor(parser): Reduce backtracking for assertion signature parsing (#11424)
    - Replace use of `lookahead` when parsing assertion signatures in favour
    of checkpoint and rewinds.
    - Remove unnecessary backtracking when parsing assertion signatures for
    one of the code paths.
    
    Relates to #11334 as part of reducing unnecessary backtracking from the
    parser.

--- crates/oxc_parser/src/ts/types.rs
+++ crates/oxc_parser/src/ts/types.rs
@@ -406,9 +406,14 @@ impl<'a> ParserImpl<'a> {
             Kind::LParen => self.parse_parenthesized_type(),
             Kind::Import => TSType::TSImportType(self.parse_ts_import_type()),
             Kind::Asserts => {
-                if self.lookahead(Self::is_next_token_identifier_or_keyword_on_same_line) {
-                    self.parse_asserts_type_predicate()
+                let checkpoint = self.checkpoint();
+                let asserts_start_span = self.start_span();
+                self.bump_any(); // bump `asserts`
+
+                if self.is_token_identifier_or_keyword_on_same_line() {
+                    self.parse_asserts_type_predicate(asserts_start_span)
                 } else {
+                    self.rewind(checkpoint);
                     self.parse_type_reference()
                 }
             }
@@ -417,8 +422,7 @@ impl<'a> ParserImpl<'a> {
         }
     }
 
-    fn is_next_token_identifier_or_keyword_on_same_line(&mut self) -> bool {
-        self.bump_any();
+    fn is_token_identifier_or_keyword_on_same_line(&self) -> bool {
         self.cur_kind().is_identifier_name() && !self.cur_token().is_on_new_line()
     }
 
@@ -729,9 +733,7 @@ impl<'a> ParserImpl<'a> {
         self.ast.ts_type_template_literal_type(self.end_span(span), quasis, types)
     }
 
-    fn parse_asserts_type_predicate(&mut self) -> TSType<'a> {
-        let span = self.start_span();
-        self.bump_any(); // bump `asserts`
+    fn parse_asserts_type_predicate(&mut self, asserts_start_span: u32) -> TSType<'a> {
         let parameter_name = if self.at(Kind::This) {
             TSTypePredicateName::This(self.parse_this_type_node())
         } else {
@@ -745,7 +747,7 @@ impl<'a> ParserImpl<'a> {
             type_annotation = Some(self.ast.ts_type_annotation(self.end_span(type_span), ty));
         }
         self.ast.ts_type_type_predicate(
-            self.end_span(span),
+            self.end_span(asserts_start_span),
             parameter_name,
             /* asserts */ true,
             type_annotation,
