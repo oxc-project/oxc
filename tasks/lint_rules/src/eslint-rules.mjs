4a9380bd515c77f615f05dff42eda9fdcb0670fd
camc314 <18101008+camc314@users.noreply.github.com>
2025-06-03 13:18:25 +0000

    feat(tasks/lint_rules): add issue for eslint-plugin-vue (#11442)

--- tasks/lint_rules/src/eslint-rules.mjs
+++ tasks/lint_rules/src/eslint-rules.mjs
@@ -39,6 +39,8 @@ import pluginPromise from 'eslint-plugin-promise';
 import pluginVitest from 'eslint-plugin-vitest';
 // https://github.com/ota-meshi/eslint-plugin-regexp
 import pluginRegexp from 'eslint-plugin-regexp';
+// https://github.com/vuejs/eslint-plugin-vue
+import pluginVue from 'eslint-plugin-vue';
 
 // destructuring default exports
 const {
@@ -81,6 +83,10 @@ const {
   configs: pluginRegexpConfigs,
   rules: pluginRegexpRules,
 } = pluginRegexp;
+const {
+  configs: pluginVueConfigs,
+  rules: pluginVueRules,
+} = pluginVue;
 
 /** @param {import("eslint").Linter} linter */
 const loadPluginTypeScriptRules = (linter) => {
@@ -284,6 +290,20 @@ const loadPluginRegexpRules = (linter) => {
   }
 };
 
+/** @param {import("eslint").Linter} linter */
+const loadPluginVueRules = (linter) => {
+  const pluginVueRecommendedRules = new Map(
+    Object.entries(pluginVueConfigs.recommended.rules || {}),
+  );
+  for (const [name, rule] of Object.entries(pluginVueRules)) {
+    const prefixedName = `vue/${name}`;
+
+    rule.meta.docs.recommended = pluginVueRecommendedRules.has(prefixedName);
+
+    linter.defineRule(prefixedName, rule);
+  }
+};
+
 /**
  * @typedef {{
  *   npm: string[];
@@ -312,6 +332,7 @@ export const ALL_TARGET_PLUGINS = new Map([
   ['promise', { npm: ['eslint-plugin-promise'], issueNo: 4655 }],
   ['vitest', { npm: ['eslint-plugin-vitest'], issueNo: 4656 }],
   ['regexp', { npm: ['eslint-plugin-regexp'], issueNo: 11439 }],
+  ['vue', { npm: ['eslint-plugin-vue'], issueNo: 11440 }],
 ]);
 
 // All rules(including deprecated, recommended) are loaded initially.
@@ -335,4 +356,5 @@ export const loadTargetPluginRules = (linter) => {
   loadPluginPromiseRules(linter);
   loadPluginVitestRules(linter);
   loadPluginRegexpRules(linter);
+  loadPluginVueRules(linter);
 };
