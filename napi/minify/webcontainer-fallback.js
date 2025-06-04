14018390360433b31aef37e0253a232ea19e7ba4
Boshen <1430279+Boshen@users.noreply.github.com>
2025-06-04 08:11:01 +0000

    feat(napi): add webcontainer fallback to transform and minify packages (#11471)

---
+++ napi/minify/webcontainer-fallback.js
@@ -0,0 +1,23 @@
+const fs = require('node:fs');
+const childProcess = require('node:child_process');
+
+const pkg = JSON.parse(
+  fs.readFileSync(require.resolve('oxc-minify/package.json'), 'utf-8'),
+);
+const version = pkg.version;
+const baseDir = `/tmp/oxc-minify-${version}`;
+const bindingEntry = `${baseDir}/node_modules/@oxc-minify/binding-wasm32-wasi/minify.wasi.cjs`;
+
+if (!fs.existsSync(bindingEntry)) {
+  fs.rmSync(baseDir, { recursive: true, force: true });
+  fs.mkdirSync(baseDir, { recursive: true });
+  const bindingPkg = `@oxc-minify/binding-wasm32-wasi@${version}`;
+  // eslint-disable-next-line: no-console
+  console.log(`[oxc-minify] Downloading ${bindingPkg} on WebContainer...`);
+  childProcess.execFileSync('pnpm', ['i', bindingPkg], {
+    cwd: baseDir,
+    stdio: 'inherit',
+  });
+}
+
+module.exports = require(bindingEntry);
--- napi/transform/index.js
+++ napi/transform/index.js
@@ -363,6 +363,14 @@ if (!nativeBinding || process.env.NAPI_RS_FORCE_WASI) {
   }
 }
 
+if (!nativeBinding && globalThis.process?.versions?.["webcontainer"]) {
+  try {
+    nativeBinding = require('./webcontainer-fallback.js');
+  } catch (err) {
+    loadErrors.push(err)
+  }
+}
+
 if (!nativeBinding) {
   if (loadErrors.length > 0) {
     // TODO Link to documentation with potential fixes
