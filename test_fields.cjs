f759b3026e5b6cb0a26c27a7abe9d0e57df68fa0
overlookmotel <theoverlookmotel@gmail.com>
2025-06-10 18:31:21 +0100

    Bump `@typescript-eslint/visitor-keys` package

--- test_fields.cjs
+++ test_fields.cjs
@@ -2,41 +2,8 @@
 
 const fs = require('node:fs');
 const pathJoin = require('node:path').join;
-let tseslintTypes = require('@typescript-eslint/visitor-keys').visitorKeys;
+const tseslintTypes = require('@typescript-eslint/visitor-keys').visitorKeys;
 const oxcTypes = require('./estree_field_orders.json');
-
-// Correct mistakes in TS-ESLint visitation order
-tseslintTypes = {...tseslintTypes};
-function fixTseslintKeys(type, keys) {
-  const existingKeys = [...tseslintTypes[type]];
-  existingKeys.sort();
-  const newKeys = [...keys];
-  newKeys.sort();
-  if (newKeys.length !== existingKeys.length || newKeys.some((key, i) => key !== existingKeys[i])) {
-    throw new Error(`New keys for ${type} differ from old in more than order`);
-  }
-
-  tseslintTypes[type] = keys;
-}
-
-// `local` should be before `exported`
-// e.g. `export { local as exported }`
-fixTseslintKeys('ExportSpecifier', ['local', 'exported']);
-// `options` should be before `qualifier` and `type_arguments`
-// e.g. `type T = import('foo', {assert:{}}).qualifier`
-// e.g. `type T = import('foo', {assert:{}})<T>`
-fixTseslintKeys('TSImportType', ['argument', 'options', 'qualifier', 'typeArguments']);
-// `objectType` should be before `indexType`
-fixTseslintKeys('TSIndexedAccessType', ['objectType', 'indexType']);
-// `key` should be before `typeParameters`
-// e.g. `interface Foo { bar<T>(a: number): string; }`
-fixTseslintKeys('TSMethodSignature', ['key', 'typeParameters', 'params', 'returnType']);
-// `key` should be before `typeParameters`
-// e.g. `interface Foo { bar: number }`
-fixTseslintKeys('TSPropertySignature', ['key', 'typeAnnotation']);
-// `parameterName` should be before `typeAnnotation`
-// e.g. `function isString(x: unknown): x is string {}`
-fixTseslintKeys('TSTypePredicate', ['parameterName', 'typeAnnotation']);
 
 // Combine Oxc and TS-ESLint types
 const types = [...new Set([...Object.keys(tseslintTypes), ...Object.keys(oxcTypes)])];
