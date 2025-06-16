oxc-project/oxc

commit ea30fff17f1ad8591a7ab98d5c1de6fed18b1f73

test(ast/estree): bump `acorn-test262` submodule (#11762)
Bump `acorn-test262` submodule, which bumps `@typescript-eslint/parser`. Latest version of TS-ESLint contains fixes for bugs which I reported. Currently this breaks 1 conformance test - will fix in following PR.

tasks/coverage/snapshots/estree_typescript.snap
@@ -2,7 +2,9 @@ commit: 81c95189
 
 estree_typescript Summary:
 AST Parsed     : 6489/6489 (100.00%)
-Positive Passed: 6486/6489 (99.95%)
+Positive Passed: 6485/6489 (99.94%)
+Mismatch: tasks/coverage/typescript/tests/cases/conformance/classes/constructorDeclarations/quotedConstructors.ts
+
 Mismatch: tasks/coverage/typescript/tests/cases/conformance/jsx/jsxReactTestSuite.tsx
 
 Mismatch: tasks/coverage/typescript/tests/cases/conformance/jsx/tsxReactEmitEntities.tsx

.github/actions/clone-submodules/action.yml
@@ -38,4 +38,4 @@ runs:
         show-progress: false
         repository: oxc-project/acorn-test262
         path: tasks/coverage/acorn-test262
-        ref: 9bfb9ab035c3df785284d27004a80824fb8a2dd6 # Latest main at 8/6/25
+        ref: 25df720d20c5299b016dcaa99ce0ca917e2df5b9 # Latest main at 16/6/25

justfile
@@ -40,7 +40,7 @@ submodules:
   just clone-submodule tasks/coverage/babel https://github.com/babel/babel.git 1d4546bcb80009303aab386b59f4df1fd335c1d5
   just clone-submodule tasks/coverage/typescript https://github.com/microsoft/TypeScript.git 81c951894e93bdc37c6916f18adcd80de76679bc
   just clone-submodule tasks/prettier_conformance/prettier https://github.com/prettier/prettier.git 7584432401a47a26943dd7a9ca9a8e032ead7285
-  just clone-submodule tasks/coverage/acorn-test262 https://github.com/oxc-project/acorn-test262 9bfb9ab035c3df785284d27004a80824fb8a2dd6
+  just clone-submodule tasks/coverage/acorn-test262 https://github.com/oxc-project/acorn-test262 25df720d20c5299b016dcaa99ce0ca917e2df5b9
   just update-transformer-fixtures
 
 # Install git pre-commit to format files

tasks/coverage/snapshots/estree_acorn_jsx.snap
@@ -1,4 +1,4 @@
-commit: 9bfb9ab0
+commit: 25df720d
 
 estree_acorn_jsx Summary:
 AST Parsed     : 39/39 (100.00%)

commit 6feab7e7483f26ec95ceebdb8a153ad280aba5e7

fix(ast/estree): remove custom serializer for `MethodDefinition` `key` field (#11763)
Remove the custom ESTree serializer for `MethodDefinition`'s `key` field. This serializer was required to replicate a bug in TS-ESLint, which they've now fixed. This fixes the test case which was broken by #11762.

crates/oxc_ast/src/ast/js.rs
@@ -2097,7 +2097,6 @@ pub struct MethodDefinition<'a> {
     /// This will always be true when an `abstract` modifier is used on the method.
     pub r#type: MethodDefinitionType,
     pub decorators: Vec<'a, Decorator<'a>>,
-    #[estree(via = MethodDefinitionKey)]
     pub key: PropertyKey<'a>,
     #[visit(args(flags = match self.kind {
         MethodDefinitionKind::Get => ScopeFlags::Function | ScopeFlags::GetAccessor,

crates/oxc_ast/src/generated/derive_estree.rs
@@ -1499,7 +1499,7 @@ impl ESTree for MethodDefinition<'_> {
         state.serialize_field("start", &self.span.start);
         state.serialize_field("end", &self.span.end);
         state.serialize_field("decorators", &self.decorators);
-        state.serialize_field("key", &crate::serialize::js::MethodDefinitionKey(self));
+        state.serialize_field("key", &self.key);
         state.serialize_field("value", &self.value);
         state.serialize_field("kind", &self.kind);
         state.serialize_field("computed", &self.computed);

crates/oxc_ast/src/serialize/js.rs
@@ -3,7 +3,6 @@ use oxc_estree::{
     Concat2, ConcatElement, ESTree, JsonSafeString, SequenceSerializer, Serializer,
     StructSerializer,
 };
-use oxc_span::GetSpan;
 
 use crate::ast::*;
 
@@ -379,52 +378,6 @@ impl ESTree for ExportAllDeclarationWithClause<'_, '_> {
 // ----------------------------------------
 // Misc
 // ----------------------------------------
-
-/// Serializer for `key` field of `MethodDefinition`.
-///
-/// In TS-ESTree `"constructor"` in `class C { "constructor"() {} }`
-/// is represented as an `Identifier`.
-/// In Acorn and Espree, it's a `Literal`.
-/// <https://github.com/typescript-eslint/typescript-eslint/issues/11084>
-#[ast_meta]
-#[estree(
-    ts_type = "PropertyKey",
-    raw_deser = "
-        /* IF_JS */
-        DESER[PropertyKey](POS_OFFSET.key)
-        /* END_IF_JS */
-
-        /* IF_TS */
-        let key = DESER[PropertyKey](POS_OFFSET.key);
-        if (THIS.kind === 'constructor') {
-            key = {
-                type: 'Identifier',
-                start: key.start,
-                end: key.end,
-                decorators: [],
-                name: 'constructor',
-                optional: false,
-                typeAnnotation: null,
-            };
-        }
-        key
-        /* END_IF_TS */
-    "
-)]
-pub struct MethodDefinitionKey<'a, 'b>(pub &'b MethodDefinition<'a>);
-
-impl ESTree for MethodDefinitionKey<'_, '_> {
-    fn serialize<S: Serializer>(&self, serializer: S) {
-        let method = self.0;
-        if S::INCLUDE_TS_FIELDS && method.kind == MethodDefinitionKind::Constructor {
-            // `key` can only be either an identifier `constructor`, or string `"constructor"`
-            let span = method.key.span();
-            IdentifierName { span, name: Atom::from("constructor") }.serialize(serializer);
-        } else {
-            method.key.serialize(serializer);
-        }
-    }
-}
 
 /// Serializer for `body` field of `ArrowFunctionExpression`.
 ///

napi/parser/generated/deserialize/ts.js
@@ -956,27 +956,14 @@ function deserializeClassBody(pos) {
 }
 
 function deserializeMethodDefinition(pos) {
-  const kind = deserializeMethodDefinitionKind(pos + 57);
-  let key = deserializePropertyKey(pos + 32);
-  if (kind === 'constructor') {
-    key = {
-      type: 'Identifier',
-      start: key.start,
-      end: key.end,
-      decorators: [],
-      name: 'constructor',
-      optional: false,
-      typeAnnotation: null,
-    };
-  }
   return {
     type: deserializeMethodDefinitionType(pos + 56),
     start: deserializeU32(pos),
     end: deserializeU32(pos + 4),
     decorators: deserializeVecDecorator(pos + 8),
-    key,
+    key: deserializePropertyKey(pos + 32),
     value: deserializeBoxFunction(pos + 48),
-    kind,
+    kind: deserializeMethodDefinitionKind(pos + 57),
     computed: deserializeBool(pos + 58),
     static: deserializeBool(pos + 59),
     override: deserializeBool(pos + 60),

tasks/coverage/snapshots/estree_typescript.snap
@@ -2,9 +2,7 @@ commit: 81c95189
 
 estree_typescript Summary:
 AST Parsed     : 6489/6489 (100.00%)
-Positive Passed: 6485/6489 (99.94%)
-Mismatch: tasks/coverage/typescript/tests/cases/conformance/classes/constructorDeclarations/quotedConstructors.ts
-
+Positive Passed: 6486/6489 (99.95%)
 Mismatch: tasks/coverage/typescript/tests/cases/conformance/jsx/jsxReactTestSuite.tsx
 
 Mismatch: tasks/coverage/typescript/tests/cases/conformance/jsx/tsxReactEmitEntities.tsx