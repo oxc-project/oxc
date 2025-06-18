862db6c7619a01bff8cbfb37e29c126dabcc76da
bacarybruno <brunobacary.bodian@doctolib.com>
2025-06-18 07:54:41 +0200

    feat: program range

--- crates/oxc_ast/src/serialize/mod.rs
+++ crates/oxc_ast/src/serialize/mod.rs
@@ -200,6 +200,9 @@ impl ESTree for ProgramConverter<'_, '_> {
         state.serialize_field("type", &JsonSafeString("Program"));
         state.serialize_field("start", &span_start);
         state.serialize_field("end", &program.span.end);
+        if state.range() {
+            state.serialize_field("range", &[span_start, program.span.end]);
+        }
         state.serialize_field("body", &Concat2(&program.directives, &program.body));
         state.serialize_field("sourceType", &program.source_type.module_kind());
         state.serialize_field("hashbang", &program.hashbang);
