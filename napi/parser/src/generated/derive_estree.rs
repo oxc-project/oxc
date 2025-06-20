1088610bf57795c8b84643d5d47c18235f130fbb
bacarybruno <brunobacary.bodian@doctolib.com>
2025-06-20 21:03:52 +0200

    chore: revert generated code

--- napi/parser/src/generated/derive_estree.rs
+++ napi/parser/src/generated/derive_estree.rs
@@ -44,14 +44,10 @@ impl ESTree for ErrorSeverity {
 
 impl ESTree for ErrorLabel<'_> {
     fn serialize<S: Serializer>(&self, serializer: S) {
-        let range = serializer.range();
         let mut state = serializer.serialize_struct();
         state.serialize_field("message", &self.message);
         state.serialize_field("start", &self.span.start);
         state.serialize_field("end", &self.span.end);
-        if range {
-            state.serialize_field("range", &[self.span.start, self.span.end]);
-        }
         state.end();
     }
 }
@@ -70,29 +66,21 @@ impl ESTree for EcmaScriptModule<'_> {
 
 impl ESTree for StaticImport<'_> {
     fn serialize<S: Serializer>(&self, serializer: S) {
-        let range = serializer.range();
         let mut state = serializer.serialize_struct();
         state.serialize_field("start", &self.span.start);
         state.serialize_field("end", &self.span.end);
         state.serialize_field("moduleRequest", &self.module_request);
         state.serialize_field("entries", &self.entries);
-        if range {
-            state.serialize_field("range", &[self.span.start, self.span.end]);
-        }
         state.end();
     }
 }
 
 impl ESTree for StaticExport<'_> {
     fn serialize<S: Serializer>(&self, serializer: S) {
-        let range = serializer.range();
         let mut state = serializer.serialize_struct();
         state.serialize_field("start", &self.span.start);
         state.serialize_field("end", &self.span.end);
         state.serialize_field("entries", &self.entries);
-        if range {
-            state.serialize_field("range", &[self.span.start, self.span.end]);
-        }
         state.end();
     }
 }
