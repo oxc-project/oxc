543b8f4fd00f0200daa832b2b091589d278b9b73
leaysgur <6259812+leaysgur@users.noreply.github.com>
2025-06-27 09:42:58 +0000

    test(coverage/typescript): Update `TS18xxx` error codes handling (#11933)
    Part of #11582

--- tasks/coverage/src/typescript/mod.rs
+++ tasks/coverage/src/typescript/mod.rs
@@ -51,6 +51,10 @@ impl<T: Case> Suite<T> for TypeScriptSuite<T> {
             "unicodeExtendedEscapesInRegularExpressions15.ts",
             "unicodeExtendedEscapesInRegularExpressions16.ts",
             "unicodeExtendedEscapesInRegularExpressions18.ts",
+            // TS18010, but requires JSDoc TS parsing
+            "privateNamesIncompatibleModifiersJs.ts",
+            // Exporting JSDoc types from `.js`
+            "importingExportingTypes.ts",
         ]
         .iter()
         .any(|p| path.to_string_lossy().contains(p));
@@ -129,5 +133,17 @@ impl Case for TypeScriptCase {
 
 // TODO: Filter out more not-supported error codes here
 static NOT_SUPPORTED_ERROR_CODES: phf::Set<&'static str> = phf::phf_set![
-    "2315", // Type 'U' is not generic.
+    "2315",  // Type 'U' is not generic.
+    "18028", // Private identifiers are only available when targeting ECMAScript 2015 and higher.
+    "18033", // Type 'Number' is not assignable to type 'number' as required for computed enum member values.
+    "18035", // Invalid value for 'jsxFragmentFactory'. '234' is not a valid identifier or qualified-name.
+    "18042", // 'Prop' is a type and cannot be imported in JavaScript files. Use 'import("./component").Prop' in a JSDoc type annotation.
+    "18043", // Types cannot appear in export declarations in JavaScript files.
+    "18045", // Properties with the 'accessor' modifier are only available when targeting ECMAScript 2015 and higher.
+    "18046", // 'x' is of type 'unknown'.
+    "18047", // 'x' is possibly 'null'.
+    "18048", // 'x' is possibly 'undefined'.
+    "18049", // 'x' is possibly 'null' or 'undefined'.
+    "18055", // 'A.a' has a string type, but must have syntactically recognizable string syntax when 'isolatedModules' is enabled.
+    "18057", // String literal import and export names are not supported when the '--module' flag is set to 'es2015' or 'es2020'.
 ];
