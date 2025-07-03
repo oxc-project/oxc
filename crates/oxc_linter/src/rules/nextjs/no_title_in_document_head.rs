a7e9f502334e466cdd7c2c2518629becb953d5da
Sysix <3897725+Sysix@users.noreply.github.com>
2025-07-03 21:02:05 +0000

    docs(linter): add good/bad example for `nextjs/no-title-in-document-head` (#12065)

--- crates/oxc_linter/src/rules/nextjs/no_title_in_document_head.rs
+++ crates/oxc_linter/src/rules/nextjs/no_title_in_document_head.rs
@@ -20,18 +20,43 @@ pub struct NoTitleInDocumentHead;
 declare_oxc_lint!(
     /// ### What it does
     ///
+    /// Prevent usage of `<title>` with `Head` component from `next/document`.
     ///
     /// ### Why is this bad?
     ///
+    /// A `<title>` element should only be used for any `<head>` code that is common for all pages.
+    /// Title tags should be defined at the page-level using `next/head` instead.
     ///
     /// ### Examples
     ///
     /// Examples of **incorrect** code for this rule:
     /// ```javascript
+    /// import {Head} from 'next/document'
+    ///
+    /// export function Home() {
+    ///   return (
+    ///     <div>
+    ///       <Head>
+    ///         <title>My page title</title>
+    ///       </Head>
+    ///     </div>
+    ///   )
+    /// }
     /// ```
     ///
     /// Examples of **correct** code for this rule:
     /// ```javascript
+    /// import Head from 'next/head'
+    ///
+    /// export function Home() {
+    ///   return (
+    ///     <div>
+    ///       <Head>
+    ///         <title>My page title</title>
+    ///       </Head>
+    ///     </div>
+    ///   )
+    /// }
     /// ```
     NoTitleInDocumentHead,
     nextjs,
