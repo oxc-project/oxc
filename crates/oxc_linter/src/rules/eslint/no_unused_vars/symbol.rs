387762d78195b074e511fbc386472d98b6610eb3
Dunqing <29533304+Dunqing@users.noreply.github.com>
2025-07-03 04:26:07 +0000

    refactor(linter/no-unused-vars): simplify check for export nodes (#12044)
    This PR is a follow-up to https://github.com/oxc-project/oxc/pull/12022
    
    This method checks whether a symbol has been exported, and only `ExportNamedDeclaration` and `ExportDefaultDeclaration` can produce a Symbol, so the `module.is_export()` is unnecessary.

--- crates/oxc_linter/src/rules/eslint/no_unused_vars/symbol.rs
+++ crates/oxc_linter/src/rules/eslint/no_unused_vars/symbol.rs
@@ -186,8 +186,8 @@ impl<'a> Symbol<'_, 'a> {
     fn in_export_node(&self) -> bool {
         for parent in self.nodes().ancestors(self.declaration_id()).skip(1) {
             match parent.kind() {
-                m if m.is_module_declaration() => {
-                    return m.as_module_declaration_kind().unwrap().is_export();
+                AstKind::ExportNamedDeclaration(_) | AstKind::ExportDefaultDeclaration(_) => {
+                    return true;
                 }
                 AstKind::VariableDeclaration(_)
                 | AstKind::ArrayExpression(_)
