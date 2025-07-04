0878414bbf2534743da48d2d0c6e8040e0b12ef5
Sysix <3897725+Sysix@users.noreply.github.com>
2025-07-04 10:02:49 +0000

    docs(linter): add good/bad example for `nextjs/no-head-import-in-document` (#12061)

--- crates/oxc_linter/src/rules/nextjs/no_head_import_in_document.rs
+++ crates/oxc_linter/src/rules/nextjs/no_head_import_in_document.rs
@@ -21,18 +21,56 @@ pub struct NoHeadImportInDocument;
 declare_oxc_lint!(
     /// ### What it does
     ///
+    /// Prevents the usage of `next/head` inside a Next.js document.
     ///
     /// ### Why is this bad?
     ///
+    /// Importing `next/head` inside `pages/_document.js` can cause
+    /// unexpected issues in your Next.js application.
     ///
     /// ### Examples
     ///
     /// Examples of **incorrect** code for this rule:
-    /// ```javascript
+    /// ```jsx
+    /// import Document, { Html, Main, NextScript } from 'next/document'
+    /// import Head from 'next/head';
+    ///
+    /// class MyDocument extends Document {
+    ///   static async getInitialProps(ctx) {
+    ///     //...
+    ///   }
+    ///
+    ///   render() {
+    ///     return (
+    ///       <Html>
+    ///         <Head></Head>
+    ///       </Html>
+    ///     )
+    ///   }
+    /// }
+    ///
+    /// export default MyDocument
     /// ```
     ///
     /// Examples of **correct** code for this rule:
-    /// ```javascript
+    /// ```jsx
+    /// import Document, { Html, Head, Main, NextScript } from 'next/document'
+    ///
+    /// class MyDocument extends Document {
+    ///   static async getInitialProps(ctx) {
+    ///     //...
+    ///   }
+    ///
+    ///   render() {
+    ///     return (
+    ///       <Html>
+    ///         <Head></Head>
+    ///       </Html>
+    ///     )
+    ///   }
+    /// }
+    ///
+    /// export default MyDocument
     /// ```
     NoHeadImportInDocument,
     nextjs,
