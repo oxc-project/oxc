9240342f7d143e927179d6c59e8ef60fbf28317e
Sysix <3897725+Sysix@users.noreply.github.com>
2025-07-06 11:45:00 +0000

    docs(linter): add docs for `nextjs/no-script-component-in-head` (#12091)

--- crates/oxc_linter/src/rules/nextjs/no_script_component_in_head.rs
+++ crates/oxc_linter/src/rules/nextjs/no_script_component_in_head.rs
@@ -20,18 +20,45 @@ pub struct NoScriptComponentInHead;
 declare_oxc_lint!(
     /// ### What it does
     ///
+    /// Prevent usage of `next/script` in `next/head` component.
     ///
     /// ### Why is this bad?
     ///
+    /// The `next/script` component should not be used in a `next/head component.
+    /// Instead move the `<Script />` component outside of `<Head>` instead.
     ///
     /// ### Examples
     ///
     /// Examples of **incorrect** code for this rule:
-    /// ```javascript
+    /// ```jsx
+    /// import Script from 'next/script'
+    /// import Head from 'next/head'
+    ///
+    /// export default function Index() {
+    ///   return (
+    ///     <Head>
+    ///       <title>Next.js</title>
+    ///       <Script src="/my-script.js" />
+    ///     </Head>
+    ///   )
+    /// }
     /// ```
     ///
     /// Examples of **correct** code for this rule:
-    /// ```javascript
+    /// ```jsx
+    /// import Script from 'next/script'
+    /// import Head from 'next/head'
+    ///
+    /// export default function Index() {
+    ///   return (
+    ///     <>
+    ///       <Head>
+    ///         <title>Next.js</title>
+    ///       </Head>
+    ///       <Script src="/my-script.js" />
+    ///     </>
+    ///   )
+    /// }
     /// ```
     NoScriptComponentInHead,
     nextjs,
