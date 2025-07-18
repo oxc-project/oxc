b492458a76647a2b91c3f964d83cd7d3400203d7
Yuji Sugiura <6259812+leaysgur@users.noreply.github.com>
2025-07-18 17:39:12 +0900

    test(coverage/typescript): Update `5xxx` error codes handling (#12385)
    Part of #11582

--- tasks/coverage/src/typescript/mod.rs
+++ tasks/coverage/src/typescript/mod.rs
@@ -57,6 +57,8 @@ impl<T: Case> Suite<T> for TypeScriptSuite<T> {
             "importingExportingTypes.ts",
             // This is just a binary file
             "TransportStream.ts",
+            // Emits TS5052 (complains compilerOptions are invalid, which we do not support), also implies TS2564 but not emitted
+            "esDecorators-emitDecoratorMetadata.ts",
         ]
         .iter()
         .any(|p| path.to_string_lossy().contains(p));
@@ -134,10 +136,40 @@ impl Case for TypeScriptCase {
 }
 
 // spellchecker:off
-// TODO: Filter out more not-supported error codes here
 static NOT_SUPPORTED_ERROR_CODES: phf::Set<&'static str> = phf::phf_set![
+    // TODO: More not-supported error codes here
     "2315",  // Type 'U' is not generic.
     "2665", // Invalid module name in augmentation. Module 'foo' resolves to an untyped module at '/node_modules/foo/index.js', which cannot be augmented.
+    "5009", // Cannot find the common subdirectory path for the input files.
+    "5052", // Option 'checkJs' cannot be specified without specifying option 'allowJs'.
+    "5053", // Option 'mapRoot' cannot be specified with option 'inlineSourceMap'.
+    "5055", // Cannot write file 'a.d.ts' because it would overwrite input file.
+    "5056", // Cannot write file 'a.js' because it would be overwritten by multiple input files.
+    "5059", // Invalid value for '--reactNamespace'. 'my-React-Lib' is not a valid identifier.
+    "5061", // Pattern 'too*many*asterisks' can have at most one '*' character.
+    "5062", // Substitution '*2*' in pattern '*1*' can have at most one '*' character.
+    "5063", // Substitutions for pattern '*' should be an array.
+    "5064", // Substitution '1' for pattern '*' has incorrect type, expected 'string', got 'number'.
+    "5066", // Substitutions for pattern 'foo' shouldn't be an empty array.
+    "5067", // Invalid value for 'jsxFactory'. 'Element.createElement=' is not a valid identifier or qualified-name.
+    "5069", // Option 'emitDeclarationOnly' cannot be specified without specifying option 'declaration' or option 'composite'.
+    "5070", // Option '--resolveJsonModule' cannot be specified when 'moduleResolution' is set to 'classic'.
+    "5071", // Option '--resolveJsonModule' cannot be specified when 'module' is set to 'none', 'system', or 'umd'.
+    "5074", // Option '--incremental' can only be specified using tsconfig, emitting to single file or when option '--tsBuildInfoFile' is specified.
+    "5088", // The inferred type of 'foo' references a type with a cyclic structure which cannot be trivially serialized. A type annotation is necessary.
+    "5090", // Non-relative paths are not allowed when 'baseUrl' is not set. Did you forget a leading './'?
+    "5091", // Option 'preserveConstEnums' cannot be disabled when 'isolatedModules' is enabled.
+    "5095", // Option 'bundler' can only be used when 'module' is set to 'preserve' or to 'es2015' or later.
+    "5097", // An import path can only end with a '.ts' extension when 'allowImportingTsExtensions' is enabled.
+    "5098", // Option 'customConditions' can only be used when 'moduleResolution' is set to 'node16', 'nodenext', or 'bundler'.
+    "5101", // Option 'noImplicitUseStrict' is deprecated and will stop functioning in TypeScript 5.5. Specify compilerOption '"ignoreDeprecations": "5.0"' to silence this error.
+    "5102", // Option 'noImplicitUseStrict' has been removed. Please remove it from your configuration.
+    "5103", // Invalid value for '--ignoreDeprecations'.
+    "5105", // Option 'verbatimModuleSyntax' cannot be used when 'module' is set to 'UMD', 'AMD', or 'System'.
+    "5107", // Option 'target=ES3' is deprecated and will stop functioning in TypeScript 5.5. Specify compilerOption '"ignoreDeprecations": "5.0"' to silence this error.
+    "5108", // Option 'target=ES3' has been removed. Please remove it from your configuration.
+    "5109", // Option 'moduleResolution' must be set to 'Node16' (or left unspecified) when option 'module' is set to 'Node18'.
+    "5110", // Option 'module' must be set to 'Node16' when option 'moduleResolution' is set to 'Node16'.
     "6053", // File 'invalid.ts' not found.
     "6054", // File 'b.js.map' has an unsupported extension. The only supported extensions are '.ts', '.tsx', '.d.ts', '.js', '.jsx', '.cts', '.d.cts', '.cjs', '.mts', '.d.mts', '.mjs'.
     "6082", // Only 'amd' and 'system' modules are supported alongside --outFile.
