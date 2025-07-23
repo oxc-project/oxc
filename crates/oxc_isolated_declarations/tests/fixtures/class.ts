oxc-project/oxc

commit 42976a8ed40354202a30ff6f1f38cf2823087e99

fix(isolated-declarations): produce incorrect types when getter/setter with different types (#12046)
close: #12023

crates/oxc_isolated_declarations/src/class.rs
@@ -1,4 +1,4 @@
-use oxc_allocator::{Box as ArenaBox, CloneIn, Vec as ArenaVec};
+use oxc_allocator::{Allocator, Box as ArenaBox, CloneIn, Vec as ArenaVec};
 use oxc_ast::{NONE, ast::*};
 use oxc_span::{ContentEq, GetSpan, SPAN};
 
@@ -9,6 +9,27 @@ use crate::{
         method_must_have_explicit_return_type, property_must_have_explicit_type,
     },
 };
+
+struct AccessorAnnotation<'a> {
+    setter: Option<ArenaBox<'a, TSTypeAnnotation<'a>>>,
+    getter: Option<ArenaBox<'a, TSTypeAnnotation<'a>>>,
+}
+
+impl<'a> AccessorAnnotation<'a> {
+    fn get_setter_annotation(
+        &self,
+        allocator: &'a Allocator,
+    ) -> Option<ArenaBox<'a, TSTypeAnnotation<'a>>> {
+        self.setter.as_ref().or(self.getter.as_ref()).map(|t| t.clone_in(allocator))
+    }
+
+    fn get_getter_annotation(
+        &self,
+        allocator: &'a Allocator,
+    ) -> Option<ArenaBox<'a, TSTypeAnnotation<'a>>> {
+        self.getter.as_ref().or(self.setter.as_ref()).map(|t| t.clone_in(allocator))
+    }
+}
 
 impl<'a> IsolatedDeclarations<'a> {
     pub(crate) fn is_literal_key(key: &PropertyKey<'a>) -> bool {
@@ -294,8 +315,8 @@ impl<'a> IsolatedDeclarations<'a> {
     fn collect_accessor_annotations(
         &self,
         decl: &Class<'a>,
-    ) -> Vec<(PropertyKey<'a>, ArenaBox<'a, TSTypeAnnotation<'a>>)> {
-        let mut method_annotations = Vec::new();
+    ) -> Vec<(PropertyKey<'a>, AccessorAnnotation<'a>)> {
+        let mut method_annotations: Vec<(PropertyKey<'_>, AccessorAnnotation<'_>)> = Vec::new();
         for element in &decl.body.body {
             if let ClassElement::MethodDefinition(method) = element {
                 if (method.key.is_private_identifier()
@@ -313,15 +334,33 @@ impl<'a> IsolatedDeclarations<'a> {
                         if let Some(annotation) =
                             first_param.pattern.type_annotation.clone_in(self.ast.allocator)
                         {
-                            method_annotations
-                                .push((method.key.clone_in(self.ast.allocator), annotation));
+                            if let Some(entry) = method_annotations
+                                .iter_mut()
+                                .find(|(key, _)| method.key.content_eq(key))
+                            {
+                                entry.1.setter = Some(annotation);
+                            } else {
+                                method_annotations.push((
+                                    method.key.clone_in(self.ast.allocator),
+                                    AccessorAnnotation { setter: Some(annotation), getter: None },
+                                ));
+                            }
                         }
                     }
                     MethodDefinitionKind::Get => {
                         let function = &method.value;
                         if let Some(annotation) = self.infer_function_return_type(function) {
-                            method_annotations
-                                .push((method.key.clone_in(self.ast.allocator), annotation));
+                            if let Some(entry) = method_annotations
+                                .iter_mut()
+                                .find(|(key, _)| method.key.content_eq(key))
+                            {
+                                entry.1.getter = Some(annotation);
+                            } else {
+                                method_annotations.push((
+                                    method.key.clone_in(self.ast.allocator),
+                                    AccessorAnnotation { setter: None, getter: Some(annotation) },
+                                ));
+                            }
                         }
                     }
                     _ => {}
@@ -400,13 +439,16 @@ impl<'a> IsolatedDeclarations<'a> {
                                     if let Some(annotation) =
                                         accessor_annotations.iter().find_map(|(key, annotation)| {
                                             if method.key.content_eq(key) {
-                                                Some(annotation.clone_in(self.ast.allocator))
+                                                Some(
+                                                    annotation
+                                                        .get_setter_annotation(self.ast.allocator),
+                                                )
                                             } else {
                                                 None
                                             }
                                         })
                                     {
-                                        param.pattern.type_annotation = Some(annotation);
+                                        param.pattern.type_annotation = annotation;
                                     }
                                 }
                                 params
@@ -461,7 +503,13 @@ impl<'a> IsolatedDeclarations<'a> {
                         MethodDefinitionKind::Get => {
                             let rt = accessor_annotations.iter().find_map(|(key, annotation)| {
                                 if method.key.content_eq(key) {
-                                    Some(annotation.clone_in(self.ast.allocator))
+                                    // No explicit return type for getter, should infer it from the first parameter of setter, if not exists,
+                                    // use the inferred return type of getter.
+                                    if method.value.return_type.is_none() {
+                                        annotation.get_setter_annotation(self.ast.allocator)
+                                    } else {
+                                        annotation.get_getter_annotation(self.ast.allocator)
+                                    }
                                 } else {
                                     None
                                 }

crates/oxc_isolated_declarations/tests/snapshots/set-get-accessor.snap
@@ -35,7 +35,7 @@ declare class GlobalSymbol2 {
 // Infer type from the parameter type of the setter
 declare class GlobalSymbol3 {
 	set [Symbol.toStringTag](v: number);
-	get [Symbol.toStringTag](): number;
+	get [Symbol.toStringTag](): string;
 }
 // Cannot infer type from the function body of the getter
 declare class GlobalSymbol4 {

commit 9a9d78d71aff5f6565ae7ec7a511c37385bd960d

fix(isolated-declarations): transformed parameter property has an incorrect type annotation (#12450)
* close #12361

Regressed by https://github.com/oxc-project/oxc/pull/12046

crates/oxc_isolated_declarations/src/class.rs
@@ -277,29 +277,35 @@ impl<'a> IsolatedDeclarations<'a> {
         }
     }
 
-    fn transform_constructor_params_to_class_properties(
+    /// Transform constructor parameters to class properties.
+    ///
+    /// For example:
+    ///
+    /// `class C { constructor(public x: string) {} }`
+    ///
+    /// to
+    ///
+    /// `class C { public x: string; constructor(x: string) {} }`
+    fn transform_constructor_parameter_properties(
         &self,
         function: &Function<'a>,
         params: &FormalParameters<'a>,
     ) -> ArenaVec<'a, ClassElement<'a>> {
-        self.ast.vec_from_iter(
-            function
-                .params
-                .items
-                .iter()
-                .filter(|param| param.has_modifier())
-                .enumerate()
-                .filter_map(|(index, param)| {
-                    let type_annotation =
-                        if param.accessibility.is_some_and(TSAccessibility::is_private) {
-                            None
-                        } else {
-                            // transformed params will definitely have type annotation
-                            params.items[index].pattern.type_annotation.clone_in(self.ast.allocator)
-                        };
-                    self.transform_formal_parameter_to_class_property(param, type_annotation)
-                }),
-        )
+        self.ast.vec_from_iter(function.params.items.iter().enumerate().filter_map(
+            |(index, param)| {
+                if !param.has_modifier() {
+                    return None;
+                }
+                let type_annotation =
+                    if param.accessibility.is_some_and(TSAccessibility::is_private) {
+                        None
+                    } else {
+                        // transformed params will definitely have type annotation
+                        params.items[index].pattern.type_annotation.clone_in(self.ast.allocator)
+                    };
+                self.transform_formal_parameter_to_class_property(param, type_annotation)
+            },
+        ))
     }
 
     /// Collect return_type of getter and first parma type of setter
@@ -462,9 +468,7 @@ impl<'a> IsolatedDeclarations<'a> {
                                 self.transform_formal_parameters(&function.params, is_private);
                             elements.splice(
                                 0..0,
-                                self.transform_constructor_params_to_class_properties(
-                                    function, &params,
-                                ),
+                                self.transform_constructor_parameter_properties(function, &params),
                             );
 
                             if is_function_overloads && function.body.is_some() {

crates/oxc_isolated_declarations/tests/fixtures/class.ts
@@ -75,3 +75,10 @@ export class ConstructorOverloadsClass {
   constructor(a: string);
   constructor(readonly a: any) {}
 }
+
+export class ClassParameterProperties {
+	constructor(
+		a: number,
+		readonly b: string,
+	) {}
+}

crates/oxc_isolated_declarations/tests/snapshots/class.snap
@@ -64,6 +64,10 @@ export declare class ConstructorOverloadsClass {
 	constructor(a: number);
 	constructor(a: string);
 }
+export declare class ClassParameterProperties {
+	readonly b: string;
+	constructor(a: number, b: string);
+}
 
 
 ==================== Errors ====================