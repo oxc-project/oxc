66200d55fc9d4b2a7c90a36a5f35c0e1da590e77
Cameron Clark <cameron.clark@hey.com>
2025-07-25 21:36:22 +0100

    feat(oxlint2): bundle with tsdown

--- napi/oxlint2/src-js/index.js
+++ napi/oxlint2/src-js/index.js
@@ -9,8 +9,8 @@ import { addVisitorToCompiled, compiledVisitor, finalizeCompiledVisitor, initCom
 // and need to make sure get same instance of modules as it uses internally,
 // otherwise `TOKEN` here won't be same `TOKEN` as used within `oxc-parser`.
 const require = createRequire(import.meta.url);
-const { TOKEN } = require('../../parser/raw-transfer/lazy-common.js'),
-  walkProgram = require('../../parser/generated/lazy/walk.js');
+const { TOKEN } = require('../dist/parser/raw-transfer/lazy-common.cjs'),
+  walkProgram = require('../dist/parser/generated/lazy/walk.cjs');
 
 // --------------------
 // Plugin loading
@@ -42,7 +42,9 @@ async function loadPlugin(path) {
 
 async function loadPluginImpl(path) {
   if (registeredPluginPaths.has(path)) {
-    return JSON.stringify({ Failure: 'This plugin has already been registered' });
+    return JSON.stringify({
+      Failure: 'This plugin has already been registered',
+    });
   }
 
   const { default: plugin } = await import(path);
@@ -56,7 +58,10 @@ async function loadPluginImpl(path) {
 
   for (const [ruleName, rule] of Object.entries(plugin.rules)) {
     ruleNames.push(ruleName);
-    registeredRules.push({ rule, context: new Context(`${pluginName}/${ruleName}`) });
+    registeredRules.push({
+      rule,
+      context: new Context(`${pluginName}/${ruleName}`),
+    });
   }
 
   return JSON.stringify({ Success: { name: pluginName, offset, ruleNames } });
@@ -176,7 +181,10 @@ function lintFile([filePath, bufferId, buffer, ruleIds]) {
   initCompiledVisitor();
   for (let i = 0; i < ruleIds.length; i++) {
     const ruleId = ruleIds[i];
-    const { rule: { create }, context } = registeredRules[ruleId];
+    const {
+      rule: { create },
+      context,
+    } = registeredRules[ruleId];
     setupContextForFile(context, i, filePath);
     const visitor = create(context);
     addVisitorToCompiled(visitor);
@@ -194,7 +202,14 @@ function lintFile([filePath, bufferId, buffer, ruleIds]) {
 
     const sourceText = textDecoder.decode(buffer.subarray(0, sourceByteLen));
     const sourceIsAscii = sourceText.length === sourceByteLen;
-    const ast = { buffer, sourceText, sourceByteLen, sourceIsAscii, nodes: new Map(), token: TOKEN };
+    const ast = {
+      buffer,
+      sourceText,
+      sourceByteLen,
+      sourceIsAscii,
+      nodes: new Map(),
+      token: TOKEN,
+    };
 
     walkProgram(programPos, ast, compiledVisitor);
   }
