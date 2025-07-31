3cbb1db11a0f9deca515598e06c458008a3e240a
Boshen <1430279+Boshen@users.noreply.github.com>
2025-07-31 02:26:39 +0000

    refactor(napi/playground): display more info for symbol symbols (#12650)

--- napi/playground/src/lib.rs
+++ napi/playground/src/lib.rs
@@ -20,11 +20,11 @@ use oxc::{
     minifier::{CompressOptions, MangleOptions, Minifier, MinifierOptions},
     parser::{ParseOptions, Parser, ParserReturn},
     semantic::{
-        ReferenceId, ScopeFlags, ScopeId, Scoping, SemanticBuilder, SymbolFlags,
+        ReferenceId, ScopeFlags, ScopeId, Scoping, SemanticBuilder, SymbolFlags, SymbolId,
         dot::{DebugDot, DebugDotContext},
     },
     span::{SourceType, Span},
-    syntax::reference::Reference,
+    syntax::reference::ReferenceFlags,
     transformer::{TransformOptions, Transformer},
 };
 use oxc_formatter::{FormatOptions, Formatter};
@@ -413,30 +413,38 @@ impl Oxc {
     fn get_symbols_text(scoping: &Scoping) -> napi::Result<String> {
         #[derive(Serialize)]
         struct Data {
+            symbol_id: SymbolId,
             span: Span,
             name: String,
             flags: SymbolFlags,
             scope_id: ScopeId,
-            resolved_references: Vec<ReferenceId>,
-            references: Vec<Reference>,
+            references: Vec<ReferenceData>,
         }
-
+        #[derive(Serialize)]
+        struct ReferenceData {
+            reference_id: ReferenceId,
+            symbol_id: Option<SymbolId>,
+            flags: ReferenceFlags,
+        }
         let data = scoping
             .symbol_ids()
             .map(|symbol_id| Data {
+                symbol_id,
                 span: scoping.symbol_span(symbol_id),
                 name: scoping.symbol_name(symbol_id).into(),
                 flags: scoping.symbol_flags(symbol_id),
                 scope_id: scoping.symbol_scope_id(symbol_id),
-                resolved_references: scoping
-                    .get_resolved_reference_ids(symbol_id)
-                    .iter()
-                    .copied()
-                    .collect::<Vec<_>>(),
                 references: scoping
                     .get_resolved_reference_ids(symbol_id)
                     .iter()
-                    .map(|reference_id| scoping.get_reference(*reference_id).clone())
+                    .map(|&reference_id| {
+                        let reference = scoping.get_reference(reference_id);
+                        ReferenceData {
+                            reference_id,
+                            symbol_id: reference.symbol_id(),
+                            flags: reference.flags(),
+                        }
+                    })
                     .collect::<Vec<_>>(),
             })
             .collect::<Vec<_>>();
