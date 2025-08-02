41e0099ac6931e38f17141f43a3dc26b74bda968
overlookmotel <theoverlookmotel@gmail.com>
2025-08-02 18:44:51 +0100

    Create less `Promise`s

--- napi/parallel-test/src-js/direct/index.js
+++ napi/parallel-test/src-js/direct/index.js
@@ -4,7 +4,7 @@ import { run } from '../bindings.js';
 
 const WORKER_URL = new URL('./worker.js', import.meta.url);
 
-let workers;
+const workers = [];
 const success = await run(startWorkers);
 await stopWorkers();
 
@@ -19,13 +19,24 @@ if (!success) process.exitCode = 1;
  * @param {number} count - Number of workers
  * @returns {Promise<undefined>}
  */
-async function startWorkers(count) {
+function startWorkers(count) {
   console.log('> Starting', count, 'workers');
-  workers = await Promise.all(Array.from({ length: count }, (_, id) => {
-    const worker = new Worker(WORKER_URL, { workerData: { id } });
-    return new Promise(resolve => worker.addListener('message', () => resolve(worker)));
-  }));
-  console.log('> Started', count, 'workers');
+
+  return new Promise((resolve) => {
+    let remainingCount = count;
+    function done() {
+      if (--remainingCount === 0) {
+        resolve();
+        console.log('> Started', count, 'workers');
+      }
+    }
+
+    for (let id = 0; id < count; id++) {
+      const worker = new Worker(WORKER_URL, { workerData: { id } });
+      worker.addListener('message', done);
+      workers.push(worker);
+    }
+  });
 }
 
 function stopWorkers() {
