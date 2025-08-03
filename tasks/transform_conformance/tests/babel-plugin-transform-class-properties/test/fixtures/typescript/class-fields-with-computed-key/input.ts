d02e17c79d961588e955648adadaa54b3f1098f1
Dunqing <29533304+Dunqing@users.noreply.github.com>
2025-08-03 07:16:26 +0000

    fix(transformer/class-properties): preserve computed keys if they may have side effects when `remove_class_fields_without_initializer` is enabled (#12772)
    When `remove_class_fields_without_initializer` is enabled, fields without initializers would be removed. However, if the key is computed, and it may have side effects, then we should extract it and place it before class, so that it can keep the same behavior as before.
    
    **Example:**
    
    Input:
    ```ts
    import { Collection } from './collection.ts';
    
    export class Obj {
      public readonly [Collection.identifier] = true;
      public readonly [Collection.identifier2];
      public static readonly [Collection.identifier3] = true;
      public static readonly  [Collection.identifier4];
    }
    ```
    
    Options:
    ```json
    {
      "assumptions": {
        "setPublicClassFields": true
      },
      "plugins": [
        "transform-class-properties",
        [
          "transform-typescript",
          {
            "removeClassFieldsWithoutInitializer": true
          }
        ]
      ]
    }
    ```
    
    Output:
    ```js
    import { Collection } from "./collection.ts";
    let _Collection$identifie, _Collection$identifie2;
    _Collection$identifie = Collection.identifier;
    Collection.identifier2;
    _Collection$identifie2 = Collection.identifier3;
    Collection.identifier4;
    export class Obj {
      constructor() {
        this[_Collection$identifie] = true;
      }
    }
    Obj[_Collection$identifie2] = true;
    ```

--- tasks/transform_conformance/tests/babel-plugin-transform-class-properties/test/fixtures/typescript/class-fields-with-computed-key/input.ts
+++ tasks/transform_conformance/tests/babel-plugin-transform-class-properties/test/fixtures/typescript/class-fields-with-computed-key/input.ts
@@ -2,4 +2,7 @@ import { Collection } from './collection.ts';
 
 export class Obj {
   public readonly [Collection.identifier] = true;
+  public readonly [Collection.identifier2];
+  public static readonly [Collection.identifier3] = true;
+  public static readonly  [Collection.identifier4];
 }
