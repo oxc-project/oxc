61f1928188745e46627eccd8988c41bcd2347cfc
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-08-11 11:17:54 -0700

    fix: add jest test name

--- crates/oxc_linter/src/rules/jest/padding_around_test_blocks.rs
+++ crates/oxc_linter/src/rules/jest/padding_around_test_blocks.rs
@@ -1,8 +1,10 @@
+use std::ops::Deref;
+
 use itertools::Itertools;
 use oxc_ast::AstKind;
 use oxc_diagnostics::OxcDiagnostic;
 use oxc_macros::declare_oxc_lint;
-use oxc_span::{GetSpan, Span};
+use oxc_span::{CompactStr, GetSpan, Span};
 
 use crate::{
     AstNode,
@@ -14,9 +16,9 @@ use crate::{
     },
 };
 
-fn padding_around_test_blocks_diagnostic(span: Span) -> OxcDiagnostic {
-    OxcDiagnostic::warn("Missing padding before test/it block")
-        .with_help("Make sure there is an empty new line before the test/it block")
+fn padding_around_test_blocks_diagnostic(span: Span, name: &CompactStr) -> OxcDiagnostic {
+    OxcDiagnostic::warn(format!("Missing padding before {name} block"))
+        .with_help(format!("Make sure there is an empty new line before the {name} block"))
         .with_label(span)
 }
 
@@ -66,7 +68,7 @@ declare_oxc_lint!(
 );
 
 enum NodeType {
-    Test,
+    Test(CompactStr),
     Statement,
 }
 
@@ -80,12 +82,12 @@ impl Rule for PaddingAroundTestBlocks {
                     return None;
                 };
                 let jest_fn_call = parse_general_jest_fn_call(call_expr, possible_jest_node, ctx)?;
-                let ParsedGeneralJestFnCall { kind, .. } = &jest_fn_call;
+                let ParsedGeneralJestFnCall { kind, name, .. } = &jest_fn_call;
                 let kind = kind.to_general()?;
                 if kind != JestGeneralFnKind::Test {
                     return None;
                 }
-                Some((NodeType::Test, node))
+                Some((NodeType::Test(name.deref().into()), node))
             })
             .collect_vec();
         for node in ctx.nodes() {
@@ -97,7 +99,7 @@ impl Rule for PaddingAroundTestBlocks {
         let mut prev_node: Option<&AstNode<'_>> = None;
         for (node_type, node) in nodes {
             match node_type {
-                NodeType::Test => {
+                NodeType::Test(name) => {
                     if let Some(prev_node) = prev_node {
                         if prev_node.span().end > node.span().start {
                             continue;
@@ -126,15 +128,15 @@ impl Rule for PaddingAroundTestBlocks {
                         let span_between = Span::new(span_between_start, span_between_end);
                         let content = ctx.source_range(span_between);
                         if content.matches('\n').count() < 2 {
-                            let spaces_after_last_line = content
-                                .rfind('\n')
-                                .map_or("", |index| content.split_at(index + 1).1);
                             ctx.diagnostic_with_fix(
-                                padding_around_test_blocks_diagnostic(Span::new(
-                                    span_between_end,
-                                    span_between_end,
-                                )),
+                                padding_around_test_blocks_diagnostic(
+                                    Span::new(span_between_end, span_between_end),
+                                    &name,
+                                ),
                                 |fixer| {
+                                    let spaces_after_last_line = content
+                                        .rfind('\n')
+                                        .map_or("", |index| content.split_at(index + 1).1);
                                     fixer.replace(
                                         span_between,
                                         format!("\n\n{spaces_after_last_line}"),
@@ -170,6 +172,18 @@ fn test() {
         "test('foo', () => {});\ntest('bar', () => {});",
         "it('foo', () => {});\nfit('bar', () => {});\ntest('baz', () => {});",
         r"
+const foo = 'bar';
+const bar = 'baz';
+it('foo', () => {
+  // stuff
+});
+fit('bar', () => {
+  // stuff
+});
+test('foo foo', () => {});
+test('bar bar', () => {});
+
+// Nesting
  describe('other bar', () => {
      const thing = 123;
      test('is another bar w/ test', () => {
@@ -201,6 +215,18 @@ fn test() {
         ),
         (
             r"
+const foo = 'bar';
+const bar = 'baz';
+it('foo', () => {
+  // stuff
+});
+fit('bar', () => {
+  // stuff
+});
+test('foo foo', () => {});
+test('bar bar', () => {});
+
+// Nesting
 describe('other bar', () => {
     const thing = 123;
     test('is another bar w/ test', () => {
@@ -216,6 +242,22 @@ test
 xit('bar foo', () => {});
         ",
             r"
+const foo = 'bar';
+const bar = 'baz';
+
+it('foo', () => {
+  // stuff
+});
+
+fit('bar', () => {
+  // stuff
+});
+
+test('foo foo', () => {});
+
+test('bar bar', () => {});
+
+// Nesting
 describe('other bar', () => {
     const thing = 123;
 
