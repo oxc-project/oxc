9815426565d830c81cec2aae35aabf70155a57c6
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-08-10 18:28:00 -0700

    feat(linter): add jest/padding-around-test-blocks rule
    [autofix.ci] apply automated fixes

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -304,6 +304,7 @@ mod jest {
     pub mod no_test_prefixes;
     pub mod no_test_return_statement;
     pub mod no_untyped_mock_factory;
+    pub mod padding_around_test_blocks;
     pub mod prefer_called_with;
     pub mod prefer_comparison_matcher;
     pub mod prefer_each;
@@ -827,6 +828,7 @@ oxc_macros::declare_all_lint_rules! {
     jest::no_test_prefixes,
     jest::no_test_return_statement,
     jest::no_untyped_mock_factory,
+    jest::padding_around_test_blocks,
     jest::prefer_each,
     jest::prefer_called_with,
     jest::prefer_comparison_matcher,
---
+++ crates/oxc_linter/src/rules/jest/padding_around_test_blocks.rs
@@ -0,0 +1,246 @@
+use itertools::Itertools;
+use oxc_ast::AstKind;
+use oxc_diagnostics::OxcDiagnostic;
+use oxc_macros::declare_oxc_lint;
+use oxc_span::{GetSpan, Span};
+
+use crate::{
+    AstNode,
+    context::LintContext,
+    rule::Rule,
+    utils::{
+        JestGeneralFnKind, ParsedGeneralJestFnCall, collect_possible_jest_call_node,
+        parse_general_jest_fn_call,
+    },
+};
+
+fn padding_around_test_blocks_diagnostic(span: Span) -> OxcDiagnostic {
+    OxcDiagnostic::warn("Missing padding before test/it block")
+        .with_help("Make sure there is an empty new line before the test/it block")
+        .with_label(span)
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct PaddingAroundTestBlocks;
+
+declare_oxc_lint!(
+    /// ### What it does
+    ///
+    /// This rule enforces a line of padding before and after 1 or more test/it statements
+    ///
+    /// ### Examples
+    ///
+    /// Examples of **incorrect** code for this rule:
+    /// ```js
+    /// const thing = 123;
+    /// test('foo', () => {});
+    /// test('bar', () => {});
+    /// ```
+    ///
+    /// ```js
+    /// const thing = 123;
+    /// it('foo', () => {});
+    /// it('bar', () => {});
+    /// ```
+    ///
+    /// Examples of **correct** code for this rule:
+    /// ```js
+    /// const thing = 123;
+    ///
+    /// test('foo', () => {});
+    ///
+    /// test('bar', () => {});
+    /// ```
+    ///
+    /// ```js
+    /// const thing = 123;
+    ///
+    /// it('foo', () => {});
+    ///
+    /// it('bar', () => {});
+    /// ```
+    PaddingAroundTestBlocks,
+    jest,
+    style,
+    fix
+);
+
+enum NodeType {
+    Test,
+    Statement,
+}
+
+impl Rule for PaddingAroundTestBlocks {
+    fn run_once(&self, ctx: &LintContext<'_>) {
+        let mut nodes = collect_possible_jest_call_node(ctx)
+            .iter()
+            .filter_map(|possible_jest_node| {
+                let node = possible_jest_node.node;
+                let AstKind::CallExpression(call_expr) = node.kind() else {
+                    return None;
+                };
+                let jest_fn_call = parse_general_jest_fn_call(call_expr, possible_jest_node, ctx)?;
+                let ParsedGeneralJestFnCall { kind, .. } = &jest_fn_call;
+                let kind = kind.to_general()?;
+                if kind != JestGeneralFnKind::Test {
+                    return None;
+                }
+                Some((NodeType::Test, node))
+            })
+            .collect_vec();
+        for node in ctx.nodes() {
+            if node.kind().is_statement() {
+                nodes.push((NodeType::Statement, node));
+            }
+        }
+        nodes.sort_by_key(|(_, node)| node.span().end);
+        let mut prev_node: Option<&AstNode<'_>> = None;
+        for (node_type, node) in nodes {
+            match node_type {
+                NodeType::Test => {
+                    if let Some(prev_node) = prev_node {
+                        if prev_node.span().end > node.span().start {
+                            continue;
+                        }
+                        let mut comments_range =
+                            ctx.comments_range(prev_node.span().end..node.span().start);
+                        let mut span_between_start = prev_node.span().end;
+                        let mut span_between_end = node.span().start;
+                        if let Some(last_comment_span) =
+                            comments_range.next_back().map(|comment| comment.span)
+                        {
+                            let space_after_last_comment = ctx
+                                .source_range(Span::new(last_comment_span.end, node.span().start));
+                            let space_before_last_comment = ctx.source_range(Span::new(
+                                prev_node.span().end,
+                                last_comment_span.start,
+                            ));
+                            if space_after_last_comment.matches('\n').count() > 1
+                                || space_before_last_comment.matches('\n').count() == 0
+                            {
+                                span_between_start = last_comment_span.end;
+                            } else {
+                                span_between_end = last_comment_span.start;
+                            }
+                        }
+                        let span_between = Span::new(span_between_start, span_between_end);
+                        let content = ctx.source_range(span_between);
+                        if content.matches('\n').count() < 2 {
+                            let spaces_after_last_line = content
+                                .rfind('\n')
+                                .map_or("", |index| content.split_at(index + 1).1);
+                            ctx.diagnostic_with_fix(
+                                padding_around_test_blocks_diagnostic(Span::new(
+                                    span_between_end,
+                                    span_between_end,
+                                )),
+                                |fixer| {
+                                    fixer.replace(
+                                        span_between,
+                                        format!("\n\n{spaces_after_last_line}"),
+                                    )
+                                },
+                            );
+                        }
+                    }
+                    prev_node = Some(node);
+                }
+                NodeType::Statement => {
+                    prev_node = Some(node);
+                }
+            }
+        }
+    }
+}
+
+#[test]
+fn test() {
+    use crate::tester::Tester;
+
+    let pass = vec![
+        "test('foo', () => {});",
+        "test('foo', () => {});\n\ntest('bar', () => {});",
+        "const thing = 123;\n\ntest('foo', () => {});",
+        "{ test('foo', () => {}); }",
+        "describe('foo', () => {\ntest('bar', () => {});\n});",
+    ];
+
+    let fail = vec![
+        "test('foo', () => {});test('bar', () => {});",
+        "test('foo', () => {});\ntest('bar', () => {});",
+        "it('foo', () => {});\nfit('bar', () => {});\ntest('baz', () => {});",
+        r"
+ describe('other bar', () => {
+     const thing = 123;
+     test('is another bar w/ test', () => {
+     });
+     // With a comment
+     it('is another bar w/ it', () => {
+     });
+     test.skip('skipping', () => {}); // Another comment
+     it.skip('skipping too', () => {});
+ });xtest('weird', () => {});
+ test
+   .skip('skippy skip', () => {});
+ xit('bar foo', () => {});
+            ",
+    ];
+
+    let fix = vec![
+        (
+            "test('foo', () => {});test('bar', () => {});",
+            "test('foo', () => {});\n\ntest('bar', () => {});",
+        ),
+        (
+            "test('foo', () => {});\ntest('bar', () => {});",
+            "test('foo', () => {});\n\ntest('bar', () => {});",
+        ),
+        (
+            "it('foo', () => {});\nfit('bar', () => {});\ntest('baz', () => {});",
+            "it('foo', () => {});\n\nfit('bar', () => {});\n\ntest('baz', () => {});",
+        ),
+        (
+            r"
+describe('other bar', () => {
+    const thing = 123;
+    test('is another bar w/ test', () => {
+    });
+    // With a comment
+    it('is another bar w/ it', () => {
+    });
+    test.skip('skipping', () => {}); // Another comment
+    it.skip('skipping too', () => {});
+});xtest('weird', () => {});
+test
+  .skip('skippy skip', () => {});
+xit('bar foo', () => {});
+        ",
+            r"
+describe('other bar', () => {
+    const thing = 123;
+
+    test('is another bar w/ test', () => {
+    });
+
+    // With a comment
+    it('is another bar w/ it', () => {
+    });
+
+    test.skip('skipping', () => {}); // Another comment
+
+    it.skip('skipping too', () => {});
+});
+
+xtest('weird', () => {});
+
+test
+  .skip('skippy skip', () => {});
+
+xit('bar foo', () => {});
+        ",
+        ),
+    ];
+    Tester::new(PaddingAroundTestBlocks::NAME, PaddingAroundTestBlocks::PLUGIN, pass, fail)
+        .expect_fix(fix)
+        .test_and_snapshot();
+}
