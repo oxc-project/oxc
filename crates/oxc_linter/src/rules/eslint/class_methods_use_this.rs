a4229330892512996e809aa322c1366ddc09c623
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-08-11 20:56:30 -0700

    fix: use visitor pattern and fix failing tests

--- crates/oxc_linter/src/rules/eslint/class_methods_use_this.rs
+++ crates/oxc_linter/src/rules/eslint/class_methods_use_this.rs
@@ -1,21 +1,24 @@
+use std::borrow::Cow;
+
 use oxc_ast::{
     AstKind,
-    ast::{AccessorProperty, MethodDefinition, MethodDefinitionKind, PropertyDefinition},
+    ast::{AccessorProperty, Expression, PropertyDefinition},
 };
-use oxc_cfg::{BlockNodeId, EdgeType, visit::neighbors_filtered_by_edge_weight};
+use oxc_ast_visit::Visit;
 use oxc_diagnostics::OxcDiagnostic;
 use oxc_macros::declare_oxc_lint;
+use oxc_semantic::AstNode;
 use oxc_span::{GetSpan, Span};
-use rustc_hash::FxHashSet;
 
 #[derive(Debug, Default, Clone)]
 pub struct ClassMethodsUseThis;
 
 use crate::{LintContext, rule::Rule};
 
-fn class_methods_use_this_diagnostic(span: Span) -> OxcDiagnostic {
-    OxcDiagnostic::warn("Expected method to have this.")
-        .with_help("Consider converting method to a static method.")
+fn class_methods_use_this_diagnostic(span: Span, name: Option<Cow<'_, str>>) -> OxcDiagnostic {
+    let method_name_str = name.map_or(String::new(), |name| format!(" `{name}`"));
+    OxcDiagnostic::warn(format!("Expected method{method_name_str} to have this."))
+        .with_help(format!("Consider converting method{method_name_str} to a static method."))
         .with_label(span)
 }
 
@@ -61,71 +64,87 @@ declare_oxc_lint!(
 );
 
 impl Rule for ClassMethodsUseThis {
-    fn run_once(&self, ctx: &LintContext) {
-        let mut wanted_nodes = Vec::new();
-        let mut basic_blocks_with_this_called = FxHashSet::<BlockNodeId>::default();
-        let mut basic_blocks_with_functions = FxHashSet::<BlockNodeId>::default();
-        for node in ctx.nodes() {
-            match node.kind() {
-                AstKind::ThisExpression(_) | AstKind::Super(_) => {
-                    basic_blocks_with_this_called.insert(node.cfg_id());
+    fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
+        let function_pair = match node.kind() {
+            AstKind::AccessorProperty(accessor) => {
+                if accessor.r#static {
+                    return;
                 }
-                AstKind::Function(_) | AstKind::ArrowFunctionExpression(_) => {
-                    let parent = ctx.nodes().parent_kind(node.id());
-                    if let Some(MethodDefinition { kind, r#static, .. }) =
-                        parent.as_method_definition()
-                    {
-                        if !r#static && *kind != MethodDefinitionKind::Constructor {
-                            wanted_nodes.push(node);
-                        }
-                    } else if let Some(PropertyDefinition { r#static, .. }) =
-                        parent.as_property_definition()
-                    {
-                        if !r#static {
-                            wanted_nodes.push(node);
-                        }
-                    } else if let Some(AccessorProperty { r#static, .. }) =
-                        parent.as_accessor_property()
-                    {
-                        if !r#static {
-                            wanted_nodes.push(node);
-                        }
-                    } else if node.kind().as_function().is_some() {
-                        basic_blocks_with_functions.insert(node.cfg_id());
+                accessor.value.as_ref().and_then(|value| match value {
+                    Expression::ArrowFunctionExpression(arrow_function) => {
+                        Some((&arrow_function.body, &accessor.key))
                     }
+                    Expression::FunctionExpression(function_expression) => {
+                        Some((function_expression.body.as_ref()?, &accessor.key))
+                    }
+                    _ => None,
+                })
+            }
+            AstKind::MethodDefinition(method_definition) => {
+                if method_definition.r#static || method_definition.kind.is_constructor() {
+                    return;
                 }
-                _ => {}
+                let Some(function_body) = method_definition.value.body.as_ref() else { return };
+                Some((function_body, &method_definition.key))
             }
-        }
-        let cfg = ctx.cfg();
-        for node in wanted_nodes {
-            let output = neighbors_filtered_by_edge_weight(
-                &cfg.graph,
-                node.cfg_id(),
-                &|edge| match edge {
-                    EdgeType::Jump | EdgeType::Normal | EdgeType::NewFunction => None,
-                    EdgeType::Unreachable
-                    | EdgeType::Join
-                    | EdgeType::Error(_)
-                    | EdgeType::Finalize
-                    | EdgeType::Backedge => Some(false),
-                },
-                &mut |basic_block_id, _| {
-                    if basic_blocks_with_functions.contains(basic_block_id) {
-                        (false, false)
-                    } else if basic_blocks_with_this_called.contains(basic_block_id) {
-                        (true, false)
-                    } else {
-                        (false, true)
+            AstKind::PropertyDefinition(property_definition) => {
+                if property_definition.r#static {
+                    return;
+                }
+                property_definition.value.as_ref().and_then(|value| match value {
+                    Expression::ArrowFunctionExpression(arrow_function) => {
+                        Some((&arrow_function.body, &property_definition.key))
                     }
-                },
-            );
-            let has_this = output.iter().any(|y| *y);
-            if !has_this {
-                ctx.diagnostic(class_methods_use_this_diagnostic(node.span()));
+                    Expression::FunctionExpression(function_expression) => {
+                        Some((function_expression.body.as_ref()?, &property_definition.key))
+                    }
+                    _ => None,
+                })
             }
+            _ => None,
+        };
+        let Some((function_body, name)) = function_pair else { return };
+        let mut finder = ThisFinder::new();
+        finder.visit_function_body(function_body);
+        if !finder.has_this {
+            ctx.diagnostic(class_methods_use_this_diagnostic(name.span(), name.name()));
         }
     }
+}
+
+struct ThisFinder {
+    has_this: bool,
+}
+
+impl ThisFinder {
+    fn new() -> Self {
+        Self { has_this: false }
+    }
+}
+
+impl Visit<'_> for ThisFinder {
+    fn visit_this_expression(&mut self, _it: &oxc_ast::ast::ThisExpression) {
+        self.has_this = true;
+    }
+
+    fn visit_super(&mut self, _it: &oxc_ast::ast::Super) {
+        self.has_this = true;
+    }
+
+    fn visit_function(
+        &mut self,
+        _it: &oxc_ast::ast::Function<'_>,
+        _flags: oxc_semantic::ScopeFlags,
+    ) {
+    }
+
+    fn visit_static_block(&mut self, _it: &oxc_ast::ast::StaticBlock<'_>) {}
+
+    fn visit_property_definition(&mut self, it: &PropertyDefinition<'_>) {
+        self.visit_property_key(&it.key);
+    }
+
+    fn visit_accessor_property(&mut self, _it: &AccessorProperty<'_>) {}
 }
 
 #[test]
@@ -177,16 +196,16 @@ fn test() {
         ("class A { #foo() {} }", None, None),
         ("class A { get #foo() {} }", None, None),
         ("class A { set #foo(x) {} }", None, None),
-        // ("class A { foo () { return class { foo = this }; } }", None, None),
+        ("class A { foo () { return class { foo = this }; } }", None, None),
         ("class A { foo () { return function () { foo = this }; } }", None, None),
-        // ("class A { foo () { return class { static { this; } } } }", None, None),
+        ("class A { foo () { return class { static { this; } } } }", None, None),
         ("class Foo { private method() {} }", None, None),
         ("class Foo { protected method() {} }", None, None),
         ("class Foo { accessor method = function () {} }", None, None),
         ("class Foo { accessor method = () => {} }", None, None),
         ("class Foo { private accessor method = () => {} }", None, None),
         ("class Foo { protected accessor method = () => {} }", None, None),
-        // ("class A { foo () { return class { accessor bar = this }; } }", None, None),
+        ("class A { foo () { return class { accessor bar = this }; } }", None, None),
         ("class Derived extends Base { override method() {} }", None, None),
         ("class Derived extends Base { property = () => {} }", None, None),
         ("class Derived extends Base { public property = () => {} }", None, None),
