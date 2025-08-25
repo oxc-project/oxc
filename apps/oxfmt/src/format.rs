b16cc0def7710ecbe8bc975cd55b174dba648793
leaysgur <6259812+leaysgur@users.noreply.github.com>
2025-08-25 00:31:32 +0000

    feat(oxfmt): Add apps/oxfmt crate with minimum formatting usage (#13259)
    Part of #10573
    
    - Add `apps/oxfmt` crate
    - Copy minimum functionalities from `apps/oxlint`
      - Currently, these two are very similar
        - However, as following Prettier more, they may become different
        - There may still have in common, but at this point, it is unclear
      - `oxc_linter` also has many optimizations
    - Format files with `oxc_formatter` and save
      - Usage not optimized

---
+++ apps/oxfmt/src/format.rs
@@ -0,0 +1,90 @@
+use std::{env, ffi::OsStr, io::Write, path::PathBuf, sync::Arc};
+
+use ignore::overrides::OverrideBuilder;
+
+use crate::{
+    cli::{CliRunResult, FormatCommand},
+    service::FormatService,
+    walk::Walk,
+};
+
+#[derive(Debug)]
+pub struct FormatRunner {
+    options: FormatCommand,
+    cwd: PathBuf,
+}
+
+impl FormatRunner {
+    pub(crate) fn new(options: FormatCommand) -> Self {
+        Self { options, cwd: env::current_dir().expect("Failed to get current working directory") }
+    }
+
+    pub(crate) fn run(self, stdout: &mut dyn Write) -> CliRunResult {
+        let cwd = self.cwd;
+        let FormatCommand { paths, basic_options: _, .. } = self.options;
+
+        let (exclude_patterns, regular_paths): (Vec<_>, Vec<_>) =
+            paths.into_iter().partition(|p| p.to_string_lossy().starts_with('!'));
+
+        // Need at least one regular path
+        if regular_paths.is_empty() {
+            print_and_flush_stdout(
+                stdout,
+                "Expected at least one target file/dir/glob(non-override pattern)\n",
+            );
+            return CliRunResult::FormatNoFilesFound;
+        }
+
+        // Build exclude patterns if any exist
+        let override_builder = (!exclude_patterns.is_empty())
+            .then(|| {
+                let mut builder = OverrideBuilder::new(cwd);
+                for pattern in exclude_patterns {
+                    builder.add(&pattern.to_string_lossy()).ok()?;
+                }
+                builder.build().ok()
+            })
+            .flatten();
+
+        let walker = Walk::new(&regular_paths, override_builder);
+        let paths = walker.paths();
+
+        let files_to_format = paths
+            .into_iter()
+            // .filter(|path| !config_store.should_ignore(Path::new(path)))
+            .collect::<Vec<Arc<OsStr>>>();
+
+        if files_to_format.is_empty() {
+            print_and_flush_stdout(stdout, "Expected at least one target file\n");
+            return CliRunResult::FormatNoFilesFound;
+        }
+
+        // let (mut diagnostic_service, tx_error) =
+        //     Self::get_diagnostic_service(&output_formatter, &warning_options, &misc_options);
+
+        // rayon::spawn(move || {
+        let mut format_service = FormatService::new();
+        format_service.with_paths(files_to_format);
+        format_service.run();
+        // });
+
+        // let diagnostic_result = diagnostic_service.run(stdout);
+
+        CliRunResult::FormatSucceeded
+    }
+}
+
+pub fn print_and_flush_stdout(stdout: &mut dyn Write, message: &str) {
+    use std::io::{Error, ErrorKind};
+    fn check_for_writer_error(error: Error) -> Result<(), Error> {
+        // Do not panic when the process is killed (e.g. piping into `less`).
+        if matches!(error.kind(), ErrorKind::Interrupted | ErrorKind::BrokenPipe) {
+            Ok(())
+        } else {
+            Err(error)
+        }
+    }
+
+    stdout.write_all(message.as_bytes()).or_else(check_for_writer_error).unwrap();
+    stdout.flush().unwrap();
+}
