oxc-project/oxc

commit 65aca9eeb93fea56b59975968a5692c671bbf15e

fix(parser): reset popped lexer errors when rewinding (#13494)
Closes #12816.
Alternative to #13460.

This PR is the slightly cleaner way to fix the issue compared to the
other PR, but comes at a potential performance cost because now instead
of remembering how many errors there were, we are storing a clone of all
errors and re-assign them when rewinding.

This makes it possible to fix the issue because now even errors before a
checkpoint, that were popped/removed after the checkpoint, are properly
rewinded.

crates/oxc_parser/src/lexer/mod.rs
@@ -40,14 +40,11 @@ pub use token::Token;
 use source::{Source, SourcePosition};
 use trivia_builder::TriviaBuilder;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct LexerCheckpoint<'a> {
-    /// Current position in source
-    position: SourcePosition<'a>,
-
+    source_position: SourcePosition<'a>,
     token: Token,
-
-    errors_pos: usize,
+    errors: Vec<OxcDiagnostic>,
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
@@ -141,16 +138,16 @@ impl<'a> Lexer<'a> {
     /// Use `rewind` to restore the lexer to the state stored in the checkpoint.
     pub fn checkpoint(&self) -> LexerCheckpoint<'a> {
         LexerCheckpoint {
-            position: self.source.position(),
+            source_position: self.source.position(),
             token: self.token,
-            errors_pos: self.errors.len(),
+            errors: self.errors.clone(),
         }
     }
 
     /// Rewinds the lexer to the same state as when the passed in `checkpoint` was created.
     pub fn rewind(&mut self, checkpoint: LexerCheckpoint<'a>) {
-        self.errors.truncate(checkpoint.errors_pos);
-        self.source.set_position(checkpoint.position);
+        self.errors = checkpoint.errors;
+        self.source.set_position(checkpoint.source_position);
         self.token = checkpoint.token;
     }
 

tasks/coverage/misc/fail/oxc-12816.ts
@@ -1,0 +1,1 @@
+{if(\ ()<1){}}

tasks/coverage/snapshots/parser_misc.snap
@@ -1,7 +1,7 @@
 parser_misc Summary:
 AST Parsed     : 49/49 (100.00%)
 Positive Passed: 49/49 (100.00%)
-Negative Passed: 77/77 (100.00%)
+Negative Passed: 78/78 (100.00%)
 
   × Cannot assign to 'arguments' in strict mode
    ╭─[misc/fail/arguments-eval.ts:1:10]
@@ -435,6 +435,12 @@ Negative Passed: 77/77 (100.00%)
    ╭─[misc/fail/oxc-12612-4.ts:1:2]
  1 │ (foo() as number as any) = 123;
    ·  ──────────────────────
+   ╰────
+
+  × Invalid Unicode escape sequence
+   ╭─[misc/fail/oxc-12816.ts:1:6]
+ 1 │ {if(\ ()<1){}}
+   ·      ─
    ╰────
 
   × 'super' can only be referenced in members of derived classes or object literal expressions.

commit 3ead0dd60852493b1d7696a7efaf91ea02cb132a

perf(parser): store Option<diagnostic> for lexer errors (#13520)
partially addresses the perf regression in https://github.com/oxc-project/oxc/pull/13494

Not quite as fast as originally, but a little bit faster

crates/oxc_parser/src/lexer/mod.rs
@@ -44,7 +44,7 @@ use trivia_builder::TriviaBuilder;
 pub struct LexerCheckpoint<'a> {
     source_position: SourcePosition<'a>,
     token: Token,
-    errors: Vec<OxcDiagnostic>,
+    errors: Option<Vec<OxcDiagnostic>>,
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
@@ -140,13 +140,13 @@ impl<'a> Lexer<'a> {
         LexerCheckpoint {
             source_position: self.source.position(),
             token: self.token,
-            errors: self.errors.clone(),
+            errors: { if self.errors.is_empty() { None } else { Some(self.errors.clone()) } },
         }
     }
 
     /// Rewinds the lexer to the same state as when the passed in `checkpoint` was created.
     pub fn rewind(&mut self, checkpoint: LexerCheckpoint<'a>) {
-        self.errors = checkpoint.errors;
+        self.errors = checkpoint.errors.unwrap_or_default();
         self.source.set_position(checkpoint.source_position);
         self.token = checkpoint.token;
     }