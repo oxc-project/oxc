b794af1924c62f134f17a2866f374709d514195c
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-09-12 01:01:49 -0700

    feat(rulegen): add config struct (#13320)
    Parses the meta.schema field of the eslint rule to code generate the
    structs and enums needed for the rule. It also adds the derive
    attributes so that documentation code generation can read the struct
    shape and `from_configuration` can easily deserialize.
    
    Improvements that I would prefer to tackle for follow up PRs:
    - Resolve the schema value in javascript (a la `tasks/lint_rules`
    package), then parse in rust
    - Parse other parts of the json schema for validation, like `minItems`
    and `maximum`, and use a validation crate in `from_configuration`
    - Support json schema constants like `true`, number literals like `1`
    and `null`
    - Support other json schema concepts like `allOf`, `not`,
    `patternProperties`, `additionalProperties`, and others
    - Support migrating existing rules to this new format
    - Use configs in `oxlintrc.json` schema generation for lsps to validate
    in the editor
    
    ---------
    
    Co-authored-by: Cameron Clark <cameron.clark@hey.com>

--- tasks/rulegen/src/template.rs
+++ tasks/rulegen/src/template.rs
@@ -52,11 +52,16 @@ impl<'a> Template<'a> {
         let out_path = path.join(format!("{}.rs", self.context.snake_rule_name));
 
         File::create(out_path.clone())?.write_all(rendered.as_bytes())?;
-        format_rule_output(&out_path).map(|mut child| {
-            child.wait().expect("failed to format");
-        })?;
+        println!("Saved file to {}", out_path.display());
 
-        println!("Saved test file to {}", out_path.display());
+        let res =
+            format_rule_output(&out_path).map(|mut child| child.wait().expect("failed to format"));
+
+        match res {
+            Ok(exit_status) if exit_status.success() => println!("Formatted rule file"),
+            Ok(exit_status) => println!("Failed to format rule file: exited with {exit_status}"),
+            Err(e) => println!("Failed to format rule file: {e}"),
+        }
 
         Ok(())
     }
