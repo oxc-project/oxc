3df083c28451568a3ca3140a460cc0c4e0baf510
Cameron Clark <cameron.clark@hey.com>
2025-09-19 09:40:45 +0100

    fix

--- crates/oxc_linter/src/generated/rule_runner_impls.rs
+++ crates/oxc_linter/src/generated/rule_runner_impls.rs
@@ -2689,11 +2689,11 @@ impl RuleRunner for crate::rules::vue::no_multiple_slot_args::NoMultipleSlotArgs
         Some(&AstTypesBitset::from_types(&[AstType::CallExpression]));
 }
 
-impl RuleRunner for crate::rules::vue::require_typed_ref::RequireTypedRef {
+impl RuleRunner for crate::rules::vue::no_required_prop_with_default::NoRequiredPropWithDefault {
     const NODE_TYPES: Option<&AstTypesBitset> = None;
 }
 
-impl RuleRunner for crate::rules::vue::no_required_prop_with_default::NoRequiredPropWithDefault {
+impl RuleRunner for crate::rules::vue::require_typed_ref::RequireTypedRef {
     const NODE_TYPES: Option<&AstTypesBitset> = None;
 }
 
