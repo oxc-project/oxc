ad14a41aa6f125ed4cd48bea31bcd2a7d9a2b64b
Sysix <3897725+Sysix@users.noreply.github.com>
2025-09-19 14:40:10 +0000

    test(editor): fix test setup (#13929)
    The tests are always passing, because we are not generating them:
    https://github.com/oxc-project/oxc/actions/runs/17858412366/job/50782751216?pr=13700
    
    introduced in https://github.com/oxc-project/oxc/pull/13737

--- editors/vscode/rolldown.config.ts
+++ editors/vscode/rolldown.config.ts
@@ -1,14 +1,26 @@
-import { defineConfig } from 'rolldown';
+import { defineConfig, type RolldownOptions } from 'rolldown';
+import { globSync } from 'tinyglobby';
+
+const input: RolldownOptions['input'] = process.env.TEST === 'true'
+  ? globSync('tests/**/*.ts')
+  : ['client/extension.ts'];
+
+const output: RolldownOptions['output'] = {
+  sourcemap: true,
+  format: 'cjs',
+  banner: `"use strict";\n`,
+  minify: true,
+};
+
+if (process.env.TEST === 'true') {
+  output.dir = 'out';
+} else {
+  output.file = 'out/main.js';
+}
 
 export default defineConfig({
-  input: 'client/extension.ts',
-  output: {
-    file: 'out/main.js',
-    sourcemap: true,
-    format: 'cjs',
-    banner: `"use strict";\n`,
-    minify: true,
-  },
+  input,
+  output,
   external: ['vscode'],
   platform: 'node',
   transform: {
