15eb326b57f5cb42ee085433a3827ecfb979f4a6
yefan <wany3657@gmail.com>
2025-09-19 16:44:36 +0800

    feat(linter): add `vue/no-required-prop-with-default` rule (#13613)
    related https://github.com/oxc-project/oxc/issues/11440
    
    https://eslint.vuejs.org/rules/no-required-prop-with-default.html
    
    ---------
    
    Co-authored-by: Cameron Clark <cameron.clark@hey.com>

--- crates/oxc_linter/src/generated/rule_runner_impls.rs
+++ crates/oxc_linter/src/generated/rule_runner_impls.rs
@@ -2690,6 +2690,10 @@ impl RuleRunner for crate::rules::vue::no_multiple_slot_args::NoMultipleSlotArgs
         Some(&AstTypesBitset::from_types(&[AstType::CallExpression]));
 }
 
+impl RuleRunner for crate::rules::vue::no_required_prop_with_default::NoRequiredPropWithDefault {
+    const NODE_TYPES: Option<&AstTypesBitset> = None;
+}
+
 impl RuleRunner for crate::rules::vue::require_typed_ref::RequireTypedRef {
     const NODE_TYPES: Option<&AstTypesBitset> =
         Some(&AstTypesBitset::from_types(&[AstType::CallExpression]));
