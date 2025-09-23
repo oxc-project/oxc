601c876022cfb3fe8baa644c02f5a548512bc292
overlookmotel <557937+overlookmotel@users.noreply.github.com>
2025-09-23 09:05:46 +0000

    docs(linter): add comment explaining why Mimalloc is feature-gated (#14037)

--- apps/oxlint/src/lib.rs
+++ apps/oxlint/src/lib.rs
@@ -33,6 +33,9 @@ mod generated {
 #[cfg(all(feature = "napi", target_pointer_width = "64", target_endian = "little"))]
 mod js_plugins;
 
+// Use Mimalloc as the global allocator if `--features allocator` is enabled.
+// Mimalloc has better performance, but this is feature-gated because it's slow to compile.
+// `--features allocator` is only used in release builds.
 #[cfg(all(feature = "allocator", not(miri), not(target_family = "wasm")))]
 #[global_allocator]
 static GLOBAL: mimalloc_safe::MiMalloc = mimalloc_safe::MiMalloc;
