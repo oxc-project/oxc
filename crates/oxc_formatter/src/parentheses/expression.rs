ae32e7b25e8fcbb06a8a1fe53a70b92a3efabb81
Tyler Earls <tyler.a.earls@gmail.com>
2025-09-27 12:17:34 -0500

    undo change that caused regression

--- crates/oxc_formatter/src/parentheses/expression.rs
+++ crates/oxc_formatter/src/parentheses/expression.rs
@@ -131,6 +131,7 @@ impl<'a> NeedsParentheses<'a> for AstNode<'a, IdentifierReference<'a>> {
                 let mut parent = self.parent;
                 loop {
                     match parent {
+                        AstNodes::Program(_) | AstNodes::ExpressionStatement(_) => return false,
                         AstNodes::ForOfStatement(stmt) => {
                             return stmt.left.span().contains_inclusive(self.span);
                         }
@@ -146,7 +147,6 @@ impl<'a> NeedsParentheses<'a> for AstNode<'a, IdentifierReference<'a>> {
                                 .as_ref()
                                 .is_some_and(|init| init.span().contains_inclusive(self.span));
                         }
-                        AstNodes::Program(_) => break,
                         _ => parent = parent.parent(),
                     }
                 }
