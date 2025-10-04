8dcfd05ffcf3fb1f1c714ffbf3c31f440818b6d8
Tyler Earls <tyler.a.earls@gmail.com>
2025-10-04 15:40:20 -0500

    re-add vue rules that were mistakenly removed during rebase

--- crates/oxc_linter/src/generated/rule_runner_impls.rs
+++ crates/oxc_linter/src/generated/rule_runner_impls.rs
@@ -2910,3 +2910,48 @@ impl RuleRunner for crate::rules::vitest::require_local_test_context_for_concurr
 impl RuleRunner for crate::rules::vitest::valid_title::ValidTitle {
     const NODE_TYPES: Option<&AstTypesBitset> = None;
 }
+
+impl RuleRunner for crate::rules::vue::define_emits_declaration::DefineEmitsDeclaration {
+    const NODE_TYPES: Option<&AstTypesBitset> =
+        Some(&AstTypesBitset::from_types(&[AstType::CallExpression]));
+}
+
+impl RuleRunner for crate::rules::vue::define_props_declaration::DefinePropsDeclaration {
+    const NODE_TYPES: Option<&AstTypesBitset> =
+        Some(&AstTypesBitset::from_types(&[AstType::CallExpression]));
+}
+
+impl RuleRunner for crate::rules::vue::define_props_destructuring::DefinePropsDestructuring {
+    const NODE_TYPES: Option<&AstTypesBitset> =
+        Some(&AstTypesBitset::from_types(&[AstType::CallExpression]));
+}
+
+impl RuleRunner for crate::rules::vue::max_props::MaxProps {
+    const NODE_TYPES: Option<&AstTypesBitset> = None;
+}
+
+impl RuleRunner for crate::rules::vue::no_multiple_slot_args::NoMultipleSlotArgs {
+    const NODE_TYPES: Option<&AstTypesBitset> =
+        Some(&AstTypesBitset::from_types(&[AstType::CallExpression]));
+}
+
+impl RuleRunner for crate::rules::vue::no_required_prop_with_default::NoRequiredPropWithDefault {
+    const NODE_TYPES: Option<&AstTypesBitset> = None;
+}
+
+impl RuleRunner for crate::rules::vue::prefer_import_from_vue::PreferImportFromVue {
+    const NODE_TYPES: Option<&AstTypesBitset> = None;
+}
+
+impl RuleRunner for crate::rules::vue::require_typed_ref::RequireTypedRef {
+    const NODE_TYPES: Option<&AstTypesBitset> =
+        Some(&AstTypesBitset::from_types(&[AstType::CallExpression]));
+}
+
+impl RuleRunner for crate::rules::vue::valid_define_emits::ValidDefineEmits {
+    const NODE_TYPES: Option<&AstTypesBitset> = None;
+}
+
+impl RuleRunner for crate::rules::vue::valid_define_props::ValidDefineProps {
+    const NODE_TYPES: Option<&AstTypesBitset> = None;
+}
