ae99b326fd8a99f28fe596908615298851ba4c51
Boshen <boshenc@gmail.com>
2025-10-06 21:39:04 +0800

    u

--- napi/playground/index.js
+++ napi/playground/index.js
@@ -109,7 +109,24 @@ function requireNative() {
     }
   } else if (process.platform === 'win32') {
     if (process.arch === 'x64') {
+      if (process.report?.getReport?.()?.header?.osName?.startsWith?.('MINGW')) {
+        try {
+        return require('./playground.win32-x64-gnu.node')
+      } catch (e) {
+        loadErrors.push(e)
+      }
       try {
+        const binding = require('@oxc-playground/binding-win32-x64-gnu')
+        const bindingPackageVersion = require('@oxc-playground/binding-win32-x64-gnu/package.json').version
+        if (bindingPackageVersion !== '0.0.0' && process.env.NAPI_RS_ENFORCE_VERSION_CHECK && process.env.NAPI_RS_ENFORCE_VERSION_CHECK !== '0') {
+          throw new Error(`Native binding package version mismatch, expected 0.0.0 but got ${bindingPackageVersion}. You can reinstall dependencies to fix this issue.`)
+        }
+        return binding
+      } catch (e) {
+        loadErrors.push(e)
+      }
+      } else {
+        try {
         return require('./playground.win32-x64-msvc.node')
       } catch (e) {
         loadErrors.push(e)
@@ -123,6 +140,7 @@ function requireNative() {
         return binding
       } catch (e) {
         loadErrors.push(e)
+      }
       }
     } else if (process.arch === 'ia32') {
       try {
