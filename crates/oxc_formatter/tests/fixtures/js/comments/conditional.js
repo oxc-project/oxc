4f19504588ffb9f2fc43f6b0c129efc8a074a53c
Dunqing <29533304+Dunqing@users.noreply.github.com>
2025-10-12 16:13:20 +0000

    fix(formatter): block comments without a leading linebreak that are around the operator of the conditional expression should be printed as-is (#14474)

---
+++ crates/oxc_formatter/tests/fixtures/js/comments/conditional.js
@@ -0,0 +1,5 @@
+test /* comment1 */ ? /* comment2 */ consequent /* comment3 */ : /* comment4 */ alternative
+
+test 
+/* comment1 */ ? /* comment2 */ consequent 
+/* comment3 */ : /* comment4 */ alternative
