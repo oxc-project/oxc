be3809594e6672abb8d9ee9e3565fc9cc3fe4697
Dunqing <29533304+Dunqing@users.noreply.github.com>
2025-10-13 06:16:05 +0000

    fix(formatter): should break even if the right side of the assignment is a require when it has a leading own line comment (#14499)

--- crates/oxc_formatter/tests/fixtures/js/comments/assignments.js
+++ crates/oxc_formatter/tests/fixtures/js/comments/assignments.js
@@ -1,2 +1,6 @@
 var longlonglonglonglonglong = /*#__PURE__*/_interopDefaultLegacy(aaaaaaaaaaaaaaa);
 var short = /*#__PURE__*/_interopDefaultLegacy(b);
+
+const jestPackageJson =
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  require(jestPath);
