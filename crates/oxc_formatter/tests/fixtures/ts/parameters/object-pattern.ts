9dbf679a27314d21f03c7d71652da8c1c2c156ec
Dunqing <dengqing0821@gmail.com>
2025-10-17 23:12:49 +0800

    fix(formatter): should inline ObjectPattern if it is a parameter can hug

--- crates/oxc_formatter/tests/fixtures/ts/parameters/object-pattern.ts
+++ crates/oxc_formatter/tests/fixtures/ts/parameters/object-pattern.ts
@@ -11,4 +11,18 @@
 export function useCopyToClipboard({ timeout = 2000, onCopy }: {
   timeout?: number;
   onCopy?: () => void;
-} = {}) {}
+} = {}) { }
+
+function callbackUrl(
+  { baseUrl, params }: { baseUrl: string; params?: string } = {
+    baseUrl: "",
+    params: undefined,
+  }
+) { }
+
+function parseTitle(
+  item: PageObjectResponse | DatabaseObjectResponse,
+  {
+    maxLength = DocumentValidation.maxTitleLength,
+  }: { maxLength?: number } = {}
+) {}
