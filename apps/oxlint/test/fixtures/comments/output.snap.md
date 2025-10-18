16bf4c92299040dd7b353e073a8a1a8943e17009
overlookmotel <theoverlookmotel@gmail.com>
2025-10-18 13:05:42 +0100

    Test output comments before/inside/after for all var and function decls

--- apps/oxlint/test/fixtures/comments/output.snap.md
+++ apps/oxlint/test/fixtures/comments/output.snap.md
@@ -3,15 +3,18 @@
 
 # stdout
 ```
-  x test-comments(test-comments): getCommentsBefore(topLevelVariable1) returned 0 comments:
-  | 
+  x test-comments(test-comments): VariableDeclaration(topLevelVariable1):
+  | getCommentsBefore: 0 comments
+  | getCommentsInside: 0 comments
+  | getCommentsAfter: 1 comment
+  |   [0] Line: " Line comment 1" at [29, 46]
    ,-[files/test.js:1:1]
  1 | const topLevelVariable1 = 1;
    : ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  2 | // Line comment 1
    `----
 
-  x test-comments(test-comments): getAllComments() returned 9 comments:
+  x test-comments(test-comments): getAllComments: 9 comments
   |   [0] Line: " Line comment 1" at [29, 46]
   |   [1] Block: " Block comment 1 " at [76, 97]
   |   [2] Block: "*\n * JSDoc comment\n " at [99, 123]
@@ -47,6 +50,20 @@
  23 | `-> const topLevelVariable3 = 2;
     `----
 
+  x test-comments(test-comments): VariableDeclaration(topLevelVariable2):
+  | getCommentsBefore: 1 comment
+  |   [0] Line: " Line comment 1" at [29, 46]
+  | getCommentsInside: 0 comments
+  | getCommentsAfter: 2 comments
+  |   [0] Block: " Block comment 1 " at [76, 97]
+  |   [1] Block: "*\n * JSDoc comment\n " at [99, 123]
+   ,-[files/test.js:3:1]
+ 2 | // Line comment 1
+ 3 | const topLevelVariable2 = 1; /* Block comment 1 */
+   : ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ 4 | 
+   `----
+
   x test-comments(test-comments): commentsExistBetween(topLevelVariable, topLevelFunction): true
    ,-[files/test.js:3:1]
  2 | // Line comment 1
@@ -55,12 +72,16 @@
  4 | 
    `----
 
-  x test-comments(test-comments): getCommentsInside(topLevelFunction) returned 5 comments:
+  x test-comments(test-comments): FunctionDeclaration(topLevelFunction):
+  | getCommentsBefore: 0 comments
+  | getCommentsInside: 5 comments
   |   [0] Line: " Line comment 2" at [163, 180]
   |   [1] Block: " Block comment 2 " at [183, 204]
   |   [2] Block: "*\n   * JSDoc comment 2\n   " at [256, 286]
   |   [3] Line: " Line comment 3" at [321, 338]
   |   [4] Line: " Line comment 4" at [405, 422]
+  | getCommentsAfter: 1 comment
+  |   [0] Block: " Block comment 3 " at [426, 447]
     ,-[files/test.js:8:8]
   7 |      */
   8 | ,-> export function topLevelFunction() {
@@ -79,7 +100,12 @@
  21 |     
     `----
 
-  x test-comments(test-comments): getCommentsAfter(functionScopedVariable) returned 1 comments:
+  x test-comments(test-comments): VariableDeclaration(functionScopedVariable):
+  | getCommentsBefore: 2 comments
+  |   [0] Line: " Line comment 2" at [163, 180]
+  |   [1] Block: " Block comment 2 " at [183, 204]
+  | getCommentsInside: 0 comments
+  | getCommentsAfter: 1 comment
   |   [0] Block: "*\n   * JSDoc comment 2\n   " at [256, 286]
     ,-[files/test.js:11:3]
  10 |   /* Block comment 2 */
@@ -88,25 +114,33 @@
  12 |   /**
     `----
 
-  x test-comments(test-comments): getCommentsBefore(functionScopedVariable) returned 2 comments:
-  |   [0] Line: " Line comment 2" at [163, 180]
-  |   [1] Block: " Block comment 2 " at [183, 204]
-    ,-[files/test.js:11:3]
- 10 |   /* Block comment 2 */
- 11 |   let functionScopedVariable = topLevelVariable;
-    :   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
- 12 |   /**
+  x test-comments(test-comments): FunctionDeclaration(nestedFunction):
+  | getCommentsBefore: 1 comment
+  |   [0] Block: "*\n   * JSDoc comment 2\n   " at [256, 286]
+  | getCommentsInside: 1 comment
+  |   [0] Line: " Line comment 3" at [321, 338]
+  | getCommentsAfter: 0 comments
+    ,-[files/test.js:15:3]
+ 14 |        */
+ 15 | ,->   function nestedFunction() {
+ 16 | |       // Line comment 3
+ 17 | |       return functionScopedVariable;
+ 18 | `->   }
+ 19 |       return nestedFunction(); // Line comment 4
     `----
 
-  x test-comments(test-comments): getCommentsAfter(topLevelVariable3) returned 0 comments:
-  | 
+  x test-comments(test-comments): VariableDeclaration(topLevelVariable3):
+  | getCommentsBefore: 1 comment
+  |   [0] Block: " Block comment 3 " at [426, 447]
+  | getCommentsInside: 0 comments
+  | getCommentsAfter: 0 comments
     ,-[files/test.js:23:1]
  22 | /* Block comment 3 */
  23 | const topLevelVariable3 = 2;
     : ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     `----
 
-Found 0 warnings and 7 errors.
+Found 0 warnings and 8 errors.
 Finished in Xms on 1 file using X threads.
 ```
 
--- apps/oxlint/test/fixtures/comments/plugin.ts
+++ apps/oxlint/test/fixtures/comments/plugin.ts
@@ -1,27 +1,32 @@
 import assert from 'node:assert';
 
-import type { Plugin, Rule } from '../../../dist/index.js';
+import type { Comment, Plugin, Rule } from '../../../dist/index.js';
+
+function formatComments(comments: Comment[]): string {
+  let text = `${comments.length} comment${comments.length === 1 ? '' : 's'}`;
+  if (comments.length > 0) {
+    text += '\n';
+    text += comments
+      .map((c, i) => `  [${i}] ${c.type}: ${JSON.stringify(c.value)} at [${c.range[0]}, ${c.range[1]}]`)
+      .join('\n');
+  }
+  return text;
+}
 
 const testCommentsRule: Rule = {
   create(context) {
     const { sourceCode } = context;
     const { ast } = sourceCode;
-    const { body } = ast;
 
-    const comments = sourceCode.getAllComments();
     context.report({
-      message: `getAllComments() returned ${comments.length} comments:\n` +
-        comments.map((c, i) => `  [${i}] ${c.type}: ${JSON.stringify(c.value)} at [${c.range[0]}, ${c.range[1]}]`).join(
-          '\n',
-        ),
-      node: context.sourceCode.ast,
+      message: `getAllComments: ${formatComments(sourceCode.getAllComments())}`,
+      node: ast,
     });
 
-    const [topLevelVariable1, topLevelVariable2, topLevelFunctionExport, topLevelVariable3] = body;
+    const [, topLevelVariable2, topLevelFunctionExport] = ast.body;
     assert(topLevelFunctionExport.type === 'ExportNamedDeclaration');
     const topLevelFunction = topLevelFunctionExport.declaration;
     assert(topLevelFunction.type === 'FunctionDeclaration');
-    const [functionScopedVariable] = topLevelFunction.body.body;
 
     const commentsBetween = sourceCode.commentsExistBetween(topLevelVariable2, topLevelFunction);
     context.report({
@@ -29,54 +34,31 @@ const testCommentsRule: Rule = {
       node: topLevelVariable2,
     });
 
-    const commentsBeforeFunctionScopedVariable = sourceCode.getCommentsBefore(functionScopedVariable);
-    context.report({
-      message:
-        `getCommentsBefore(functionScopedVariable) returned ${commentsBeforeFunctionScopedVariable.length} comments:\n` +
-        commentsBeforeFunctionScopedVariable.map((c, i) =>
-          `  [${i}] ${c.type}: ${JSON.stringify(c.value)} at [${c.range[0]}, ${c.range[1]}]`
-        ).join(
-          '\n',
-        ),
-      node: functionScopedVariable,
-    });
+    return {
+      VariableDeclaration(node) {
+        const { declarations } = node;
+        assert(declarations.length >= 1);
+        const firstDeclarationId = declarations[0].id;
+        assert(firstDeclarationId.type === 'Identifier');
 
-    const commentsBeforeTopLevelVariable1 = sourceCode.getCommentsBefore(topLevelVariable1);
-    context.report({
-      message: `getCommentsBefore(topLevelVariable1) returned ${commentsBeforeTopLevelVariable1.length} comments:\n`,
-      node: topLevelVariable1,
-    });
-
-    const commentsAfterFunctionScopedVariable = sourceCode.getCommentsAfter(functionScopedVariable);
-    context.report({
-      message:
-        `getCommentsAfter(functionScopedVariable) returned ${commentsAfterFunctionScopedVariable.length} comments:\n` +
-        commentsAfterFunctionScopedVariable.map((c, i) =>
-          `  [${i}] ${c.type}: ${JSON.stringify(c.value)} at [${c.range[0]}, ${c.range[1]}]`
-        ).join(
-          '\n',
-        ),
-      node: functionScopedVariable,
-    });
-
-    const commentsAfterTopLevelVariable3 = sourceCode.getCommentsAfter(topLevelVariable3);
-    context.report({
-      message: `getCommentsAfter(topLevelVariable3) returned ${commentsAfterTopLevelVariable3.length} comments:\n`,
-      node: topLevelVariable3,
-    });
-
-    const commentsInsideTopLevelFunction = sourceCode.getCommentsInside(topLevelFunction);
-    context.report({
-      message: `getCommentsInside(topLevelFunction) returned ${commentsInsideTopLevelFunction.length} comments:\n` +
-        commentsInsideTopLevelFunction.map((c, i) =>
-          `  [${i}] ${c.type}: ${JSON.stringify(c.value)} at [${c.range[0]}, ${c.range[1]}]`
-        ).join(
-          '\n',
-        ),
-      node: topLevelFunction,
-    });
-
-    return {};
+        context.report({
+          message: `VariableDeclaration(${firstDeclarationId.name}):\n` +
+            `getCommentsBefore: ${formatComments(sourceCode.getCommentsBefore(node))}\n` +
+            `getCommentsInside: ${formatComments(sourceCode.getCommentsInside(node))}\n` +
+            `getCommentsAfter: ${formatComments(sourceCode.getCommentsAfter(node))}`,
+          node,
+        });
+      },
+      FunctionDeclaration(node) {
+        context.report({
+          message: `FunctionDeclaration(${node.id.name}):\n` +
+            `getCommentsBefore: ${formatComments(sourceCode.getCommentsBefore(node))}\n` +
+            `getCommentsInside: ${formatComments(sourceCode.getCommentsInside(node))}\n` +
+            `getCommentsAfter: ${formatComments(sourceCode.getCommentsAfter(node))}`,
+          node,
+        });
+      },
+    };
   },
 };
 
