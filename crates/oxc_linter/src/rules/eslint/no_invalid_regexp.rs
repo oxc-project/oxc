a02567f12c2345eed6f9ab8a6d47e8f471671f8f
Connor Shea <connor.james.shea@gmail.com>
2025-10-21 01:58:05 -0600

    docs(linter): Update no-invalid-regexp rule to expose documentation for config options (#14838)
    Results in the following generated docs:
    
    ```md
    ## Configuration
    
    This rule accepts a configuration object with the following properties:
    
    ### allowConstructorFlags
    
    type: `string[]`
    
    default: `[]`
    
    Case-sensitive array of flags that will be allowed.
    ```
    
    Part of #14743.
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- crates/oxc_linter/src/rules/eslint/no_invalid_regexp.rs
+++ crates/oxc_linter/src/rules/eslint/no_invalid_regexp.rs
@@ -5,6 +5,7 @@ use oxc_macros::declare_oxc_lint;
 use oxc_regular_expression::{ConstructorParser, Options};
 use oxc_span::Span;
 use rustc_hash::FxHashSet;
+use schemars::JsonSchema;
 use serde::Deserialize;
 
 use crate::{AstNode, context::LintContext, rule::Rule};
@@ -54,12 +55,13 @@ declare_oxc_lint!(
     NoInvalidRegexp,
     eslint,
     correctness,
+    config = NoInvalidRegexpConfig,
 );
 
-#[derive(Debug, Clone, Deserialize, Default)]
+#[derive(Debug, Clone, Deserialize, Default, JsonSchema)]
+#[serde(rename_all = "camelCase", default)]
 struct NoInvalidRegexpConfig {
-    #[serde(default, rename = "allowConstructorFlags")]
-    /// Case-sensitive array of flags.
+    /// Case-sensitive array of flags that will be allowed.
     allow_constructor_flags: Vec<char>,
 }
 
