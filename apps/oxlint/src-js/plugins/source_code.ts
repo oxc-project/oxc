1b4cfa39002da391a9168956b56bb52361e97dc5
Arsh <69170106+lilnasy@users.noreply.github.com>
2025-10-22 23:03:06 +0530

    remove `markVariableAsUsed`

--- apps/oxlint/src-js/plugins/source_code.ts
+++ apps/oxlint/src-js/plugins/source_code.ts
@@ -222,7 +222,6 @@ export const SOURCE_CODE = Object.freeze({
   isGlobalReference: scopeMethods.isGlobalReference,
   getDeclaredVariables: scopeMethods.getDeclaredVariables,
   getScope: scopeMethods.getScope,
-  markVariableAsUsed: scopeMethods.markVariableAsUsed,
 
   // Token methods
   getTokens: tokenMethods.getTokens,
