1e11018dbbe6bc1d9e323a1eb0224fe59be2f667
Mert Can Altin <mertgold60@gmail.com>
2025-10-26 16:01:30 +0300

    changes for review comment

--- crates/oxc_ast/src/serialize/mod.rs
+++ crates/oxc_ast/src/serialize/mod.rs
@@ -219,11 +219,10 @@ fn get_ts_start_span(program: &Program<'_>) -> u32 {
     let start = first_stmt.span().start;
     match first_stmt {
         Statement::ExportNamedDeclaration(decl) => {
-            if let Some(Declaration::ClassDeclaration(class)) = &decl.declaration {
-                if let Some(decorator) = class.decorators.first() {
-                    return cmp::min(start, decorator.span.start);
-                }
-            }
+            if let Some(Declaration::ClassDeclaration(class)) = &decl.declaration
+                && let Some(decorator) = class.decorators.first()
+            {
+                return cmp::min(start, decorator.span.start);
         }
         Statement::ExportDefaultDeclaration(decl) => {
             if let ExportDefaultDeclarationKind::ClassDeclaration(class) = &decl.declaration
