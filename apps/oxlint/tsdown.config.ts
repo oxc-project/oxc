bbd4a61f104c281405a1a1c09dcc16a36231088a
Arsh <69170106+lilnasy@users.noreply.github.com>
2025-11-02 00:52:20 +0530

    bundle `@typescript-eslint/scope-manager`

--- apps/oxlint/tsdown.config.ts
+++ apps/oxlint/tsdown.config.ts
@@ -15,6 +15,9 @@ const commonConfig: UserConfig = {
     // Not bundled, to avoid needing sourcemaps when debugging.
     /^\.\.?\/.*\/dist\//,
   ],
+  noExternal: [
+    '@typescript-eslint/scope-manager'
+  ],
   // At present only compress syntax.
   // Don't mangle identifiers or remove whitespace, so `dist` code remains somewhat readable.
   minify: { compress: true, mangle: false, codegen: { removeWhitespace: false } },
