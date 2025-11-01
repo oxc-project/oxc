377e90450fad74f0c5c8548a754e5fd3e3efff2c
Liang Mi <mi_yyds@126.com>
2025-11-01 20:46:40 +0800

    fix(linter): ignore script tag in code comment (#15202)
    Partial loader tries parsing the `<script>` tag in the code comment
    incorrectly
    
    For example, we have a `.vue` file
    
    ```vue
    <!-- <script setup lang="tsx"> -->
    <script setup lang="ts">
    const count = ref(0)
    </script>
    ```
    
    And the loader will regard these as script part
    
    ```
     -->
    <script setup lang="ts">
    const count = ref(0)
    ```
    
    So this PR added some `Finder`s for code comment to ignore the
    `<script>` in code comment, also works on `.svelte` and `.astro` files

--- crates/oxc_linter/src/loader/partial_loader/mod.rs
+++ crates/oxc_linter/src/loader/partial_loader/mod.rs
@@ -1,3 +1,4 @@
+use memchr::{memmem::Finder, memmem::FinderRev};
 use oxc_span::VALID_EXTENSIONS;
 
 use crate::loader::JavaScriptSource;
@@ -11,6 +12,8 @@ pub use vue::VuePartialLoader;
 
 const SCRIPT_START: &str = "<script";
 const SCRIPT_END: &str = "</script>";
+const COMMENT_START: &str = "<!--";
+const COMMENT_END: &str = "-->";
 
 /// File extensions that can contain JS/TS code in certain parts, such as in `<script>` tags, and can
 /// be loaded using the [`PartialLoader`].
@@ -68,4 +71,32 @@ fn find_script_closing_angle(source_text: &str, pointer: usize) -> Option<usize>
     }
 
     None
+}
+
+fn find_script_start(
+    source_text: &str,
+    pointer: usize,
+    script_start_finder: &Finder<'_>,
+    comment_start_finder: &FinderRev<'_>,
+    comment_end_finder: &Finder<'_>,
+) -> Option<usize> {
+    let mut new_pointer = pointer;
+
+    loop {
+        new_pointer +=
+            script_start_finder.find(&source_text.as_bytes()[new_pointer..])? + SCRIPT_START.len();
+
+        if let Some(offset) = comment_start_finder.rfind(&source_text.as_bytes()[..new_pointer]) {
+            if comment_end_finder
+                .find(&source_text.as_bytes()[offset + COMMENT_START.len()..new_pointer])
+                .is_some()
+            {
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+
+    Some(new_pointer - pointer)
 }
