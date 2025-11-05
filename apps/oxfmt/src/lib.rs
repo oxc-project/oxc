167f13c7a4bffb99a8589972340eade08d179450
Yuji Sugiura <6259812+leaysgur@users.noreply.github.com>
2025-11-05 18:48:04 +0900

    fix(oxfmt): Remove main.rs (#15294)
    Follows `oxlint`.
    
    May relate to #15295

--- apps/oxfmt/src/lib.rs
+++ apps/oxfmt/src/lib.rs
@@ -5,14 +5,6 @@ mod result;
 mod service;
 mod walk;
 
-pub mod cli {
-    pub use crate::{
-        command::{FormatCommand, format_command},
-        format::FormatRunner,
-        result::CliRunResult,
-    };
-}
-
 // Only include code to run formatter when the `napi` feature is enabled.
 #[cfg(feature = "napi")]
 mod prettier_plugins;
@@ -24,65 +16,3 @@ pub use run::*;
 #[cfg(all(feature = "allocator", not(miri), not(target_family = "wasm")))]
 #[global_allocator]
 static GLOBAL: mimalloc_safe::MiMalloc = mimalloc_safe::MiMalloc;
-
-use std::io::BufWriter;
-
-use crate::cli::{CliRunResult, FormatRunner, format_command};
-
-pub fn format_cli() -> CliRunResult {
-    init_tracing();
-    init_miette();
-
-    let mut args = std::env::args_os();
-    // If first arg is `node`, also skip script path (`node script.js ...`).
-    // Otherwise, just skip first arg (`oxfmt ...`).
-    if args.next().is_some_and(|arg| arg == "node") {
-        args.next();
-    }
-    let args = args.collect::<Vec<_>>();
-
-    // Parse command line arguments
-    let command = match format_command().run_inner(&*args) {
-        Ok(cmd) => cmd,
-        Err(e) => {
-            e.print_message(100);
-            return if e.exit_code() == 0 {
-                // e.g. `-V` and `--help`
-                CliRunResult::None
-            } else {
-                // e.g. Unknown options
-                CliRunResult::InvalidOptionConfig
-            };
-        }
-    };
-    command.handle_threads();
-
-    // stdio is blocked by LineWriter, use a BufWriter to reduce syscalls.
-    // See `https://github.com/rust-lang/rust/issues/60673`.
-    let mut stdout = BufWriter::new(std::io::stdout());
-    FormatRunner::new(command).run(&mut stdout)
-}
-
-/// Initialize the data which relies on `is_atty` system calls so they don't block subsequent threads.
-pub(crate) fn init_miette() {
-    miette::set_hook(Box::new(|_| Box::new(miette::MietteHandlerOpts::new().build()))).unwrap();
-}
-
-/// To debug `oxc_formatter`:
-/// `OXC_LOG=oxc_formatter oxfmt`
-pub(crate) fn init_tracing() {
-    use tracing_subscriber::{filter::Targets, prelude::*};
-
-    // Usage without the `regex` feature.
-    // <https://github.com/tokio-rs/tracing/issues/1436#issuecomment-918528013>
-    tracing_subscriber::registry()
-        .with(std::env::var("OXC_LOG").map_or_else(
-            |_| Targets::new(),
-            |env_var| {
-                use std::str::FromStr;
-                Targets::from_str(&env_var).unwrap()
-            },
-        ))
-        .with(tracing_subscriber::fmt::layer())
-        .init();
-}
