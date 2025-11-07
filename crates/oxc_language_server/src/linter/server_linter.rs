76f5a55e297d8f6a211dff0a864485ef1ca5f0b4
Sysix <3897725+Sysix@users.noreply.github.com>
2025-11-07 23:15:39 +0000

    refactor(language_server): move code actions logic to `ServerLinter` (#15455)

--- crates/oxc_language_server/src/linter/server_linter.rs
+++ crates/oxc_language_server/src/linter/server_linter.rs
@@ -9,7 +9,9 @@ use tokio::sync::Mutex;
 use tower_lsp_server::{
     UriExt,
     jsonrpc::ErrorCode,
-    lsp_types::{Diagnostic, Pattern, Uri, WorkspaceEdit},
+    lsp_types::{
+        CodeActionKind, CodeActionOrCommand, Diagnostic, Pattern, Range, Uri, WorkspaceEdit,
+    },
 };
 
 use oxc_linter::{
@@ -20,7 +22,10 @@ use oxc_linter::{
 use crate::{
     ConcurrentHashMap, LINT_CONFIG_FILE,
     linter::{
-        code_actions::fix_all_text_edit,
+        code_actions::{
+            CODE_ACTION_KIND_SOURCE_FIX_ALL_OXC, apply_all_fix_code_action, apply_fix_code_actions,
+            fix_all_text_edit,
+        },
         commands::{FIX_ALL_COMMAND_ID, FixAllCommandArgs},
         config_walker::ConfigWalker,
         error_with_position::DiagnosticReport,
@@ -518,6 +523,55 @@ impl ServerLinter {
             change_annotations: None,
         }))
     }
+
+    pub async fn get_code_actions_or_commands(
+        &self,
+        uri: &Uri,
+        range: &Range,
+        only_code_action_kinds: Option<Vec<CodeActionKind>>,
+    ) -> Vec<CodeActionOrCommand> {
+        let value = if let Some(cached_diagnostics) = self.get_cached_diagnostics(uri) {
+            cached_diagnostics
+        } else {
+            let diagnostics = self.run_single(uri, None).await;
+            diagnostics.unwrap_or_default()
+        };
+
+        if value.is_empty() {
+            return vec![];
+        }
+
+        let reports = value
+            .iter()
+            .filter(|r| r.diagnostic.range == *range || range_overlaps(*range, r.diagnostic.range));
+
+        let is_source_fix_all_oxc = only_code_action_kinds
+            .is_some_and(|only| only.contains(&CODE_ACTION_KIND_SOURCE_FIX_ALL_OXC));
+
+        if is_source_fix_all_oxc {
+            return apply_all_fix_code_action(reports.map(|report| &report.fixed_content), uri)
+                .map_or(vec![], |code_actions| {
+                    vec![CodeActionOrCommand::CodeAction(code_actions)]
+                });
+        }
+
+        let mut code_actions_vec: Vec<CodeActionOrCommand> = vec![];
+
+        for report in reports {
+            if let Some(fix_actions) =
+                apply_fix_code_actions(&report.fixed_content, &report.diagnostic.message, uri)
+            {
+                code_actions_vec
+                    .extend(fix_actions.into_iter().map(CodeActionOrCommand::CodeAction));
+            }
+        }
+
+        code_actions_vec
+    }
+}
+
+fn range_overlaps(a: Range, b: Range) -> bool {
+    a.start <= b.end && a.end >= b.start
 }
 
 #[cfg(test)]
