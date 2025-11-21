986cac18996d4ad0ad5d91acd4337bf7716ec8a4
Arsh <emmagoldmanvevo+github@pm.me>
2025-11-21 23:09:02 +0530

    feat(linter/plugins): Token-related `SourceCode` APIs (TS ESLint implementation) (#15861)
    - "Slow but working" implementation for
    https://github.com/oxc-project/oxc/issues/14829#issuecomment-3512679496
    - Uses the parser from `@typescript-eslint/typescript-estree` for
    tokens. The source code is fully parsed and everything but the tokens
    and comments is discarded.
    - Tests are directly adapted from `eslint`.
    - Direct commits from maintainers welcome.
    
    ### Tasks
    - [x] `getTokens`
    - [x] Move token types to `src-js/plugins/tokens.ts`
    - [x] Bundle typescript, handling `_filename` usage in ES modules.
    - [x] ~[Prevent class name mangling by
    `tsdown`](https://github.com/oxc-project/oxc/pull/15861#discussion_r2542816947).
    Could be deferred.~
    https://github.com/oxc-project/oxc/pull/15906#pullrequestreview-3487271352
    - [x] [Test regex-divide ambiguous
    syntax](https://github.com/oxc-project/oxc/pull/15861#discussion_r2542919409).
    - [x] Test for conformance directly against `eslint` tests.
    - [ ] [Parse as JSX only if source text is
    JSX](https://github.com/oxc-project/oxc/pull/15861#discussion_r2542850640).
    Could be deferred.
    - [ ] Lazy load the parser on demand. Could be deferred.
    
    ### Future work
    - [ ] `getFirstToken`
    - [ ] `getFirstTokens`
    - [ ] `getLastToken`
    - [ ] `getLastTokens`
    - [ ] `getTokenBefore`
    - [ ] `getTokenOrCommentBefore`
    - [ ] `getTokensBefore`
    - [ ] `getTokenAfter`
    - [ ] `getTokenOrCommentAfter`
    - [ ] `getTokensAfter`
    - [ ] `getTokensBetween`
    - [ ] `getFirstTokenBetween`
    - [ ] `getFirstTokensBetween`
    - [ ] `getLastTokenBetween`
    - [ ] `getLastTokensBetween`
    - [ ] `getTokenByRangeStart`
    - [ ] `isSpaceBetween`
    - [ ] `isSpaceBetweenTokens`
    
    ### Decisions
    - [x] `@typescript-eslint/typescript-estree` peer-depends on
    `typescript`. How should we package it? Currently, `typescript` is being
    made an optional dependency.
    - **overlookmotel (on discord): ideally bundled, direct runtime
    dependency otherwise.**
    - [x] Deprecated methods are being removed altogether in ESLint 10:
    `getTokenOrCommentBefore`, `getTokenOrCommentAfter`, and
    `isSpaceBetweenTokens`. These are surface level deprecations: the
    functionality was merged with other methods (the `includeComments: true`
    option) and plugins can migrate with a one line change. I'm guessing we
    are targeting fairly modern, actively developed projects. Should we
    expose them?
    - **[overlookmotel and
    camc314](https://github.com/oxc-project/oxc/pull/15861#issuecomment-3552651295):
    yes.**
    - [x] `Program` range is different between ESLint and TS-ESLint. Which
    one should we follow?
    -
    **[overlookmotel](https://github.com/oxc-project/oxc/pull/15861#discussion_r2547915407):
    they are going to both change and align soon, we should update our range
    calculation to match when they do.**
    
    ---------
    
    Co-authored-by: overlookmotel <theoverlookmotel@gmail.com>

---
+++ apps/oxlint/test/tokens.test.ts
@@ -0,0 +1,106 @@
+import assert from 'node:assert';
+import { describe, it, vi } from 'vitest';
+import { getTokens } from '../src-js/plugins/tokens.js';
+import type { Node } from '../src-js/plugins/types.js';
+
+let sourceText = 'null;';
+
+vi.mock('../src-js/plugins/source_code.ts', () => {
+  return {
+    get sourceText() {
+      return sourceText;
+    },
+  };
+});
+
+// https://github.com/eslint/eslint/blob/v9.39.1/tests/lib/languages/js/source-code/token-store.js#L62
+describe('when calling getTokens', () => {
+  sourceText = '/*A*/var answer/*B*/=/*C*/a/*D*/* b/*E*///F\n    call();\n/*Z*/';
+
+  // TODO: We are lying about `Program`'s range here.
+  // The range provided by `@typescript-eslint/typescript-estree` does not match the assertions for that of `espree`.
+  // The deviation is being corrected in upcoming releases of ESLint and TS-ESLint.
+  // https://eslint.org/blog/2025/10/whats-coming-in-eslint-10.0.0/#updates-to-program-ast-node-range-coverage
+  // https://github.com/typescript-eslint/typescript-eslint/issues/11026#issuecomment-3421887632
+  const Program = { range: [5, 55] } as Node;
+  const BinaryExpression = { range: [26, 35] } as Node;
+
+  it('should retrieve all tokens for root node', () => {
+    assert.deepStrictEqual(
+      getTokens(Program).map((token) => token.value),
+      ['var', 'answer', '=', 'a', '*', 'b', 'call', '(', ')', ';'],
+    );
+  });
+
+  it('should retrieve all tokens for binary expression', () => {
+    assert.deepStrictEqual(
+      getTokens(BinaryExpression).map((token) => token.value),
+      ['a', '*', 'b'],
+    );
+  });
+
+  it('should retrieve all tokens plus one before for binary expression', () => {
+    assert.deepStrictEqual(
+      getTokens(BinaryExpression, 1).map((token) => token.value),
+      ['=', 'a', '*', 'b'],
+    );
+  });
+
+  it('should retrieve all tokens plus one after for binary expression', () => {
+    assert.deepStrictEqual(
+      getTokens(BinaryExpression, 0, 1).map((token) => token.value),
+      ['a', '*', 'b', 'call'],
+    );
+  });
+
+  it('should retrieve all tokens plus two before and one after for binary expression', () => {
+    assert.deepStrictEqual(
+      getTokens(BinaryExpression, 2, 1).map((token) => token.value),
+      ['answer', '=', 'a', '*', 'b', 'call'],
+    );
+  });
+
+  it('should retrieve all matched tokens for root node with filter', () => {
+    assert.deepStrictEqual(
+      getTokens(Program, (t) => t.type === 'Identifier').map((token) => token.value),
+      ['answer', 'a', 'b', 'call'],
+    );
+    assert.deepStrictEqual(
+      getTokens(Program, {
+        filter: (t) => t.type === 'Identifier',
+      }).map((token) => token.value),
+      ['answer', 'a', 'b', 'call'],
+    );
+  });
+
+  it('should retrieve all tokens and comments in the node for root node with includeComments option', () => {
+    assert.deepStrictEqual(
+      getTokens(Program, { includeComments: true }).map((token) => token.value),
+      ['var', 'answer', 'B', '=', 'C', 'a', 'D', '*', 'b', 'E', 'F', 'call', '(', ')', ';'],
+    );
+  });
+
+  it('should retrieve matched tokens and comments in the node for root node with includeComments and filter options', () => {
+    assert.deepStrictEqual(
+      getTokens(Program, {
+        includeComments: true,
+        filter: (t) => t.type.startsWith('Block'),
+      }).map((token) => token.value),
+      ['B', 'C', 'D', 'E'],
+    );
+  });
+
+  it('should retrieve all tokens and comments in the node for binary expression with includeComments option', () => {
+    assert.deepStrictEqual(
+      getTokens(BinaryExpression, { includeComments: true }).map((token) => token.value),
+      ['a', 'D', '*', 'b'],
+    );
+  });
+
+  it('should handle out of range nodes gracefully', () => {
+    assert.deepStrictEqual(
+      getTokens({ range: [1000, 1001] } as Node).map((token) => token.value),
+      [],
+    );
+  });
+});
