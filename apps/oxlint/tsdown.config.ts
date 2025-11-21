152e5de5ecf7a4398e99d5dc794d68aa280113ba
overlookmotel <557937+overlookmotel@users.noreply.github.com>
2025-11-21 14:59:24 +0000

    refactor(linter/plugins): ensure debug assertions always shaken out of release build (#15946)
    The `assert*` functions are no-ops in release builds, and minifier removes them as dead code.
    
    However, there's an annoyance - we have to ensure that they're only used in files which end up in the `plugins.js` chunk. If we use those assertions elsewhere (e.g. in `cli.ts`), TSDown creates a shared chunk containing them, and then minifier can't see that they can be removed, and leaves all the assertions in the output.
    
    Fix this problem by adding a plugin to TSDown for the release build which replaces `import`s from `asserts.ts` with inlined empty functions.
    
    ```ts
    // Original code
    import { assertIs, assertIsNonNull } from '../utils/asserts.ts';
    ```
    
    ```ts
    // After transform
    function assertIs() {}
    function assertIsNonNull() {}
    ```
    
    This allows us to use the assertion functions anywhere, and minifier can always remove them as dead code.

--- apps/oxlint/tsdown.config.ts
+++ apps/oxlint/tsdown.config.ts
@@ -1,4 +1,13 @@
-import { defineConfig, type UserConfig } from 'tsdown';
+import { join } from 'node:path';
+import { defineConfig } from 'tsdown';
+
+import type { UserConfig } from 'tsdown';
+import type { Plugin } from 'rolldown';
+
+const ASSERTS_PATH = join(import.meta.dirname, 'src-js/utils/asserts.ts');
+
+const replaceAssertsPlugin = createReplaceAssertsPlugin();
+const plugins = [replaceAssertsPlugin];
 
 const commonConfig: UserConfig = {
   format: 'esm',
@@ -22,6 +31,11 @@ const commonConfig: UserConfig = {
     codegen: { removeWhitespace: false },
   },
   define: { DEBUG: 'false' },
+  plugins,
+  inputOptions: {
+    // For `replaceAssertsPlugin`
+    experimental: { nativeMagicString: true },
+  },
 };
 
 // Only generate `.d.ts` file for main export, not for CLI
@@ -44,7 +58,70 @@ const debugConfigs = configs.map((config) => ({
   ...config,
   outDir: 'debug',
   define: { DEBUG: 'true' },
+  plugins: plugins.filter((plugin) => plugin !== replaceAssertsPlugin),
 }));
 configs.push(...debugConfigs);
 
 export default configs;
+
+/**
+ * Create a plugin to remove imports of `assert*` functions from `src-js/utils/asserts.ts`,
+ * and replace those imports with empty function declarations.
+ *
+ * ```ts
+ * // Original code
+ * import { assertIs, assertIsNonNull } from '../utils/asserts.ts';
+ *
+ * // After transform
+ * function assertIs() {}
+ * function assertIsNonNull() {}
+ * ```
+ *
+ * Minifier can already remove all calls to these functions as dead code, but only if the functions are defined
+ * in the same file as the call sites.
+ *
+ * Problem is that `asserts.ts` is imported by files which end up in all output chunks.
+ * So without this transform, TSDown creates a shared chunk for `asserts.ts`. Minifier works chunk-by-chunk,
+ * so can't see that these functions are no-ops, and doesn't remove the function calls.
+ *
+ * Inlining these functions in each file solves the problem, and minifier removes all trace of them.
+ *
+ * @returns Plugin
+ */
+function createReplaceAssertsPlugin(): Plugin {
+  return {
+    name: 'replace-asserts',
+    transform: {
+      // Only process TS files in `src-js` directory
+      filter: { id: /\/src-js\/.+\.ts$/ },
+
+      async handler(code, id, meta) {
+        const magicString = meta.magicString!;
+        const program = this.parse(code, { lang: 'ts' });
+
+        stmts: for (const stmt of program.body) {
+          if (stmt.type !== 'ImportDeclaration') continue;
+
+          // Check if import is from `utils/asserts.ts`.
+          // `endsWith` check is just a shortcut to avoid resolving the specifier to a full path for most imports.
+          const source = stmt.source.value;
+          if (!source.endsWith('/asserts.ts') && !source.endsWith('/asserts.js')) continue;
+          // oxlint-disable-next-line no-await-in-loop
+          const importedId = await this.resolve(source, id);
+          if (importedId === null || importedId.id !== ASSERTS_PATH) continue;
+
+          // Replace `import` statement with empty function declarations
+          let functionsCode = '';
+          for (const specifier of stmt.specifiers) {
+            // Skip this `import` statement if it's a default or namespace import - can't handle those
+            if (specifier.type !== 'ImportSpecifier') continue stmts;
+            functionsCode += `function ${specifier.local.name}() {}\n`;
+          }
+          magicString.overwrite(stmt.start, stmt.end, functionsCode);
+        }
+
+        return { code: magicString };
+      },
+    },
+  };
+}
