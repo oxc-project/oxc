2d09fde08921a01abca039fea6865d5fc6f0318b
Connor Shea <connor.james.shea@gmail.com>
2025-11-22 17:01:25 -0700

    docs(linter): Add config docs for unicorn/catch-error-name rule.
    This shows that we can add configuration documentation a rule with Regex options! :D

--- crates/oxc_linter/tests/rule_configuration_documentation_test.rs
+++ crates/oxc_linter/tests/rule_configuration_documentation_test.rs
@@ -55,7 +55,6 @@ fn test_rules_with_custom_configuration_have_schema() {
         "typescript/ban-ts-comment",
         "typescript/consistent-type-imports",
         // unicorn
-        "unicorn/catch-error-name",
         "unicorn/filename-case",
     ];
 
