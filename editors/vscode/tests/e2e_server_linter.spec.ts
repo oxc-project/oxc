864e35b9285b09d03d80e6fc368bb7f7e0e21a81
liangmiQwQ <github@liangmi.dev>
2025-11-26 21:35:50 +0800

    test: update testing

--- editors/vscode/tests/e2e_server_linter.spec.ts
+++ editors/vscode/tests/e2e_server_linter.spec.ts
@@ -282,4 +282,18 @@ suite('E2E Server Linter', () => {
     const secondDiagnostics = await getDiagnostics('index.ts');
     strictEqual(secondDiagnostics.length, 1);
   });
+
+  testSingleFolderMode('changing oxc.enable will update the client status', async () => {
+    await loadFixture('changing_enable');
+
+    const firstDiagnostics = await getDiagnostics('debugger.js');
+    strictEqual(firstDiagnostics.length, 1);
+
+    await workspace.getConfiguration('oxc').update('enable', false);
+    await workspace.saveAll();
+    await waitForDiagnosticChange();
+
+    const secondDiagnostics = await getDiagnostics('debugger.js');
+    strictEqual(secondDiagnostics.length, 0);
+  })
 });
