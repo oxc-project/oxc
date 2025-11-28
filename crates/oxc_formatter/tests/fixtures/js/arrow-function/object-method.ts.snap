oxc-project/oxc

commit 889d2e756b9bddb9a3c50270b6d189894bc8e521

fix(formatter): handle poor layout for grouped call arguments (#16093)
This PR is a follow-up on #16036

Move `PoorLayout` handling from `Function` and `ArrowFunctionExpression` to call arguments

crates/oxc_formatter/src/write/arrow_function_expression.rs
@@ -200,56 +200,27 @@ impl<'a, 'b> FormatJsArrowFunctionExpression<'a, 'b> {
                         || (matches!(self.arrow.parent, AstNodes::JSXExpressionContainer(container)
                             if !f.context().comments().has_comment_in_range(arrow.span.end, container.span.end)));
 
-                    let body_is_condition_type =
-                        matches!(arrow_expression, Some(Expression::ConditionalExpression(_)));
+                    write!(
+                        f,
+                        [
+                            formatted_signature,
+                            group(&format_args!(
+                                soft_line_indent_or_hard_space(&format_with(|f| {
+                                    if should_add_parens {
+                                        write!(f, if_group_fits_on_line(&"("));
+                                    }
 
-                    if body_is_condition_type {
-                        write!(
-                            f,
-                            [
-                                formatted_signature,
-                                group(&format_args!(
-                                    soft_line_indent_or_hard_space(&format_with(|f| {
-                                        if should_add_parens {
-                                            write!(f, if_group_fits_on_line(&"("));
-                                        }
+                                    write!(f, format_body);
 
-                                        write!(f, format_body);
-
-                                        if should_add_parens {
-                                            write!(f, if_group_fits_on_line(&")"));
-                                        }
-                                    })),
-                                    is_last_call_arg
-                                        .then_some(format_args!(FormatTrailingCommas::All,)),
-                                    should_add_soft_line.then_some(format_args!(soft_line_break()))
-                                ))
-                            ]
-                        );
-                    } else {
-                        write!(
-                            f,
-                            [
-                                formatted_signature,
-                                group(&format_args!(
-                                    soft_line_indent_or_space(&format_with(|f| {
-                                        if should_add_parens {
-                                            write!(f, if_group_fits_on_line(&"("));
-                                        }
-
-                                        write!(f, format_body);
-
-                                        if should_add_parens {
-                                            write!(f, if_group_fits_on_line(&")"));
-                                        }
-                                    })),
-                                    is_last_call_arg
-                                        .then_some(format_args!(FormatTrailingCommas::All,)),
-                                    should_add_soft_line.then_some(format_args!(soft_line_break()))
-                                ))
-                            ]
-                        );
-                    }
+                                    if should_add_parens {
+                                        write!(f, if_group_fits_on_line(&")"));
+                                    }
+                                })),
+                                is_last_call_arg.then_some(&FormatTrailingCommas::All),
+                                should_add_soft_line.then_some(soft_line_break())
+                            ))
+                        ]
+                    );
                 }
             }
         }
@@ -295,17 +266,18 @@ impl<'a, 'b> ArrowFunctionLayout<'a, 'b> {
         let mut middle = Vec::new();
         let mut current = arrow;
         let mut should_break = false;
+        let is_non_grouped_or_grouped_last_argument = matches!(
+            options.call_argument_layout,
+            None | Some(GroupedCallArgumentLayout::GroupedLastArgument)
+        );
 
         loop {
-            if current.expression()
+            if is_non_grouped_or_grouped_last_argument
+                && current.expression()
                 && let Some(AstNodes::ExpressionStatement(expr_stmt)) =
                     current.body().statements().first().map(AstNode::<Statement>::as_ast_nodes)
                 && let AstNodes::ArrowFunctionExpression(next) =
                     &expr_stmt.expression().as_ast_nodes()
-                && matches!(
-                    options.call_argument_layout,
-                    None | Some(GroupedCallArgumentLayout::GroupedLastArgument)
-                )
             {
                 should_break = should_break || Self::should_break_chain(current);
 
@@ -710,11 +682,11 @@ fn has_rest_object_or_array_parameter(params: &FormalParameters) -> bool {
 
 /// Writes the arrow function type parameters, parameters, and return type annotation.
 ///
-/// Formats the parameters and return type annotation without any soft line breaks if `is_first_or_last_call_argument` is `true`
+/// Formats the parameters and return type annotation without any soft line breaks if `is_grouped_call_argument` is `true`
 /// so that the parameters and return type are kept on the same line.
 fn format_signature<'a, 'b>(
     arrow: &'b AstNode<'a, ArrowFunctionExpression<'a>>,
-    is_first_or_last_call_argument: bool,
+    is_grouped_call_argument: bool,
     is_first_in_chain: bool,
     cache_mode: FunctionCacheMode,
 ) -> impl Format<'a> + 'b {
@@ -735,15 +707,13 @@ fn format_signature<'a, 'b>(
         });
         let format_head = FormatContentWithCacheMode::new(arrow.params.span, content, cache_mode);
 
-        if is_first_or_last_call_argument {
-            let mut buffer = RemoveSoftLinesBuffer::new(f);
-            let mut recording = buffer.start_recording();
-
-            write!(recording, format_head);
-
-            if recording.stop().will_break() {
-                // TODO: figure out
-                // return Err(FormatError::PoorLayout);
+        if is_grouped_call_argument {
+            // The first arrow's soft lines have already been removed in the CallArguments.
+            if is_first_in_chain {
+                write!(f, format_head);
+            } else {
+                let mut buffer = RemoveSoftLinesBuffer::new(f);
+                write!(buffer, format_head);
             }
         } else {
             write!(

crates/oxc_formatter/src/write/call_arguments.rs
@@ -7,7 +7,9 @@ use crate::{
     ast_nodes::{AstNode, AstNodes},
     format_args,
     formatter::{
-        Comments, FormatElement, Formatter, SourceText, VecBuffer, format_element,
+        Comments, FormatElement, Formatter, SourceText, VecBuffer,
+        buffer::RemoveSoftLinesBuffer,
+        format_element,
         prelude::{
             FormatElements, Tag, empty_line, expand_parent, format_once, format_with, group,
             soft_block_indent, soft_line_break_or_space, space,
@@ -634,7 +636,13 @@ fn write_grouped_arguments<'a>(
             let is_grouped_argument = (group_layout.is_grouped_first() && index == 0)
                 || (group_layout.is_grouped_last() && index == last_index);
 
-            let format_argument = format_once(|f| {
+            // We have to get the lines before the argument has been formatted, because it relies on
+            // the comments before the argument. After formatting, the comments might marked as printed,
+            // which would lead to a wrong line count.
+            let lines_before = f.source_text().get_lines_before(argument.span(), f.comments());
+            let comma = (last_index != index).then_some(",");
+
+            let interned = f.intern(&format_once(|f| {
                 if is_grouped_argument {
                     match argument.as_ast_nodes() {
                         AstNodes::Function(function)
@@ -643,40 +651,40 @@ fn write_grouped_arguments<'a>(
                                     || function_has_only_simple_parameters(&function.params)) =>
                         {
                             has_cached = true;
-                            return FormatFunction::new_with_options(
-                                function,
-                                FormatFunctionOptions {
-                                    cache_mode: FunctionCacheMode::Cache,
-                                    ..FormatFunctionOptions::default()
-                                },
-                            )
-                            .fmt(f);
+                            return write!(
+                                f,
+                                [
+                                    FormatFunction::new_with_options(
+                                        function,
+                                        FormatFunctionOptions {
+                                            cache_mode: FunctionCacheMode::Cache,
+                                            ..FormatFunctionOptions::default()
+                                        },
+                                    ),
+                                    comma
+                                ]
+                            );
                         }
                         AstNodes::ArrowFunctionExpression(arrow) => {
                             has_cached = true;
-                            return FormatJsArrowFunctionExpression::new_with_options(
-                                arrow,
-                                FormatJsArrowFunctionExpressionOptions {
-                                    cache_mode: FunctionCacheMode::Cache,
-                                    ..FormatJsArrowFunctionExpressionOptions::default()
-                                },
-                            )
-                            .fmt(f);
+                            return write!(
+                                f,
+                                [
+                                    FormatJsArrowFunctionExpression::new_with_options(
+                                        arrow,
+                                        FormatJsArrowFunctionExpressionOptions {
+                                            cache_mode: FunctionCacheMode::Cache,
+                                            ..FormatJsArrowFunctionExpressionOptions::default()
+                                        },
+                                    ),
+                                    comma
+                                ]
+                            );
                         }
                         _ => {}
                     }
                 }
-                argument.fmt(f);
-            });
-
-            // We have to get the lines before the argument has been formatted, because it relies on
-            // the comments before the argument. After formatting, the comments might marked as printed,
-            // which would lead to a wrong line count.
-            let lines_before = f.source_text().get_lines_before(argument.span(), f.comments());
-
-            let interned = f.intern(&format_with(|f| {
-                format_argument.fmt(f);
-                write!(f, (last_index != index).then_some(","));
+                write!(f, [argument, comma]);
             }));
 
             let break_type =
@@ -715,46 +723,60 @@ fn write_grouped_arguments<'a>(
     // as first or last argument.
     let mut grouped = elements;
     if has_cached {
-        match group_layout {
+        let (argument, grouped_element) = match group_layout {
             GroupedCallArgumentLayout::GroupedFirstArgument => {
-                let argument = node.first().unwrap();
-                let interned = f.intern(&format_with(|f| {
-                    FormatGroupedFirstArgument { argument }.fmt(f);
-                    write!(f, (last_index != 0).then_some(","));
-                }));
-
-                // Turns out, using the grouped layout isn't a good fit because some parameters of the
-                // grouped function or arrow expression break. In that case, fall back to the all args expanded
-                // formatting.
-                // This back tracking is required because testing if the grouped argument breaks would also return `true`
-                // if any content of the function body breaks. But, as far as this is concerned, it's only interested if
-                // any content in the signature breaks.
-                // TODO: should figure out
-                // if matches!(interned, Err(FormatError::PoorLayout)) {
-                //     return format_all_elements_broken_out(node, grouped.into_iter(), true, f);
-                // }
-
-                grouped.first_mut().unwrap().0 = interned;
+                (node.first().unwrap(), &mut grouped.first_mut().unwrap().0)
             }
             GroupedCallArgumentLayout::GroupedLastArgument => {
-                let argument = node.last().unwrap();
-                let interned = f.intern(&format_once(|f| {
-                    FormatGroupedLastArgument { argument, is_only: only_one_argument }.fmt(f);
-                }));
-
-                // Turns out, using the grouped layout isn't a good fit because some parameters of the
-                // grouped function or arrow expression break. In that case, fall back to the all args expanded
-                // formatting.
-                // This back tracking is required because testing if the grouped argument breaks would also return `true`
-                // if any content of the function body breaks. But, as far as this is concerned, it's only interested if
-                // any content in the signature breaks.
-                // TODO: should figure out
-                // if matches!(interned, Err(FormatError::PoorLayout)) {
-                //     return format_all_elements_broken_out(node, grouped.into_iter(), true, f);
-                // }
-
-                grouped.last_mut().unwrap().0 = interned;
+                (node.last().unwrap(), &mut grouped.last_mut().unwrap().0)
             }
+        };
+
+        let function_params = match argument.as_ast_nodes() {
+            AstNodes::ArrowFunctionExpression(arrow) => Some(&arrow.params),
+            AstNodes::Function(function) => Some(&function.params),
+            _ => None,
+        };
+
+        // Turns out, using the grouped layout isn't a good fit because some parameters of the
+        // grouped function or arrow expression break. In that case, fall back to the all args expanded
+        // formatting.
+        // This back tracking is required because testing if the grouped argument breaks would also return `true`
+        // if any content of the function body breaks. But, as far as this is concerned, it's only interested if
+        // any content in the signature breaks.
+        //
+        // <https://github.com/biomejs/biome/blob/98ca2ae9f3b9b25a14d63b243223583aba6e4907/crates/biome_js_formatter/src/js/expressions/call_arguments.rs#L466-L482>
+        if let Some(params) = function_params {
+            let Some(cached_element) = f.context().get_cached_element(params.as_ref()) else {
+                unreachable!(
+                    "The parameters should have already been formatted and cached in the `FormatFunction` or `FormatJsArrowFunctionExpression`"
+                );
+            };
+
+            // Remove soft lines from the cached parameters and check if they would break.
+            // If they break even without soft lines, we need to use the expanded layout.
+            let interned = f.intern(&format_once(|f| {
+                RemoveSoftLinesBuffer::new(f).write_element(cached_element);
+            }));
+
+            if let Some(interned) = interned {
+                if interned.will_break() {
+                    return format_all_elements_broken_out(node, grouped.into_iter(), true, f);
+                }
+
+                // No break; it should print the element without soft lines.
+                // It would be used in the `FormatFunction` or `FormatJsArrowFunctionExpression`.
+                f.context_mut().cache_element(params.as_ref(), interned);
+            }
+        }
+
+        *grouped_element = if group_layout.is_grouped_first() {
+            f.intern(&format_with(|f| {
+                FormatGroupedFirstArgument { argument }.fmt(f);
+                write!(f, (last_index != 0).then_some(","));
+            }))
+        } else {
+            f.intern(&FormatGroupedLastArgument { argument, is_only: only_one_argument })
         }
     }
 

crates/oxc_formatter/src/write/function.rs
@@ -10,9 +10,7 @@ use super::{
 use crate::{
     ast_nodes::AstNode,
     format_args,
-    formatter::{
-        Buffer, Formatter, buffer::RemoveSoftLinesBuffer, prelude::*, trivia::FormatLeadingComments,
-    },
+    formatter::{Buffer, Formatter, prelude::*, trivia::FormatLeadingComments},
     write,
     write::{
         arrow_function_expression::FormatMaybeCachedFunctionBody, semicolon::OptionalSemicolon,
@@ -84,24 +82,6 @@ impl<'a, 'b> FormatFunction<'a, 'b> {
             self.params(),
             self.options.cache_mode,
         )
-        .memoized();
-
-        let format_parameters = format_with(|f: &mut Formatter<'_, 'a>| {
-            if self.options.call_argument_layout.is_some() {
-                let mut buffer = RemoveSoftLinesBuffer::new(f);
-
-                let mut recording = buffer.start_recording();
-                write!(recording, format_parameters);
-                // let recorded = recording.stop();
-
-                // TODO: figure out
-                // if recorded.will_break() {
-                //     return Err(FormatError::PoorLayout);
-                // }
-            } else {
-                format_parameters.fmt(f);
-            }
-        })
         .memoized();
 
         let format_return_type = self

commit cd70484b78d9a3066707103594e2f5e934ee01d3

fix(formatter): should not add a hard space before function body (#16221)
This is a regression I found in the VSCode repo, caused by #16093. This is a mistake I made, as we should never use a hard space.

crates/oxc_formatter/src/formatter/builders.rs
@@ -1452,6 +1452,7 @@ pub fn soft_line_indent_or_space<'ast>(content: &impl Format<'ast>) -> BlockInde
 /// # }
 /// ```
 #[inline]
+#[expect(unused)]
 pub fn soft_line_indent_or_hard_space<'ast>(content: &impl Format<'ast>) -> BlockIndent<'_, 'ast> {
     BlockIndent { content: Argument::new(content), mode: IndentMode::HardSpace }
 }

crates/oxc_formatter/src/write/arrow_function_expression.rs
@@ -205,7 +205,7 @@ impl<'a, 'b> FormatJsArrowFunctionExpression<'a, 'b> {
                         [
                             formatted_signature,
                             group(&format_args!(
-                                soft_line_indent_or_hard_space(&format_with(|f| {
+                                soft_line_indent_or_space(&format_with(|f| {
                                     if should_add_parens {
                                         write!(f, if_group_fits_on_line(&"("));
                                     }

crates/oxc_formatter/tests/fixtures/js/arrow-function/object-method.ts
@@ -1,0 +1,7 @@
+let object = {
+	id: '_files.pickFolderAndOpen',
+	handler: (
+		accessor: ServicesAccessor,
+		options: { forceNewWindow: boolean }
+	) => accessor.get(IFileDialogService).pickFolderAndOpen(options)
+}

crates/oxc_formatter/tests/fixtures/js/arrow-function/object-method.ts.snap
@@ -1,0 +1,31 @@
+---
+source: crates/oxc_formatter/tests/fixtures/mod.rs
+---
+==================== Input ====================
+let object = {
+	id: '_files.pickFolderAndOpen',
+	handler: (
+		accessor: ServicesAccessor,
+		options: { forceNewWindow: boolean }
+	) => accessor.get(IFileDialogService).pickFolderAndOpen(options)
+}
+==================== Output ====================
+------------------
+{ printWidth: 80 }
+------------------
+let object = {
+  id: "_files.pickFolderAndOpen",
+  handler: (accessor: ServicesAccessor, options: { forceNewWindow: boolean }) =>
+    accessor.get(IFileDialogService).pickFolderAndOpen(options),
+};
+
+-------------------
+{ printWidth: 100 }
+-------------------
+let object = {
+  id: "_files.pickFolderAndOpen",
+  handler: (accessor: ServicesAccessor, options: { forceNewWindow: boolean }) =>
+    accessor.get(IFileDialogService).pickFolderAndOpen(options),
+};
+
+===================== End =====================