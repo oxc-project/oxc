fd02931e842a4efe590ae97c4229ac5964e4b222
leaysgur <6259812+leaysgur@users.noreply.github.com>
2025-11-28 09:04:25 +0000

    test(formatter): Add tests for #16201 (#16233)
    Closes https://github.com/oxc-project/oxc/issues/16201
    
    Bug itself was already fixed by https://github.com/oxc-project/oxc/pull/16221

--- crates/oxc_formatter/tests/fixtures/ts/arrow-function/object-method.ts
+++ crates/oxc_formatter/tests/fixtures/ts/arrow-function/object-method.ts
@@ -5,3 +5,8 @@ let object = {
 		options: { forceNewWindow: boolean }
 	) => accessor.get(IFileDialogService).pickFolderAndOpen(options)
 }
+
+// https://github.com/oxc-project/oxc/issues/16201
+//34567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+//     10|       20|       30|       40|       50|       60|       70|       80|       90|      100|
+const xxxxxxxxxxx = async (slug: string, startItem: string, preview: boolean) => 1;
