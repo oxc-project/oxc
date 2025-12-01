a07eec1a831df6a94f0f7abd6b1f388b42540036
Dunqing <29533304+Dunqing@users.noreply.github.com>
2025-12-01 13:22:34 +0000

    fix(formatter): incorrect indentation for class extends and implements (#16334)
    Align output with [Prettier](https://prettier.io/playground/#N4Igxg9gdgLgprEAuEAdKAbOMAEHoDmASgK5RQCWUBACgE4QBuFAJnHTgLw5RwDuOABRgMAQwDO49DhwUAtgAcschDHF5R1EqIJxxAOgDKFAlFEwSdOAAk4GBfSat26YAF8AlAG506LLnxqUnIqWgZmNjoAJi4efiERCSkoGTgAD3goFnUxLR09IxMzCytbe0cIlyh3b19MbDxCYMpqCuc6AGZY3gFhMUlpHHTM7I083QNjU3NLGzsHcPaAHjL8AD5XTx8oPwbA4jIWsKdIgBZu+L6kwfklOBVYHM0CbQnC6ZK58sXIlbt1za1HbA3Ywfb7ZqhNqRWKJAYpWSKZSqJ7jApTYqzMoLE5VdzbUHgpqHKE-dgxbhw5KpDIIUa5F75SZFGalebQvFuAn1MGECEk1pkzqw-rUoa0rKoxlvDGsr44yp0P4YAHVLl1fxEoIC46K86U0U3JH3FFjaXolmfbEcpWrCAbNXbEAAGhAEAUMAo0HEyFAojoDD4NH9CB9KFEGD4ogAnj7XQAjOiiMAAa2whlEKgAMlQ4MgAGYR8RwV0QeMAKzgYBgAHUkwpkCAFFZi3RGHmE0nU+mFMnQsgYHQSCWQMW5BQB0OR+kFOx5KoIwB5WdJmAQOg0CDiCie6CNukukAzucPGARgAq7DMdAoegLRZH2+oWAAiiQIPB7xhi67y+I0sYz5wG+H55kghbfiOACO77wI4ChhiAEgALS8HAbAsIeg6iBQGChAAwhAchyKIjYRhgh5PgQWAAIIwIOFDxiQcHsDmvBfj+IAABYwHIGA1lxO56L2YBwIYoY7hQzAwNGjZgJIh6MMOACSWSqIYYA3h6NFZIYMlYBxI7NlucB1qIDYoM2ejsO2h50ou+ZsWBIAYPmh5UK2MD0DoJGGa6vZ0K2jbxqI8Z2MhaiHs2VC1qwMBccgAAcAAMrpWDBFBWN5BC+eBD6ume8Y1nFCVIFErokMW56hWGEGcfcYUsBhWbPK8cAAGLriR9H9uGzEQCAbhuEAA)
    
    Some cases' output is not good. I will create an issue in Prettier later

--- crates/oxc_formatter/tests/fixtures/ts/class/issue-16259.ts
+++ crates/oxc_formatter/tests/fixtures/ts/class/issue-16259.ts
@@ -3,4 +3,35 @@ export class longlonglonglonglonglonglonglonglonglonglongclassname
 
 export interface longlonglonglonglonglonglonglonglonglonglongclassname
   extends someobjectsomepropertysomeotherproperty.SomeClass {}
-  
+
+let longRunningProvider = new (class
+  implements languages.SignatureHelpProvider
+{});
+
+let longRunningProvider2 = new (class
+  extends languages.SignatureHelpProvider
+{});
+
+let longRunningProvider3 = new (class
+  extends languages.SignatureHelpProvider<Hello>
+{});
+
+let longRunningProvider4 = new (class
+  implements languages.SignatureHelpProvider<Hello>
+{});
+
+letlonglongRunningProvider = class
+  implements languages.SignatureHelpProvider
+{};
+
+letlonglongRunningProvider2 = class
+  extends languages.SignatureHelpProvider
+{};
+
+letlonglongRunningProvider3 = class
+  extends languages.SignatureHelpProvider<Hello>
+{};
+
+letlonglongRunningProvider4 = class
+  implements languages.SignatureHelpProvider<Hello>
+{};
