102f1b899f9e5d91a532dd9d9ffb29186b45f334
Boshen <boshenc@gmail.com>
2025-12-10 17:12:16 +0800

    feat(oxfmt): override all js formatting in prettier POC [ci skip]
    `pnpm --filter oxfmt-app build-dev && node ./apps/oxfmt/dist/cli.js -c oxfmtrc.jsonc test.vue`

---
+++ test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <header class="header">
+      <h1>{{ formatted by oxfmt expr }}</h1>
+      <p v-if="formatted by oxfmt expr">{{ formatted by oxfmt expr }}</p>
+
+      <!-- Slots -->
+      <slot name="actions" />
+
+      <!-- v-for with key + destructuring -->
+      <ul>
+        <li v-for="formatted by oxfmt in formatted by oxfmt expr" :key="formatted by oxfmt expr">
+          {{ formatted by oxfmt expr }}. {{ formatted by oxfmt expr }}
+        </li>
+      </ul>
+    </header>
+  </div>
+</template>
+
+<script lang="ts" setup>
+formatted by oxfmt
+</script>
