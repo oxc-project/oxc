3f7983e77a45e9a9445fdb370caf2e766a371868
Said Atrahouch <said.atrahouch@gmail.com>
2025-12-14 16:17:01 +0100

    feat(linter/eslint-plugin-vitest): Add prefer-to-contain as vitest compatible jest rule

--- crates/oxc_linter/src/utils/mod.rs
+++ crates/oxc_linter/src/utils/mod.rs
@@ -33,7 +33,7 @@ pub use self::{
 /// List of Jest rules that have Vitest equivalents.
 // When adding a new rule to this list, please ensure oxlint-migrate is also updated.
 // See https://github.com/oxc-project/oxlint-migrate/blob/2c336c67d75adb09a402ae66fb3099f1dedbe516/scripts/constants.ts
-const VITEST_COMPATIBLE_JEST_RULES: [&str; 36] = [
+const VITEST_COMPATIBLE_JEST_RULES: [&str; 37] = [
     "consistent-test-it",
     "expect-expect",
     "max-expects",
@@ -64,6 +64,7 @@ const VITEST_COMPATIBLE_JEST_RULES: [&str; 36] = [
     "prefer-mock-promise-shorthand",
     "prefer-strict-equal",
     "prefer-to-be",
+    "prefer-to-contain",
     "prefer-to-have-length",
     "prefer-todo",
     "require-to-throw-message",
