75f9800166e3aae7a1cc17516b0549992b41c245
overlookmotel <557937+overlookmotel@users.noreply.github.com>
2025-12-14 21:30:55 +0000

    test(parser): combine test cases for `let` as identifier (#16861)
    Follow-on after #16840 and #16857. We had a few test cases relating to the same thing, and one of them called the undescriptive `oxc.js`. Combine them, and rename the test case files with a more accurate name.

--- tasks/coverage/misc/fail/let-optional-chaining.js
+++
@@ -1,5 +0,0 @@
-// Test that `let` cannot be used as identifier with optional chaining in module
-let?.x;
-let?.y.z;
-let?.[0];
-let?.method();
--- tasks/coverage/misc/pass/let-optional-chaining.cjs
+++
@@ -1,5 +0,0 @@
-// Test that `let` can be used as identifier with optional chaining in script
-let?.x;
-let?.y.z;
-let?.[0];
-let?.method();
