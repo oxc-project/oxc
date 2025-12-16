dece1faf598b9c84e125a20f4b54a54d948f1ad5
connorshea <2977353+connorshea@users.noreply.github.com>
2025-12-16 10:49:58 +0000

    chore(linter): Update the GitHub URL for the vitest eslint plugin repo. (#16941)
    This needed to be updated in a few different places.

--- crates/oxc_linter/src/rules/jest/no_alias_methods.rs
+++ crates/oxc_linter/src/rules/jest/no_alias_methods.rs
@@ -60,7 +60,7 @@ declare_oxc_lint!(
     /// expect(a).toThrow()
     /// ```
     ///
-    /// This rule is compatible with [eslint-plugin-vitest](https://github.com/veritem/eslint-plugin-vitest/blob/v1.1.9/docs/rules/no-alias-methods.md),
+    /// This rule is compatible with [eslint-plugin-vitest](https://github.com/vitest-dev/eslint-plugin-vitest/blob/v1.1.9/docs/rules/no-alias-methods.md),
     /// to use it, add the following configuration to your `.oxlintrc.json`:
     ///
     /// ```json
