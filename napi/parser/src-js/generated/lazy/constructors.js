oxc-project/oxc

commit 5d211a8c69b6c39f0cfd4c289dfca4e1318a3e13

refactor(napi/parser): move generated JS into `src-js` (#16957)
moves the generated code into src-js to keep the same pattern as oxlint

napi/parser/src-js/generated/lazy/constructors.js
@@ -1,8 +1,8 @@
 // Auto-generated code, DO NOT EDIT DIRECTLY!
 // To edit this generated file you have to edit `tasks/ast_tools/src/generators/raw_transfer_lazy.rs`.
 
-import { constructorError, TOKEN } from "../../src-js/raw-transfer/lazy-common.js";
-import { NodeArray } from "../../src-js/raw-transfer/node-array.js";
+import { constructorError, TOKEN } from "../raw-transfer/lazy-common.js";
+import { NodeArray } from "../raw-transfer/node-array.js";
 
 const textDecoder = new TextDecoder("utf-8", { ignoreBOM: true }),
   decodeStr = textDecoder.decode.bind(textDecoder),

tasks/ast_tools/src/generators/raw_transfer_lazy.rs
@@ -37,15 +37,15 @@ impl Generator for RawTransferLazyGenerator {
 
         vec![
             Output::Javascript {
-                path: format!("{NAPI_PARSER_PACKAGE_PATH}/generated/lazy/constructors.js"),
+                path: format!("{NAPI_PARSER_PACKAGE_PATH}/src-js/generated/lazy/constructors.js"),
                 code: constructors,
             },
             Output::Javascript {
-                path: format!("{NAPI_PARSER_PACKAGE_PATH}/generated/lazy/walk.js"),
+                path: format!("{NAPI_PARSER_PACKAGE_PATH}/src-js/generated/lazy/walk.js"),
                 code: walkers,
             },
             Output::Javascript {
-                path: format!("{NAPI_PARSER_PACKAGE_PATH}/generated/lazy/type_ids.js"),
+                path: format!("{NAPI_PARSER_PACKAGE_PATH}/src-js/generated/lazy/type_ids.js"),
                 code: node_type_ids_map,
             },
         ]
@@ -157,8 +157,8 @@ fn generate(
     let constructors = &state.constructors;
     #[rustfmt::skip]
     let constructors = format!("
-        import {{ constructorError, TOKEN }} from '../../src-js/raw-transfer/lazy-common.js';
-        import {{ NodeArray }} from '../../src-js/raw-transfer/node-array.js';
+        import {{ constructorError, TOKEN }} from '../raw-transfer/lazy-common.js';
+        import {{ NodeArray }} from '../raw-transfer/node-array.js';
 
         const textDecoder = new TextDecoder('utf-8', {{ ignoreBOM: true }}),
             decodeStr = textDecoder.decode.bind(textDecoder),

commit e59132b6c0d0a53286dd11ec515d4ba913b7fac1

fix(parser/napi): fix lazy deser (#17069)
Lazy deserialization in `oxc-parser` was broken by #16957. Fix it.

napi/parser/src-js/generated/lazy/constructors.js
@@ -1,8 +1,8 @@
 // Auto-generated code, DO NOT EDIT DIRECTLY!
 // To edit this generated file you have to edit `tasks/ast_tools/src/generators/raw_transfer_lazy.rs`.
 
-import { constructorError, TOKEN } from "../raw-transfer/lazy-common.js";
-import { NodeArray } from "../raw-transfer/node-array.js";
+import { constructorError, TOKEN } from "../../raw-transfer/lazy-common.js";
+import { NodeArray } from "../../raw-transfer/node-array.js";
 
 const textDecoder = new TextDecoder("utf-8", { ignoreBOM: true }),
   decodeStr = textDecoder.decode.bind(textDecoder),

tasks/ast_tools/src/generators/raw_transfer_lazy.rs
@@ -157,8 +157,8 @@ fn generate(
     let constructors = &state.constructors;
     #[rustfmt::skip]
     let constructors = format!("
-        import {{ constructorError, TOKEN }} from '../raw-transfer/lazy-common.js';
-        import {{ NodeArray }} from '../raw-transfer/node-array.js';
+        import {{ constructorError, TOKEN }} from '../../raw-transfer/lazy-common.js';
+        import {{ NodeArray }} from '../../raw-transfer/node-array.js';
 
         const textDecoder = new TextDecoder('utf-8', {{ ignoreBOM: true }}),
             decodeStr = textDecoder.decode.bind(textDecoder),