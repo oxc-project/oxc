oxc-project/oxc

commit 61f2138e698bd2e65486f4bcea677963734e784a

refactor(linter): reduce repeated logic in JS plugin linting (#17086)
Follow-on after #17013. Pure refactor. Move logic for getting `AllocatorPool` into a function `js_allocator_pool`, to avoid lots of `#[cfg]` stuff.

crates/oxc_linter/src/service/runtime.rs
@@ -263,6 +263,17 @@ impl Runtime {
         }
     }
 
+    /// Get [`AllocatorPool`] for copying ASTs to fixed-size allocators, if one if required.
+    fn js_allocator_pool(&self) -> Option<&AllocatorPool> {
+        #[cfg(all(target_pointer_width = "64", target_endian = "little"))]
+        let pool = self.js_allocator_pool.as_ref();
+
+        #[cfg(not(all(target_pointer_width = "64", target_endian = "little")))]
+        let pool = None;
+
+        pool
+    }
+
     pub fn set_disable_directives_map(
         &mut self,
         map: Arc<Mutex<FxHashMap<PathBuf, DisableDirectives>>>,
@@ -627,17 +638,12 @@ impl Runtime {
                             return;
                         }
 
-                        #[cfg(all(target_pointer_width = "64", target_endian = "little"))]
-                        let js_pool = me.js_allocator_pool.as_ref();
-                        #[cfg(not(all(target_pointer_width = "64", target_endian = "little")))]
-                        let js_pool = None::<&AllocatorPool>;
-
                         let (mut messages, disable_directives) =
                             me.linter.run_with_disable_directives(
                                 path,
                                 context_sub_hosts,
                                 allocator_guard,
-                                js_pool,
+                                me.js_allocator_pool(),
                             );
 
                         // Store the disable directives for this file
@@ -751,14 +757,9 @@ impl Runtime {
 
                         let path = Path::new(&module_to_lint.path);
 
-                        #[cfg(all(target_pointer_width = "64", target_endian = "little"))]
-                        let js_pool = me.js_allocator_pool.as_ref();
-                        #[cfg(not(all(target_pointer_width = "64", target_endian = "little")))]
-                        let js_pool = None::<&AllocatorPool>;
-
                         let (section_messages, disable_directives) = me
                             .linter
-                            .run_with_disable_directives(path, context_sub_hosts, allocator_guard, js_pool);
+                            .run_with_disable_directives(path, context_sub_hosts, allocator_guard, me.js_allocator_pool());
 
                         if let Some(disable_directives) = disable_directives {
                             me.disable_directives_map

commit 6d053b4b4b3487d79ea20e6e92ad2dbdce0a4f7b

docs(linter): fix typo in doc comment (#17091)
Fix a typo that I introduced in #17086.

crates/oxc_linter/src/service/runtime.rs
@@ -263,7 +263,7 @@ impl Runtime {
         }
     }
 
-    /// Get [`AllocatorPool`] for copying ASTs to fixed-size allocators, if one if required.
+    /// Get [`AllocatorPool`] for copying ASTs to fixed-size allocators, if one is required.
     fn js_allocator_pool(&self) -> Option<&AllocatorPool> {
         #[cfg(all(target_pointer_width = "64", target_endian = "little"))]
         let pool = self.js_allocator_pool.as_ref();