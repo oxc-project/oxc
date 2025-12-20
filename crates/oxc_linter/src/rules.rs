c657dc09834a56e2b77bb0f173655be35546f18f
Tu Shaokun <2801884530@qq.com>
2025-12-20 12:22:32 +0800

    feat(linter): implement jest/valid-expect-in-promise rule

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -350,6 +350,7 @@ pub(crate) mod jest {
     pub mod require_top_level_describe;
     pub mod valid_describe_callback;
     pub mod valid_expect;
+    pub mod valid_expect_in_promise;
     pub mod valid_title;
 }
 
@@ -944,6 +945,7 @@ oxc_macros::declare_all_lint_rules! {
     jest::require_top_level_describe,
     jest::valid_describe_callback,
     jest::valid_expect,
+    jest::valid_expect_in_promise,
     jest::valid_title,
     jsdoc::check_access,
     jsdoc::check_property_names,
