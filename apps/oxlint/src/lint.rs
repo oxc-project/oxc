88b4a7d4cd796a810872357560297ab20e338bbb
Kenzo-Wada <used.acormhib@gmail.com>
2025-12-23 00:47:38 +0900

    update

--- apps/oxlint/src/lint.rs
+++ apps/oxlint/src/lint.rs
@@ -1461,7 +1461,7 @@ export { redundant };
             "// This file has a fixable tsgolint error: no-unnecessary-type-assertion
 // The type assertion `as string` is unnecessary because str is already a string
 const str: string = 'hello';
-const redundant = str  ;
+const redundant = str;
 
 export { redundant };
 ",
