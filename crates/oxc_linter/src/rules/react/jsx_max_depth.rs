73fe0702e83ecc806b64f8970dafd3ec34c46085
Cameron Clark <cameron.clark@hey.com>
2025-12-22 15:55:24 +0000

    re-port test cases

--- crates/oxc_linter/src/rules/react/jsx_max_depth.rs
+++ crates/oxc_linter/src/rules/react/jsx_max_depth.rs
@@ -19,7 +19,7 @@ fn jsx_max_depth_diagnostic(depth: usize, max: usize, span: Span) -> OxcDiagnost
     .with_label(span)
 }
 
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct JsxMaxDepth(Box<JsxMaxDepthConfig>);
 
 impl std::ops::Deref for JsxMaxDepth {
@@ -97,11 +97,9 @@ declare_oxc_lint!(
 
 impl Rule for JsxMaxDepth {
     fn from_configuration(value: serde_json::Value) -> Self {
-        let config = serde_json::from_value::<DefaultRuleConfig<JsxMaxDepthConfig>>(value)
+        serde_json::from_value::<DefaultRuleConfig<JsxMaxDepth>>(value)
             .map(DefaultRuleConfig::into_inner)
-            .unwrap_or_default();
-
-        Self(Box::new(config))
+            .unwrap_or_default()
     }
 
     fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
@@ -136,69 +134,248 @@ fn test() {
 
     let pass = vec![
         (
-            r"
-            const Component = () => (
-              <div>
-                <section />
-              </div>
-            );
-            ",
+            "
+			        <App />
+			      ",
             None,
         ),
         (
-            r"
-            const Component = () => (
-              <div>
-                <div>
-                  <div />
-                </div>
-              </div>
-            );
-            ",
-            Some(serde_json::json!([{ "max": 3 }])),
+            "
+			        <App>
+			          <foo />
+			        </App>
+			      ",
+            Some(serde_json::json!([{ "max": 1 }])),
         ),
         (
-            r"
-            const Component = () => (
-              <>
-                <div>
-                  <span />
-                </div>
-              </>
-            );
-            ",
+            "
+			        <App>
+			          <foo>
+			            <bar />
+			          </foo>
+			        </App>
+			      ",
+            None,
+        ),
+        (
+            "
+			        <App>
+			          <foo>
+			            <bar />
+			          </foo>
+			        </App>
+			      ",
+            Some(serde_json::json!([{ "max": 2 }])),
+        ),
+        (
+            "
+			        const x = <div><em>x</em></div>;
+			        <div>{x}</div>
+			      ",
+            Some(serde_json::json!([{ "max": 2 }])),
+        ),
+        ("const foo = (x) => <div><em>{x}</em></div>;", Some(serde_json::json!([{ "max": 2 }]))),
+        (
+            "
+			        <></>
+			      ",
+            None,
+        ),
+        (
+            "
+			        <>
+			          <foo />
+			        </>
+			      ",
+            Some(serde_json::json!([{ "max": 1 }])),
+        ),
+        (
+            "
+			        const x = <><em>x</em></>;
+			        <>{x}</>
+			      ",
+            Some(serde_json::json!([{ "max": 2 }])),
+        ),
+        (
+            "
+			        const x = (
+			          <tr>
+			            <td>1</td>
+			            <td>2</td>
+			          </tr>
+			        );
+			        <tbody>
+			          {x}
+			        </tbody>
+			      ",
+            Some(serde_json::json!([{ "max": 2 }])),
+        ),
+        (
+            "
+			        const Example = props => {
+			          for (let i = 0; i < length; i++) {
+			            return <Text key={i} />;
+			          }
+			        };
+			      ",
+            Some(serde_json::json!([{ "max": 1 }])),
+        ),
+        (
+            "
+			        export function MyComponent() {
+			          const A = <React.Fragment>{<div />}</React.Fragment>;
+			          return <div>{A}</div>;
+			        }
+			      ",
+            None,
+        ),
+        (
+            r#"
+			        function Component() {
+			          let first = "";
+			          const second = first;
+			          first = second;
+			          return <div id={first} />;
+			        };
+			      "#,
+            None,
+        ),
+        (
+            r#"
+			        function Component() {
+			          let first = "";
+			          let second = "";
+			          let third = "";
+			          let fourth = "";
+			          const fifth = first;
+			          first = second;
+			          second = third;
+			          third = fourth;
+			          fourth = fifth;
+			          return <div id={first} />;
+			        };
+			      "#,
             None,
         ),
     ];
 
     let fail = vec![
         (
-            r"
-            const Component = () => (
-              <div>
-                <div>
-                  <div>
-                    <span />
-                  </div>
-                </div>
-              </div>
-            );
-            ",
+            "
+			        <App>
+			          <foo />
+			        </App>
+			      ",
+            Some(serde_json::json!([{ "max": 0 }])),
+        ),
+        (
+            "
+			        <App>
+			          <foo>{bar}</foo>
+			        </App>
+			      ",
+            Some(serde_json::json!([{ "max": 0 }])),
+        ),
+        (
+            "
+			        <App>
+			          <foo>
+			            <bar />
+			          </foo>
+			        </App>
+			      ",
+            Some(serde_json::json!([{ "max": 1 }])),
+        ),
+        (
+            "
+			        const x = <div><span /></div>;
+			        <div>{x}</div>
+			      ",
+            Some(serde_json::json!([{ "max": 1 }])),
+        ),
+        (
+            "
+			        const x = <div><span /></div>;
+			        let y = x;
+			        <div>{y}</div>
+			      ",
+            Some(serde_json::json!([{ "max": 1 }])),
+        ),
+        (
+            "
+			        const x = <div><span /></div>;
+			        let y = x;
+			        <div>{x}-{y}</div>
+			      ",
+            Some(serde_json::json!([{ "max": 1 }])),
+        ),
+        (
+            "
+			        <div>
+			        {<div><div><span /></div></div>}
+			        </div>
+			      ",
             None,
         ),
         (
-            r"
-            const Component = () => (
-              <>
-                <div>
-                  <>
-                    <div />
-                  </>
-                </div>
-              </>
-            );
-            ",
-            Some(serde_json::json!([{ "max": 2 }])),
+            "
+			        <>
+			          <foo />
+			        </>
+			      ",
+            Some(serde_json::json!([{ "max": 0 }])),
+        ),
+        (
+            "
+			        <>
+			          <>
+			            <bar />
+			          </>
+			        </>
+			      ",
+            Some(serde_json::json!([{ "max": 1 }])),
+        ),
+        (
+            "
+			        const x = <><span /></>;
+			        let y = x;
+			        <>{x}-{y}</>
+			      ",
+            Some(serde_json::json!([{ "max": 1 }])),
+        ),
+        (
+            "
+			        const x = (
+			          <tr>
+			            <td>1</td>
+			            <td>2</td>
+			          </tr>
+			        );
+			        <tbody>
+			          {x}
+			        </tbody>
+			      ",
+            Some(serde_json::json!([{ "max": 1 }])),
+        ),
+        (
+            r#"
+			        <div className="custom_modal">
+			          <Modal className={classes.modal} open={isOpen} closeAfterTransition>
+			            <Fade in={isOpen}>
+			              <DialogContent>
+			                <Icon icon="cancel" onClick={onClose} popoverText="Close Modal" />
+			                <div className="modal_content">{children}</div>
+			                <div className={clxs('modal_buttons', classes.buttons)}>
+			                  <Button className="modal_buttons--cancel" onClick={onCancel}>
+			                    {cancelMsg ? cancelMsg : 'Cancel'}
+			                  </Button>
+			                </div>
+			              </DialogContent>
+			            </Fade>
+			          </Modal>
+			        </div>
+			      "#,
+            Some(serde_json::json!([{ "max": 4 }])),
         ),
     ];
 
