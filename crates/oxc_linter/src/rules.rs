b97101a0e0fb0b00a51f63f720c089aea708c113
mehm8128 <mehm8128@gmail.com>
2025-12-23 02:13:32 +0900

    feat(linter): implement aria/proptypes (#17253)
    related to https://github.com/oxc-project/oxc/issues/1141
    
    Implemented jsx_a11y [`aria_proptypes`](https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/tree/HEAD/docs/rules/aria-proptypes.md) rule.
    
    ---------
    
    Co-authored-by: Cameron Clark <cameron.clark@hey.com>

--- crates/oxc_linter/src/rules.rs
+++ crates/oxc_linter/src/rules.rs
@@ -540,6 +540,7 @@ pub(crate) mod jsx_a11y {
     pub mod anchor_is_valid;
     pub mod aria_activedescendant_has_tabindex;
     pub mod aria_props;
+    pub mod aria_proptypes;
     pub mod aria_role;
     pub mod aria_unsupported_elements;
     pub mod autocomplete_valid;
@@ -963,6 +964,7 @@ oxc_macros::declare_all_lint_rules! {
     jsx_a11y::anchor_is_valid,
     jsx_a11y::aria_activedescendant_has_tabindex,
     jsx_a11y::aria_props,
+    jsx_a11y::aria_proptypes,
     jsx_a11y::aria_role,
     jsx_a11y::aria_unsupported_elements,
     jsx_a11y::autocomplete_valid,
