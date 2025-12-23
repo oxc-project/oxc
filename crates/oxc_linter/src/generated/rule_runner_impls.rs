b459ab772806b98e370794aabcd458d988f2c8f9
Kenzo-Wada <used.acormhib@gmail.com>
2025-12-24 00:07:15 +0900

    feat(linter/react): implement react/no-did-mount-set-state

--- crates/oxc_linter/src/generated/rule_runner_impls.rs
+++ crates/oxc_linter/src/generated/rule_runner_impls.rs
@@ -2408,6 +2408,11 @@ impl RuleRunner for crate::rules::react::no_danger_with_children::NoDangerWithCh
     const RUN_FUNCTIONS: RuleRunFunctionsImplemented = RuleRunFunctionsImplemented::Run;
 }
 
+impl RuleRunner for crate::rules::react::no_did_mount_set_state::NoDidMountSetState {
+    const NODE_TYPES: Option<&AstTypesBitset> = None;
+    const RUN_FUNCTIONS: RuleRunFunctionsImplemented = RuleRunFunctionsImplemented::Run;
+}
+
 impl RuleRunner for crate::rules::react::no_direct_mutation_state::NoDirectMutationState {
     const NODE_TYPES: Option<&AstTypesBitset> = Some(&AstTypesBitset::from_types(&[
         AstType::AssignmentExpression,
