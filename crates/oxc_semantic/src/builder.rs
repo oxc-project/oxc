f3afe57904ca1bd316a6ede626c6fca1164696a5
camc314 <18101008+camc314@users.noreply.github.com>
2025-12-27 20:31:41 +0000

    perf(semantic): inline `enter_kind` for `ImportNamespaceSpecifier` (#17393)
    Ref https://github.com/oxc-project/backlog/issues/72

--- crates/oxc_semantic/src/builder.rs
+++ crates/oxc_semantic/src/builder.rs
@@ -2129,6 +2129,15 @@ impl<'a> Visit<'a> for SemanticBuilder<'a> {
         self.visit_binding_identifier(&specifier.local);
         self.leave_node(kind);
     }
+
+    fn visit_import_namespace_specifier(&mut self, specifier: &ImportNamespaceSpecifier<'a>) {
+        let kind = AstKind::ImportNamespaceSpecifier(self.alloc(specifier));
+        self.enter_node(kind);
+        specifier.bind(self);
+        self.visit_span(&specifier.span);
+        self.visit_binding_identifier(&specifier.local);
+        self.leave_node(kind);
+    }
 }
 
 impl<'a> SemanticBuilder<'a> {
@@ -2150,9 +2159,6 @@ impl<'a> SemanticBuilder<'a> {
         /* cfg */
 
         match kind {
-            AstKind::ImportNamespaceSpecifier(specifier) => {
-                specifier.bind(self);
-            }
             AstKind::TSImportEqualsDeclaration(decl) => {
                 decl.bind(self);
             }
