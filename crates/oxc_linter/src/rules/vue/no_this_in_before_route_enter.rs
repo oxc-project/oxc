fb6a9a66825c9c4c1d7df100084366e6dbde032f
yefan <yuxuan1491433427@qq.com>
2025-12-31 15:42:26 +0800

    implement

---
+++ crates/oxc_linter/src/rules/vue/no_this_in_before_route_enter.rs
@@ -0,0 +1,242 @@
+use oxc_ast::{
+    AstKind,
+    ast::{ExportDefaultDeclarationKind, Expression, Function, ObjectPropertyKind, ThisExpression},
+};
+use oxc_ast_visit::Visit;
+use oxc_diagnostics::OxcDiagnostic;
+use oxc_macros::declare_oxc_lint;
+use oxc_semantic::ScopeFlags;
+use oxc_span::Span;
+
+use crate::{AstNode, context::LintContext, rule::Rule};
+
+fn no_this_in_before_route_enter_diagnostic(span: Span) -> OxcDiagnostic {
+    OxcDiagnostic::warn("'beforeRouteEnter' does NOT have access to `this` component instance.")
+        .with_help("Use the callback's `vm` parameter instead of `this` in `beforeRouteEnter`.")
+        .with_label(span)
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct NoThisInBeforeRouteEnter;
+
+declare_oxc_lint!(
+    /// ### What it does
+    ///
+    /// Disallow this usage in a beforeRouteEnter method.
+    ///
+    /// ### Why is this bad?
+    ///
+    /// Because lack of this in the beforeRouteEnter.
+    /// This behavior isn't obvious, so it's pretty easy to make a TypeError. Especially during some refactor.
+    ///
+    /// ### Examples
+    ///
+    /// Examples of **incorrect** code for this rule:
+    /// ```js
+    /// export default {
+    ///   beforeRouteEnter(to, from, next) {
+    ///     this.a; // Error: 'this' is not available
+    ///     next();
+    ///   }
+    /// }
+    /// ```
+    ///
+    /// Examples of **correct** code for this rule:
+    /// ```js
+    /// export default {
+    ///   beforeRouteEnter(to, from, next) {
+    ///     // anything without this
+    ///   }
+    /// }
+    /// ```
+    NoThisInBeforeRouteEnter,
+    vue,
+    correctness,
+);
+
+impl Rule for NoThisInBeforeRouteEnter {
+    fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
+        let AstKind::ExportDefaultDeclaration(export_default_decl) = node.kind() else {
+            return;
+        };
+        let ExportDefaultDeclarationKind::ObjectExpression(obj_expr) =
+            &export_default_decl.declaration
+        else {
+            return;
+        };
+
+        for prop in &obj_expr.properties {
+            if let ObjectPropertyKind::ObjectProperty(obj_prop) = prop {
+                let Some(key_name) = obj_prop.key.static_name() else {
+                    continue;
+                };
+                if key_name != "beforeRouteEnter" {
+                    continue;
+                }
+                let function_body = match &obj_prop.value {
+                    Expression::FunctionExpression(func_expr) => func_expr.body.as_ref(),
+                    _ => continue,
+                };
+
+                let Some(function_body) = function_body else {
+                    continue;
+                };
+
+                let mut finder = ThisFinder::new();
+                finder.visit_function_body(function_body);
+                for span in finder.found_this_expressions {
+                    ctx.diagnostic(no_this_in_before_route_enter_diagnostic(span));
+                }
+            }
+        }
+    }
+
+    fn should_run(&self, ctx: &crate::context::ContextHost) -> bool {
+        ctx.file_extension().is_some_and(|ext| ext == "vue")
+    }
+}
+
+struct ThisFinder {
+    found_this_expressions: Vec<Span>,
+}
+
+impl ThisFinder {
+    fn new() -> Self {
+        Self { found_this_expressions: Vec::new() }
+    }
+}
+
+impl<'a> Visit<'a> for ThisFinder {
+    fn visit_this_expression(&mut self, expr: &ThisExpression) {
+        self.found_this_expressions.push(expr.span);
+    }
+
+    fn visit_function(&mut self, _func: &Function<'a>, _flags: ScopeFlags) {}
+}
+
+#[test]
+fn test() {
+    use crate::tester::Tester;
+    use std::path::PathBuf;
+
+    let pass = vec![
+        (
+            r#"
+			<template>
+			  <p>{{ greeting }} World!</p>
+			</template>
+
+			<script>
+			export default {
+			  data () {
+			    return {
+			      greeting: "Hello"
+			    };
+			  },
+			};"#,
+            None,
+            None,
+            Some(PathBuf::from("test.vue")),
+        ),
+        (
+            r"
+			<script>
+			export default {
+			  beforeRouteEnter(to, from, next) {
+			    next();
+			  }
+			};
+			</script>",
+            None,
+            None,
+            Some(PathBuf::from("test.vue")),
+        ),
+        (
+            r"
+			<script>
+			let a = {
+			  beforeRouteEnter() {
+			    this.a;
+			  }
+			};
+			export default a;
+			</script>",
+            None,
+            None,
+            Some(PathBuf::from("test.vue")),
+        ),
+        (
+            r"
+			<script>
+			export default {
+			  beforeRouteEnter(to, from, next) {
+			    function test() {
+                  this.a;
+                }
+			  }
+			};
+			</script>",
+            None,
+            None,
+            Some(PathBuf::from("test.vue")),
+        ),
+        (
+            r"
+			export default {
+			  beforeRouteEnter(to, from, next) {
+                this.a;
+			  }
+			};
+			",
+            None,
+            None,
+            Some(PathBuf::from("test.js")),
+        ),
+    ];
+
+    let fail = vec![
+        (
+            r"
+			<script>
+			export default {
+			  beforeRouteEnter(to, from, next) {
+			    this.a;
+			    next();
+			  }
+			};
+			</script>",
+            None,
+            None,
+            Some(PathBuf::from("test.vue")),
+        ),
+        (
+            r"
+			<script>
+			export default {
+			  beforeRouteEnter: function(to, from, next) {
+			    this.b;
+			  }
+			};
+			</script>",
+            None,
+            None,
+            Some(PathBuf::from("test.vue")),
+        ),
+        (
+            r"
+			<script>
+			export default {
+			  beforeRouteEnter: function(to, from, next) {
+			    this.atrr = this.method();
+			  }
+			};
+			</script>",
+            None,
+            None,
+            Some(PathBuf::from("test.vue")),
+        ),
+    ];
+
+    Tester::new(NoThisInBeforeRouteEnter::NAME, NoThisInBeforeRouteEnter::PLUGIN, pass, fail)
+        .test_and_snapshot();
+}
