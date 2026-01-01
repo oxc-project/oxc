552920a757faafa56bd7840dd5d8ec3c86127095
nilptr <nilptr.js@gmail.com>
2026-01-01 23:08:17 +0800

    feat(oxfmt): delete ImportGroupMatcher & use GroupMatcher

--- crates/oxc_formatter/src/ir_transform/sort_imports/mod.rs
+++ crates/oxc_formatter/src/ir_transform/sort_imports/mod.rs
@@ -16,8 +16,7 @@ use crate::{
         tag::{LabelId, Tag},
     },
     ir_transform::sort_imports::{
-        group_config::parse_groups_from_strings, partitioned_chunk::PartitionedChunk,
-        source_line::SourceLine,
+        group_matcher::GroupMatcher, partitioned_chunk::PartitionedChunk, source_line::SourceLine
     },
 };
 
@@ -44,7 +43,7 @@ impl SortImportsTransform {
         }
 
         // Parse string based groups into our internal representation for performance
-        let groups = parse_groups_from_strings(&options.groups);
+        let group_matcher = GroupMatcher::new(&options.groups, &options.custom_groups);
         let prev_elements: &[FormatElement<'a>] = document;
 
         // Roughly speaking, sort-imports is a process of swapping lines.
@@ -245,7 +244,7 @@ impl SortImportsTransform {
                     // // chunk trailing
                     // ```
                     let (sorted_imports, orphan_contents, trailing_lines) =
-                        chunk.into_sorted_import_units(&groups, options);
+                        chunk.into_sorted_import_units(&group_matcher, options);
 
                     // Output leading orphan content (after_slot: None)
                     for orphan in &orphan_contents {
