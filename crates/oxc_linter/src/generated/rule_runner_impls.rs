e3c37649ceb01ad3b77728ec4486f748975ab6e3
Valentin Maerten <maerten.valentin@gmail.com>
2026-01-02 19:07:21 +0100

    refactor(linter/vitest): rewrite prefer-importing-vitest-globals with fn run

--- crates/oxc_linter/src/generated/rule_runner_impls.rs
+++ crates/oxc_linter/src/generated/rule_runner_impls.rs
@@ -4020,8 +4020,9 @@ impl RuleRunner for crate::rules::vitest::prefer_called_times::PreferCalledTimes
 impl RuleRunner
     for crate::rules::vitest::prefer_importing_vitest_globals::PreferImportingVitestGlobals
 {
-    const NODE_TYPES: Option<&AstTypesBitset> = None;
-    const RUN_FUNCTIONS: RuleRunFunctionsImplemented = RuleRunFunctionsImplemented::RunOnce;
+    const NODE_TYPES: Option<&AstTypesBitset> =
+        Some(&AstTypesBitset::from_types(&[AstType::CallExpression]));
+    const RUN_FUNCTIONS: RuleRunFunctionsImplemented = RuleRunFunctionsImplemented::Run;
 }
 
 impl RuleRunner for crate::rules::vitest::prefer_to_be_falsy::PreferToBeFalsy {
