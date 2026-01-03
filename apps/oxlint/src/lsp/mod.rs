eed56a7f53a8fa1e4e2c6ac1a867b028465c07cc
Sysix <sysix@sysix-coding.de>
2026-01-03 16:23:39 +0100

    refactor(oxlint/lsp): add `ExternalLinter` to `ServerLinterBuilder`

--- apps/oxlint/src/lsp/mod.rs
+++ apps/oxlint/src/lsp/mod.rs
@@ -15,7 +15,7 @@ pub async fn run_lsp() {
     oxc_language_server::run_server(
         "oxlint".to_string(),
         env!("CARGO_PKG_VERSION").to_string(),
-        vec![Box::new(crate::lsp::server_linter::ServerLinterBuilder)],
+        vec![Box::new(crate::lsp::server_linter::ServerLinterBuilder::default())],
     )
     .await;
 }
