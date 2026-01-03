cb428a6c847b5d21fef6224b69de31f1d319acff
Connor Shea <connor.james.shea@gmail.com>
2026-01-03 15:35:24 -0700

    Update DefaultRuleConfig docs.

--- crates/oxc_linter/src/rule.rs
+++ crates/oxc_linter/src/rule.rs
@@ -83,9 +83,7 @@ pub trait Rule: Sized + Default + fmt::Debug {
 /// ```ignore
 /// impl Rule for MyRule {
 ///     fn from_configuration(value: serde_json::Value) -> Result<Self, serde_json::error::Error> {
-///         Ok(serde_json::from_value::<DefaultRuleConfig<Self>>(value)
-///             .unwrap_or_default()
-///             .into_inner())
+///         serde_json::from_value::<DefaultRuleConfig<Self>>(value).map(DefaultRuleConfig::into_inner)
 ///     }
 /// }
 /// ```
