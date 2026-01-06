2607d569aa6fd9a0d67ca962d4670fd18fcfb77a
Cameron <cameron.clark@hey.com>
2026-01-06 11:55:20 +0000

    Apply suggestions from code review
    Signed-off-by: Cameron <cameron.clark@hey.com>

--- crates/oxc_parser/src/js/module.rs
+++ crates/oxc_parser/src/js/module.rs
@@ -273,7 +273,6 @@ impl<'a> ParserImpl<'a> {
                 match self.cur_kind() {
                     // import defaultExport, * as name from "module-name";
                     Kind::Star => {
-                        // TS1363: A type-only import can specify a default import or named bindings, but not both.
                         if self.is_ts && import_kind == ImportOrExportKind::Type {
                             self.error(diagnostics::type_only_import_default_and_named(
                                 default_span,
@@ -283,7 +282,6 @@ impl<'a> ParserImpl<'a> {
                     }
                     // import defaultExport, { export1 [ , [...] ] } from "module-name";
                     Kind::LCurly => {
-                        // TS1363: A type-only import can specify a default import or named bindings, but not both.
                         if self.is_ts && import_kind == ImportOrExportKind::Type {
                             self.error(diagnostics::type_only_import_default_and_named(
                                 default_span,
