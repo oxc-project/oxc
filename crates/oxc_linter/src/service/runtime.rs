07618a787ebdb0782906d193404a0863ff3c98bf
Dunqing <29533304+Dunqing@users.noreply.github.com>
2026-01-22 06:32:53 +0000

    perf(linter): turn off `scope_build_child_ids` for SemanticBuilder (#18360)
    No use case in the linter after #18350 and #18335, so turn it off.

--- crates/oxc_linter/src/service/runtime.rs
+++ crates/oxc_linter/src/service/runtime.rs
@@ -1029,7 +1029,6 @@ impl Runtime {
 
         let semantic_ret = SemanticBuilder::new()
             .with_cfg(true)
-            .with_scope_tree_child_ids(true)
             .with_check_syntax_error(check_syntax_errors)
             .build(allocator.alloc(ret.program));
 
