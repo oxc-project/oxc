05fe91f644659c88c7d09c0cf22dc6e4e8c67bd8
overlookmotel <557937+overlookmotel@users.noreply.github.com>
2026-01-23 18:58:57 +0000

    test(linter/plugins): add `eslint-plugin-react-hooks` to conformance tests (#18462)
    Run all the tests from `eslint-plugin-react-hooks` in the conformance tester. All pass, including the React compiler rules.

--- apps/oxlint/conformance/init.sh
+++ apps/oxlint/conformance/init.sh
@@ -2,13 +2,20 @@
 set -e
 
 ESLINT_SHA="8f360ad6a7a743d33a83eed8973ee4a50731e55b" # 10.0.0-rc.0
+REACT_SHA="612e371fb215498edde4c853bd1e0c8e9203808f" # 19.2.3
 
 # Delete existing `submodules` directory
 rm -rf submodules
+mkdir submodules
+cd submodules
+
+###############################################################################
+# ESLint
+###############################################################################
 
 # Clone ESLint repo into `submodules/eslint`
-git clone --single-branch --depth 1 https://github.com/eslint/eslint.git submodules/eslint
-cd submodules/eslint
+git clone --single-branch --depth 1 https://github.com/eslint/eslint.git eslint
+cd eslint
 git fetch --depth 1 origin "$ESLINT_SHA"
 git reset --hard "$ESLINT_SHA"
 git clean -f -q
@@ -23,3 +30,27 @@ cd node_modules/@typescript-eslint/parser
 
 # Install dependencies of TS-ESLint parser shim
 pnpm install --ignore-workspace
+
+# Return to `submodules` directory
+cd ../../../..
+
+###############################################################################
+# React
+###############################################################################
+
+# Clone React repo into `submodules/react`
+git clone --single-branch --depth 1 https://github.com/facebook/react.git react
+cd react
+git fetch --depth 1 origin "$REACT_SHA"
+git reset --hard "$REACT_SHA"
+git clean -f -q
+
+# Install dependencies
+yarn
+
+# Install `eslint-plugin-react-hooks` dependency
+cd packages/eslint-plugin-react-hooks
+yarn add eslint-plugin-react-hooks
+
+# Return to `submodules` directory
+cd ../../..
