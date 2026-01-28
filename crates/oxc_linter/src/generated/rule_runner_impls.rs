6b8a5ae022fb4fc9b0cb17f3621ee7db9219263d
Mikhail Baev <baev2m@gmail.com>
2026-01-28 16:16:37 +0500

    feat(linter): add `eslint-plugin-import/no-nodejs-modules` rule (#18006)
    this PR adds `eslint-plugin-import/no-nodejs-modules` rule, passes all [tests](https://github.com/import-js/eslint-plugin-import/blob/main/tests/src/rules/no-nodejs-modules.js) + some new, like TS import equals or export declarations
    
     issue #1117
    
    ---------
    
    Signed-off-by: Cameron <cameron.clark@hey.com>
    Co-authored-by: Cameron <cameron.clark@hey.com>

--- crates/oxc_linter/src/generated/rule_runner_impls.rs
+++ crates/oxc_linter/src/generated/rule_runner_impls.rs
@@ -153,6 +153,18 @@ impl RuleRunner for crate::rules::import::no_namespace::NoNamespace {
     const RUN_FUNCTIONS: RuleRunFunctionsImplemented = RuleRunFunctionsImplemented::RunOnce;
 }
 
+impl RuleRunner for crate::rules::import::no_nodejs_modules::NoNodejsModules {
+    const NODE_TYPES: Option<&AstTypesBitset> = Some(&AstTypesBitset::from_types(&[
+        AstType::CallExpression,
+        AstType::ExportAllDeclaration,
+        AstType::ExportNamedDeclaration,
+        AstType::ImportDeclaration,
+        AstType::ImportExpression,
+        AstType::TSImportEqualsDeclaration,
+    ]));
+    const RUN_FUNCTIONS: RuleRunFunctionsImplemented = RuleRunFunctionsImplemented::Run;
+}
+
 impl RuleRunner for crate::rules::import::no_relative_parent_imports::NoRelativeParentImports {
     const NODE_TYPES: Option<&AstTypesBitset> = Some(&AstTypesBitset::from_types(&[
         AstType::CallExpression,
