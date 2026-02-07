c43e8842c0f3a503dbcb7e2b6ee2ffca1080edcb
overlookmotel <theoverlookmotel@gmail.com>
2026-02-07 09:23:39 +0000

    feat(linter/plugins): `RuleTester` test suggestions

--- apps/oxlint/conformance/src/groups/eslint.ts
+++ apps/oxlint/conformance/src/groups/eslint.ts
@@ -37,7 +37,20 @@ const group: TestGroup = {
     if (
       ruleName === "no-invalid-this" &&
       code.includes("function (x, this: context) {") &&
-      err?.message === "Parsing failed"
+      err.message === "Parsing failed"
+    ) {
+      return true;
+    }
+
+    // Oxlint's suggestion message differs from ESLint's, but in a completely cosmetic way
+    if (
+      ruleName === "prefer-regex-literals" &&
+      code === "new RegExp(/a/ig, 'g');" &&
+      err.message.split("\n")[0] ===
+        "Suggestion at index 1: Hydrated test desc " +
+          `"Replace with a regular expression literal with flags 'ig'."` +
+          " does not match received desc " +
+          `"Replace with a regular expression literal with flags 'gi'."`
     ) {
       return true;
     }
