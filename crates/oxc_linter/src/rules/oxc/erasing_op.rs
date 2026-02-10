3f0e7196dec46bdc76ab502f8a6abf1a280f4056
drylint <80906525+drylint@users.noreply.github.com>
2026-02-10 17:17:22 +0800

    docs(linter): fix typo in oxc/erasing-op (#19205)

--- crates/oxc_linter/src/rules/oxc/erasing_op.rs
+++ crates/oxc_linter/src/rules/oxc/erasing_op.rs
@@ -22,7 +22,7 @@ pub struct ErasingOp;
 declare_oxc_lint!(
     /// ### What it does
     ///
-    /// Checks for erasing operations, e.g., `x * 0``.
+    /// Checks for erasing operations, e.g., `x * 0`.
     ///
     /// Based on https://rust-lang.github.io/rust-clippy/master/#/erasing_op
     ///
