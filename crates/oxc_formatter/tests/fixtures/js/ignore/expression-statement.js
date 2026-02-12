4828a009dd206eb618ad891c06cf519c486716f3
Andreas Lubbe <git@lubbe.org>
2026-02-12 08:57:00 +0100

    Add more tests

--- crates/oxc_formatter/tests/fixtures/js/ignore/expression-statement.js
+++ crates/oxc_formatter/tests/fixtures/js/ignore/expression-statement.js
@@ -10,3 +10,15 @@ Object.defineProperties    (    exports    , {
 )
 
 logger({ level: 'debug', message: `Really long debugging message about how ${user} called a certain part of our application with a certain ${payload}` }); // prettier-ignore
+
+if (ok) logger(  payload  ); // prettier-ignore
+else done();
+
+if (a) step();
+else if (b) logger(  payload  ); // prettier-ignore
+else if (c) return   {x:1,y:2}; // oxfmt-ignore
+else done();
+
+logger(  payload  ); /* prettier-ignore */
+if (ok) return   {a:1,b:2}; /* oxfmt-ignore */
+else done();
