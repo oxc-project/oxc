a483aff0bf7a76ed1012bbb3f41690802ef56b8b
Connor Shea <connor.james.shea@gmail.com>
2026-02-15 16:34:01 -0700

    Update the config/code in the test to have one intentional, expected lint violation.

--- apps/oxlint/fixtures/overrides_correctness_off_constant_binary/subdir/bar.tsx
+++ apps/oxlint/fixtures/overrides_correctness_off_constant_binary/subdir/bar.tsx
@@ -1,5 +1,6 @@
+// `typescript/no-inferrable-types` _should_ be triggered by this line:
+const a: number = 5;
 // no-constant-binary-expression is a correctness rule and should NOT trigger since correctness is off.
-const a = 5;
 if (a === {}) {
   console.log("foo");
 }
