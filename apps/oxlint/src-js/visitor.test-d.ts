cf873b94f79c961432a3c2e685933e6f1aa4bc38
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2026-02-24 20:21:38 +0900

    chore: use type utils from type-challenges instead of Vitest typecheck feature

--- apps/oxlint/src-js/visitor.test-d.ts
+++ apps/oxlint/src-js/visitor.test-d.ts
@@ -1,22 +1,25 @@
-import { test, expectTypeOf } from "vitest";
 import type { VisitorObject } from "./generated/visitor";
 import type { Node, CallExpression } from "./generated/types";
+import type { ExpectExtends, ExpectTrue } from "@type-challenges/utils";
 
-test("VisitorObject", () => {
+const emptyVisitorObject = {};
+const callExpressionVisitorObject = {
+  CallExpression: (_node: CallExpression) => {},
+};
+const callExpressionExitVisitorObject = {
+  "CallExpression:exit": (_node: CallExpression) => {},
+};
+const genericVisitorObject = {
+  "FunctionExpression > Identifier": (_node: Node) => {},
+  ":matches(FunctionExpression, FunctionDeclaration)": (_node: Node) => {},
+};
+
+export type cases1 = [
   // Empty visitor object is allowed
-  expectTypeOf({}).toExtend<VisitorObject>();
-
+  ExpectTrue<ExpectExtends<VisitorObject, typeof emptyVisitorObject>>,
   // Specific node visitors has a stricter type for the parameter
-  expectTypeOf({
-    CallExpression: (_node: CallExpression) => {},
-  }).toExtend<VisitorObject>();
-  expectTypeOf({
-    "CallExpression:exit": (_node: CallExpression) => {},
-  }).toExtend<VisitorObject>();
-
+  ExpectTrue<ExpectExtends<VisitorObject, typeof callExpressionVisitorObject>>,
+  ExpectTrue<ExpectExtends<VisitorObject, typeof callExpressionExitVisitorObject>>,
   // Node selectors has generic Node type for the parameter
-  expectTypeOf({
-    "FunctionExpression > Identifier": (_node: Node) => {},
-    ":matches(FunctionExpression, FunctionDeclaration)": (_node: Node) => {},
-  }).toExtend<VisitorObject>();
-});
+  ExpectTrue<ExpectExtends<VisitorObject, typeof genericVisitorObject>>,
+];
