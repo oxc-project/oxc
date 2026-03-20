38b3c1d1975f74ed934291ce3ae2dd502de99181
overlookmotel <557937+overlookmotel@users.noreply.github.com>
2026-03-20 20:40:34 +0000

    perf(napi/parser, oxlint/plugins): shorten deserializer for `WithClause` (#20575)
    Small optimization to ESTree serialization and raw transfer. Shave off a little work when serializing/deserializing `WithClause`.

--- crates/oxc_ast/src/generated/derive_estree.rs
+++ crates/oxc_ast/src/generated/derive_estree.rs
@@ -1638,7 +1638,6 @@ impl ESTree for WithClause<'_> {
     fn serialize<S: Serializer>(&self, serializer: S) {
         let mut state = serializer.serialize_struct();
         state.serialize_field("attributes", &self.with_entries);
-        state.serialize_span(self.span);
         state.end();
     }
 }
