3a01dd2ea56cc480a8e94eb66d356fb4b903ce79
Ido Rosenthal <idoros@gmail.com>
2026-04-29 09:45:24 +0300

    fix(transformer): add jsxDEV source metadata for fragments

--- tasks/transform_conformance/tests/babel-plugin-transform-react-jsx/test/fixtures/fragment-static-children/output.js
+++ tasks/transform_conformance/tests/babel-plugin-transform-react-jsx/test/fixtures/fragment-static-children/output.js
@@ -1,11 +1,45 @@
-var _jsxFileName = "<CWD>/tests/babel-plugin-transform-react-jsx/test/fixtures/fragment-static-children/input.jsx";
-import { jsxDEV as _jsxDEV, Fragment as _Fragment } from "react/jsx-dev-runtime";
-/* @__PURE__ */ _jsxDEV(_Fragment, { children: [/* @__PURE__ */ _jsxDEV("div", {}, void 0, false, {
-  fileName: _jsxFileName,
-  lineNumber: 2,
-  columnNumber: 3
-}, this), /* @__PURE__ */ _jsxDEV("div", {}, void 0, false, {
-  fileName: _jsxFileName,
-  lineNumber: 3,
-  columnNumber: 3
-}, this)] }, void 0, true);
+var _jsxFileName =
+  "<CWD>/tests/babel-plugin-transform-react-jsx/test/fixtures/fragment-static-children/input.jsx";
+import {
+  jsxDEV as _jsxDEV,
+  Fragment as _Fragment,
+} from "react/jsx-dev-runtime";
+/* @__PURE__ */ _jsxDEV(
+  _Fragment,
+  {
+    children: [
+      /* @__PURE__ */ _jsxDEV(
+        "div",
+        {},
+        void 0,
+        false,
+        {
+          fileName: _jsxFileName,
+          lineNumber: 2,
+          columnNumber: 3,
+        },
+        this,
+      ),
+      /* @__PURE__ */ _jsxDEV(
+        "div",
+        {},
+        void 0,
+        false,
+        {
+          fileName: _jsxFileName,
+          lineNumber: 3,
+          columnNumber: 3,
+        },
+        this,
+      ),
+    ],
+  },
+  void 0,
+  true,
+  {
+    fileName: _jsxFileName,
+    lineNumber: 1,
+    columnNumber: 1,
+  },
+  this,
+);
