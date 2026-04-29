953dcc56387a01c06f605a5d4ce0c9bf846d338e
Kapobajza <kapobajza@gmail.com>
2026-04-29 14:59:07 +0200

    refactor: Share padding_around_after_all_blocks rule between jest and vitest

--- crates/oxc_linter/src/rules/jest/padding_around_after_all_blocks.rs
+++ crates/oxc_linter/src/rules/jest/padding_around_after_all_blocks.rs
@@ -1,49 +1,21 @@
-use oxc_ast::AstKind;
 use oxc_macros::declare_oxc_lint;
 
 use crate::{
     context::LintContext,
     rule::Rule,
-    utils::{
-        JestGeneralFnKind, ParsedGeneralJestFnCall, PossibleJestNode, parse_general_jest_fn_call,
-        report_missing_padding_before_jest_block,
-    },
+    rules::shared::padding_around_after_all_blocks::{DOCUMENTATION, run},
+    utils::PossibleJestNode,
 };
 
 #[derive(Debug, Default, Clone)]
 pub struct PaddingAroundAfterAllBlocks;
 
 declare_oxc_lint!(
-    /// ### What it does
-    ///
-    /// This rule enforces a line of padding before and after 1 or more
-    /// `afterAll` statements.
-    ///
-    /// ### Why is this bad?
-    ///
-    /// Inconsistent formatting of code can make the code more difficult to read
-    /// and follow. This rule helps ensure that `afterAll` blocks are visually
-    /// separated from the rest of the code, making them easier to identify while
-    /// looking through test files.
-    ///
-    /// ### Examples
-    ///
-    /// Examples of **incorrect** code for this rule:
-    /// ```js
-    /// const thing = 123;
-    /// afterAll(() => {});
-    /// ```
-    ///
-    /// Examples of **correct** code for this rule:
-    /// ```js
-    /// const thing = 123;
-    ///
-    /// afterAll(() => {});
-    /// ```
     PaddingAroundAfterAllBlocks,
     jest,
     style,
     fix,
+    docs = DOCUMENTATION,
     version = "1.59.0",
 );
 
@@ -53,24 +25,7 @@ impl Rule for PaddingAroundAfterAllBlocks {
         jest_node: &PossibleJestNode<'a, 'c>,
         ctx: &'c LintContext<'a>,
     ) {
-        let node = jest_node.node;
-        let AstKind::CallExpression(call_expr) = node.kind() else {
-            return;
-        };
-        let Some(jest_fn_call) = parse_general_jest_fn_call(call_expr, jest_node, ctx) else {
-            return;
-        };
-        let ParsedGeneralJestFnCall { kind, name, .. } = &jest_fn_call;
-        let Some(kind) = kind.to_general() else {
-            return;
-        };
-        if kind != JestGeneralFnKind::Hook {
-            return;
-        }
-        if name != "afterAll" {
-            return;
-        }
-        report_missing_padding_before_jest_block(node, ctx, name);
+        run(jest_node, ctx);
     }
 }
 
