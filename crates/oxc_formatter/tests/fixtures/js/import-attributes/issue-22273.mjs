e2dc00396c28dee0df8be1862a8d96d70db366a2
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2026-05-10 17:39:31 +0300

    Add another test

--- crates/oxc_formatter/tests/fixtures/js/import-attributes/issue-22273.mjs
+++ crates/oxc_formatter/tests/fixtures/js/import-attributes/issue-22273.mjs
@@ -4,3 +4,7 @@ import myEmbeddedDb from './my.db' with { type: 'sqlite', embed: 'true' }
 // Should be spread over multiple lines
 import myEmbeddedDb from './my.db' with { type: 'sqlite', embed: 'true', veryLongAttribute: "true" }
 
+// Shouldn't have a comma for a single attribute
+import myEmbeddedDb from './my.db' with { type: 'sqlite' }
+
+
