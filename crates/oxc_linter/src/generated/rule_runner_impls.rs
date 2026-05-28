8422e8bc44db47033ce516f9375867624e265823
Mikhail Baev <baev2m@gmail.com>
2026-05-29 00:34:08 +0500

    feat(linter/jsdoc): implement `require-yields-description` rule (#22805)
    this PR implements `jsdoc/require-yields-description` rule. 
    
    [rule doc](https://github.com/gajus/eslint-plugin-jsdoc/blob/main/docs/rules/require-yields-description.md)
    [rule source](https://github.com/gajus/eslint-pluginjsdoc/blob/3d53b88baed2b49e04b4aa71599d1b449efcc3f8/src/index.js#L245)
    
    issue #1170
    
    ---------
    
    Co-authored-by: Cameron <cameron.clark@hey.com>

--- crates/oxc_linter/src/generated/rule_runner_impls.rs
+++ crates/oxc_linter/src/generated/rule_runner_impls.rs
@@ -4404,6 +4404,11 @@ impl RuleRunner for crate::rules::jsdoc::require_yields::RequireYields {
     const RUN_FUNCTIONS: RuleRunFunctionsImplemented = RuleRunFunctionsImplemented::Run;
 }
 
+impl RuleRunner for crate::rules::jsdoc::require_yields_description::RequireYieldsDescription {
+    const NODE_TYPES: Option<&AstTypesBitset> = None;
+    const RUN_FUNCTIONS: RuleRunFunctionsImplemented = RuleRunFunctionsImplemented::RunOnce;
+}
+
 impl RuleRunner for crate::rules::jsdoc::require_yields_type::RequireYieldsType {
     const NODE_TYPES: Option<&AstTypesBitset> = None;
     const RUN_FUNCTIONS: RuleRunFunctionsImplemented = RuleRunFunctionsImplemented::RunOnce;
