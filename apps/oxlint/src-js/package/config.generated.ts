d4fc5a068d0e83e8ddf38ae98f3412cf7d704db7
WaterWhisperer <waterwhisperer24@qq.com>
2026-06-14 22:53:08 +0800

    feat(linter): add schema for `eslint/prefer-destructuring`

--- apps/oxlint/src-js/package/config.generated.ts
+++ apps/oxlint/src-js/package/config.generated.ts
@@ -151,6 +151,7 @@ export type Location = "start" | "anywhere";
 export type HandleCallbackErrConfig = string;
 export type ShorthandType = "always" | "methods" | "properties" | "consistent" | "consistent-as-needed" | "never";
 export type Destructuring = "any" | "all";
+export type PreferDestructuringOption = PreferDestructuringTargetOption | PreferDestructuringAssignmentConfig;
 export type TerminationMethod = string | string[];
 export type RadixType = "always" | "as-needed";
 export type NativeAllowList = AllKeyword | string[];
@@ -1208,7 +1209,10 @@ export interface DummyRuleMap {
   "oxc/uninvoked-array-callback"?: RuleNoConfig;
   "prefer-arrow-callback"?: RuleNoConfig | [AllowWarnDeny, PreferArrowCallbackConfig];
   "prefer-const"?: RuleNoConfig | [AllowWarnDeny, PreferConstConfig];
-  "prefer-destructuring"?: DummyRule;
+  "prefer-destructuring"?:
+    | RuleNoConfig
+    | [AllowWarnDeny, PreferDestructuringOption]
+    | [AllowWarnDeny, PreferDestructuringOption, PreferDestructuringRenamedPropertiesConfig];
   "prefer-exponentiation-operator"?: RuleNoConfig;
   "prefer-named-capture-group"?: RuleNoConfig;
   "prefer-numeric-literals"?: RuleNoConfig;
@@ -3851,6 +3855,17 @@ export interface PreferConstConfig {
    * This is mainly useful for preventing conflicts with the `typescript/no-use-before-define` rule.
    */
   ignoreReadBeforeAssign?: boolean;
+}
+export interface PreferDestructuringTargetOption {
+  array?: boolean;
+  object?: boolean;
+}
+export interface PreferDestructuringAssignmentConfig {
+  AssignmentExpression?: PreferDestructuringTargetOption;
+  VariableDeclarator?: PreferDestructuringTargetOption;
+}
+export interface PreferDestructuringRenamedPropertiesConfig {
+  enforceForRenamedProperties?: boolean;
 }
 export interface PreferPromiseRejectErrors {
   /**
