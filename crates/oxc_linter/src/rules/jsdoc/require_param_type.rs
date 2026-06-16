b522b505e4078a9ffe7c736f1ec8025871c43b7b
Cameron Clark <cameron.clark@hey.com>
2026-06-16 17:01:10 +0100

    revert unnecesary changes

--- crates/oxc_linter/src/rules/jsdoc/require_param_type.rs
+++ crates/oxc_linter/src/rules/jsdoc/require_param_type.rs
@@ -90,12 +90,8 @@ impl Rule for RequireParamType {
     fn run<'a>(&self, node: &AstNode<'a>, ctx: &LintContext<'a>) {
         // Collected targets from `FormalParameters`
         let params_to_check = match node.kind() {
-            AstKind::Function(func) if !func.is_typescript_syntax() => {
-                collect_params(&func.params, false)
-            }
-            AstKind::ArrowFunctionExpression(arrow_func) => {
-                collect_params(&arrow_func.params, false)
-            }
+            AstKind::Function(func) if !func.is_typescript_syntax() => collect_params(&func.params),
+            AstKind::ArrowFunctionExpression(arrow_func) => collect_params(&arrow_func.params),
             // If not a function, skip
             _ => return,
         };
