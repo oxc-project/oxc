dce520e88e56c46df8bc8fc884f64d6d2148bc8c
Cameron Clark <cameron.clark@hey.com>
2026-06-16 16:52:42 +0100

    style: sort imports

--- crates/oxc_linter/src/rules/jsdoc/require_param_description.rs
+++ crates/oxc_linter/src/rules/jsdoc/require_param_description.rs
@@ -1,10 +1,11 @@
+use schemars::JsonSchema;
+use serde::Deserialize;
+
 use oxc_ast::AstKind;
 use oxc_diagnostics::OxcDiagnostic;
 use oxc_macros::declare_oxc_lint;
 use oxc_span::Span;
 use oxc_str::CompactStr;
-use schemars::JsonSchema;
-use serde::Deserialize;
 
 use crate::{
     AstNode,
