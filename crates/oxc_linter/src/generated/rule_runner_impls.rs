e9de5a18bfe492cba6244393fd7bb35fefab048b
Cam McHenry <camchenry@users.noreply.github.com>
2026-07-27 06:48:37 -0400

    perf(linter/jest-vitest/no-test-return-statement): only run on return statements (#24167)
    This rule had a nice target AST node that appears infrequently:
    `ReturnStatement`. If a file doesn't contain that node type, we can
    completely skip this rule. This also resolved an issue where we reported
    duplicate diagnostics because we reported once for the `return` and once
    for the function call chain.
    
    ---------
    
    Co-authored-by: Cameron <cameron.clark@hey.com>

--- crates/oxc_linter/src/generated/rule_runner_impls.rs
+++ crates/oxc_linter/src/generated/rule_runner_impls.rs
@@ -2355,7 +2355,7 @@ impl RuleRunner for crate::rules::jest::no_test_prefixes::NoTestPrefixes {
 
 impl RuleRunner for crate::rules::jest::no_test_return_statement::NoTestReturnStatement {
     const NODE_TYPES: Option<&AstTypesBitset> =
-        Some(&AstTypesBitset::from_types(&[AstType::CallExpression, AstType::Function]));
+        Some(&AstTypesBitset::from_types(&[AstType::ReturnStatement]));
     const RUN_FUNCTIONS: RuleRunFunctionsImplemented = RuleRunFunctionsImplemented::Run;
 }
 
@@ -4789,7 +4789,7 @@ impl RuleRunner for crate::rules::vitest::no_test_prefixes::NoTestPrefixes {
 
 impl RuleRunner for crate::rules::vitest::no_test_return_statement::NoTestReturnStatement {
     const NODE_TYPES: Option<&AstTypesBitset> =
-        Some(&AstTypesBitset::from_types(&[AstType::CallExpression, AstType::Function]));
+        Some(&AstTypesBitset::from_types(&[AstType::ReturnStatement]));
     const RUN_FUNCTIONS: RuleRunFunctionsImplemented = RuleRunFunctionsImplemented::Run;
 }
 
