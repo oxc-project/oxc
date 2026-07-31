42cdbc9a029d2482a42321ceadce75b5c8c50f8d
Sysix <sysix@sysix-coding.de>
2026-07-31 15:50:27 +0200

    perf(lsp): increase concurrency level by available threads

--- crates/oxc_language_server/src/lib.rs
+++ crates/oxc_language_server/src/lib.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{num::NonZero, sync::Arc};
 
 use futures::future::BoxFuture;
 use rustc_hash::FxBuildHasher;
@@ -73,5 +73,13 @@ async fn run_server_impl(
     })
     .finish();
 
-    Server::new(stdin, stdout, socket).serve(service).await;
+    // If the system has more than 6 threads, we use 2 less than the available threads
+    // to avoid overwhelming the system. Otherwise, we use all available threads.
+    let current_threads = std::thread::available_parallelism().map_or(1, NonZero::get);
+    let max = if current_threads >= 6 { current_threads - 2 } else { current_threads };
+    // Ensure that the concurrency level is at least 4, defaulting to the old behavior if the system has fewer than 4 threads.
+    // Server-Requests can trigger Client-Requests, which will fill up the thread pool quickly, so we want to ensure that we have enough threads to handle both.
+    let level = std::cmp::max(4, max);
+
+    Server::new(stdin, stdout, socket).concurrency_level(level).serve(service).await;
 }
