0e975752fa6152c28cb176b361de8aea99ef9e83
Cameron <cameron.clark@hey.com>
2026-08-05 11:51:43 +0100

    u

--- crates/oxc_linter/src/rules/jsx_a11y/anchor_has_content.rs
+++ crates/oxc_linter/src/rules/jsx_a11y/anchor_has_content.rs
@@ -1,3 +1,6 @@
+use schemars::JsonSchema;
+use serde::Deserialize;
+
 use oxc_ast::{
     AstKind,
     ast::{JSXAttributeItem, JSXChild, JSXElement},
@@ -6,8 +9,6 @@ use oxc_diagnostics::OxcDiagnostic;
 use oxc_macros::declare_oxc_lint;
 use oxc_span::Span;
 use oxc_str::CompactStr;
-use schemars::JsonSchema;
-use serde::Deserialize;
 
 use crate::{
     AstNode,
