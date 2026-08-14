cf8608fcb91e552ee17ecce35a9d27b32fcc5c44
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2026-08-14 16:53:25 -0700

    fix(linter/unicorn/prefer-bigint-literals): unwrap redundant constructor call; don't auto-fix for lost precision

--- crates/oxc_linter/src/utils/mod.rs
+++ crates/oxc_linter/src/utils/mod.rs
@@ -22,6 +22,7 @@ mod jest;
 mod jsdoc;
 mod nextjs;
 mod node;
+mod number;
 mod promise;
 mod react;
 mod react_perf;
@@ -38,7 +39,7 @@ pub mod vue_casing;
 
 pub use self::{
     comment::*, config::*, control_flow::*, express::*, jest::*, jsdoc::*, nextjs::*, node::*,
-    promise::*, react::*, react_perf::*, regex::*, schemars::*, static_value::*,
+    number::*, promise::*, react::*, react_perf::*, regex::*, schemars::*, static_value::*,
     this_expression::*, typescript::*, unicorn::*, url::*, vitest::*, vue::*,
 };
 
