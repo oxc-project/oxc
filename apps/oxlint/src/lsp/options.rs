7e441ff02f400d9a12ca1cb27049f9fb35cd8dd9
Daniel Oliveira <daniel.oliveira@figma.com>
2026-08-31 09:19:17 -0700

    Update apps/oxlint/src/lsp/options.rs
    Co-authored-by: Alexander S. <sysix@sysix-coding.de>
    Signed-off-by: Daniel Oliveira <daniel.oliveira@figma.com>

--- apps/oxlint/src/lsp/options.rs
+++ apps/oxlint/src/lsp/options.rs
@@ -32,10 +32,11 @@ pub enum Run {
 #[serde(rename_all = "camelCase")]
 pub enum SuppressedViolationSeverity {
     Hint,
-    Information,
+    Info,
     #[default]
-    Warning,
+    Warn,
     Error,
+    Off
 }
 
 /// LSP Options
