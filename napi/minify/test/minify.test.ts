2d63d6bebed9d4015101d2f0432f53bc90d03416
Cameron Clark <cameron.clark@hey.com>
2026-09-09 14:59:11 +0100

    chore: disable typos where needed

--- napi/minify/test/minify.test.ts
+++ napi/minify/test/minify.test.ts
@@ -31,6 +31,7 @@ describe("simple", () => {
     const ret = minifySync("test.js", "export let café = 'naïve ☕';", {
       codegen: { asciiOnly: true },
     });
+    // spellchecker:disable-next-line
     expect(ret.code).toBe("export let caf\\u00E9=`na\\u00EFve \\u2615`;");
     expect(ret.errors.length).toBe(0);
   });
