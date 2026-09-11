c78d7074f8529d69ec2b2064b881ff7cc6bcff61
Armano <armano2@users.noreply.github.com>
2026-09-11 02:47:45 +0200

    perf(minifier): process newly created stmt in handle_if_statement (#26541)
    After creation new expression if/expression, we will now directly pass
    it though `minimize_statement`, instead of waiting for next full pass

--- crates/oxc_minifier/src/peephole/minimize_statements.rs
+++ crates/oxc_minifier/src/peephole/minimize_statements.rs
@@ -619,8 +619,8 @@ impl<'a> PeepholeOptimizations {
                             Self::try_minimize_if(&mut if_stmt, ctx).unwrap_or_else(|| {
                                 Statement::IfStatement(ArenaBox::new_in(if_stmt, ctx))
                             });
-                        result.push(if_stmt);
                         ctx.notice_change();
+                        Self::minimize_statement(if_stmt, stmts, result, ctx);
                         return;
                     }
                 }
