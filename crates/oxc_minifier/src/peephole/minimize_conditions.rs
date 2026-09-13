e48b90e350960aca51c2d52a6627dba76c933565
armano <armano2@users.noreply.github.com>
2026-09-14 00:59:12 +0200

    perf(minifier): reduce allocations when minimizing assignments

--- crates/oxc_minifier/src/peephole/minimize_conditions.rs
+++ crates/oxc_minifier/src/peephole/minimize_conditions.rs
@@ -224,10 +224,12 @@ impl<'a> PeepholeOptimizations {
         let reference = ctx.scoping_mut().get_reference_mut(write_id_ref.reference_id());
         reference.flags_mut().insert(ReferenceFlags::Read);
 
-        let new_op = logical_expr.operator.to_assignment_operator();
-        let new_right = logical_expr.right.take_in(ctx);
-        expr.operator = new_op;
-        ctx.replace_expression(&mut expr.right, new_right);
+        expr.operator = logical_expr.operator.to_assignment_operator();
+        ctx.drop_expression(&logical_expr.left);
+        ctx.replace_expression_with(&mut expr.right, |e, _ctx| {
+            let Expression::LogicalExpression(e) = e else { unreachable!() };
+            e.unbox().right
+        });
     }
 
     /// Compress `a = a + b` to `a += b`
@@ -238,7 +240,7 @@ impl<'a> PeepholeOptimizations {
         if !matches!(expr.operator, AssignmentOperator::Assign) {
             return;
         }
-        let Expression::BinaryExpression(binary_expr) = &mut expr.right else { return };
+        let Expression::BinaryExpression(binary_expr) = &expr.right else { return };
         let Some(new_op) = binary_expr.operator.to_assignment_operator() else { return };
         if !Self::has_no_side_effect_for_evaluation_same_target(&expr.left, &binary_expr.left, ctx)
         {
@@ -247,9 +249,12 @@ impl<'a> PeepholeOptimizations {
 
         Self::mark_assignment_target_as_read(&expr.left, ctx);
 
-        let new_right = binary_expr.right.take_in(ctx);
         expr.operator = new_op;
-        ctx.replace_expression(&mut expr.right, new_right);
+        ctx.drop_expression(&binary_expr.left);
+        ctx.replace_expression_with(&mut expr.right, |e, _ctx| {
+            let Expression::BinaryExpression(e) = e else { unreachable!() };
+            e.unbox().right
+        });
     }
 
     /// Compress `a -= 1` to `--a` and `a -= -1` to `++a`
@@ -259,7 +264,9 @@ impl<'a> PeepholeOptimizations {
         ctx: &mut TraverseCtx<'a>,
     ) {
         let Expression::AssignmentExpression(e) = expr else { return };
-        if !matches!(e.operator, AssignmentOperator::Subtraction) {
+        if !matches!(e.operator, AssignmentOperator::Subtraction)
+            || !e.left.is_simple_assignment_target()
+        {
             return;
         }
         let operator = if let Expression::NumericLiteral(num) = &e.right {
@@ -273,9 +280,14 @@ impl<'a> PeepholeOptimizations {
         } else {
             return;
         };
-        let Some(target) = e.left.as_simple_assignment_target_mut() else { return };
-        let target = target.take_in(ctx);
-        let new_expr = Expression::new_update_expression(e.span, operator, true, target, ctx);
-        ctx.replace_expression(expr, new_expr);
+
+        ctx.replace_expression_with(expr, |e, ctx| {
+            let Expression::AssignmentExpression(e) = e else {
+                unreachable!();
+            };
+            let e = e.unbox();
+            let target = e.left.into_simple_assignment_target();
+            Expression::new_update_expression(e.span, operator, true, target, ctx)
+        })
     }
 }
